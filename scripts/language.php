a0fda24feb20e0a43ca76daaa0b737c3ad1cfd60
pmurph <pmurph@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-05-08 20:47:51 +0000

    Uploaded language.php script

---
+++ scripts/language.php
@@ -0,0 +1,60 @@
+<?php
+//Date: May 4, 2009
+//Version: .7 Beta
+//To Do: Change direct path names for file to correct path names
+//Notes:
+
+	//Creates export file
+	$filee = fopen("C:\\wamp\\www\\atim2\\app\\locale\\eng\\LC_MESSAGES\\default.po", "w+t");
+	$filef = fopen("C:\\wamp\\www\\atim2\\app\\locale\\fre\\LC_MESSAGES\\default.po", "w+t");
+	$filex = fopen("C:\\wamp\\www\\atim2\\app\\locale\\error\\ERROR_MESSAGES\\error.po", "w+t");
+	
+	//Establishes a connection to the MySQL server
+	$connection = @mysql_connect("localhost", "root", "1qaz1qaz")
+					or die("Could not connect to MySQL");
+					
+	//Selects the languages database
+	@mysql_select_db("atim_2")
+					or die("Could not select database");
+    
+	//Executes query
+	$query = "SELECT * FROM i18n";
+	mysql_query("SET NAMES 'utf8'");
+	$result = mysql_query($query);
+	
+	
+	//Displays all information returned from query
+	for ( $count = 0; $count <= mysql_numrows($result)-1; $count++){
+		//Takes information returned by query and creates variable for each field
+		$id = mysql_result($result, $count, "id");
+		$en = mysql_result( $result, $count, "en");
+		if(strlen($en) > 1014){
+		    $error = "msgid\t\"$id\"\nen\t\"$en\"\n";
+			$en = substr($en, 0, 1014);
+		}
+		$fr = mysql_result( $result, $count, "fr");
+		if(strlen($fr) > 1014){
+		    if(strlen($error) > 2 ){
+				$error = "$error\\nmsgstr\t\"$fr\"\n";
+			}else{
+				$error = "msgid\t\"$id\"\nmsgstr=\"$fr\"\n";
+			}
+			$fr = substr($fr, 0, 1014);
+		}
+		$english = "msgid\t\"$id\"\nmsgstr\t\"$en\"\n";
+		$french = "msgid=\"$id\"\nmsgstr=\"$fr\";\n";
+		
+		//Writes output to file
+		fwrite($filee, $english);
+		fwrite($filef, $french);
+		
+	}
+	
+	///Close file
+	fclose($filee);
+	fclose($filef);
+	fclose($filex);
+	
+	//Close connection
+	mysql_close();
+?>
