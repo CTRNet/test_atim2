3288855d9889a3d6a7a123e701138ae002b36d4f
pmurph <pmurph@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-05-19 19:57:16 +0000

    Consent Controller with fixed list all function

--- app/plugins/clinicalannotation/controllers/consents_controller.php
+++ app/plugins/clinicalannotation/controllers/consents_controller.php
@@ -3,11 +3,11 @@
 class ConsentsController extends ClinicalannotationAppController {
 
 	var $uses = array('Consent','Participant');
-	var $paginate = array('Consent'=>array('limit'=>10,'order'=>'Consent.date DSC')); 
+	var $paginate = array('Consent'=>array('limit'=>10,'order'=>'Consent.date ASC')); 
 
 	function listall( $participant_id=null ) {
 		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id) );
-		$this->data = $this->paginate($this->Consent);
+		$this->data = $this->paginate($this->Consent, array('Consent.participant_id'=>$participant_id));
 	}	
 
 	function detail( $participant_id=null, $consent_id=null ) {
