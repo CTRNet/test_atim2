0f7a36ce40af023c03d1e69180c6f05419f9c961
pmurph <pmurph@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-05-20 22:11:39 +0000

    Updated controllers made for family history, diagnosis, and misc identifiers

--- trunk/app/plugins/clinicalannotation/controllers/misc_identifiers_controller.php
+++ trunk/app/plugins/clinicalannotation/controllers/misc_identifiers_controller.php
@@ -1,277 +1,64 @@
 <?php
 
-class MiscIdentifierController extends ClinicalAnnotationAppController {
+class MiscIdentifiersController extends ClinicalAnnotationAppController {
 
-	$uses = array('MiscIndentifier');
-	$paginate = array('MiscIdentifier'=>array('limit'=>10,'order'=>'MiscIdentifier.participant_id ASC'));
+	var $uses = array('MiscIdentifier','Participant');
+	var $paginate = array('MiscIdentifier'=>array('limit'=>10,'order'=>'MiscIdentifier.participant_id ASC'));
 	
 	function listall( $participant_id ) {
-		$this->data = $this->paginate($this->MiscIdentifier);
+		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+
+		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id));
+		$this->data = $this->paginate($this->MiscIdentifier, array('MiscIdentifier.participant_id'=>$participant_id));
 	}
 	
 	function detail( $participant_id, $misc_identifier_id ) {
+		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+		if ( !$misc_identifier_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+		
 		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id, 'MiscIdentifier.id'=>$misc_identifier_id) );
 		$this->data = $this->MiscIdentifier->find('first',array('conditions'=>array('MiscIdentifier.id'=>$misc_identifier_id)));
 	}
 	
 	function add( $participant_id ) {
-		$this->set( 'atim_menu', $this->Menus->get('/clinicalannotation/misc_identifier/listall/'.$particiant_id) );
+		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+	
+		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id));
 		
 		if ( !empty($this->data) ) {
-			if ( $this->MiscIdentifier->save($this->data) ) $this->flash( 'Your data has been updated.','/clinicalannotation/misc_identifier/detail/'.$participant_id.$this->MiscIdentifier->id );
+			$this->data['MiscIdentifier']['participant_id'] = $participant_id;
+			if ( $this->MiscIdentifier->save($this->data) ) {
+				$this->flash( 'Your data has been updated.','/clinicalannotation/misc_identifiers/detail/'.$participant_id.'/'.$this->MiscIdentifier->id );
+			}
 		}
 	}
 	
 	function edit( $participant_id, $misc_identifier_id) {
+		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+		if ( !$misc_identifier_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+		
 		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id, 'MiscIdentifier.id'=>$misc_identifier_id) );
 		
 		if ( !empty($this->data) ) {
 			$this->MiscIdentifier->id = $misc_identifier_id;
-			if ( $this->MiscIdentifier->save($this->data) ) $this->flash( 'Your data has been updated.','/clinicalannotation/misc_identifier/detail/'.$participant_id.$misc_identifier_id );
+			if ( $this->MiscIdentifier->save($this->data) ) {
+				$this->flash( 'Your data has been updated.','/clinicalannotation/misc_identifiers/detail/'.$participant_id.'/'.$misc_identifier_id );
+			}
 		} else {
 			$this->data = $this->MiscIdentifier->find('first',array('conditions'=>array('MiscIdentifier.id'=>$misc_identifier_id)));
 		}
 	}
 
-/*
-	var $name = 'MiscIdentifier';
-	var $uses = array('MiscIdentifier');
-
-	var $components = array('Summaries');
-	var $helpers = array('Summaries');
-
-	function beforeFilter() {
-
-		// $auth_conf array hardcoded in oth_auth component, due to plugins compatibility
-		$this->othAuth->controller = &$this;
-		$this->othAuth->init();
-		$this->othAuth->check();
-
-		// CakePHP function to re-combine dat/time select fields
-		$this->cleanUpFields();
-
-	}
-
-	function index() {
-		// nothing...
-	}
-
-	function listall( $participant_id=null ) {
-		// missing VARS, send to ERROR page
-		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id' ); exit; }
-		
-		// set MENU varible for echo on VIEW
-		$ctrapp_menu[] = $this->Menus->tabs( 'clin_CAN_1', 'clin_CAN_24', $participant_id );
-		$this->set( 'ctrapp_menu', $ctrapp_menu );
-
-		// set FORM variable, for HELPER call on VIEW
-		$this->set( 'ctrapp_form', $this->Forms->getFormArray('misc_identifiers') );
-
-		// set SUMMARY varible from plugin's COMPONENTS
-		$this->set( 'ctrapp_summary', $this->Summaries->build( $participant_id ) );
-
-		// set SIDEBAR variable, for HELPER call on VIEW
-		// use PLUGIN_CONTROLLER_ACTION by default, but any ALIAS string that matches in the SIDEBARS datatable will do...
-		$this->set( 'ctrapp_sidebar', $this->Sidebars->getColsArray( $this->params['plugin'].'_'.$this->params['controller'].'_'.$this->params['action'] ) );
-
-		// set FORM variable, for HELPER call on VIEW
-		$this->set( 'participant_id', $participant_id );
-
-		$criteria = array();
-		$criteria['participant_id'] = $participant_id;
-		$criteria = array_filter($criteria);
-
-		list( $order, $limit, $page ) = $this->Pagination->init( $criteria );
-		$misc_identifier_data = $this->MiscIdentifier->findAll( $criteria, NULL, $order, $limit, $page );
-		
-		// look for CUSTOM HOOKS, "format"
-		$custom_controller_hook = APP . 'plugins' . DS . $this->params['plugin'] . DS . 'controllers' . DS . 'hooks' . DS . $this->params['controller'].'_'.$this->params['action'].'_format.php';
-		if ( file_exists($custom_controller_hook) ) { require($custom_controller_hook); }
-		
-		$this->set( 'misc_identifiers',  $misc_identifier_data);
-	}
-
-	function detail( $participant_id=null, $misc_identifier_id=null ) {
-		// missing VARS, send to ERROR page
-		if ( !$participant_id  ) { $this->redirect( '/pages/err_clin-ann_no_part_id' ); exit; }
-		if ( !$misc_identifier_id ) { $this->redirect( '/pages/err_clin-ann_no_misc_id' ); exit; }
-		
-		// set MENU varible for echo on VIEW
-		$ctrapp_menu[] = $this->Menus->tabs( 'clin_CAN_1', 'clin_CAN_24', $participant_id );
-		$this->set( 'ctrapp_menu', $ctrapp_menu );
-
-		// set FORM variable, for HELPER call on VIEW
-		$this->set( 'ctrapp_form', $this->Forms->getFormArray('misc_identifiers') );
-
-		// set SUMMARY varible from plugin's COMPONENTS
-		$this->set( 'ctrapp_summary', $this->Summaries->build( $participant_id ) );
-
-		// set SIDEBAR variable, for HELPER call on VIEW
-		// use PLUGIN_CONTROLLER_ACTION by default, but any ALIAS string that matches in the SIDEBARS datatable will do...
-		$this->set( 'ctrapp_sidebar', $this->Sidebars->getColsArray( $this->params['plugin'].'_'.$this->params['controller'].'_'.$this->params['action'] ) );
-
-		// set FORM variable, for HELPER call on VIEW
-		$this->set( 'participant_id', $participant_id );
-		$this->set( 'misc_identifier_id', $misc_identifier_id );
-
-		$this->MiscIdentifier->id = $misc_identifier_id;
-		$misc_data = $this->MiscIdentifier->read();
-		
-		if ( empty( $misc_data ) ) { $this->redirect( '/pages/err_clin-ann_no_misc_data' ); exit; }		
-
-		// look for CUSTOM HOOKS, "format"
-		$custom_controller_hook = APP . 'plugins' . DS . $this->params['plugin'] . DS . 'controllers' . DS . 'hooks' . DS . $this->params['controller'].'_'.$this->params['action'].'_format.php';
-		if ( file_exists($custom_controller_hook) ) { require($custom_controller_hook); }
-		
-		$this->set( 'data',  $misc_data);
-	}
-
-	function add( $participant_id=null ) {
-		// missing VARS, send to ERROR page
-		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id' ); exit; }
-		
-		// set MENU varible for echo on VIEW
-		$ctrapp_menu[] = $this->Menus->tabs( 'clin_CAN_1', 'clin_CAN_24', $participant_id );
-		$this->set( 'ctrapp_menu', $ctrapp_menu );
-
-		// set FORM variable, for HELPER call on VIEW
-		$this->set( 'ctrapp_form', $this->Forms->getFormArray('misc_identifiers') );
-
-		// set SUMMARY varible from plugin's COMPONENTS
-		$this->set( 'ctrapp_summary', $this->Summaries->build( $participant_id ) );
-
-		// set SIDEBAR variable, for HELPER call on VIEW
-		// use PLUGIN_CONTROLLER_ACTION by default, but any ALIAS string that matches in the SIDEBARS datatable will do...
-		$this->set( 'ctrapp_sidebar', $this->Sidebars->getColsArray( $this->params['plugin'].'_'.$this->params['controller'].'_'.$this->params['action'] ) );
-
-		// set FORM variable, for HELPER call on VIEW
-		$this->set( 'participant_id', $participant_id );
-
-		// look for CUSTOM HOOKS, "format"
-		$custom_controller_hook = APP . 'plugins' . DS . $this->params['plugin'] . DS . 'controllers' . DS . 'hooks' . DS . $this->params['controller'].'_'.$this->params['action'].'_format.php';
-		if ( file_exists($custom_controller_hook) ) { require($custom_controller_hook); }
-		
-		if ( !empty($this->data) ) {
-			// setup MODEL(s) validation array(s) for displayed FORM 
-			foreach ( $this->Forms->getValidateArray('misc_identifiers') as $validate_model=>$validate_rules ) {
-				$this->{ $validate_model }->validate = $validate_rules;
-			}
-							
-			// set a FLAG
-			$submitted_data_validates = true;
-			
-			// VALIDATE submitted data
-			if ( !$this->MiscIdentifier->validates( $this->data ) ) {
-				$submitted_data_validates = false;
-			}
-			
-			// look for CUSTOM HOOKS, "validation"
-			$custom_controller_hook = APP . 'plugins' . DS . $this->params['plugin'] . DS . 'controllers' . DS . 'hooks' . DS . $this->params['controller'].'_'.$this->params['action'].'_validation.php';
-			if ( file_exists($custom_controller_hook) ) { require($custom_controller_hook); }
-			
-			// if data VALIDATE, then save data
-			if ( $submitted_data_validates ) {
-				if ( $this->MiscIdentifier->save( $this->data ) ) {
-					$this->flash( 'Your data has been saved.', '/misc_identifiers/listall/'.$participant_id );
-				} else {
-					$this->redirect( '/pages/err_clin-ann_misc_record' ); 
-					exit; 
-				}
-			}
-		}
-	}
-
-	function edit( $participant_id=null, $misc_identifier_id=null ) {
-		// missing VARS, send to ERROR page
-		if ( !$participant_id  ) { $this->redirect( '/pages/err_clin-ann_no_part_id' ); exit; }
-		if ( !$misc_identifier_id ) { $this->redirect( '/pages/err_clin-ann_no_misc_id' ); exit; }
-		
-		// set MENU varible for echo on VIEW
-		$ctrapp_menu[] = $this->Menus->tabs( 'clin_CAN_1', 'clin_CAN_24', $participant_id );
-		$this->set( 'ctrapp_menu', $ctrapp_menu );
-
-		// set FORM variable, for HELPER call on VIEW
-		$this->set( 'ctrapp_form', $this->Forms->getFormArray('misc_identifiers') );
-
-		// set SUMMARY varible from plugin's COMPONENTS
-		$this->set( 'ctrapp_summary', $this->Summaries->build( $participant_id ) );
-
-		// set SIDEBAR variable, for HELPER call on VIEW
-		// use PLUGIN_CONTROLLER_ACTION by default, but any ALIAS string that matches in the SIDEBARS datatable will do...
-		$this->set( 'ctrapp_sidebar', $this->Sidebars->getColsArray( $this->params['plugin'].'_'.$this->params['controller'].'_'.$this->params['action'] ) );
-
-		// set FORM variable, for HELPER call on VIEW
-		$this->set( 'participant_id', $participant_id );
-		$this->set( 'misc_identifier_id', $misc_identifier_id );
-
-		$this->MiscIdentifier->id = $misc_identifier_id;
-		$misc_data = $this->MiscIdentifier->read();
-		
-		if ( empty( $misc_data ) ) { $this->redirect( '/pages/err_clin-ann_no_misc_data' ); exit; }				
-
-		// look for CUSTOM HOOKS, "format"
-		$custom_controller_hook = APP . 'plugins' . DS . $this->params['plugin'] . DS . 'controllers' . DS . 'hooks' . DS . $this->params['controller'].'_'.$this->params['action'].'_format.php';
-		if ( file_exists($custom_controller_hook) ) { require($custom_controller_hook); }
-	
-		if ( empty($this->data) ) {
-			$this->data = $misc_data;
-			$this->set( 'data', $this->data );
-
-		} else {
-			// setup MODEL(s) validation array(s) for displayed FORM 
-			foreach ( $this->Forms->getValidateArray('misc_identifiers') as $validate_model=>$validate_rules ) {
-				$this->{ $validate_model }->validate = $validate_rules;
-			}
-						
-			// set a FLAG
-			$submitted_data_validates = true;
-			
-			// VALIDATE submitted data
-			if ( !$this->MiscIdentifier->validates( $this->data ) ) {
-				$submitted_data_validates = false;
-			}
-			
-			// look for CUSTOM HOOKS, "validation"
-			$custom_controller_hook = APP . 'plugins' . DS . $this->params['plugin'] . DS . 'controllers' . DS . 'hooks' . DS . $this->params['controller'].'_'.$this->params['action'].'_validation.php';
-			if ( file_exists($custom_controller_hook) ) { require($custom_controller_hook); }
-			
-			// if data VALIDATE, then save data
-			if ( $submitted_data_validates ) {
-				
-				if ( $this->MiscIdentifier->save( $this->data['MiscIdentifier'] ) ) {
-					$this->flash( 'Your data has been updated.','/misc_identifiers/detail/'.$participant_id.'/'.$misc_identifier_id );
-				} else {
-					$this->redirect( '/pages/err_clin-ann_misc_record' ); 
-					exit; 
-				}
-			}			
-		}
-	}
-*/
 	function delete( $participant_id=null, $misc_identifier_id=null ) {
-	/*
-		// missing VARS, send to ERROR page
-		if ( !$participant_id  ) { $this->redirect( '/pages/err_clin-ann_no_part_id' ); exit; }
-		if ( !$misc_identifier_id ) { $this->redirect( '/pages/err_clin-ann_no_misc_id' ); exit; }
-
-		// Verify consent exists
-		$this->MiscIdentifier->id = $misc_identifier_id;
-		$misc_data = $this->MiscIdentifier->read();
-		
-		if ( empty( $misc_data ) ) { $this->redirect( '/pages/err_clin-ann_no_misc_data' ); exit; }				
-		
-		// look for CUSTOM HOOKS, "validation"
-		$custom_controller_hook = APP . 'plugins' . DS . $this->params['plugin'] . DS . 'controllers' . DS . 'hooks' . DS . $this->params['controller'].'_'.$this->params['action'].'_validation.php';
-		if ( file_exists($custom_controller_hook) ) { require($custom_controller_hook); }
+		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+		if ( !$misc_identifier_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
 		
 		if( $this->MiscIdentifier->del( $misc_identifier_id ) ) {
-			$this->flash( 'Your data has been deleted.', '/misc_identifiers/listall/'.$participant_id );
+			$this->flash( 'Your data has been deleted.', '/clinicalannotation/misc_identifiers/listall/'.$participant_id );
 		} else {
-			$this->redirect( '/pages/err_clin-ann_misc_deletion' ); 
-			exit;
+			$this->flash( 'Your data has been deleted.', '/clinicalannotation/misc_identifiers/listall/'.$participant_id );
 		}
 	}
-*/
 }
 
 ?>
