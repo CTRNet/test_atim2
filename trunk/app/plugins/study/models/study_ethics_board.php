2d3db2a48e57aadd9d0171506c3f42b989d87035
pmurph <pmurph@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-06-24 20:03:09 +0000

    Updated orders plugin, progressReport, and test data script.

--- trunk/app/plugins/study/models/study_ethics_board.php
+++ trunk/app/plugins/study/models/study_ethics_board.php
@@ -3,7 +3,7 @@
 class StudyEthicsBoard extends StudyAppModel
 {
     var $name = 'StudyEthicsBoard';
-	var $useTable = 'study_ethicsboards';
+	var $useTable = 'study_ethics_boards';
 	
 	var $validate = array();
 }
