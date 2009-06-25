782f622b6337a0afdc855f9e95c297d148024ad0
pmurph <pmurph@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-06-25 21:24:06 +0000

    Updated treatment master model with appropriate summary function.

--- trunk/app/plugins/clinicalannotation/models/treatment_master.php
+++ trunk/app/plugins/clinicalannotation/models/treatment_master.php
@@ -1,8 +1,35 @@
 <?php
 
 class TreatmentMaster extends ClinicalannotationAppModel {
+	
+	var $name = 'TreatmentMaster';
 	var $useTable = 'tx_masters';
     var $actAs = array('MasterDetail');
+	
+	function summary( $variables=array() ) {
+		$return = false;
+		
+		if ( isset($variables['TreatmentMaster.id']) ) {
+			
+			$result = $this->find('first', array('conditions'=>array('TreatmentMaster.id'=>$variables['TreatmentMaster.id'])));
+			
+			$return = array(
+				'Summary' => array(
+					'menu'        => array( NULL, $result['TreatmentMaster']['tx_group'] ),
+					'title'		  => array( NULL, $result['TreatmentMaster']['tx_group'] ),
+
+					'description' => array(
+						'disease site'	=>	$result['TreatmentMaster']['disease_site'],
+						'start date'	=>	$result['TreatmentMaster']['start_date'],
+						'end date'      =>  $result['TreatmentMaster']['finish_date'],
+						'summary'       =>  $result['TreatmentMaster']['summary']
+					)
+				)
+			);
+		}
+		
+		return $return;
+	}
 }
 
 ?>
