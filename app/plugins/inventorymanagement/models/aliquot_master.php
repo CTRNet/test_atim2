a9c9c987f91e9062807d3d88db8e768734ba6f95
pmurph <pmurph@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-06-25 21:38:35 +0000

    Added summary information for sample master, and aliquot master.

--- app/plugins/inventorymanagement/models/aliquot_master.php
+++ app/plugins/inventorymanagement/models/aliquot_master.php
@@ -2,6 +2,35 @@
 
 class AliquotMaster extends InventoryManagementAppModel
 {
+	var $name = 'AliquotMaster';
+	var $useTable = 'aliquot_masters';
+	var $actAs = array('MasterDetail');
+	
+	function summary( $variables=array() ) {
+		$return = false;
+		
+		if ( isset($variables['AliquotMaster.id']) ) {
+			
+			$result = $this->find('first', array('conditions'=>array('AliquotMaster.id'=>$variables['AliquotMaster.id'])));
+			
+			$return = array(
+				'Summary' => array(
+					'menu'        => array( NULL, $result['AliquotMaster']['barcode'] ),
+					'title'		  => array( NULL, $result['AliquotMaster']['barcode'] ),
+
+					'description' => array(
+						'product code' => $result['AliquotMaster']['product_code'],
+						'type'      =>  $result['AliquotMaster']['aliquot_type'],
+						'category'	=>	$result['AliquotMaster']['current_volume'].' '.$result['AliquotMaster']['aliquot_volume_unit'],
+						'status'	=>	$result['AliquotMaster']['status']
+					)
+				)
+			);
+		}
+		
+		return $return;
+	}
+/*
     var $name = 'AliquotMaster';
     
 	var $useTable = 'aliquot_masters';
@@ -25,7 +54,7 @@ class AliquotMaster extends InventoryManagementAppModel
 			 	'finderSql'   => ''));
 		
 	var $validate = array();
-
+*/
 }
 
 ?>
