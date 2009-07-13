1c90264b1551e3d6eb623a9a9f3d8601380ee707
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-07-13 19:00:23 +0000

    Removed old .thtml views and fixed small menu issue with Forms manager.

--- app/plugins/rtbform/controllers/rtbforms_controller.php
+++ app/plugins/rtbform/controllers/rtbforms_controller.php
@@ -2,66 +2,64 @@
 
 class RtbformsController extends RtbformAppController {
 	
-  var $uses = array('Rtbform.Rtbform');
-  var $paginate = array('Rtbform'=>array('limit'=>10,'order'=>'Rtbform.frmTitle'));
+	var $uses = array('Rtbform.Rtbform');
+	var $paginate = array('Rtbform'=>array('limit'=>10,'order'=>'Rtbform.frmTitle'));
   
-  function index() {
-    $_SESSION['ctrapp_core']['search'] = NULL; // clear SEARCH criteria
-  }
+	function index() {
+		$_SESSION['ctrapp_core']['search'] = NULL; // clear SEARCH criteria
+	}
   
-  function search() {
-    
-    if ( $this->data ) $_SESSION['ctrapp_core']['search']['criteria'] = $this->Structures->parse_search_conditions();
+	function search() {
+		$this->set( 'atim_menu', $this->Menus->get('/rtbform/rtbforms/index') );
 		
-	$this->data = $this->paginate($this->Rtbform, $_SESSION['ctrapp_core']['search']['criteria']);
+		if ( $this->data ) $_SESSION['ctrapp_core']['search']['criteria'] = $this->Structures->parse_search_conditions();
 		
-	// if SEARCH form data, save number of RESULTS and URL
-	$_SESSION['ctrapp_core']['search']['results'] = $this->params['paging']['Rtbform']['count'];
-	$_SESSION['ctrapp_core']['search']['url'] = '/rtbform/rtbforms/search';
-    
-  }
+		$this->data = $this->paginate($this->Rtbform, $_SESSION['ctrapp_core']['search']['criteria']);
+		
+		// if SEARCH form data, save number of RESULTS and URL
+		$_SESSION['ctrapp_core']['search']['results'] = $this->params['paging']['Rtbform']['count'];
+		$_SESSION['ctrapp_core']['search']['url'] = '/rtbform/rtbforms/search';
+	}
 	
-  function profile( $rtbform_id=null ) {
-	if ( !$rtbform_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+	function profile( $rtbform_id=null ) {
+		if ( !$rtbform_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
   
-    $this->set( 'atim_menu_variables', array('Rtbform.id'=>$rtbform_id) );
-	$this->data = $this->Rtbform->find('first',array('conditions'=>array('Rtbform.id'=>$rtbform_id)));
-  }
+		$this->set( 'atim_menu_variables', array('Rtbform.id'=>$rtbform_id) );
+		$this->data = $this->Rtbform->find('first',array('conditions'=>array('Rtbform.id'=>$rtbform_id)));
+	}
   
 
-  function add() {
-    $this->set( 'atim_menu', $this->Menus->get('/rtbform/rtbforms/index') );
-		
+	function add() {
 		if ( !empty($this->data) ) {
 			if ( $this->Rtbform->save($this->data) ) $this->flash( 'Your data has been updated.','/rtbform/rtbforms/profile/'.$this->Rtbform->id );
 		}
-  }
+	}
   
 
-  function edit( $rtbform_id=null ) {
-	if ( !$rtbform_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+	function edit( $rtbform_id=null ) {
+		if ( !$rtbform_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
 		
-	$this->set( 'atim_menu_variables', array('Rtbform.id'=>$rtbform_id) );
+		$this->set( 'atim_menu_variables', array('Rtbform.id'=>$rtbform_id) );
 		
-	if ( !empty($this->data) ) {
-		$this->Rtbform->id = $rtbform_id;
-		if ( $this->Rtbform->save($this->data) ) {
-			$this->flash( 'Your data has been updated.','/rtbform/rtbforms/profile/'.$rtbform_id );
+		if ( !empty($this->data) ) {
+			$this->Rtbform->id = $rtbform_id;
+			if ( $this->Rtbform->save($this->data) ) {
+				$this->flash( 'Your data has been updated.','/rtbform/rtbforms/profile/'.$rtbform_id );
+			}
+		} else {
+			$this->data = $this->Rtbform->find('first',array('conditions'=>array('Rtbform.id'=>$rtbform_id)));
 		}
-	} else {
-		$this->data = $this->Rtbform->find('first',array('conditions'=>array('Rtbform.id'=>$rtbform_id)));
 	}
-  }
   
-  function delete( $rtbform_id=null ) {
-    if ( !$rtbform_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+	function delete( $rtbform_id=null ) {
+		if ( !$rtbform_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
 		
-	if( $this->Rtbform->del( $rtbform_id ) ) {
-		$this->flash( 'Your data has been deleted.', '/rtbform/rtbforms/index/');
-	} else {
-		$this->flash( 'Your data has been deleted.', '/rtbform/rtbforms/index/');
+		if( $this->Rtbform->del( $rtbform_id ) ) {
+			$this->flash( 'Your data has been deleted.', '/rtbform/rtbforms/search/');
+		} else {
+			$this->flash( 'Your data has been deleted.', '/rtbform/rtbforms/search/');
+		}
 	}
-  }
   
 }
 
