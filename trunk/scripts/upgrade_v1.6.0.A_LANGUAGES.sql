129772c4a73d474103b05eb345bacefa08d02039
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-08-10 21:46:08 +0000

    Added drug lookup support for treatment extends. Still outstanding bug with $override and select lists. Also dropped all diagnosis text areas from annotation forms.

---
+++ trunk/scripts/upgrade_v1.6.0.A_LANGUAGES.sql
@@ -0,0 +1,5 @@
+-- This file contains any new language translations that are required for v2.0.0
+-- A seperate script file is needed due to potential conflicts with existing bank translations
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+'administration', 'global', 'Administration', '');
