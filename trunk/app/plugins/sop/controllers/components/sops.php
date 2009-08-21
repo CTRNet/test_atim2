a9a7d4ab0b5801326658fe49ec69eb6412e855b5
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-08-21 20:13:51 +0000

    Added general function to get SOP... to complete

---
+++ trunk/app/plugins/sop/controllers/components/sops.php
@@ -0,0 +1,16 @@
+<?php
+
+class SopsComponent extends Object {
+	
+	function initialize(&$controller, $settings=array()) {
+		$this->controller =& $controller;
+	}
+
+	// TODO: Develop this function to retrun SOP list according to object (AliquotMaster, TmaSlide, etc) and different parameters
+	function getSop() {
+		return array();
+	}
+	
+}
+
+?>
