eed76f0ecb6953fb983f26c700f215a9fe05060e
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-09-04 17:27:56 +0000

    little update for 2.0

--- trunk/app/plugins/storagelayout/models/tma_slide.php
+++ trunk/app/plugins/storagelayout/models/tma_slide.php
@@ -1,14 +1,5 @@
 <?php
 class TmaSlide extends StoragelayoutAppModel {
-	
-	var $belongsTo = array(
-		'StorageMaster' =>
-			array('className' => 'StorageMaster',
-				'conditions' => '',
-				'order' => '',
-				'foreignKey' => 'storage_master_id'
-		)
-	);
 			
 }
 ?>
