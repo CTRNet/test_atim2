0550f278d6dec768501d29a9ae9796c7e71a779b
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-09-22 15:24:53 +0000

    Merged core changes from VL.

--- app/models/user.php
+++ app/models/user.php
@@ -31,13 +31,16 @@ class User extends AppModel {
 		if ( isset($variables['User.id']) ) {
 			$result = $this->find('first', array('conditions'=>array('User.id'=>$variables['User.id'])));
 			
+			$display_name = trim($result['User']['first_name'].' '.$result['User']['last_name']);
+			$display_name = $display_name ? $display_name : $result['User']['username'];
+			
 			$return = array(
 				'Summary' => array(
-					'menu'			=>	array( NULL, $result['User']['name'] ),
-					'title'			=>	array( NULL, $result['User']['name'] ),
+					'menu'			=>	array( NULL, $display_name ),
+					'title'			=>	array( NULL, $display_name ),
 					
 					'description'	=>	array(
-						'username'		=>	$result['User']['username'],
+						'username'	=>	$result['User']['username'],
 						'email'		=>	$result['User']['email'],
 						'created'	=>	$result['User']['created']
 					)
