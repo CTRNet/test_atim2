04f300a43747a85bfba191043c6b8c27eea1648e
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-10-26 15:28:16 +0000

    Diagnosis expansion (Blood & Tissue)

---
+++ app/plugins/clinicalannotation/models/diagnosis_master.php
@@ -0,0 +1,47 @@
+<?php
+
+class DiagnosisMaster extends ClinicalannotationAppModel {
+	var $belongsTo = array(        
+		'DiagnosisControl' => array(            
+		'className'    => 'Clinicalannotation.DiagnosisControl',            
+		'foreignKey'    => 'diagnosis_control_id'
+		)    
+	);
+	
+function summary( $variables=array() ) {
+		$return = false;
+		if ( isset($variables['DiagnosisMaster.id']) ) {
+			$result = $this->find('first', array('conditions'=>array('DiagnosisMaster.id'=>$variables['DiagnosisMaster.id'])));
+			
+			$return = array(
+				'Summary'	 => array(
+					'menu' => array(NULL, __($result['DiagnosisMaster']['type'], TRUE)),
+					'title' => array(NULL, 'NOT WORKING title'),
+					'description' => array(
+						__('nature', TRUE) => __($result['DiagnosisMaster']['dx_nature'], TRUE),
+						__('origin', TRUE) => __($result['DiagnosisMaster']['dx_origin'], TRUE),
+						__('date', TRUE) => __($result['DiagnosisMaster']['dx_date'], TRUE),
+						__('method', TRUE) => __($result['DiagnosisMaster']['dx_method'], TRUE)
+					)
+				)
+			);
+			
+		}
+		return $return;
+	}
+}
+/*$return = array(
+				'Summary'	 => array(
+					'menu'			=>	array( NULL, __($result['Participant']['first_name'].' '.$result['Participant']['last_name'], TRUE) ),
+					'title'			=>	array( NULL, __($result['Participant']['first_name'].' '.$result['Participant']['last_name'], TRUE) ),
+					
+					'description'		=>	array(
+						__('tumour bank number',TRUE)	=>	__($result['Participant']['tb_number'], TRUE),
+						__('date of birth', TRUE)		=>	__($result['Participant']['date_of_birth'], TRUE),
+						__('marital status', TRUE)		=>	__($result['Participant']['marital_status'], TRUE),
+						__('vital status', TRUE)		=>	__($result['Participant']['vital_status'], TRUE),
+						__('sex', TRUE)					=>	__($result['Participant']['sex'], TRUE)
+					)
+				)
+			);*/
+?>
