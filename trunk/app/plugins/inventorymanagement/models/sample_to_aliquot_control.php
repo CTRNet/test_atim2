80718ea29260403b762eabcedae9b1df692c4569
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-11-18 21:19:20 +0000

    temp update

--- trunk/app/plugins/inventorymanagement/models/sample_to_aliquot_control.php
+++ trunk/app/plugins/inventorymanagement/models/sample_to_aliquot_control.php
@@ -3,12 +3,12 @@
 class SampleToAliquotControl extends InventoryManagementAppModel {
 	
 	var $belongsTo = array(
-	'SampleControl' => array(
-		'className'   => 'Inventorymanagement.SampleControl',
-		 'foreignKey'  => 'sample_control_id'),
-	'AliquotControl' => array(
-		'className'   => 'Inventorymanagement.AliquotControl',
-		 'foreignKey'  => 'aliquot_control_id'));  	
+		'SampleControl' => array(
+			'className'   => 'Inventorymanagement.SampleControl',
+			'foreignKey'  => 'sample_control_id'),
+		'AliquotControl' => array(
+			'className'   => 'Inventorymanagement.AliquotControl',
+			'foreignKey'  => 'aliquot_control_id'));  	
 		
 }
 
