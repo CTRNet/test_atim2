f560d8b870377492eda3c1a5fe50c06a5f6c24b3
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-11-30 03:09:01 +0000

    Updated participant messages code with hooks and validations.

--- app/plugins/clinicalannotation/controllers/participant_messages_controller.php
+++ app/plugins/clinicalannotation/controllers/participant_messages_controller.php
@@ -2,75 +2,151 @@
 
 class ParticipantMessagesController extends ClinicalAnnotationAppController {
 	
-	var $uses = array('Clinicalannotation.ParticipantMessage', 'Clinicalannotation.Participant');
+	var $uses = array(
+		'Clinicalannotation.ParticipantMessage',
+		'Clinicalannotation.Participant');
 	var $paginate = array('ParticipantMessage'=>array('limit'=>10,'order'=>'ParticipantMessage.date_requested'));
 
-	function listall( $participant_id=null ) {
-		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+	function listall( $participant_id ) {
+		if ( !$participant_id ) { $this->redirect( 'err_clin_funct_param_missing', NULL, TRUE ); }
 
-		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id));
-		
-		$this->hook();
-		
+		// MANAGE DATA
+		$participant_data = $this->Participant->find('first', array('conditions'=>array('Participant.id'=>$participant_id), 'recursive' => '-1'));		
+		if(empty($participant_data)) { $this->redirect( '/pages/err_clin_no_data', null, true ); }		
+			
 		$this->data = $this->paginate($this->ParticipantMessage, array('ParticipantMessage.participant_id'=>$participant_id));
+		
+		// MANAGE FORM, MENU AND ACTION BUTTONS
+		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id));
+				
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 	}
 	
-	function detail( $participant_id=null, $participant_message_id=null ) {
-		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
-		if ( !$participant_message_id ) { $this->redirect( '/pages/err_clin-ann_no_message_id', NULL, TRUE ); }
+	function detail( $participant_id, $participant_message_id ) {
+		if ( !$participant_id && !$participant_message_id ) { $this->redirect( '/pages/err_clin_funct_param_missing', NULL, TRUE ); }
+
+		// MANAGE DATA
+		$participant_messsage_data = $this->ParticipantMessage->find('first', array('conditions'=>array('ParticipantMessage.id'=>$participant_message_id, 'ParticipantMessage.participant_id'=>$participant_id), 'recursive' => '-1'));		
+		if(empty($participant_messsage_data)) { $this->redirect( '/pages/err_clin_no_data', null, true ); }
+		$this->data = $participant_messsage_data;
 		
+		// MANAGE FORM, MENU AND ACTION BUTTONS
 		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id, 'ParticipantMessage.id'=>$participant_message_id) );
 		
-		$this->hook();
-		
-		$this->data = $this->ParticipantMessage->find('first',array('conditions'=>array('ParticipantMessage.id'=>$participant_message_id)));
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 	}
 	
 	function add( $participant_id=null ) {
-		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
+		if ( !$participant_id ) { $this->redirect( '/pages/err_clin_funct_param_missing', NULL, TRUE ); }
 	
+		// MANAGE DATA
+		$participant_data = $this->Participant->find('first', array('conditions'=>array('Participant.id'=>$participant_id), 'recursive' => '-1'));
+		if(empty($participant_data)) { $this->redirect( '/pages/err_clin_no_data', null, true ); }
+	
+		// MANAGE FORM, MENU AND ACTION BUTTONS
 		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id));
 		
-		$this->hook();
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 		
 		if ( !empty($this->data) ) {
 			$this->data['ParticipantMessage']['participant_id'] = $participant_id;
-			if ( $this->ParticipantMessage->save($this->data) ) {
-				$this->flash( 'Your data has been updated.','/clinicalannotation/participant_messages/detail/'.$participant_id.'/'.$this->ParticipantMessage->id );
+			
+			$submitted_data_validates = true;
+			// ... special validations
+			
+			// CUSTOM CODE: PROCESS SUBMITTED DATA BEFORE SAVE
+			$hook_link = $this->hook('presave_process');
+			if( $hook_link ) { require($hook_link); }	
+			
+			if($submitted_data_validates) {
+				if ( $this->ParticipantMessage->save($this->data) ) {
+					$this->flash( 'Your data has been updated.','/clinicalannotation/participant_messages/detail/'.$participant_id.'/'.$this->ParticipantMessage->id );
+				}			
 			}
 		}
 	}
 	
-	function edit( $participant_id=null, $participant_message_id=null) {
-		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
-		if ( !$participant_message_id ) { $this->redirect( '/pages/err_clin-ann_no_message_id', NULL, TRUE ); }
+	function edit( $participant_id, $participant_message_id ) {
+		if ( !$participant_id && !$participant_message_id ) { $this->redirect( '/pages/err_clin_funct_param_missing', NULL, TRUE ); }
+
+		// MANAGE DATA
+		$participant_message_data = $this->ParticipantMessage->find('first', array('conditions'=>array('ParticipantMessage.id'=>$participant_message_id, 'ParticipantMessage.participant_id'=>$participant_id), 'recursive' => '-1'));		
+		if(empty($participant_message_data)) { $this->redirect( '/pages/err_clin_no_data', null, true ); }
 		
+		// MANAGE FORM, MENU AND ACTION BUTTONS
 		$this->set( 'atim_menu_variables', array('Participant.id'=>$participant_id, 'ParticipantMessage.id'=>$participant_message_id) );
 		
-		$this->hook();
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 		
-		if ( !empty($this->data) ) {
-			$this->ParticipantMessage->id = $participant_message_id;
-			if ( $this->ParticipantMessage->save($this->data) ) {
-				$this->flash( 'Your data has been updated.','/clinicalannotation/participant_messages/detail/'.$participant_id.'/'.$participant_message_id );
-			}
+		if(empty($this->data)) {
+			$this->data = $participant_message_data;	
 		} else {
-			$this->data = $this->ParticipantMessage->find('first',array('conditions'=>array('ParticipantMessage.id'=>$participant_message_id)));
+			$submitted_data_validates = true;
+			// ... special validations
+			
+			// CUSTOM CODE: PROCESS SUBMITTED DATA BEFORE SAVE
+			$hook_link = $this->hook('presave_process');
+			if( $hook_link ) { require($hook_link); }	
+			
+			if($submitted_data_validates) {
+				$this->ParticipantMessage->id = $participant_message_id;
+				if ( $this->ParticipantMessage->save($this->data) ) {
+					$this->flash( 'Your data has been updated.','/clinicalannotation/participant_messages/detail/'.$participant_id.'/'.$participant_message_id );
+				}			
+			}
 		}
 	}
 
-	function delete( $participant_id=null, $participant_message_id=null ) {
-		if ( !$participant_id ) { $this->redirect( '/pages/err_clin-ann_no_part_id', NULL, TRUE ); }
-		if ( !$participant_message_id ) { $this->redirect( '/pages/err_clin-ann_no_message_id', NULL, TRUE ); }
+	function delete( $participant_id, $participant_message_id ) {
+		if ( !$participant_id && !$participant_message_id ) { $this->redirect( '/pages/err_clin_funct_param_missing', NULL, TRUE ); }
 		
-		$this->hook();
+		// MANAGE DATA
+		$participant_message_data = $this->ParticipantMessage->find('first', array('conditions'=>array('ParticipantMessage.id'=>$participant_message_id, 'ParticipantMessage.participant_id'=>$participant_id), 'recursive' => '-1'));		
+		if(empty($participant_message_data)) { $this->redirect( '/pages/err_clin_no_data', null, true ); }
+
+		$arr_allow_deletion = $this->allowParticipantMessageDeletion($participant_message_id);
 		
-		if( $this->ParticipantMessage->atim_delete( $participant_message_id ) ) {
-			$this->flash( 'Your data has been deleted.', '/clinicalannotation/participant_messages/listall/'.$participant_id );
+		if($arr_allow_deletion['allow_deletion']) {
+		
+			if( $this->ParticipantMessage->atim_delete( $participant_message_id ) ) {
+				$this->flash( 'Your data has been deleted.', '/clinicalannotation/participant_messages/listall/'.$participant_id );
+			} else {
+				$this->flash( 'Error deleting data - Contact administrator.', '/clinicalannotation/participant_messages/listall/'.$participant_id );
+			}		
 		} else {
-			$this->flash( 'Error deleting data - Contact administrator.', '/clinicalannotation/participant_messages/listall/'.$participant_id );
+			$this->flash($arr_allow_deletion['msg'], '/clinicalannotation/participant_messages/detail/'.$participant_id.'/'.$misc_identifier_id);
 		}
 	}
 	
+	/* --------------------------------------------------------------------------
+	 * ADDITIONAL FUNCTIONS
+	 * -------------------------------------------------------------------------- */
+
+	/**
+	 * Check if a record can be deleted.
+	 * 
+	 * @param $participant_message_id Id of the studied record.
+	 * 
+	 * @return Return results as array:
+	 * 	['allow_deletion'] = true/false
+	 * 	['msg'] = message to display when previous field equals false
+	 * 
+	 * @author N. Luc
+	 * @since 2007-10-16
+	 */	
+	 
+	function allowParticipantMessageDeletion( $participant_message_id ) {
+		//$returned_nbr = $this->LinkedModel->find('count', array('conditions' => array('LinkedModel.family_history_id' => $family_history_id), 'recursive' => '-1'));
+		//if($returned_nbr > 0) { return array('allow_deletion' => false, 'msg' => 'a LinkedModel exists for the deleted family history'); }
+		return array('allow_deletion' => true, 'msg' => '');
+	}	
 }
 ?>
--- app/plugins/clinicalannotation/views/participant_messages/listall.ctp
+++ app/plugins/clinicalannotation/views/participant_messages/listall.ctp
@@ -7,6 +7,15 @@
 		)
 	);
 	
-	$structures->build( $atim_structure, array('type'=>'index','links'=>$structure_links) );
+	// Set form structure and option 
+	$final_atim_structure = $atim_structure; 
+	$final_options = array('type'=>'index','links'=>$structure_links);
+	
+	// CUSTOM CODE
+	$hook_link = $structures->hook();
+	if( $hook_link ) { require($hook_link); }
+		
+	// BUILD FORM
+	$structures->build( $final_atim_structure, $final_options );
 
 ?>
