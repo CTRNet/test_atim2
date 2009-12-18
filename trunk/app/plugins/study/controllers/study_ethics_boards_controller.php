653ac769e8e954d619c6e8917cd7b0ed674a94aa
mnatividad <mnatividad@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-12-18 17:33:16 +0000

    edited study controllers

--- trunk/app/plugins/study/controllers/study_ethics_boards_controller.php
+++ trunk/app/plugins/study/controllers/study_ethics_boards_controller.php
@@ -5,73 +5,200 @@ class StudyEthicsBoardsController extends StudyAppController {
 	var $uses = array('Study.StudyEthicsBoard','Study.StudySummary');
 	var $paginate = array('StudyEthicsBoard'=>array('limit'=>10,'order'=>'StudyEthicsBoard.ethics_board'));
 	
-	function listall( $study_summary_id=null ) {
-		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
+	function listall( $study_summary_id ) {
+		// Missing or empty function variable, send to ERROR page
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
 
-		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id));
-		
-		$this->hook();
-		
+
+    	// MANAGE DATA
+    	$study_ethics_board_data= $this->StudySummary->find('first',array('conditions'=>array('StudySummary.id'=>$study_summary_id), 'recursive' => '-1'));
+        if(empty($study_ethics_board_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+
 		$this->data = $this->paginate($this->StudyEthicsBoard, array('StudyEthicsBoard.study_summary_id'=>$study_summary_id));
+
+		// MANAGE FORM, MENU AND ACTION BUTTONS
+    	$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id));
+
+
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 		
 	}
 
-	function add( $study_summary_id=null) {
-		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
-	
-		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id));
-		
-		$this->hook();
-		
-		if ( !empty($this->data) ) {
-			$this->data['StudyEthicsBoard']['study_summary_id'] = $study_summary_id;
-			if ( $this->StudyEthicsBoard->save($this->data) ) {
-				$this->flash( 'Your data has been updated.','/study/study_ethics_boards/detail/'.$study_summary_id.'/'.$this->StudyEthicsBoard->id );
-			}
-		}
-  	}
-  
-	function edit( $study_summary_id=null, $study_ethics_board_id=null ) {
-    	if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_ethics_id', NULL, TRUE ); }
-		if ( !$study_ethics_board_id ) { $this->redirect( '/pages/err_study_no_ethics_id', NULL, TRUE ); }
-		
-		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id, 'StudyEthicsoard.id'=>$study_ethics_board_id) );
-		
-		$this->hook();
-		
-		if ( !empty($this->data) ) {
-			$this->StudyEthicsBoard->id = $study_ethics_board_id;
-			if ( $this->StudyEthicsBoard->save($this->data) ) {
-				$this->flash( 'Your data has been updated.','/study/study_ethics_boards/detail/'.$study_summary_id.'/'.$study_ethics_board_id );
-			}
-		} else {
-			$this->data = $this->StudyEthicsBoard->find('first',array('conditions'=>array('StudyEthicsBoard.id'=>$study_ethics_board_id)));
-		}
-  	}
-	
-	function detail( $study_summary_id=null, $study_ethics_board_id=null ) {
-		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
-		if ( !$study_ethics_board_id ) { $this->redirect( '/pages/err_study_no_ethics_id', NULL, TRUE ); }
-		
+	function detail( $study_summary_id, $study_ethics_board_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+		if ( !$study_ethics_board_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+
+
+    	// MANAGE DATA
+    	$study_ethics_board_data= $this->StudyEthicsBoard->find('first',array('conditions'=>array('StudyEthicsBoard.id'=>$study_ethics_board_id, 'StudyEthicsBoard.study_summary_id'=>$study_summary_id)));
+    	if(empty($study_ethics_board_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+    	$this->data = $study_ethics_board_data;
+
+
+    	// MANAGE FORM, MENU AND ACTION BUTTONS
 		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id, 'StudyEthicsBoard.id'=>$study_ethics_board_id) );
 		
-		$this->hook();
+    	// CUSTOM CODE: FORMAT DISPLAY DATA
+   		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 		
-		$this->data = $this->StudyEthicsBoard->find('first',array('conditions'=>array('StudyEthicsBoard.id'=>$study_ethics_board_id)));
 	}
+
+
+	function add( $study_summary_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+	
+        // MANAGE DATA
+        $study_ethics_board_data= $this->StudySummary->find('first',array('conditions'=>array('StudySummary.id'=>$study_summary_id), 'recursive' => '-1'));
+        if(empty($study_ethics_board_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+
+        // MANAGE FORM, MENU AND ACTION BUTTONS
+
+		// $this->set('atim_structure', $this->Structures->get('form', 'familyhistories'));
+		// $this->set('atim_menu', $this->Menus->get('/clinicalannotation/family_histories/listall/%%Participant.id%%'));
+		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id));
+		
+		
+       	// CUSTOM CODE: FORMAT DISPLAY DATA
+
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+		
+		if ( !empty($this->data) ) {
+
+			// LAUNCH SAVE PROCESS
+			// 1- SET ADDITIONAL DATA
+
+			$this->data['StudyEthicsBoard']['study_summary_id'] = $study_summary_id;
+
+			// 2- LAUNCH SPECIAL VALIDATION PROCESS
+			$submitted_data_validates = true;
+
+			// ... special validations
+
+			// 3- CUSTOM CODE: PROCESS SUBMITTED DATA BEFORE SAVE
+			$hook_link = $this->hook('presave_process');
+			if( $hook_link ) { require($hook_link); }
+
+			if($submitted_data_validates) {
+
+				// 4- SAVE
+
+				if ( $this->StudyEthicsBoard->save($this->data) ) {
+					$this->flash( 'your data has been saved.','/study/study_ethics_boards/detail/'.$study_summary_id.'/'.$this->StudyEthicsBoard->id );
+					}
+				}
+			}
+ 	}
+
+
   
-	function delete( $study_summary_id=null, $study_ethics_board_id=null ) {
-		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
-		if ( !$study_ethics_board_id ) { $this->redirect( '/pages/err_study_no_ethics_id', NULL, TRUE ); }
+	function edit( $study_summary_id, $study_ethics_board_id ) {
+    	if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+		if ( !$study_ethics_board_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
 		
-		$this->hook();
+		// MANAGE DATA
+		$study_ethics_board_data= $this->StudyEthicsBoard->find('first',array('conditions'=>array('StudyEthicsBoard.id'=>$study_ethics_board_id, 'StudyEthicsBoard.study_summary_id'=>$study_summary_id)));
+		if(empty($study_ethics_board_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+
+
+		// MANAGE FORM, MENU AND ACTION BUTTONS
+		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id, 'StudyEthicsBoard.id'=>$study_ethics_board_id) );
 		
-		if( $this->StudyEthicsBoard->atim_delete( $study_ethics_board_id ) ) {
-			$this->flash( 'Your data has been deleted.', '/study/study_ethics_boards/listall/'.$study_summary_id );
-		} else {
-			$this->flash( 'Error deleting data - Contact administrator.', '/study/study_ethics_boards/listall/'.$study_summary_id );
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+		
+
+
+		if (empty($this->data) ) {
+			$this->data = $study_ethics_board_data
+			} else {
+				// 1- SET ADDITIONAL DATA
+
+				//....
+
+				// 2- LAUNCH SPECIAL VALIDATION PROCESS
+
+				$submitted_data_validates = true;
+
+				// ... special validations
+
+				// 3- CUSTOM CODE: PROCESS SUBMITTED DATA BEFORE SAVE
+
+				$hook_link = $this->hook('presave_process');
+				if( $hook_link ) { require($hook_link); }
+
+				if($submitted_data_validates) {
+
+					// 4- SAVE
+
+					$this->StudyEthicsBoard->id = $study_ethics_board_id;
+					if ( $this->StudyEthicsBoard->save($this->data) ) {
+						$this->flash( 'your data has been updated.','/study/study_ethics_board/detail/'.$study_summary_id.'/'.$study_ethics_board_id );
+						}
+					}
+				}
+	}
+
+  
+	function delete( $study_summary_id, $study_ethics_board_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+		if ( !$study_ethics_board_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+		
+		// MANAGE DATA
+		$study_ethics_board_data= $this->StudyEthicsBoard->find('first',array('conditions'=>array('StudyEthicsBoard.id'=>$study_ethics_board_id, 'StudyEthicsBoard.study_summary_id'=>$study_summary_id)));
+		if(empty($study_ethics_board_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+
+		$arr_allow_deletion = $this->allowStudyEthicsBoardDeletion($study_ethics_board_id);
+
+
+		// CUSTOM CODE
+		$hook_link = $this->hook('delete');
+		if( $hook_link ) { require($hook_link); }
+
+
+		if($arr_allow_deletion['allow_deletion']) {
+
+			// DELETE DATA
+
+			if( $this->StudyEthicsBoard->atim_delete( $study_ethics_board_id ) ) {
+				$this->flash( 'your data has been deleted.', '/study/study_ethics_board/listall/'.$study_summary_id );
+			} else {
+				$this->flash( 'error deleting data - contact administrator.', '/study/study_ethics_board/listall/'.$study_summary_id );
+			}
+		}else {
+				$this->flash($arr_allow_deletion['msg'], '/study/study_ethics_board/detail/'.$study_summary_id.'/'.$study_ethics_board_id);
 		}
-  	}
+	}
+
+
+/* --------------------------------------------------------------------------
+* ADDITIONAL FUNCTIONS
+* -------------------------------------------------------------------------- */
+
+/**
+ * Check if a record can be deleted.
+ *
+ * @param $family_history_id Id of the studied record.
+ *
+ * @return Return results as array:
+ * 	['allow_deletion'] = true/false
+ * 	['msg'] = message to display when previous field equals false
+ *
+ * @author N. Luc
+ * @since 2007-10-16
+ */
+
+	function allowStudyEthicsBoardDeletion($study_ethics_board_id){
+		//$returned_nbr = $this->LinkedModel->find('count', array('conditions' => array('LinkedModel.family_history_id' => $family_history_id), 'recursive' => '-1'));
+		//if($returned_nbr > 0) { return array('allow_deletion' => false, 'msg' => 'a LinkedModel exists for the deleted family history'); }
+
+		return array('allow_deletion' => true, 'msg' => '');
+	}
+
 }
 
 ?>
--- trunk/app/plugins/study/controllers/study_results_controller.php
+++ trunk/app/plugins/study/controllers/study_results_controller.php
@@ -1,76 +1,202 @@
 <?php
 
 class StudyResultsController extends StudyAppController {
-	var $uses = array('Study.StudyResult','Study.StudySummary');
-	var $paginate = array('StudyResult'=>array('limit'=>10,'order'=>'StudyResult.abstract'));
+			
+	var $uses = array('Study.StudyResults','Study.StudySummary');
+	var $paginate = array('StudyResults'=>array('limit'=>10,'order'=>'StudyResults.title'));
 	
-	function listall( $study_summary_id=null ) {
-		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
+	function listall( $study_summary_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
 
+    	// MANAGE DATA
+    	$study_results_data= $this->StudySummary->find('first',array('conditions'=>array('StudySummary.id'=>$study_summary_id), 'recursive' => '-1'));
+        if(empty($study_results_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+
+		$this->data = $this->paginate($this->StudyResults, array('StudyResults.study_summary_id'=>$study_summary_id));
+
+
+    	// MANAGE FORM, MENU AND ACTION BUTTONS
 		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id));
 		
-		$this->hook();
+    	// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 		
-		$this->data = $this->paginate($this->StudyResult, array('StudyResult.study_summary_id'=>$study_summary_id));
 		
 	}
 
-	function add( $study_summary_id=null) {
-		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
-	
-		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id));
+	function detail( $study_summary_id, $study_results_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+		if ( !$study_results_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
 		
-		$this->hook();
+
+    	// MANAGE DATA
+    	$study_results_data= $this->StudyResults->find('first',array('conditions'=>array('StudyResults.id'=>$study_results_id, 'StudyResults.study_summary_id'=>$study_summary_id)));
+    	if(empty($study_results_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+    	$this->data = $study_results_data;
+
+    	// MANAGE FORM, MENU AND ACTION BUTTONS
+		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id, 'StudyResults.id'=>$study_results_id) );
 		
-		if ( !empty($this->data) ) {
-			$this->data['StudyResult']['study_summary_id'] = $study_summary_id;
-			if ( $this->StudyResult->save($this->data) ) {
-				$this->flash( 'Your data has been updated.','/study/study_results/detail/'.$study_summary_id.'/'.$this->StudyResult->id );
-			}
-		}
-  	}
-  
-	function edit( $study_summary_id=null, $study_result_id=null ) {
-    	if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
-		if ( !$study_result_id ) { $this->redirect( '/pages/err_study_no_result_id', NULL, TRUE ); }
+    	// CUSTOM CODE: FORMAT DISPLAY DATA
+   		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 		
-		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id, 'StudyResult.id'=>$study_result_id) );
-		
-		$this->hook();
-		
-		if ( !empty($this->data) ) {
-			$this->StudyResult->id = $study_result_id;
-			if ( $this->StudyResult->save($this->data) ) {
-				$this->flash( 'Your data has been updated.','/study/study_results/detail/'.$study_summary_id.'/'.$study_result_id );
-			}
-		} else {
-			$this->data = $this->StudyResult->find('first',array('conditions'=>array('StudyResult.id'=>$study_result_id)));
-		}
-  	}
-	
-	function detail( $study_summary_id=null, $study_result_id=null ) {
-		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
-		if ( !$study_result_id ) { $this->redirect( '/pages/err_study_no_result_id', NULL, TRUE ); }
-		
-		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id, 'StudyResult.id'=>$study_result_id) );
-		
-		$this->hook();
-		
-		$this->data = $this->StudyResult->find('first',array('conditions'=>array('StudyResult.id'=>$study_result_id)));
 	}
+
+
+	function add( $study_summary_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+
+        // MANAGE DATA
+        $study_results_data= $this->StudySummary->find('first',array('conditions'=>array('StudySummary.id'=>$study_summary_id), 'recursive' => '-1'));
+        if(empty($study_results_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+	
+
+        // MANAGE FORM, MENU AND ACTION BUTTONS
+
+		// $this->set('atim_structure', $this->Structures->get('form', 'familyhistories'));
+		// $this->set('atim_menu', $this->Menus->get('/clinicalannotation/family_histories/listall/%%Participant.id%%'));
+		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id));
+
+		
+       	// CUSTOM CODE: FORMAT DISPLAY DATA
+
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+		
+
+		if ( !empty($this->data) ) {
+
+			// LAUNCH SAVE PROCESS
+			// 1- SET ADDITIONAL DATA
+
+			$this->data['StudyResults']['study_summary_id'] = $study_summary_id;
+
+			// 2- LAUNCH SPECIAL VALIDATION PROCESS
+			$submitted_data_validates = true;
+
+			// ... special validations
+
+			// 3- CUSTOM CODE: PROCESS SUBMITTED DATA BEFORE SAVE
+			$hook_link = $this->hook('presave_process');
+			if( $hook_link ) { require($hook_link); }
+
+			if($submitted_data_validates) {
+
+				// 4- SAVE
+
+				if ( $this->StudyResults->save($this->data) ) {
+					$this->flash( 'your data has been saved.','/study/study_results/detail/'.$study_summary_id.'/'.$this->StudyResults->id );
+					}
+				}
+			}
+ 	}
+
+
+
+	function edit( $study_summary_id, $study_results_id ) {
+    	if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+		if ( !$study_results_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+		
+		// MANAGE DATA
+		$study_results_data= $this->StudyResults->find('first',array('conditions'=>array('StudyResults.id'=>$study_results_id, 'StudyResults.study_summary_id'=>$study_summary_id)));
+		if(empty($study_results_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+
+
+		// MANAGE FORM, MENU AND ACTION BUTTONS
+		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id, 'StudyResults.id'=>$study_results_id) );
+		
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+
+		
+		if (empty($this->data) ) {
+			$this->data = $study_results_data
+			} else {
+				// 1- SET ADDITIONAL DATA
+
+				//....
+
+				// 2- LAUNCH SPECIAL VALIDATION PROCESS
+
+				$submitted_data_validates = true;
+
+				// ... special validations
+
+				// 3- CUSTOM CODE: PROCESS SUBMITTED DATA BEFORE SAVE
+
+				$hook_link = $this->hook('presave_process');
+				if( $hook_link ) { require($hook_link); }
+
+				if($submitted_data_validates) {
+
+					// 4- SAVE
+
+					$this->StudyResults->id = $study_results_id;
+					if ( $this->StudyResults->save($this->data) ) {
+						$this->flash( 'your data has been updated.','/study/study_results/detail/'.$study_summary_id.'/'.$study_results_id );
+						}
+					}
+				}
+	}
+
   
-	function delete( $study_summary_id=null, $study_result_id=null ) {
-		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_no_summary_id', NULL, TRUE ); }
-		if ( !$study_result_id ) { $this->redirect( '/pages/err_study_no_result_id', NULL, TRUE ); }
+	function delete( $study_summary_id, $study_results_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
+		if ( !$study_results_id ) { $this->redirect( '/pages/err_study_funct_param_missing', NULL, TRUE ); }
 		
-		$this->hook();
+		// MANAGE DATA
+		$study_results_data= $this->StudyResults->find('first',array('conditions'=>array('StudyResults.id'=>$study_results_id, 'StudyResults.study_summary_id'=>$study_summary_id)));
+		if(empty($study_results_data)) { $this->redirect( '/pages/err_study_no_data', null, true ); }
+
+		$arr_allow_deletion = $this->allowStudyResultsDeletion($study_results_id);
+
+
+		// CUSTOM CODE
+		$hook_link = $this->hook('delete');
+		if( $hook_link ) { require($hook_link); }
+
+			if($arr_allow_deletion['allow_deletion']) {
+
+				// DELETE DATA
+
+				if( $this->StudyResults->atim_delete( $study_results_id ) ) {
+					$this->flash( 'your data has been deleted.', '/study/study_results/listall/'.$study_summary_id );
+				} else {
+					$this->flash( 'error deleting data - contact administrator.', '/study/study_results/listall/'.$study_summary_id );
+				}
+			}else {
+					$this->flash($arr_allow_deletion['msg'], '/study/study_results/detail/'.$study_summary_id.'/'.$study_results_id);
+			}
+	}
+
 		
-		if( $this->StudyResult->atim_delete( $study_result_id ) ) {
-			$this->flash( 'Your data has been deleted.', '/study/study_results/listall/'.$study_summary_id );
-		} else {
-			$this->flash( 'Error deleting data - Contact administrator.', '/study/study_results/listall/'.$study_summary_id );
-		}
-  	}
+
+/* --------------------------------------------------------------------------
+* ADDITIONAL FUNCTIONS
+* -------------------------------------------------------------------------- */
+
+/**
+ * Check if a record can be deleted.
+ *
+ * @param $family_history_id Id of the studied record.
+ *
+ * @return Return results as array:
+ * 	['allow_deletion'] = true/false
+ * 	['msg'] = message to display when previous field equals false
+ *
+ * @author N. Luc
+ * @since 2007-10-16
+ */
+
+	function allowStudyResultsDeletion($study_results_id){
+		//$returned_nbr = $this->LinkedModel->find('count', array('conditions' => array('LinkedModel.family_history_id' => $family_history_id), 'recursive' => '-1'));
+		//if($returned_nbr > 0) { return array('allow_deletion' => false, 'msg' => 'a LinkedModel exists for the deleted family history'); }
+
+		return array('allow_deletion' => true, 'msg' => '');
+	}
 }
 
 ?>
