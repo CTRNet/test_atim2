55a795b514de8cfede5270e49ed073ca0da60921
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2009-12-22 16:56:31 +0000

    In listall and detail views, the icd10code description is now displayed

--- app/plugins/codingicd10/models/coding_icd10.php
+++ app/plugins/codingicd10/models/coding_icd10.php
@@ -7,6 +7,19 @@ class CodingIcd10 extends AppModel {
 
 	var $validate = array();
 	
+	/**
+	 * Gets the description matching a given id
+	 * @param $id
+	 * @return a description string
+	 */
+	function getDescription($id){
+		if($id != null && strlen($id) > 0){
+			$query_result = $this->find('first', array('fields' => array('CodingIcd10.description'), 'conditions' => array('CodingIcd10.id' => $id)));
+			return $query_result['CodingIcd10']['description'];
+		}else{
+			return "";
+		}
+	}
 }
 
 ?>
