78fc29cc92933fb7c9f82818932050a2e86c43a4
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-01-05 15:57:31 +0000

    Attempt to fix input fields related to eventum issue 649.

--- trunk/app/config/core.php
+++ trunk/app/config/core.php
@@ -42,7 +42,7 @@
 /**
  * Application wide charset encoding
  */
-	Configure::write('App.encoding', 'UTF-8');
+	Configure::write('App.encoding', 'ISO-8859-1');
 /**
  * To configure CakePHP *not* to use mod_rewrite and to
  * use CakePHP pretty URLs, remove these .htaccess
