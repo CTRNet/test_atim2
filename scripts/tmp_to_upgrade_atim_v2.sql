2fcf703470c8ded13abfe895496db6f45f6dd6c6
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-01-07 22:10:44 +0000

    tmp update

--- scripts/tmp_to_upgrade_atim_v2.sql
+++ scripts/tmp_to_upgrade_atim_v2.sql
@@ -12,3 +12,40 @@ UPDATE `structure_fields` SET `language_label` = 'country',
 
 UPDATE `structure_fields` SET `language_label` = 'mail_code',
 `language_tag` = '' WHERE `structure_fields`.`id` =72 LIMIT 1 ;
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+('new search', 'global', 'New Search', 'Nouvelle recherche'),
+('new search type', 'global', 'New Search Type', 'Nouveau type de recherche');
+
+UPDATE `structure_fields`
+SET field = 'block_aliquot_master_id'
+WHERE old_id = 'CAN-999-999-000-999-1137';
+
+UPDATE `structure_fields`
+SET field = 'gel_matrix_aliquot_master_id'
+WHERE old_id = 'CAN-999-999-000-999-1243';
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+('cell count should be a positif decimal', 'global', 'Cell count should be a positive decimal!', 'Le nombre de cellules doit &ecirc;tre un d&eacute;cimal positif!');
+
+UPDATE `menus` 
+SET `use_link` = '/clinicalannotation/product_masters/productsTreeView/%%Participant.id%%'
+WHERE `id` = 'clin_CAN_57';
+
+DELETE FROM `menus` 
+WHERE `id` IN ('clin_CAN_571', 'clin_CAN_572', 'clin_CAN_573');
+
+INSERT INTO `menus` (`id`, `parent_id`, `is_root`, `display_order`, `language_title`, `language_description`, `use_link`, `use_params`, `use_summary`, `active`, `created`, `created_by`, `modified`, `modified_by`) VALUES
+('clin_CAN_571', 'clin_CAN_57', 0, 1, 'tree view', NULL, '/clinicalannotation/product_masters/productsTreeView/%%Participant.id%%', '', 'Clinicalannotation.ClinicalCollectionLink::productFilterSummary', 'yes', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', ''),
+('clin_CAN_572', 'clin_CAN_57', 0, 2, 'listall participant samples', NULL, '/underdevelopment/', '', '', 'yes', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', ''),
+('clin_CAN_573', 'clin_CAN_57', 0, 3, 'listall participant aliquots', NULL, '/underdevelopment/', '', '', 'yes', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '');
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+('products', 'global', 'Products', 'Produits');
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+('listall participant aliquots', 'global', 'Aliquots', 'Aliquots'),
+('listall participant samples', 'global', 'Samples', '&Eacute;chantillons');
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+('all products', 'global', 'All Products', 'Tous les produits');
