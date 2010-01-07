94230856ba1d692e31872fb23112e480a6e34a6c
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-01-07 19:09:08 +0000

    Eventum 703 - splitting fields for city, region, country and postal code on separate lines

--- trunk/scripts/tmp_to_upgrade_atim_v2.sql
+++ trunk/scripts/tmp_to_upgrade_atim_v2.sql
@@ -1,2 +1,14 @@
 UPDATE `structure_permissible_values` SET value='fre' WHERE id=757;
 UPDATE `structure_permissible_values` SET value='eng' WHERE id=758;
+
+UPDATE `structure_fields` SET `language_label` = 'city',
+`language_tag` = '' WHERE `structure_fields`.`id` =69 LIMIT 1 ;
+
+UPDATE `structure_fields` SET `language_label` = 'region',
+`language_tag` = '' WHERE `structure_fields`.`id` =70 LIMIT 1 ;
+
+UPDATE `structure_fields` SET `language_label` = 'country',
+`language_tag` = '' WHERE `structure_fields`.`id` =71 LIMIT 1 ;
+
+UPDATE `structure_fields` SET `language_label` = 'mail_code',
+`language_tag` = '' WHERE `structure_fields`.`id` =72 LIMIT 1 ;
