1238e1f6ad7a7c4e11071cd4e1da85ea5ee71bc7
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-01-15 21:22:43 +0000

    Validation for participant ID

--- trunk/scripts/tmp_to_upgrade_atim_v2.sql
+++ trunk/scripts/tmp_to_upgrade_atim_v2.sql
@@ -7,3 +7,10 @@ INSERT INTO `structure_formats` (`id`, `old_id`, `structure_id`, `structure_old_
 (null, 'CANM-00016_CAN-999-999-000-999-1131', 195, 'CANM-00016', 248, 'CAN-999-999-000-999-1131', 0, 11, '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', ''),
 (null, 'CANM-00016_CAN-999-999-000-999-1267', 195, 'CANM-00016', 381, 'CAN-999-999-000-999-1267', 0, 12, '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', ''),
 (null, 'CANM-00016_CAN-999-999-000-999-1266', 195, 'CANM-00016', 380, 'CAN-999-999-000-999-1266', 0, 13, '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '');
+
+-- Add validation rule
+INSERT INTO `structure_validations` (`id`,`old_id`,`structure_field_id` ,`structure_field_old_id`,`rule`,`flag_empty`,`flag_required`,`on_action`,`language_message`,`created`,`created_by`,`modified`,`modifed_by`) VALUES
+(null, '0', '521', 'CAN-999-999-000-999-26', 'isUnique', '1', '1', '', 'error_participant identifier must be unique', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '');
+
+INSERT INTO `i18n` (`id` ,`page_id` ,`en` ,`fr`) VALUES
+('error_participant identifier must be unique', '', 'Error - Participant Identifier must be unique', '');
