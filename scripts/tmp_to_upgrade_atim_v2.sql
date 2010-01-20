7586254bf6a25689a7a0f69bbdf9aba16372d2fb
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-01-20 21:16:48 +0000

    Accents for French translations for December and February

--- scripts/tmp_to_upgrade_atim_v2.sql
+++ scripts/tmp_to_upgrade_atim_v2.sql
@@ -2,3 +2,8 @@ UPDATE `structure_validations`
 SET `flag_empty` = '0'
 WHERE `rule` LIKE 'notEmpty'
 AND `flag_empty` LIKE '1';
+
+UPDATE `i18n` SET fr='Décembre' WHERE id='December';
+update `i18n` set fr='Déc' WHERE id='dec';
+update `i18n` set fr='Février' WHERE id='February';
+update `i18n` set fr='Fév' WHERE id='feb';
