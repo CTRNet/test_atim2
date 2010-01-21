65a7486bc7b8385897c79baa73e2c7bd880e8e04
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-01-21 20:50:19 +0000

    Add query tool data to test.

--- trunk/scripts/tmp_to_upgrade_atim_v2.sql
+++ trunk/scripts/tmp_to_upgrade_atim_v2.sql
@@ -1,4 +1,4 @@
-UPDATE `structure_validations`
+﻿UPDATE `structure_validations`
 SET `flag_empty` = '0'
 WHERE `rule` LIKE 'notEmpty'
 AND `flag_empty` LIKE '1';
@@ -7,3 +7,15 @@ UPDATE `i18n` SET fr='Décembre' WHERE id='December';
 update `i18n` set fr='Déc' WHERE id='dec';
 update `i18n` set fr='Février' WHERE id='February';
 update `i18n` set fr='Fév' WHERE id='feb';
+
+UPDATE `structure_fields` 
+SET `structure_value_domain` = (SELECT id FROM `structure_value_domains` WHERE `domain_name`  = 'custom_laboratory_staff'),
+type = 'select',
+setting = null
+WHERE old_id = 'CAN-999-999-000-999-499';
+
+DELETE FROM `structure_formats` WHERE `structure_id` IN (SELECT `id` FROM `structures` WHERE `alias` = 'available_bank_participants_aliquots');
+DELETE FROM `structures` WHERE `alias` = 'available_bank_participants_aliquots';
+
+DELETE FROM `structure_formats` WHERE `structure_id` IN (SELECT `id` FROM `structures` WHERE `alias` = 'aliquot_masters_for_search_result');
+DELETE FROM `structures` WHERE `alias` = 'aliquot_masters_for_search_result';
