d30a25e79e1ededfc690eddbce87091d83ca46f5
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-01-27 19:49:26 +0000

    Minor clinical form changes.

--- trunk/scripts/tmp_to_upgrade_atim_v2.sql
+++ trunk/scripts/tmp_to_upgrade_atim_v2.sql
@@ -42,3 +42,23 @@ ALTER TABLE `ed_breast_screening_mammogram`
   ADD FOREIGN KEY (`event_master_id`) REFERENCES `event_masters` (`id`)
   ON DELETE RESTRICT
   ON UPDATE RESTRICT;
+  
+ /* Clinical Annotation */
+    -- /Diagnosis/
+    UPDATE `structure_fields` SET `setting` = 'size=1,maxlength=3' WHERE `structure_fields`.`id` =830 LIMIT 1 ;
+    UPDATE `structure_fields` SET `setting` = 'size=1,maxlength=3' WHERE `structure_fields`.`id` =831 LIMIT 1 ;
+    UPDATE `structure_fields` SET `setting` = 'size=1,maxlength=3' WHERE `structure_fields`.`id` =832 LIMIT 1 ;
+    UPDATE `structure_fields` SET `language_label` = '',
+    `language_tag` = 'summary',
+    `setting` = 'size=1,maxlength=3' WHERE `structure_fields`.`id` =833 LIMIT 1 ;
+
+    UPDATE `structure_fields` SET `setting` = 'size=1,maxlength=3' WHERE `structure_fields`.`id` =834 LIMIT 1 ;
+    UPDATE `structure_fields` SET `setting` = 'size=1,maxlength=3' WHERE `structure_fields`.`id` =835 LIMIT 1 ;
+    UPDATE `structure_fields` SET `setting` = 'size=1,maxlength=3' WHERE `structure_fields`.`id` =836 LIMIT 1 ;
+    UPDATE `structure_fields` SET `language_label` = '',
+    `language_tag` = 'summary',
+    `setting` = 'size=1, maxlength=3' WHERE `structure_fields`.`id` =837 LIMIT 1 ;
+    
+    -- flag Origin to index
+    
+    UPDATE `structure_formats` SET `flag_index` = '1' WHERE `structure_formats`.`id` =2207 LIMIT 1 ;
