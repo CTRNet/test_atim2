d7f3cba1f2aa93c17fdf4ead84f55a998e7dafc8
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-02-05 22:48:10 +0000

    Updated translation files. Minor form changes.

--- scripts/tmp_upgrade_script.sql
+++ scripts/tmp_upgrade_script.sql
@@ -1,73 +1,50 @@
 -- Add any new SQL changes to this file which will then be merged with the master
--- upgrade scripts.
+-- upgrade scripts for v2.0.1
 
+/*
+  Main Page
+*/
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+('credits_title', '', 'Credits', ''),
+('credits_body', '', 'ATiM is an open-source project development by leading tumour banks across Canada. For more information on our development team, questions, comments or suggestions please visit our website at http://www.ctrnet.ca', '');
+
+UPDATE `pages` SET `language_title` = 'about_title', `language_body` = 'about_body' WHERE `pages`.`id` = 'about';
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+('about_title', '', 'About', ''),
+('about_body', '', 'The Canadian Tumour Repository Network (CTRNet) is a translational cancer research resource, funded by Canadian Institutes of Health Research, that furthers Canadian health research by linking cancer researchers with provincial tumour banks.', '');
+
+UPDATE `pages` SET `language_title` = 'installation_title', `language_body` = 'installation_body' WHERE `pages`.`id` = 'installation';
+
+INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
+('installation_title', '', 'Installation', ''),
+('installation_body', '', 'To view your installed version number open the Administration Tool and select ATiM Version from the first menu. ATiM is built on the CakePHP framework (www.cakephp.org).', '');
+
+/*
+  CLINCIAL ANNOTATION  
+*/ 
+
+-- Add field for uploading images to the Study form
 ALTER TABLE `ed_all_study_research` ADD `file_path` VARCHAR( 255 ) NOT NULL AFTER `event_master_id`  ;
 
-INSERT INTO `structure_fields` (
-`id` ,
-`public_identifier` ,
-`old_id` ,
-`plugin` ,
-`model` ,
-`tablename` ,
-`field` ,
-`language_label` ,
-`language_tag` ,
-`type` ,
-`setting` ,
-`default` ,
-`structure_value_domain` ,
-`language_help` ,
-`validation_control` ,
-`value_domain_control` ,
-`field_control` ,
-`created` ,
-`created_by` ,
-`modified` ,
-`modified_by`
-)
-VALUES (
-NULL , '', 'CANM-00024', 'Clinicalannotation', 'EventDetail', '', 'file_name', 'Picture', '', 'file', '', '', NULL , '', 'open', 'open', 'open', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', ''
-);
+INSERT INTO `structure_fields` (`id`, `public_identifier`, `old_id`, `plugin`, `model`, `tablename`, `field`, `language_label`, `language_tag`,
+`type`, `setting`, `default`, `structure_value_domain`, `language_help`, `validation_control`, `value_domain_control`, `field_control`,
+`created`, `created_by`, `modified`, `modified_by`) VALUES
+(NULL , '', 'CANM-00024', 'Clinicalannotation', 'EventDetail', '', 'file_name', 'Picture', '', 'file', '', '', NULL , '', 'open', 'open', 'open', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '');
 
-INSERT INTO `structure_formats` (
-`id` ,
-`old_id` ,
-`structure_id` ,
-`structure_old_id` ,
-`structure_field_id` ,
-`structure_field_old_id` ,
-`display_column` ,
-`display_order` ,
-`language_heading` ,
-`flag_override_label` ,
-`language_label` ,
-`flag_override_tag` ,
-`language_tag` ,
-`flag_override_help` ,
-`language_help` ,
-`flag_override_type` ,
-`type` ,
-`flag_override_setting` ,
-`setting` ,
-`flag_override_default` ,
-`default` ,
-`flag_add` ,
-`flag_add_readonly` ,
-`flag_edit` ,
-`flag_edit_readonly` ,
-`flag_search` ,
-`flag_search_readonly` ,
-`flag_datagrid` ,
-`flag_datagrid_readonly` ,
-`flag_index` ,
-`flag_detail` ,
-`created` ,
-`created_by` ,
-`modified` ,
-`modified_by`
-)
-VALUES (
-NULL , 'CAN-999-999-000-999-65_CANM-00024', '144', 'CAN-999-999-000-999-65', '908', 'CANM-00024', '1', '4', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', ''
-);
+INSERT INTO `structure_formats` (`id`, `old_id`, `structure_id`, `structure_old_id`, `structure_field_id`, `structure_field_old_id`, `display_column`,
+`display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`,
+`flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`,
+`flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_datagrid`, `flag_datagrid_readonly`, `flag_index`, `flag_detail`, `created`,
+`created_by`, `modified`, `modified_by`) VALUES 
+(NULL , 'CAN-999-999-000-999-65_CANM-00024', '144', 'CAN-999-999-000-999-65', '908', 'CANM-00024', '1', '4', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0000-00-00 00:00:00', '', '0000-00-00 00:00:00', '');
 
+-- Fixed field for laterality
+ALTER TABLE `dxd_tissues` CHANGE `text_field` `laterality` VARCHAR( 50 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT ''
+ALTER TABLE `dxd_tissues_revs` CHANGE `text_field` `laterality` VARCHAR( 50 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT ''
+
+
+/*
+  INVENTORY MANAGEMENT  
+*/ 
