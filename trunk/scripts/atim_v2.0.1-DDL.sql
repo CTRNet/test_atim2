a9937791be1199f0bc509336aa24a969c78361b9
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-02-08 19:18:55 +0000

    Updated SQL scripts

--- trunk/scripts/atim_v2.0.1-DDL.sql
+++ trunk/scripts/atim_v2.0.1-DDL.sql
@@ -17,6 +17,8 @@
 ALTER TABLE `dxd_tissues` CHANGE `text_field` `laterality` VARCHAR( 50 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '';
 ALTER TABLE `dxd_tissues_revs` CHANGE `text_field` `laterality` VARCHAR( 50 ) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '';
 
+-- Study - Add field for uploading images to the Study form
+ALTER TABLE `ed_all_study_research` ADD `file_path` VARCHAR( 255 ) NOT NULL AFTER `event_master_id`  ;
 
 /*
 	Module: Inventory Management
