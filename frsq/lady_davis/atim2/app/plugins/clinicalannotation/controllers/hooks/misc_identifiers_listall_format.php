b9595869e09e55536db65d9b002bef8adc8861af
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-02-16 21:05:40 +0000

    customizations

---
+++ frsq/lady_davis/atim2/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_listall_format.php
@@ -0,0 +1,14 @@
+<?php
+//$this->set('identifier_controls_list', $this->MiscIdentifierControl->find('all', array('conditions' => array('status' => 'active'))));
+
+$mil = $this->MiscIdentifier->find('all', array('conditions' =>array('MiscIdentifier.participant_id' => $participant_id)));
+$identifier_controls_list = $this->MiscIdentifierControl->find('all', array('conditions' => array('status' => 'active')));
+foreach($identifier_controls_list as $mic_key => $mic){
+	foreach($mil as $mi){
+		if($mic['MiscIdentifierControl']['misc_identifier_name'] == $mi['MiscIdentifier']['identifier_name']){
+			unset($identifier_controls_list[$mic_key]);
+		}
+	}
+}
+$this->set('identifier_controls_list', $identifier_controls_list);
+?>
