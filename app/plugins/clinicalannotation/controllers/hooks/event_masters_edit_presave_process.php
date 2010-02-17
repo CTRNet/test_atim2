cdbec406636736af58ce65bb62024eef2ab26649
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-02-17 14:41:01 +0000

    Correct previous commit error.

--- app/plugins/clinicalannotation/controllers/hooks/event_masters_edit_presave_process.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	// --------------------------------------------------------------------------------
-	// clinical.hepatobiliary.presentation: Add BMI value
-	// --------------------------------------------------------------------------------
-	$this->data = $this->addBmiValue($this->data, $event_group, $event_master_data['EventControl']['disease_site'], $event_master_data['EventControl']['event_type']);
-	
-?>
