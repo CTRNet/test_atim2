c69eac01994823966cc10dc1514238b28a736079
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-02-24 20:27:36 +0000

    jobs done on medical imaging

--- chum/hepato-billary/app/plugins/clinicalannotation/controllers/hooks/event_masters_edit_format.php
+++ chum/hepato-billary/app/plugins/clinicalannotation/controllers/hooks/event_masters_edit_format.php
@@ -13,24 +13,34 @@
 	// --------------------------------------------------------------------------------
 	$this->setMedicalPastHistoryPrecisions(array('EventControl' => $event_master_data['EventControl']));
 	
+	$this->Structures->set('QC_CHUM_HB_date', 'QC_CHUM_HB_date');
+	
 	$event_control_data = $this->EventControl->find('first',array('conditions'=>array('EventControl.id' => $event_master_data['EventMaster']['event_control_id'])));
 	if(strpos($event_control_data['EventControl']['form_alias'], 'segment') > 0){
 		//load segment
-		$this->Structures->set('QC_CHUM_HB_segment', 'QC_CHUM_HB_segment');
+		$QC_CHUM_HB_segment = $this->Structures->get('form', 'QC_CHUM_HB_segment');
 	}
 
 	if(strpos($event_control_data['EventControl']['form_alias'], 'other') > 0){
 		//load other
-		$this->Structures->set('QC_CHUM_HB_other_localisations', 'QC_CHUM_HB_other_localisations');
+		$QC_CHUM_HB_other_localisations = $this->Structures->get('form', 'QC_CHUM_HB_other_localisations');
 	}
-	
 	
 	if(strpos($event_control_data['EventControl']['form_alias'], 'pancreas') > 0){
-		$this->Structures->set('QC_CHUM_HB_pancreas', 'atim_structure');
+		$this->Structures->set('QC_CHUM_HB_pancreas');
 	}else if(strpos($event_control_data['EventControl']['form_alias'], 'other') > 0){
-		$this->Structures->set('QC_CHUM_HB_other_locaisations', 'atim_structure');
+		$this->Structures->set('QC_CHUM_HB_other_localisations');
+		unset($QC_CHUM_HB_other_localisations);
 	}else if(strpos($event_control_data['EventControl']['form_alias'], 'segment') > 0){
-		$this->Structures->set('QC_CHUM_HB_segment', 'atim_structure');
+		$this->Structures->set('QC_CHUM_HB_segment');
+		unset($QC_CHUM_HB_segment);
 	}
 	
+	if(isset($QC_CHUM_HB_segment)){
+		$this->set('QC_CHUM_HB_segment', $QC_CHUM_HB_segment);
+	}
+	if(isset($QC_CHUM_HB_other_localisations)){
+		$this->set('QC_CHUM_HB_other_localisations', $QC_CHUM_HB_other_localisations);
+	}
+		
 ?>
