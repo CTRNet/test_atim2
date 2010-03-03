59fc21e2cd301c455fc9607e71f043fbf782bb33
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-03-03 19:29:05 +0000

    controller: Added a filter event_format: Prevent duplicates misc_format: formatted code

--- chum/hepato-billary/app/plugins/clinicalannotation/controllers/customs/event_masters_controller.php
+++ chum/hepato-billary/app/plugins/clinicalannotation/controllers/customs/event_masters_controller.php
@@ -75,7 +75,7 @@ class EventMastersControllerCustom extends EventMastersController {
 	}
 	
 	function imageryReport( $participant_id ){
-		$this->data = $this->EventMaster->find('all', array('conditions' => array('EventMaster.participant_id' => $participant_id)));
+		$this->data = $this->EventMaster->find('all', array('conditions' => array('EventMaster.participant_id' => $participant_id, 'EventControl.form_alias LIKE "qc_hb_imaging_%"')));
 		$this->Structures->set('empty');
 		$atim_menu_variables['Participant.id'] = $participant_id;
 		$this->set('atim_menu_variables', $atim_menu_variables);
--- chum/hepato-billary/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_add_format.php
+++ chum/hepato-billary/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_add_format.php
@@ -5,7 +5,10 @@
 	// -------------------------------------------------------------------------------- 	
  	if($controls['MiscIdentifierControl']['autoincrement_name'] == 'hepato_bil_bank_participant_id') {
  		$is_identifier_already_created = $this->MiscIdentifier->find('count', array('conditions'=>array('MiscIdentifier.identifier_name'=>$controls['MiscIdentifierControl']['autoincrement_name'], 'MiscIdentifier.participant_id'=>$participant_id), 'recursive' => '-1'));		
- 		if($is_identifier_already_created) { $this->flash( 'this identifier has already been created for your participant', '/clinicalannotation/misc_identifiers/listall/'.$participant_id ); return; }
+ 		if($is_identifier_already_created) {
+ 			$this->flash( 'this identifier has already been created for your participant', '/clinicalannotation/misc_identifiers/listall/'.$participant_id ); 
+ 			return; 
+ 		}
  	} 
  
 ?>
