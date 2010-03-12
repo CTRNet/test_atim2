6aff6d647bba7d764cad102dd7d47773d402f91b
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-03-12 16:29:35 +0000

    Tmp commit

--- branches/qc/frsq/cusm/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_format.php
+++ branches/qc/frsq/cusm/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_format.php
@@ -6,9 +6,9 @@
 	if(empty($this->data)) {
 		$default_aliquot_barcode = '';
 		if($aliquot_control_data['AliquotControl']['aliquot_type'] == 'whatman paper') {
-			$default_aliquot_barcode = substr($sample_data['SampleMaster']['sample_label'], 0 , strrpos($sample_data['SampleMaster']['sample_label'], '-')) . '-WHT';
+			$default_aliquot_barcode = substr($sample_data['SampleMaster']['qc_cusm_sample_label'], 0 , strrpos($sample_data['SampleMaster']['qc_cusm_sample_label'], '-')) . '-WHT';
 		} else {
-			$default_aliquot_barcode = $sample_data['SampleMaster']['sample_label'];
+			$default_aliquot_barcode = $sample_data['SampleMaster']['qc_cusm_sample_label'];
 		}
 		$this->set('default_aliquot_barcode', $default_aliquot_barcode);
 	}
