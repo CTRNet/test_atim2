bb82a1c80a650d8cceaa678a4e2014fc02919bf4
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-03-16 19:59:39 +0000

    Add direct creation of Prostate Bank Participant Id.

--- frsq/cusm/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_add_format.php
+++ frsq/cusm/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_add_format.php
@@ -4,8 +4,17 @@
 	// Create one prostate_bank_participant_id per participant
 	// -------------------------------------------------------------------------------- 	
  	if($controls['MiscIdentifierControl']['autoincrement_name'] == 'prostate_bank_participant_id') {
+ 		// Check id already created
  		$is_identifier_already_created = $this->MiscIdentifier->find('count', array('conditions'=>array('MiscIdentifier.identifier_name'=>$controls['MiscIdentifierControl']['autoincrement_name'], 'MiscIdentifier.participant_id'=>$participant_id), 'recursive' => '-1'));		
- 		if($is_identifier_already_created) { $this->flash( 'this identifier has already been created for your participant', '/clinicalannotation/misc_identifiers/listall/'.$participant_id ); return; }
+ 		if($is_identifier_already_created) { 
+ 			$this->flash( 'this identifier has already been created for your participant', '/clinicalannotation/misc_identifiers/listall/'.$participant_id ); 
+ 			return; 
+ 		}
+ 		
+ 		// Set data for direct creation
+		$this->data = array();			
+		$this->data['MiscIdentifier']['identifier_name'] = $controls['MiscIdentifierControl']['misc_identifier_name'];
+		$this->data['MiscIdentifier']['identifier_abrv'] = $controls['MiscIdentifierControl']['misc_identifier_name_abbrev'];
  	} 
  
 ?>
