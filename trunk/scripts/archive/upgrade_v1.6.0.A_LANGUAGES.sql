6daf4ae39557356b801614a849bad6f6ed2b74ce
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-04-06 19:08:32 +0000

    New /utilities folder for all misc. php scripts. Dropped old archive folder.

--- trunk/scripts/archive/upgrade_v1.6.0.A_LANGUAGES.sql
+++
@@ -1,5 +0,0 @@
--- This file contains any new language translations that are required for v2.0.0
--- A seperate script file is needed due to potential conflicts with existing bank translations
-
-INSERT INTO `i18n` (`id`, `page_id`, `en`, `fr`) VALUES
-'administration', 'global', 'Administration', '');
