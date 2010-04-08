682e14d63e249387be1c6e90a8e772d9d7f06bec
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-04-08 17:41:50 +0000

    Eventum 714: Group add/edit/delete repaired

--- app/plugins/administrate/controllers/groups_controller.php
+++ app/plugins/administrate/controllers/groups_controller.php
@@ -2,7 +2,7 @@
 
 class GroupsController extends AdministrateAppController {
 	
-	var $uses = array('Group');
+	var $uses = array('Group',	'Aco', 'Aro');
 	var $paginate = array('Group'=>array('limit'=>10,'order'=>'Group.name ASC')); 
 	
 	function index( $bank_id ) {
@@ -27,13 +27,14 @@ class GroupsController extends AdministrateAppController {
 	}
 	
 
-	function add() {
+	function add($bank_id) {
 		$this->set( 'atim_menu_variables', array('Bank.id'=>$bank_id) );
 		
 		$this->hook();
 		
 		if (!empty($this->data)) {
 			$this->Group->create();
+			$this->data['Group']['bank_id'] = $bank_id;
 			if ($this->Group->save($this->data)) {
 				
 				$group_id = $this->Group->id;
@@ -43,10 +44,9 @@ class GroupsController extends AdministrateAppController {
 				$this->Aro->id = $aro_data['Aro']['id'];
 				$this->Aro->save($aro_data);
 				
-				$this->Session->setFlash(__('The Group has been saved', true));
-				$this->redirect(array('action'=>'index'));
+				$this->flash('your data has been saved', '/administrate/groups/detail/'.$bank_id.'/'.$group_id);
 			} else {
-				$this->Session->setFlash(__('The Group could not be saved. Please, try again.', true));
+				$this->flash('The Group could not be saved. Please, try again.', '/administrate/groups/add/'.$bank_id.'/');
 			}
 		}
 	}
@@ -62,23 +62,11 @@ class GroupsController extends AdministrateAppController {
 		$this->hook();
 		
 		if (!empty($this->data)) {
+			$this->Group->id = $group_id;
 			if ($this->Group->save($this->data)) {
-				foreach($this->data['Aro']['Permission'] as $permission){
-					if($permission['remove'] && $permission['id']){
-						$this->Permission->delete( $permission['id'] );
-					}else if(!$permission['remove']){
-						$this->Permission->id = isset($permission['id']) ? $permission['id'] : NULL;
-						$permission['_read'] = $permission['_create'];
-						$permission['_update'] = $permission['_create'];
-						$permission['_delete'] = $permission['_create'];
-						$this->Permission->save( array('Permission' => $permission) );
-						$this->Permission->id = NULL;
-					}
-				}
-				$this->Session->setFlash(__('The Group has been saved', true));
-				$this->redirect(array('action'=>'index'));
+				$this->flash('your data has been updated', '/administrate/groups/detail/'.$bank_id.'/'.$group_id);
 			} else {
-				$this->Session->setFlash(__('The Group could not be saved. Please, try again.', true));
+				$this->flash('The Group could not be saved. Please, try again.', '/administrate/groups/edit/'.$bank_id.'/'.$group_id);
 			}
 		}
 		if (empty($this->data)) {
@@ -110,152 +98,15 @@ class GroupsController extends AdministrateAppController {
 
 	function delete( $bank_id, $group_id = null ) {
 		if (!$group_id) {
-			$this->Session->setFlash(__('Invalid id for Group', true));
-			$this->redirect(array('action'=>'index'));
+			$this->flash('Invalid id for Group', '/administrate/groups/index/'.$bank_id.'/');
 		}
 		
 		$this->hook();
-		
-		if ($this->Group->del($group_id)) {
-			$this->Session->setFlash(__('Group deleted', true));
-			$this->redirect(array('action'=>'index'));
-		}
-	}
 
-	/*
-	function index( $bank_id ) {
-		
-		// set MENU varible for echo on VIEW 
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_41', 'core_CAN_73', '' );
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_73', 'core_CAN_86', $bank_id );
-		$this->set( 'ctrapp_menu', $ctrapp_menu );
-		
-		// set FORM variable, for HELPER call on VIEW 
-		$this->set( 'ctrapp_form', $this->Forms->getFormArray('groups') );
-		
-		// set SUMMARY varible from plugin's COMPONENTS 
-		$this->set( 'ctrapp_summary', $this->Summaries->build( $this->othAuth->user('id') ) );
-		
-		// set SIDEBAR variable, for HELPER call on VIEW 
-		// use PLUGIN_CONTROLLER_ACTION by default, but any ALIAS string that matches in the SIDEBARS datatable will do...
-		$this->set( 'ctrapp_sidebar', $this->Sidebars->getColsArray( $this->params['plugin'].'_'.$this->params['controller'].'_'.$this->params['action'] ) );
-		
-		// set FORM variable, for HELPER call on VIEW 
-		$this->set( 'bank_id', $bank_id );
-		
-			$criteria = array();
-			$criteria[] = 'bank_id="'.$bank_id.'"';
-			$criteria = array_filter($criteria);
-			
-		list( $order, $limit, $page ) = $this->Pagination->init( $criteria );
-		$this->set( 'groups', $this->Group->findAll( $criteria, NULL, $order, $limit, $page ) );
-		
-	}
-	
-	function detail( $bank_id, $group_id ) {
-		
-		// set MENU varible for echo on VIEW 
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_41', 'core_CAN_73', '' );
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_73', 'core_CAN_86', $bank_id );
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_86', 'core_CAN_87', $bank_id.'/'.$group_id );
-		$this->set( 'ctrapp_menu', $ctrapp_menu );
-		
-		// set FORM variable, for HELPER call on VIEW 
-		// $this->set( 'ctrapp_form', $this->Forms->getFormArray('participants') );
-		$this->set( 'ctrapp_form', $this->Forms->getFormArray('groups') );
-		
-		// set SUMMARY varible from plugin's COMPONENTS 
-		$this->set( 'ctrapp_summary', $this->Summaries->build( $this->othAuth->user('id') ) );
-		
-		// set SIDEBAR variable, for HELPER call on VIEW 
-		// use PLUGIN_CONTROLLER_ACTION by default, but any ALIAS string that matches in the SIDEBARS datatable will do...
-		$this->set( 'ctrapp_sidebar', $this->Sidebars->getColsArray( $this->params['plugin'].'_'.$this->params['controller'].'_'.$this->params['action'] ) );
-		
-		// set FORM variable, for HELPER call on VIEW 
-		$this->set( 'bank_id', $bank_id );
-		$this->set( 'group_id', $group_id );
-		
-		$this->Group->id = $group_id;
-		$this->set( 'data', $this->Group->read() );
-	}
-	
-	function add( $bank_id ) {
-		
-		// setup MODEL(s) validation array(s) for displayed FORM 
-		foreach ( $this->Forms->getValidateArray('groups') as $validate_model=>$validate_rules ) {
-			$this->{ $validate_model }->validate = $validate_rules;
-		}
-		
-		// set MENU varible for echo on VIEW 
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_41', 'core_CAN_73', '' );
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_73', 'core_CAN_86', $bank_id );
-		$this->set( 'ctrapp_menu', $ctrapp_menu );
-		
-		// set FORM variable, for HELPER call on VIEW 
-		$this->set( 'ctrapp_form', $this->Forms->getFormArray('groups') );
-		
-		// set SUMMARY varible from plugin's COMPONENTS 
-		$this->set( 'ctrapp_summary', $this->Summaries->build( $this->othAuth->user('id') ) );
-		
-		// set SIDEBAR variable, for HELPER call on VIEW 
-		// use PLUGIN_CONTROLLER_ACTION by default, but any ALIAS string that matches in the SIDEBARS datatable will do...
-		$this->set( 'ctrapp_sidebar', $this->Sidebars->getColsArray( $this->params['plugin'].'_'.$this->params['controller'].'_'.$this->params['action'] ) );
-		
-		// set FORM variable, for HELPER call on VIEW 
-		$this->set( 'bank_id', $bank_id );
-		
-		if ( !empty($this->data) ) {
-			
-			if ( $this->Group->save( $this->data ) ) {
-				$this->flash( 'your data has been saved', '/groups/index/'.$bank_id );
-			}
-			
-		}
-		
-	}
-	
-	function edit( $bank_id, $group_id ) {
-		
-		// setup MODEL(s) validation array(s) for displayed FORM 
-		foreach ( $this->Forms->getValidateArray('groups') as $validate_model=>$validate_rules ) {
-			$this->{ $validate_model }->validate = $validate_rules;
-		}
-		
-		// set MENU varible for echo on VIEW 
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_41', 'core_CAN_73', '' );
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_73', 'core_CAN_86', $bank_id );
-		$ctrapp_menu[] = $this->Menus->tabs( 'core_CAN_86', 'core_CAN_87', $bank_id.'/'.$group_id );
-		$this->set( 'ctrapp_menu', $ctrapp_menu );
-		
-		// set FORM variable, for HELPER call on VIEW 
-		$this->set( 'ctrapp_form', $this->Forms->getFormArray('groups') );
-		
-		// set SUMMARY varible from plugin's COMPONENTS 
-		$this->set( 'ctrapp_summary', $this->Summaries->build( $this->othAuth->user('id') ) );
-		
-		// set SIDEBAR variable, for HELPER call on VIEW 
-		// use PLUGIN_CONTROLLER_ACTION by default, but any ALIAS string that matches in the SIDEBARS datatable will do...
-		$this->set( 'ctrapp_sidebar', $this->Sidebars->getColsArray( $this->params['plugin'].'_'.$this->params['controller'].'_'.$this->params['action'] ) );
-		
-		// set FORM variable, for HELPER call on VIEW 
-		$this->set( 'bank_id', $bank_id );
-		$this->set( 'group_id', $group_id );
-		
-		if ( empty($this->data) ) {
-			
-			$this->Group->id = $group_id;
-			$this->data = $this->Group->read();
-			$this->set( 'data', $this->data );
-			
-		} else {
-			
-			if ( $this->User->save( $this->data['Group'] ) ) {
-				$this->flash( 'your data has been updated','/groups/detail/'.$bank_id.'/'.$group_id );
-			}
-			
+		if ($this->Group->del($group_id)) {
+			$this->flash('Group deleted', '/administrate/groups/index/'.$bank_id.'/');
 		}
 	}
-	*/
 
 }
 
--- app/plugins/administrate/views/groups/edit.ctp
+++ app/plugins/administrate/views/groups/edit.ctp
@@ -1,66 +1,11 @@
-<div class="groups form">
-<?php echo $form->create('Group');?>
-	<fieldset>
- 		<legend><?php __('Edit Group');?></legend>
-	<?php
-		echo $form->input('Group.id');
-		echo $form->input('Group.name');
-	?>
-	</fieldset>
-	<fieldset>
- 		<legend><?php __('Permissions');?></legend>
-<?php
-	$options = array(1 => 'Allow', -1 => 'Deny');
-
-	if(count($this->data['Aro']['Permission'])){
-		foreach($this->data['Aro']['Permission'] as $i => $ar){
-			echo '<fieldset style="padding: 5px;" id="permission_'.$i.'">';
-				echo $form->input('Aro.Permission.'.$i.'.id', array('type' => 'hidden'));
-				echo $form->input('Aro.Permission.'.$i.'.aro_id', array('type' => 'hidden'));
-				echo $form->input('Aro.Permission.'.$i.'.remove', array('type' => 'checkbox', 'value' => 1));
-				echo $form->input('Aro.Permission.'.$i.'.aco_id', array('label' => false, 'type' => 'select', 'options' => $aco_options));
-				echo $form->input('Aro.Permission.'.$i.'._create', array('label' => false, 'type' => 'select', 'options' => $options));
-			echo '</fieldset>';
-		}
-	}else{
-		echo '<fieldset style="padding: 5px;" id="permission_0">';
-			echo $form->input('Aro.Permission.0.aro_id', array('type' => 'hidden'));
-			echo $form->input('Aro.Permission.0.remove', array('type' => 'hidden', 'value' => 0));
-			echo $form->input('Aro.Permission.0.aco_id', array('type' => 'select', 'options' => $aco_options));
-			echo $form->input('Aro.Permission.0._create', array('label' => false, 'type' => 'select', 'options' => $options));
-		echo '</fieldset>';
-	}
-	$permissionsTemplate =
-		'<fieldset style="padding: 5px;" id="permission_#{id}">'
-		.$form->input('Aro.Permission.#{id}.aro_id', array('type' => 'hidden', 'value' => $this->data['Aro']['id']))
-		.$form->input('Aro.Permission.#{id}.remove', array('type' => 'hidden', 'value' => 0))
-		.$form->input('Aro.Permission.#{id}.aco_id', array('label' => false, 'type' => 'select', 'options' => $aco_options))
-		.$form->input('Aro.Permission.#{id}._create', array('label' => false, 'type' => 'select', 'options' => $options))
-		.'<a href="#" onclick="$(\'permission_#{id}\').remove();return false;" class="remove">Remove</a>'
-		.'</fieldset>';
-	$permissionsTemplate = preg_replace('/\n/','',preg_replace('/"/',"'", preg_replace('/\'/','&quot;',$permissionsTemplate)));
+<?php 
+$structure_links = array(
+		'top' => '/administrate/groups/edit/'.$atim_menu_variables['Bank.id'].'/%%Group.id%%',
+		'bottom'=>array(
+			'cancel'=>'/administrate/groups/detail/'.$atim_menu_variables['Bank.id'].'/%%Group.id%%', 
+		)
+	);
 	
-	echo '<a id="permissions" href="#" onclick="addPermissions(); return false;">Add</a>
-					<script type="text/javascript">
-						var next_permission = "'.(count($this->data['Aro']['Permission']) > 0 ? count($this->data['Aro']['Permission']) : 1).'";
-						function addPermissions(){
-							$("permissions").insert({
-								before: "'.$permissionsTemplate.'".interpolate({
-									id: next_permission
-								})
-							});
-						next_permission++;
-						}
-					</script>';
-
+	$structures->build( $atim_structure, array('links'=>$structure_links) );
+	
 ?>
-	</fieldset>
-<?php echo $form->end('Submit');?>
-
-</div>
-<div class="actions">
-	<ul>
-		<li><?php echo $html->link(__('Delete', true), array('action'=>'delete', $form->value('Group.id')), null, sprintf(__('Are you sure you want to delete # %s?', true), $form->value('Group.id'))); ?></li>
-		<li><?php echo $html->link(__('List Groups', true), array('action'=>'index'));?></li>
-	</ul>
-</div>
