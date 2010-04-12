b0d9fef6b4beef50598b432435868e958f0ebf2c
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-04-12 14:10:55 +0000

    MERGED WITH REV 1178

--- branches/qc/frsq/lady_davis/atim2/app/plugins/clinicalannotation/controllers/consent_masters_controller.php
+++ branches/qc/frsq/lady_davis/atim2/app/plugins/clinicalannotation/controllers/consent_masters_controller.php
@@ -46,6 +46,8 @@ class ConsentMastersController extends ClinicalannotationAppController {
 		$consent_control_data = $this->ConsentControl->find('first', array('conditions' => array('ConsentControl.id' => $this->data['ConsentMaster']['consent_control_id'])));
 		$this->Structures->set($consent_control_data['ConsentControl']['form_alias']);
 		
+		$this->set('consent_type', $consent_control_data['ConsentControl']['controls_type']);
+		
 		// CUSTOM CODE: FORMAT DISPLAY DATA
 		$hook_link = $this->hook('format');
 		if( $hook_link ) { require($hook_link); }
