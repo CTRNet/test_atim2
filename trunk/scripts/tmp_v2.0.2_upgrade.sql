7f6934867fdd78aa1b50c4d9fa23ec2257d4500b
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-04-12 17:05:38 +0000

    eventum 848 - no more number in i18n under 6

--- trunk/scripts/tmp_v2.0.2_upgrade.sql
+++ trunk/scripts/tmp_v2.0.2_upgrade.sql
@@ -6,3 +6,6 @@
 UPDATE `versions` 
 SET `version_number` = 'v2.0.2', `date_installed` = '2010-04-12', `build_number` = ''
 WHERE `versions`.`id` =1;
+
+-- Eventum 848
+DELETE FROM i18n WHERE id IN ('1', '2', '3', '4', '5');
