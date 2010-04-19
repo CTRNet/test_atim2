7b20f37530dbd7c4f3d0d83c215f9a74e07bd3f3
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-04-19 17:28:43 +0000

    MERGED dev/jquery

--- trunk/app/webroot/js/default.js
+++ trunk/app/webroot/js/default.js
@@ -73,31 +73,128 @@ function uncheckAll( $div ) {
 	/*
 		Start observing filter menus to auto-scroll them if they are too long to show in the available 250px window.
 	*/
-	Event.observe(window, 'load', function() {
-		var menus = $$('.filter_menu');
-		for(var i=0; i< menus.length; i++){
-			var el = menus[i];
-			Event.observe(el, 'mousemove',function(ev){
-				var ul = el.getElementsByTagName('ul');
-				var H = el.getHeight();
-				
-				if(ul.length > 0){
-						var h = ul[0].getHeight();
-					
-					if(h > H){
-						Position.absolutize(ul[0]);
-						
-						var el_pos = Position.cumulativeOffset(el);
-						var ul_pos = Position.cumulativeOffset(ul[0]);
-						
-						var y = ( ev.pointerY() - el_pos[1] ) / H;
-						var yh = Math.floor(y * h);
-						var yH = Math.floor(y * H)
-						var y_offset = yH - yh;
-						
-						new Effect.Move( ul[0], { x:0, y: y_offset, mode:'absolute' } );
-					}
-				}
-			});
+//	FMLHHHHHHHHHHHHHH
+//	Event.observe(window, 'load', function() {
+//		var menus = $$('.filter_menu');
+//		for(var i=0; i< menus.length; i++){
+//			var el = menus[i];
+//			Event.observe(el, 'mousemove',function(ev){
+//				var ul = el.getElementsByTagName('ul');
+//				var H = el.getHeight();
+//				
+//				if(ul.length > 0){
+//						var h = ul[0].getHeight();
+//					
+//					if(h > H){
+//						Position.absolutize(ul[0]);
+//						
+//						var el_pos = Position.cumulativeOffset(el);
+//						var ul_pos = Position.cumulativeOffset(ul[0]);
+//						
+//						var y = ( ev.pointerY() - el_pos[1] ) / H;
+//						var yh = Math.floor(y * h);
+//						var yH = Math.floor(y * H)
+//						var y_offset = yH - yh;
+//						
+//						new Effect.Move( ul[0], { x:0, y: y_offset, mode:'absolute' } );
+//					}
+//				}
+//			});
+//		}
+//	});
+	
+	function getJsonFromClass(cssClass){
+		var startIndex = cssClass.indexOf("{");
+		return eval ('(' + cssClass.substr(startIndex, cssClass.lastIndexOf("}") - startIndex + 1) + ')');
+	}
+	
+	$(function(){
+		//field highlighting
+		if($("#table1row0").length == 1){
+			//gridview
+			$('form').highlight('td');
+		}else{
+			$('form').highlight('tr');
 		}
+		
+		//tree view controls
+		$(".reveal:not(.not_allowed)").each(function(){
+			var json = getJsonFromClass($(this).attr("class"));
+			$(this).toggle(function(){
+				$("#tree_" + json.tree).stop(true, true);
+				$("#tree_" + json.tree).show("blind", {}, 350);
+			}, function(){
+				$("#tree_" + json.tree).stop(true, true);
+				$("#tree_" + json.tree).hide("blind", {}, 350);
+			});
+		});
+		
+		//ajax controls
+		//evals the json within the class of the element and calls the method defined in callback
+		//the callback method needs to take this and json as parameters
+		$(".ajax").click(function(){
+			var json = getJsonFromClass($(this).attr("class"));
+			var fct = eval("(" + json.callback + ")");
+			fct.apply(this, [this, json]);
+			return false;
+		});
+		
+		//calendar controls
+		$.datepicker.setDefaults($.datepicker.regional[locale]);
+		$(".datepicker").each(function(){
+			initDatepicker(this);
+		});
+		//datepicker style
+		$("#ui-datepicker-div").addClass("jquery_cupertino");
+		
+		//add line controls
+//		if($("#addLineLink").length > 0){
+//			var tableBody = $("#addLineLink").parent().parent().parent().parent().children("tbody:first");
+//			var tmpLine = $(tableBody).children("tr:nth-child(2)").html();
+//			$("#addLineLink").click(function(){
+//				$(tableBody).append("<tr>" + tmpLine + "</tr>\n");
+//			});
+//		}	
 	});
+
+	function initDatepicker(element){
+		var tmpId = element.id.substr(0, element.id.length - 7);
+		if($('#' + tmpId).val().length > 0 && $('#' + tmpId + "-mm").val().length > 0 && $('#' + tmpId + "-dd").val().length > 0){
+			//set the current field date into the datepicker
+			$(element).data('val', $('#' + tmpId).val() + "-" + $('#' + tmpId + "-mm").val() + "-" + $('#' + tmpId + "-dd").val());
+		}
+		$(element).datepicker({
+			changeMonth: true,
+			changeYear: true,
+			dateFormat: 'yy-mm-dd',
+			firstDay: 0,
+			beforeShow: function(input, inst){
+				//put the date back in place
+				//because of datagrids copy controls we cannot keep the date in tmp
+				var tmpDate = $('#' + tmpId).val() + "-" + $('#' + tmpId + "-mm").val() + "-" + $('#' + tmpId + "-dd").val();
+				if(tmpDate.length == 10){
+					$(this).datepicker('setDate', tmpDate);
+				}
+				//show fake button to hide real button value
+				var item = $(this).parent().children("img")[0];
+				$(item).css("z-index", "1");
+			},
+			onClose: function(dateText,picker) {
+				//hide the date
+				$(this).val("");
+				var dateSplit = dateText.split(/-/);
+				$('#' + tmpId).val(dateSplit[0]); 
+	        	$('#' + tmpId + "-mm").val(dateSplit[1]);
+	        	$('#' + tmpId + "-dd").val(dateSplit[2]);
+	        	//hide fake button
+	        	var item = $(this).parent().children("img")[0];
+				$(item).css("z-index", "-1");
+		    }
+		});
+		
+		//activate fake_datepicker in case there is a problem with z-index
+		var currentDatepicker = this;
+		$(element).parent().children("img").click(function(){
+			$(currentDatepicker).datepicker('show');
+		});
+	}
