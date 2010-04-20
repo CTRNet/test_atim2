dccbd5480c37efd7a9d0e2c9a4acbf3607205807
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-04-20 20:54:03 +0000

    Merge trunk->branch v1247-1254

--- frsq/cusm/app/config/database.php
+++ frsq/cusm/app/config/database.php
@@ -83,8 +83,8 @@ class DATABASE_CONFIG {
 		'persistent' => false,
 		'host' => 'localhost',
 		'login' => 'root',
-		'password' => 'root',
-		'database' => 'atim_new',
+		'password' => '',
+		'database' => 'frsq_cusm',
 		'prefix' => '',
 		'encoding' => 'utf8'
 	);
