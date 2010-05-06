b72cd5c5b31b9b716aad4b7c1e6b502ce0045c44
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-05-06 20:17:07 +0000

    Eventum 666 - removed advanced search options for range values

--- app/webroot/js/default.js
+++ app/webroot/js/default.js
@@ -112,29 +112,27 @@ function uncheckAll( $div ) {
 			fct.apply(this, [this, json]);
 			return false;
 		});
+
+		initAdvancedControls();
 		
 		//calendar controls
 		$.datepicker.setDefaults($.datepicker.regional[locale]);
 		$(".datepicker").each(function(){
 			initDatepicker(this);
-			$(this).click(function(){
-				//bug fix for Safari and Chrome
-				$(this).datepicker('show');
-			});
 		});
 		//datepicker style
 		$("#ui-datepicker-div").addClass("jquery_cupertino");
 		//autocomplete style
 		$(".ui-autocomplete").addClass("jquery_cupertino");
 		
-		initAdvancedControls();
 	});
 
 	function initDatepicker(element){
-		var tmpId = element.id.substr(0, element.id.length - 7);
-		if($('#' + tmpId).val().length > 0 && $('#' + tmpId + "-mm").val().length > 0 && $('#' + tmpId + "-dd").val().length > 0){
+		var tmpId = element.id.substr(0, element.id.indexOf("_button"));
+		var tmpSuffix = element.id.indexOf("_button_") != -1 ? "_" + element.id.substr(element.id.indexOf("_button_") + 8) : "";
+		if($('#' + tmpId + tmpSuffix).val().length > 0 && $('#' + tmpId + "-mm" + tmpSuffix).val().length > 0 && $('#' + tmpId + "-dd" + tmpSuffix).val().length > 0){
 			//set the current field date into the datepicker
-			$(element).data('val', $('#' + tmpId).val() + "-" + $('#' + tmpId + "-mm").val() + "-" + $('#' + tmpId + "-dd").val());
+			$(element).data('val', $('#' + tmpId + tmpSuffix).val() + "-" + $('#' + tmpId + "-mm" + tmpSuffix).val() + "-" + $('#' + tmpId + "-dd" + tmpSuffix).val());
 		}
 		$(element).datepicker({
 			changeMonth: true,
@@ -146,7 +144,7 @@ function uncheckAll( $div ) {
 			beforeShow: function(input, inst){
 				//put the date back in place
 				//because of datagrids copy controls we cannot keep the date in tmp
-				var tmpDate = $('#' + tmpId).val() + "-" + $('#' + tmpId + "-mm").val() + "-" + $('#' + tmpId + "-dd").val();
+				var tmpDate = $('#' + tmpId + tmpSuffix).val() + "-" + $('#' + tmpId + "-mm" + tmpSuffix).val() + "-" + $('#' + tmpId + "-dd" + tmpSuffix).val();
 				if(tmpDate.length == 10){
 					$(this).datepicker('setDate', tmpDate);
 				}
@@ -158,9 +156,9 @@ function uncheckAll( $div ) {
 				//hide the date
 				$(this).val(" ");//space required for Safari and Chome or the button disappears
 				var dateSplit = dateText.split(/-/);
-				$('#' + tmpId).val(dateSplit[0]); 
-	        	$('#' + tmpId + "-mm").val(dateSplit[1]);
-	        	$('#' + tmpId + "-dd").val(dateSplit[2]);
+				$('#' + tmpId + tmpSuffix).val(dateSplit[0]); 
+	        	$('#' + tmpId + "-mm" + tmpSuffix).val(dateSplit[1]);
+	        	$('#' + tmpId + "-dd" + tmpSuffix).val(dateSplit[2]);
 	        	//hide fake button
 	        	var item = $(this).parent().children("img")[0];
 				$(item).css("z-index", "-1");
@@ -169,6 +167,11 @@ function uncheckAll( $div ) {
 		
 		//activate fake_datepicker in case there is a problem with z-index
 		$(element).parent().children("img").click(function(){
+			$(element).datepicker('show');
+		});
+		
+		//bug fix for Safari and Chrome
+		$(element).click(function(){
 			$(element).datepicker('show');
 		});
 	}
@@ -200,39 +203,48 @@ function uncheckAll( $div ) {
 		
 		//for each add or button
 		$(".btn_add_or").each(function(){
-			//find the matching input/select
-			var $input = $(this).parent().parent().find("input:last");
-			if($($input).length == 0){
-				$input = $(this).parent().parent().find("select:last");
-			}
-			//update its id and name
-			var id = $($input).attr("id");
-			var idIncrement = 1;
-			$($input).attr({
-				"id" : id + "_0",
-				"name" : $($input).attr("name") + "[]"
-			});
-			
-			//when we click
-			$(this).click(function(){
-				//get the input html
-				var inputHtml = $($input).parent().html();
-				//append it into the text field with "or" string + btn_remove
-				$(this).parent().parent().append("<span class='adv_ctrl'>" + STR_OR + inputHtml + "<a href='#' onclick='return false;' class='adv_ctrl btn_rmv_or'>(-)</a></span> ");
-				//find the newly generated input
-				var $newInput = $(this).parent().parent().find("input:last");
-				if($($newInput).length == 0){
-					$newInput = $(this).parent().parent().find("select:last");
-				}
-				//update its id
-				$($newInput).attr("id", id + "_" + idIncrement ++);
-				//bind the remove command to the remove button
-				$(this).parent().parent().find(".btn_rmv_or:last").click(function(){
-					$(this).parent().remove();
+			var $field = $(this).parent().parent().find("span:first");
+			if($($field).find("input, select").length == 1){
+				$($field).find("input, select").each(function(){
+					$(this).attr("name", $(this).attr("name") + "[]");
 				});
-				//move the add button to the end
-				$(this).parent().parent().append($(this).parent());
-			});
+				//html template for that field
+				var fieldHTML = $($field).html();
+				
+				//update its id
+				var idIncrement = 1;
+				$($field).find("input, select").each(function(){
+					$(this).attr("id", $(this).attr("id") + "_0");
+				});
+				
+				//when we click
+				$(this).click(function(){
+					//append it into the text field with "or" string + btn_remove
+					$(this).parent().parent().append("<span class='adv_ctrl'>" + STR_OR + fieldHTML + "<a href='#' onclick='return false;' class='adv_ctrl btn_rmv_or'>(-)</a></span> ");
+					//find the newly generated input
+					var $newField = $(this).parent().parent().find("span.adv_ctrl:last");
+					//update its id
+					$($newField).find("input, select").each(function(){
+						$(this).attr("id", $(this).attr("id") + "_" + idIncrement);
+					});
+					++ idIncrement;
+					
+					$($newField).find(".datepicker").each(function(){
+						initDatepicker(this);
+					});
+					
+					
+					//bind the remove command to the remove button
+					$(this).parent().parent().find(".btn_rmv_or:last").click(function(){
+						$(this).parent().remove();
+					});
+					//move the add button to the end
+					$(this).parent().parent().append($(this).parent());
+				});
+			}else{
+				//range values, no add options
+				$(this).remove();
+			}
 		});
 		
 		//one actions are binded hide the advanced controls (bind doesn't work on already hidden components)
