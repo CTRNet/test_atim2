0954120643696c14d02c3a94ad6e180c12fe3468
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-05-10 14:30:05 +0000

    tmp

--- branches/qc/chum/notre_dame/scripts/UpgradeScripts v1.6-v2.0/v1_icm_customisation_scripts/datamart_queries/process 001 - AliquotMaster - Add To Order.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM `datamart_batch_processes` 
-WHERE `id` = 1;
-
-INSERT INTO `datamart_batch_processes` 
-( `id` , `name` , `model` , `url` )
-VALUES (
-1 , 'Add To Order', 'AliquotMaster', '/order/orders/process_add_aliquots/');
