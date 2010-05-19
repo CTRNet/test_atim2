4264ec05eaae11bb90fee672ca0d9ad14a9d7eb0
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-05-19 19:23:03 +0000

    Eventum 896: Move banks under group and make them optional - more testing is required

--- app/plugins/administrate/models/bank.php
+++ app/plugins/administrate/models/bank.php
@@ -1,7 +1,13 @@
 <?php
 
 class Bank extends AdministrateAppModel {
-			
+	function listAllArray(){
+		$result = array();
+		foreach($this->find('all') as $bank){
+			$result[] = array("value" => $bank["Bank"]["id"], "default" => $bank["Bank"]["name"]);
+		}
+		return $result;
+	}
 }
 
 ?>
