8ed537ad0c0ede0b2194fcb2a587e5e0839bee17
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-02 14:46:00 +0000

    Removed all temporary scripts before tagging release.

--- trunk/scripts/tmp_v2.0.3_upgrade.sql
+++
@@ -1,9 +0,0 @@
--- Version: v2.0.3
--- Description: This SQL script is an upgrade for ATiM v2.0.2. to 2.0.3. and must be run against
--- an existing ATiM installation. Be sure to backup your database before running this script!
-
--- Update version information
-UPDATE `versions` 
-SET `version_number` = 'v2.0.3', `date_installed` = CURDATE(), `build_number` = ''
-WHERE `versions`.`id` =1;
-
