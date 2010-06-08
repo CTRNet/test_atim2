a248a5081eee4ab885fb47a3fbc9a4e6b07e571f
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-08 15:19:28 +0000

    Eventum 798: Print a warning before editing a protocol that is being used.

--- trunk/app/plugins/protocol/models/protocol_master.php
+++ trunk/app/plugins/protocol/models/protocol_master.php
@@ -59,6 +59,38 @@ class ProtocolMaster extends ProtocolAppModel {
 		return $result;
 	}	
 	
+	/**
+	 * Check if a record can be deleted.
+	 * 
+	 * @param $protocol_master_id Id of the studied record.
+	 * @param $protocol_extend_tablename
+	 * 
+	 * @return Return results as array:
+	 * 	['allow_deletion'] = true/false
+	 * 	['msg'] = message to display when previous field equals false
+	 * 
+	 * @author N. Luc
+	 * @since 2010-04-18
+	 * @moved from controller to model on 2010-06-08 by Mich
+	 */
+	 
+	function isUsedByTreatment($protocol_master_id, $protocol_extend_tablename){
+		App::import('Model', "Clinicalannotation.TreatmentMaster");
+		$this->TreatmentMaster = new TreatmentMaster();	
+		$nbr_trt_masters = $this->TreatmentMaster->find('count', array('conditions'=>array('TreatmentMaster.protocol_master_id'=>$protocol_master_id), 'recursive' => '-1'));
+		if ($nbr_trt_masters > 0){
+			return array('is_used' => true, 'msg' => 'protocol is defined as protocol of at least one participant treatment'); 
+		}
+		
+		$this->ProtocolExtend = new ProtocolExtend(false, $protocol_extend_tablename);
+		$nbr_extends = $this->ProtocolExtend->find('count', array('conditions'=>array('ProtocolExtend.protocol_master_id'=>$protocol_master_id), 'recursive' => '-1'));
+		if ($nbr_extends > 0){
+			return array('is_used' => true, 'msg' => 'at least one drug is defined as protocol component'); 
+		}
+		
+		return array('is_used' => false, 'msg' => '');
+	}
+	
 }
 
 ?>
