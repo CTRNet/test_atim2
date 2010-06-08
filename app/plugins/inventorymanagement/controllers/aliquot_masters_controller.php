ca5e59be9c6a60aad6b67fd547e7f45855779e17
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-08 13:52:26 +0000

    Eventum 947, 948: Fixed master detail validations

--- app/plugins/inventorymanagement/controllers/aliquot_masters_controller.php
+++ app/plugins/inventorymanagement/controllers/aliquot_masters_controller.php
@@ -368,15 +368,17 @@ class AliquotMastersController extends InventoryManagementAppController {
 					
 			// -> Fields validation
 			foreach($this->data as $key => $new_aliquot) {
-				// AliquotMaster
+				$this->data[$key]['AliquotMaster']['aliquot_control_id'] = $aliquot_control_id;
 				$this->AliquotMaster->set($this->data[$key]);
 				$submitted_data_validates = ($this->AliquotMaster->validates())? $submitted_data_validates: false;
-				foreach($this->AliquotMaster->invalidFields() as $field => $error) { $errors['AliquotMaster'][$field][$error] = '-'; }
-
-				// AliquotDetail
-				$this->AliquotDetail->set($this->data[$key]);
-				$submitted_data_validates = ($this->AliquotDetail->validates())? $submitted_data_validates: false;			
-				foreach($this->AliquotDetail->invalidFields() as $field => $error) { $errors['AliquotDetail'][$field][$error] = '-'; }
+				pr($this->AliquotMaster->validationErrors);
+				
+				$this->AliquotDetail->validationErrors = $this->AliquotMaster->validationErrors;
+				foreach($this->AliquotMaster->validationErrors as $field => $error) {
+					$errors['AliquotMaster'][$field][$error] = '-';
+					$errors['AliquotDetail'][$field][$error] = '-'; 
+				}
+				
 			}
 			
 			// -> Barcode validation
