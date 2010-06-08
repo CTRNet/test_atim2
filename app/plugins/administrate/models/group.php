8991c7f3958e9243a1aa85f0ca20989eaf6b84d7
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-08 18:21:26 +0000

    Evenutm 918: Group delete is blocked if a user is in the group.

--- app/plugins/administrate/models/group.php
+++ app/plugins/administrate/models/group.php
@@ -1,5 +1,26 @@
 <?php
 class Group extends AdministrateAppModel {
-	
+	var $hasMany = array("User" => array(
+			'className' => "Administrate.User",
+			'foreignKey' => "group_id"
+		));
+		
+	function summary( $variables=array() ) {
+		$return = false;
+		if ( isset($variables['Group.id']) ) {
+			$result = $this->find('first', array('conditions'=>array('Group.id'=>$variables['Group.id'])));
+			$return = array(
+				'Summary'	 => array(
+					'menu'			=>	array( NULL, $result['Group']['name']),
+					'title'			=>	array( NULL, $result['Group']['name']),
+					'description'	=>	array(
+						__('users', true) 		=> count($result['User']),
+						__('created', TRUE)		=>	__($result['Group']['created'], TRUE)
+					)
+				)
+			);
+		}
+		return $return;
+	}
 }
 ?>
