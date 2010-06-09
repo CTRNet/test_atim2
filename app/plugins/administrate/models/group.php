f1ece36d387fafe152733a5ad4d37c9e4015b496
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-09 14:15:34 +0000

    Added missing function

--- app/plugins/administrate/models/group.php
+++ app/plugins/administrate/models/group.php
@@ -22,5 +22,29 @@ class Group extends AdministrateAppModel {
 		}
 		return $return;
 	}
+	
+	function bindToPermissions(){
+		$this->bindModel(array('hasOne' => array(
+			'Aro' => array(
+				'className' => 'Aro',
+				'foreignKey' => 'foreign_key',
+				'conditions' => 'Aro.model="Group"'
+			)
+		)));
+		
+		$this->Aro->unbindModel(
+			array('hasAndBelongsToMany' => array('Aco'))
+		);
+		
+		$this->Aro->bindModel(
+			array('hasMany' => array(
+				'Permission' => array(
+					'className' => 'Permission',
+					'foreign_key'	=>'aco_id'
+					)
+				)
+			)
+		);
+	}
 }
 ?>
