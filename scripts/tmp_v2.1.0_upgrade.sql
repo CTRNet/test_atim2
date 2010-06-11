f6660bd2fef4a3f2b7dc9193187ac68bcdcfc441
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-11 15:05:57 +0000

    Removed temp upgrade scripts for tagging of v2.0.2A

--- scripts/tmp_v2.1.0_upgrade.sql
+++
@@ -1,8 +0,0 @@
--- Version: v2.1.0
--- Description: This SQL script is an upgrade for ATiM v2.0.2A to 2.1.0 and must be run against
--- an existing ATiM installation. Be sure to backup your database before running this script!
-
--- Update version information
-UPDATE `versions` 
-SET `version_number` = 'v2.1.0pre', `date_installed` = CURDATE(), `build_number` = ''
-WHERE `versions`.`id` =1;
