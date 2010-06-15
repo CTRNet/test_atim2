cba793256185838c99611ac1f5a81dbe3ed6a0fb
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-15 22:17:19 +0000

    tmp

--- frsq/cusm/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_format.php
+++ frsq/cusm/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_format.php
@@ -7,7 +7,10 @@
 		$default_aliquot_barcode = '';
 		if($aliquot_control_data['AliquotControl']['aliquot_type'] == 'whatman paper') {
 			$default_aliquot_barcode = substr($sample_data['SampleMaster']['qc_cusm_sample_label'], 0 , strrpos($sample_data['SampleMaster']['qc_cusm_sample_label'], '-')) . '-WHT';
-		} else {
+		} else if($aliquot_control_data['AliquotControl']['aliquot_type'] == 'block') {pr('bip');
+			$default_aliquot_barcode = $sample_data['SampleMaster']['qc_cusm_sample_label'];
+			//TODO crééer deux blocks par défaut: un FRZ un FFPE
+		}else {
 			$default_aliquot_barcode = $sample_data['SampleMaster']['qc_cusm_sample_label'];
 		}
 		$this->set('default_aliquot_barcode', $default_aliquot_barcode);
