fe6f7af6a2c3261a3e8a4d94a1f51e233ff92c6b
wilalambre <wilalambre@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-17 16:31:39 +0000

    Updating $this->Auth->actionPath for all plugins.

--- branches/dev/cake1_3_2_migration/app/plugins/rtbform/rtbform_app_controller.php
+++ branches/dev/cake1_3_2_migration/app/plugins/rtbform/rtbform_app_controller.php
@@ -4,7 +4,7 @@ class RtbformAppController extends AppController
 {	
 	function beforeFilter() {
 		parent::beforeFilter();
-		$this->Auth->actionPath = 'controllers/Rtbform/';
+		$this->Auth->actionPath = 'controllers/';
 	}
 	
 }
