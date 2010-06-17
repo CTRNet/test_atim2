9e543f2f14ec345798e78141ef81da6a0317300c
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-06-17 17:41:52 +0000

    Updated scripts for Lady davis

--- branches/qc/frsq/lady_davis/atim2/scripts/v2.0.2/custom_scriptPre202.sql
+++ branches/qc/frsq/lady_davis/atim2/scripts/v2.0.2/custom_scriptPre202.sql
@@ -439,7 +439,3 @@ INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_col
 
 ALTER TABLE `sd_der_serums` MODIFY `qc_lady_coagulation_time_sec` SMALLINT UNSIGNED DEFAULT NULL AFTER `deleted_date`;
 ALTER TABLE `sd_der_serums_revs` MODIFY `qc_lady_coagulation_time_sec` SMALLINT UNSIGNED DEFAULT NULL AFTER `deleted_date`;
-
-UPDATE structure_value_domains SET source=NULL WHERE domain_name='tissue_source_list';
-INSERT IGNORE INTO structure_permissible_values (`value`, `language_alias`) VALUES("ganglion", "ganglion");
-INSERT INTO structure_value_domains_permissible_values (`structure_value_domain_id`, `structure_permissible_value_id`, `display_order`, `flag_active`) VALUES((SELECT id FROM structure_value_domains WHERE domain_name="tissue_source_list"),  (SELECT id FROM structure_permissible_values WHERE value="ganglion" AND language_alias="ganglion"), "0", "1");
