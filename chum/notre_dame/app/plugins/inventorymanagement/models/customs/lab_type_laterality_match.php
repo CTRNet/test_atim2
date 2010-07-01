a01de00e1c2a9a5953067890ab97998751ab70cc
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-07-01 16:51:11 +0000

    Add Labo talterality, etc checks

--- chum/notre_dame/app/plugins/inventorymanagement/models/customs/lab_type_laterality_match.php
+++ chum/notre_dame/app/plugins/inventorymanagement/models/customs/lab_type_laterality_match.php
@@ -4,6 +4,42 @@ class LabTypeLateralityMatch extends InventorymanagementAppModel {
 		
 	var $useTable = 'lab_type_laterality_match';
 
+	function getLaboTypeCodes() {
+		$result = array();
+		
+		foreach($this->find('all', array('conditions' => array("selected_type_code NOT IN ('unknown', 'other')"), 'field' => 'DISTINCT selected_type_code', 'order' => 'selected_type_code')) as $new_record) {
+			$result[] = array('value' => $new_record['LabTypeLateralityMatch']['selected_type_code'], 'default' => $new_record['LabTypeLateralityMatch']['selected_type_code']);
+		}
+		$result[] = array('value' => 'other', 'default' => __('other', true));
+		$result[] = array('value' => 'unknown', 'default' => __('unknown', true));
+		
+		return $result;
+	}
+	
+	function getLaboLaterality() {
+		$result = array();
+		
+		foreach($this->find('all', array('conditions' => array("selected_labo_laterality NOT IN ('unknown')"), 'field' => 'DISTINCT selected_labo_laterality', 'order' => 'selected_labo_laterality')) as $new_record) {
+			$result[] = array('value' => $new_record['LabTypeLateralityMatch']['selected_labo_laterality'], 'default' => __('lab laterality '.$new_record['LabTypeLateralityMatch']['selected_labo_laterality'] ,true));
+		}
+		$result[] = array('value' => 'unknown', 'default' => __('unknown', true));
+			
+		return $result;
+	}
+	
+	
+	function getTissueSourcePermissibleValues() {
+		$result = array();
+		
+		foreach($this->find('all', array('conditions' => array("tissue_source_matching NOT IN ('unknown', 'other')"), 'field' => 'DISTINCT tissue_source_matching', 'order' => 'tissue_source_matching')) as $new_record) {
+			$result[] = array('value' => $new_record['LabTypeLateralityMatch']['tissue_source_matching'], 'default' => __($new_record['LabTypeLateralityMatch']['tissue_source_matching'], true));
+		}
+		$result[] = array('value' => 'other', 'default' => __('other', true));
+		$result[] = array('value' => 'unknown', 'default' => __('unknown', true));
+		
+		return $result;
+	}	
+
 }
 
 ?>
