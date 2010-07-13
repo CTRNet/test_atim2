f6cb3e00c8a8f5e1eb2ba383c1e268734d247f91
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-07-13 15:18:08 +0000

    Fix bug on get...() function

--- branches/qc/chum/notre_dame/app/plugins/inventorymanagement/models/customs/lab_type_laterality_match.php
+++ branches/qc/chum/notre_dame/app/plugins/inventorymanagement/models/customs/lab_type_laterality_match.php
@@ -7,7 +7,7 @@ class LabTypeLateralityMatch extends InventorymanagementAppModel {
 	function getLaboTypeCodes() {
 		$result = array();
 		
-		foreach($this->find('all', array('conditions' => array("selected_type_code NOT IN ('unknown', 'other')"), 'field' => 'DISTINCT selected_type_code', 'order' => 'selected_type_code')) as $new_record) {
+		foreach($this->find('all', array('conditions' => array("selected_type_code NOT IN ('unknown', 'other')"), 'fields' => 'DISTINCT selected_type_code', 'order' => 'selected_type_code')) as $new_record) {
 			$result[] = array('value' => $new_record['LabTypeLateralityMatch']['selected_type_code'], 'default' => $new_record['LabTypeLateralityMatch']['selected_type_code']);
 		}
 		$result[] = array('value' => 'other', 'default' => __('other', true));
@@ -19,11 +19,11 @@ class LabTypeLateralityMatch extends InventorymanagementAppModel {
 	function getLaboLaterality() {
 		$result = array();
 		
-		foreach($this->find('all', array('conditions' => array("selected_labo_laterality NOT IN ('unknown')"), 'field' => 'DISTINCT selected_labo_laterality', 'order' => 'selected_labo_laterality')) as $new_record) {
-			$result[] = array('value' => $new_record['LabTypeLateralityMatch']['selected_labo_laterality'], 'default' => __('lab laterality '.$new_record['LabTypeLateralityMatch']['selected_labo_laterality'] ,true));
+		foreach($this->find('all', array('conditions' => array("selected_labo_laterality NOT IN ('unknown')"), 'fields' => 'DISTINCT selected_labo_laterality', 'order' => 'selected_labo_laterality')) as $new_record) {
+			$result[] = array('value' => $new_record['LabTypeLateralityMatch']['selected_labo_laterality'], 'default' => empty($new_record['LabTypeLateralityMatch']['selected_labo_laterality'])? '' : __('lab laterality '.$new_record['LabTypeLateralityMatch']['selected_labo_laterality'] ,true));
 		}
 		$result[] = array('value' => 'unknown', 'default' => __('unknown', true));
-			
+		
 		return $result;
 	}
 	
@@ -31,7 +31,7 @@ class LabTypeLateralityMatch extends InventorymanagementAppModel {
 	function getTissueSourcePermissibleValues() {
 		$result = array();
 		
-		foreach($this->find('all', array('conditions' => array("tissue_source_matching NOT IN ('unknown', 'other')"), 'field' => 'DISTINCT tissue_source_matching', 'order' => 'tissue_source_matching')) as $new_record) {
+		foreach($this->find('all', array('conditions' => array("tissue_source_matching NOT IN ('unknown', 'other')"), 'fields' => 'DISTINCT tissue_source_matching', 'order' => 'tissue_source_matching')) as $new_record) {
 			$result[] = array('value' => $new_record['LabTypeLateralityMatch']['tissue_source_matching'], 'default' => __($new_record['LabTypeLateralityMatch']['tissue_source_matching'], true));
 		}
 		$result[] = array('value' => 'other', 'default' => __('other', true));
