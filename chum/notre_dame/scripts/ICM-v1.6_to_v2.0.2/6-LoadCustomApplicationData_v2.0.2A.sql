3ac888c075d952bb4a46dff3493ad1ad1c073b44
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-07-19 20:09:40 +0000

    Change aliquot view to fix bug on details button displayed in listall form button

--- chum/notre_dame/scripts/ICM-v1.6_to_v2.0.2/6-LoadCustomApplicationData_v2.0.2A.sql
+++ chum/notre_dame/scripts/ICM-v1.6_to_v2.0.2/6-LoadCustomApplicationData_v2.0.2A.sql
@@ -3632,3 +3632,59 @@ SET sfo.flag_datagrid = '1'
 WHERE sfo.structure_field_id = sf.id
 AND sf.model = 'AliquotDetail'
 AND sf.field IN ('tmp_storage_method', 'tmp_storage_solution');
+
+-- Change aliquot view to fix bug on details button displayed in listall form button
+
+DROP VIEW IF EXISTS view_aliquots;
+CREATE VIEW view_aliquots AS 
+SELECT 
+al.id AS aliquot_master_id,
+samp.id AS sample_master_id,
+al.collection_id AS collection_id, 
+col.bank_id, 
+stor.id AS storage_master_id,
+link.participant_id, 
+link.diagnosis_master_id, 
+link.consent_master_id,
+
+part.participant_identifier, 
+ident.identifier_name,
+ident.identifier_value,
+
+col.acquisition_label, 
+col.visit_label,
+
+samp.initial_specimen_sample_type, 	
+parent_samp.sample_type AS parent_sample_type,
+samp.sample_type,
+samp.sample_label,
+
+al.barcode,
+al.aliquot_type,
+al.in_stock,
+
+stor.code,
+stor.selection_label,
+al.storage_coord_x,
+al.storage_coord_y,
+
+stor.temperature,
+stor.temp_unit,
+
+al.deleted
+
+FROM aliquot_masters as al
+INNER JOIN sample_masters as samp ON samp.id = al.sample_master_id AND samp.deleted != 1
+INNER JOIN collections AS col ON col.id = samp.collection_id AND col.deleted != 1
+LEFT JOIN sample_masters as parent_samp ON samp.parent_id = parent_samp.id AND parent_samp.deleted != 1
+LEFT JOIN clinical_collection_links AS link ON col.id = link.collection_id AND link.deleted != 1
+LEFT JOIN participants AS part ON link.participant_id = part.id AND part.deleted != 1
+LEFT JOIN storage_masters AS stor ON stor.id = al.storage_master_id AND stor.deleted != 1
+LEFT JOIN banks ON col.bank_id = banks.id AND banks.deleted != 1
+LEFT JOIN misc_identifiers AS ident ON ident.misc_identifier_control_id = banks.misc_identifier_control_id AND ident.participant_id = part.id AND ident.deleted != 1
+WHERE al.deleted != 1;
+
+
+
+
+
