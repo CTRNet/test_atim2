5fcb34c08bbbe073a8c818acb258fc639db4d13b
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-07-27 20:14:23 +0000

    Cleaned out old structure data for all solid tumour path form. Fixed an incorrect tablename.

--- trunk/scripts/tmp_v2.1.0_upgrade.sql
+++ trunk/scripts/tmp_v2.1.0_upgrade.sql
@@ -547,4 +547,16 @@ INSERT INTO structure_fields(`public_identifier`, `plugin`, `model`, `tablename`
 INSERT INTO structure_validations (`structure_field_id`, `rule`, `flag_empty`, `flag_required`, `on_action`, `language_message`) 
 (SELECT (SELECT id FROM structure_fields WHERE model='AliquotMaster' AND tablename='aliquot_masters' AND field='barcode' AND `type`='autocomplete' AND structure_value_domain IS NULL ), `rule`, `flag_empty`, `flag_required`, `on_action`, `language_message` FROM structure_validations WHERE structure_field_id=(SELECT id FROM structure_fields WHERE model='AliquotMaster' AND tablename='aliquot_masters' AND field='barcode' AND `type`='input' AND structure_value_domain  IS NULL ));
 UPDATE structure_formats SET `structure_field_id`=(SELECT `id` FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='barcode' AND `type`='autocomplete' AND `structure_value_domain` IS NULL ) WHERE structure_id=(SELECT id FROM structures WHERE alias='orderitems') AND structure_field_id=(SELECT id FROM structure_fields WHERE model='AliquotMaster' AND tablename='aliquot_masters' AND field='barcode' AND type='input' AND structure_value_domain  IS NULL );
-	
+
+-- Fix estrogen field that had the wrong tablename specified
+UPDATE `structure_fields` SET `tablename` = 'ed_breast_lab_pathology' WHERE `field` = 'estrogen';
+
+-- Remove ALL SOLID TUMOURS form from Annotation and related structure tables
+DELETE FROM `event_controls` WHERE `disease_site`='all solid tumours' AND `event_group`='lab' AND `event_type`='pathology' LIMIT 1;
+DELETE FROM `structure_formats` 
+WHERE `structure_field_id` IN (SELECT `id` FROM `structure_fields` WHERE tablename = 'ed_allsolid_lab_pathology' AND plugin = 'Clinicalannotation' AND `model` = 'EventDetail');
+DELETE FROM `structure_formats` WHERE `structure_id` = (SELECT `id` FROM `structures` WHERE `alias` = 'ed_allsolid_lab_pathology');
+DELETE FROM `structures` WHERE `alias` = 'ed_allsolid_lab_pathology';
+DELETE FROM `structure_validations` WHERE `structure_field_id` IN (SELECT `id` FROM `structure_fields` WHERE tablename = 'ed_breast_lab_pathology' AND field = 'estrogen'); 
+DELETE FROM `structure_fields` WHERE tablename = 'ed_allsolid_lab_pathology' AND plugin = 'Clinicalannotation' AND `model` = 'EventDetail';
+DELETE FROM `i18n` WHERE `id` = 'estrogens amount is required.';
