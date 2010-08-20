cb90a5ac2aca51f984d07f0e61d53068e388525a
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-08-20 19:38:59 +0000

    MERGED trunk from 1780+ to 1785

--- chum/notre_dame/app/views/helpers/structures.php
+++ chum/notre_dame/app/views/helpers/structures.php
@@ -1269,7 +1269,6 @@ class StructuresHelper extends Helper {
 					//we need to initializse some $table_indexes
 					$table_index[ $field['display_column'] ][ $row_count ]['plain'] = '';
 					$table_index[ $field['display_column'] ][ $row_count ]['tag'] = '';
-					$table_index[ $field['display_column'] ][ $row_count ]['content'] = '';
 					$table_index[ $field['display_column'] ][ $row_count ]['input'] = '';
 				}
 				
@@ -1315,6 +1314,8 @@ class StructuresHelper extends Helper {
 					$display_value_raw = $data[ $field['StructureField']['model'] ][ $field['StructureField']['field'] ];
 					if(!is_array($display_value_raw)){
 						$display_value_raw = array("" => $display_value_raw);
+					}else{
+						$display_value_raw_was_arr = false;
 					}
 					foreach($display_value_raw as $display_value_key => $display_value){
 							// swap out VALUE for OVERRIDE choice for SELECTS, NO TRANSLATION 
@@ -1467,15 +1468,25 @@ class StructuresHelper extends Helper {
 						
 						$table_index[ $field['display_column'] ][ $row_count ]['plain'] .= str_replace('&nbsp;',' ',$display_value).' ';
 						
+						if(isset($display_value_raw_was_arr) && !isset($table_index[ $field['display_column'] ][ $row_count ]['content'][$display_value_key])){
+							$table_index[ $field['display_column'] ][ $row_count ]['content'][$display_value_key] = "";
+						}else if(!isset($display_value_raw_was_arr) && !isset($table_index[ $field['display_column'] ][ $row_count ]['content'])){
+							$table_index[ $field['display_column'] ][ $row_count ]['content'] = "";
+						}
 						if ( trim($display_value)!='' ) {
-							$table_index[ $field['display_column'] ][ $row_count ]['content'][$display_value_key] = $table_index[ $field['display_column'] ][ $row_count ]['tag'].$display_value.' ';
+							if(isset($display_value_raw_was_arr)){
+								$table_index[ $field['display_column'] ][ $row_count ]['content'][$display_value_key] .= $table_index[ $field['display_column'] ][ $row_count ]['tag'].$display_value.' ';
+							}else{
+								$table_index[ $field['display_column'] ][ $row_count ]['content'] .= $table_index[ $field['display_column'] ][ $row_count ]['tag'].$display_value.' ';
+							}
 						} else {
-							$table_index[ $field['display_column'] ][ $row_count ]['content'][$display_value_key] = $table_index[ $field['display_column'] ][ $row_count ]['tag'].'<span class="empty">&ndash;</span> ';
+							if(isset($display_value_raw_was_arr)){
+								$table_index[ $field['display_column'] ][ $row_count ]['content'][$display_value_key] .= $table_index[ $field['display_column'] ][ $row_count ]['tag'].'<span class="empty">&ndash;</span> ';
+							}else{
+								$table_index[ $field['display_column'] ][ $row_count ]['content'] .= $table_index[ $field['display_column'] ][ $row_count ]['tag'].'<span class="empty">&ndash;</span> ';
+							}
 							$table_index[ $field['display_column'] ][ $row_count ]['empty']++;
 						}
-				}
-				if(isset($table_index[ $field['display_column'] ][ $row_count ]['content'][""])){
-					$table_index[ $field['display_column'] ][ $row_count ]['content'] = $table_index[ $field['display_column'] ][ $row_count ]['content']; 
 				}
 				// get INPUT for FORM
 					$current_table_index = $table_index[$field['display_column']][$row_count];
@@ -1735,8 +1746,7 @@ class StructuresHelper extends Helper {
 							}
 								
 							// if existing DATA VALUE does not exist in the SELECT OPTIONS, add EXISTING DATA into the options using OPTGROUP to make the addition clear
-								$test_subject = $this->data[$field['StructureField']['model']][$field['StructureField']['field']]; 
-								if ( isset($test_subject) && !is_array($test_subject) && !array_key_exists($this->data[$field['StructureField']['model']][$field['StructureField']['field']],$html_element_array['options']) ) {
+								if ( isset($this->data[$field['StructureField']['model']][$field['StructureField']['field']]) && !is_array($this->data[$field['StructureField']['model']][$field['StructureField']['field']]) && !array_key_exists($this->data[$field['StructureField']['model']][$field['StructureField']['field']],$html_element_array['options']) ) {
 									
 									$html_element_array['options'] = array(
 										html_entity_decode( __( 'Supported Value', true ), ENT_QUOTES, "UTF-8" ) => $html_element_array['options'],
