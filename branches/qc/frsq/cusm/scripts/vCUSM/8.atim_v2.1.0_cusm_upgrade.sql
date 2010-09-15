20ae966e3bc9959248db21de265c0cf2bbd277bf
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-09-15 12:28:53 +0000

    missing scripts

---
+++ branches/qc/frsq/cusm/scripts/vCUSM/8.atim_v2.1.0_cusm_upgrade.sql
@@ -0,0 +1,123 @@
+DROP VIEW IF EXISTS view_collections;
+CREATE VIEW view_collections AS 
+SELECT 
+col.id AS collection_id, 
+col.bank_id, 
+col.sop_master_id, 
+link.participant_id, 
+link.diagnosis_master_id, 
+link.consent_master_id, 
+
+part.participant_identifier, 
+
+col.acquisition_label, 
+col.qc_cusm_visit_label, 
+col.collection_site, 
+col.collection_datetime, 
+col.collection_datetime_accuracy, 
+col.collection_property, 
+col.collection_notes, 
+col.deleted,
+
+banks.name AS bank_name,
+
+idents.identifier_value AS qc_cusm_prostate_bank_identifier 
+
+FROM collections AS col
+LEFT JOIN clinical_collection_links AS link ON col.id = link.collection_id AND link.deleted != 1
+LEFT JOIN participants AS part ON link.participant_id = part.id AND part.deleted != 1
+LEFT JOIN banks ON col.bank_id = banks.id AND banks.deleted != 1
+LEFT JOIN misc_identifiers AS idents ON idents.participant_id=link.participant_id AND idents.deleted != 1 AND idents.identifier_name = 'prostate_bank_participant_id'
+WHERE col.deleted != 1;
+
+DROP VIEW IF EXISTS view_samples;
+CREATE VIEW view_samples AS 
+SELECT 
+samp.id AS sample_master_id,
+samp.parent_id AS parent_sample_id,
+samp.initial_specimen_sample_id,
+samp.collection_id AS collection_id,
+
+col.bank_id, 
+col.qc_cusm_visit_label,
+col.sop_master_id, 
+link.participant_id, 
+link.diagnosis_master_id, 
+link.consent_master_id,
+
+part.participant_identifier, 
+
+col.acquisition_label, 
+
+samp.initial_specimen_sample_type, 	
+parent_samp.sample_type AS parent_sample_type,
+samp.sample_type,
+samp.sample_code,
+samp.qc_cusm_sample_label,
+samp.sample_category,
+samp.deleted,
+
+idents.identifier_value AS qc_cusm_prostate_bank_identifier 
+
+FROM sample_masters as samp
+INNER JOIN collections AS col ON col.id = samp.collection_id AND col.deleted != 1
+LEFT JOIN sample_masters as parent_samp ON samp.parent_id = parent_samp.id AND parent_samp.deleted != 1
+LEFT JOIN clinical_collection_links AS link ON col.id = link.collection_id AND link.deleted != 1
+LEFT JOIN participants AS part ON link.participant_id = part.id AND part.deleted != 1
+LEFT JOIN misc_identifiers AS idents ON idents.participant_id=link.participant_id AND idents.deleted != 1 AND idents.identifier_name = 'prostate_bank_participant_id'
+WHERE samp.deleted != 1;
+
+DROP VIEW IF EXISTS view_aliquots;
+CREATE VIEW view_aliquots AS 
+SELECT 
+al.id AS aliquot_master_id,
+al.sample_master_id AS sample_master_id,
+al.collection_id AS collection_id, 
+col.bank_id, 
+col.qc_cusm_visit_label,
+al.storage_master_id AS storage_master_id,
+link.participant_id, 
+link.diagnosis_master_id, 
+link.consent_master_id,
+
+part.participant_identifier, 
+
+col.acquisition_label, 
+
+samp.initial_specimen_sample_type, 	
+parent_samp.sample_type AS parent_sample_type,
+samp.sample_type,
+
+al.barcode,
+al.aliquot_type,
+al.in_stock,
+
+stor.code,
+stor.selection_label,
+al.storage_coord_x,
+al.storage_coord_y,
+
+stor.temperature,
+stor.temp_unit,
+
+al.deleted
+
+FROM aliquot_masters as al
+INNER JOIN sample_masters as samp ON samp.id = al.sample_master_id AND samp.deleted != 1
+INNER JOIN collections AS col ON col.id = samp.collection_id AND col.deleted != 1
+LEFT JOIN sample_masters as parent_samp ON samp.parent_id = parent_samp.id AND parent_samp.deleted != 1
+LEFT JOIN clinical_collection_links AS link ON col.id = link.collection_id AND link.deleted != 1
+LEFT JOIN participants AS part ON link.participant_id = part.id AND part.deleted != 1
+LEFT JOIN storage_masters AS stor ON stor.id = al.storage_master_id AND stor.deleted != 1
+WHERE al.deleted != 1;
+
+UPDATE structure_fields
+SET structure_value_domain = (SELECT id FROM `structure_value_domains` WHERE domain_name LIKE 'yes_no_checkbox')
+WHERE `field` = 'qc_cusm_on_ice';
+
+
+
+
+
+
+ 	 	SampleDetail  	  	 	yesno
