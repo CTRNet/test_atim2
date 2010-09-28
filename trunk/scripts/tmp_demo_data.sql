fb6b602b18ce7a0032c883a79228246cd37c2503
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-09-28 18:03:07 +0000

    ICD code correction

--- trunk/scripts/tmp_demo_data.sql
+++ trunk/scripts/tmp_demo_data.sql
@@ -583,9 +583,9 @@ INSERT INTO `derivative_details_revs` (`id`, `sample_master_id`, `creation_site`
 --
 
 INSERT INTO `diagnosis_masters` (`id`, `dx_identifier`, `primary_number`, `dx_method`, `dx_nature`, `dx_origin`, `dx_date`, `dx_date_accuracy`, `primary_icd10_code`, `previous_primary_code`, `previous_primary_code_system`, `morphology`, `topography`, `tumour_grade`, `age_at_dx`, `age_at_dx_accuracy`, `ajcc_edition`, `collaborative_staged`, `clinical_tstage`, `clinical_nstage`, `clinical_mstage`, `clinical_stage_summary`, `path_tstage`, `path_nstage`, `path_mstage`, `path_stage_summary`, `survival_time_months`, `information_source`, `notes`, `diagnosis_control_id`, `participant_id`, `created`, `created_by`, `modified`, `modified_by`, `deleted`, `deleted_date`) VALUES
-(1, NULL, 1, 'radiology', 'malignant', 'primary', '2001-03-05', 'm', 'C50.1', '', '', '', '', 'well differentiated', NULL, '', '5th', 'yes', 'IV', '', '', 'IV', 'IA', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:09:11', 1, '2010-05-28 14:12:54', 1, 0, NULL),
-(2, NULL, 1, 'cytology', 'malignant', 'secondary', '2003-02-01', '', 'C50.9', '', '', '', '', 'well differentiated', NULL, '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:11:42', 1, '2010-05-28 14:11:42', 1, 0, NULL),
-(3, NULL, 2, '', 'malignant', 'primary', '2002-06-13', '', 'C53.0', '', '', '', '', '', NULL, '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:14:39', 1, '2010-05-28 14:14:39', 1, 0, NULL),
+(1, NULL, 1, 'radiology', 'malignant', 'primary', '2001-03-05', 'm', 'C501', '', '', '', '', 'well differentiated', NULL, '', '5th', 'yes', 'IV', '', '', 'IV', 'IA', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:09:11', 1, '2010-05-28 14:12:54', 1, 0, NULL),
+(2, NULL, 1, 'cytology', 'malignant', 'secondary', '2003-02-01', '', 'C509', '', '', '', '', 'well differentiated', NULL, '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:11:42', 1, '2010-05-28 14:11:42', 1, 0, NULL),
+(3, NULL, 2, '', 'malignant', 'primary', '2002-06-13', '', 'C530', '', '', '', '', '', NULL, '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:14:39', 1, '2010-05-28 14:14:39', 1, 0, NULL),
 (4, NULL, NULL, 'radiology', 'malignant', 'primary', '1997-01-14', '', 'D461', '', '', '8002/3', '', 'poorly differentiated', NULL, '', '', '', 'pT4', 'pN2', 'pMx', '', '', '', '', '', NULL, '', '', 2, 2, '2010-09-24 19:32:26', 1, '2010-09-24 19:32:26', 1, 0, NULL),
 (5, NULL, NULL, 'autopsy', 'malignant', 'primary', '1992-09-09', '', NULL, '', '', '8001/0', '', 'well differentiated', NULL, '', '', '', 'pT1', '', '', '', '', '', '', '', NULL, '', '', 2, 3, '2010-09-24 19:40:52', 1, '2010-09-24 19:40:52', 1, 0, NULL);
 
@@ -594,11 +594,11 @@ INSERT INTO `diagnosis_masters` (`id`, `dx_identifier`, `primary_number`, `dx_me
 --
 
 INSERT INTO `diagnosis_masters_revs` (`id`, `dx_identifier`, `primary_number`, `dx_method`, `dx_nature`, `dx_origin`, `dx_date`, `dx_date_accuracy`, `primary_icd10_code`, `previous_primary_code`, `previous_primary_code_system`, `morphology`, `topography`, `tumour_grade`, `age_at_dx`, `age_at_dx_accuracy`, `ajcc_edition`, `collaborative_staged`, `clinical_tstage`, `clinical_nstage`, `clinical_mstage`, `clinical_stage_summary`, `path_tstage`, `path_nstage`, `path_mstage`, `path_stage_summary`, `survival_time_months`, `information_source`, `notes`, `diagnosis_control_id`, `participant_id`, `created`, `created_by`, `modified`, `modified_by`, `deleted`, `deleted_date`, `version_id`, `version_created`) VALUES
-(1, NULL, NULL, 'radiology', 'malignant', 'primary', '2001-03-05', 'm', 'C16.6', '', '', '', '', 'well differentiated', NULL, '', '5th', 'yes', 'IV', '', '', 'IV', 'IA', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:09:11', 1, '2010-05-28 14:09:11', 1, 0, NULL, 1, '2010-05-28 14:09:12'),
-(1, NULL, NULL, 'radiology', 'malignant', 'primary', '2001-03-05', 'm', 'C50.1', '', '', '', '', 'well differentiated', NULL, '', '5th', 'yes', 'IV', '', '', 'IV', 'IA', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:09:11', 1, '2010-05-28 14:10:26', 1, 0, NULL, 2, '2010-05-28 14:10:27'),
-(2, NULL, 1, 'cytology', 'malignant', 'secondary', '2003-02-01', '', 'C50.9', '', '', '', '', 'well differentiated', NULL, '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:11:42', 1, '2010-05-28 14:11:42', 1, 0, NULL, 3, '2010-05-28 14:11:43'),
-(1, NULL, 1, 'radiology', 'malignant', 'primary', '2001-03-05', 'm', 'C50.1', '', '', '', '', 'well differentiated', NULL, '', '5th', 'yes', 'IV', '', '', 'IV', 'IA', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:09:11', 1, '2010-05-28 14:12:54', 1, 0, NULL, 4, '2010-05-28 14:12:55'),
-(3, NULL, 2, '', 'malignant', 'primary', '2002-06-13', '', 'C53.0', '', '', '', '', '', NULL, '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:14:39', 1, '2010-05-28 14:14:39', 1, 0, NULL, 5, '2010-05-28 14:14:40'),
+(1, NULL, NULL, 'radiology', 'malignant', 'primary', '2001-03-05', 'm', 'C166', '', '', '', '', 'well differentiated', NULL, '', '5th', 'yes', 'IV', '', '', 'IV', 'IA', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:09:11', 1, '2010-05-28 14:09:11', 1, 0, NULL, 1, '2010-05-28 14:09:12'),
+(1, NULL, NULL, 'radiology', 'malignant', 'primary', '2001-03-05', 'm', 'C501', '', '', '', '', 'well differentiated', NULL, '', '5th', 'yes', 'IV', '', '', 'IV', 'IA', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:09:11', 1, '2010-05-28 14:10:26', 1, 0, NULL, 2, '2010-05-28 14:10:27'),
+(2, NULL, 1, 'cytology', 'malignant', 'secondary', '2003-02-01', '', 'C509', '', '', '', '', 'well differentiated', NULL, '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:11:42', 1, '2010-05-28 14:11:42', 1, 0, NULL, 3, '2010-05-28 14:11:43'),
+(1, NULL, 1, 'radiology', 'malignant', 'primary', '2001-03-05', 'm', 'C501', '', '', '', '', 'well differentiated', NULL, '', '5th', 'yes', 'IV', '', '', 'IV', 'IA', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:09:11', 1, '2010-05-28 14:12:54', 1, 0, NULL, 4, '2010-05-28 14:12:55'),
+(3, NULL, 2, '', 'malignant', 'primary', '2002-06-13', '', 'C530', '', '', '', '', '', NULL, '', '', '', '', '', '', '', '', '', '', '', NULL, '', '', 2, 1, '2010-05-28 14:14:39', 1, '2010-05-28 14:14:39', 1, 0, NULL, 5, '2010-05-28 14:14:40'),
 (4, NULL, NULL, 'radiology', 'malignant', 'primary', '1997-01-14', '', 'D461', '', '', '8002/3', '', 'poorly differentiated', NULL, '', '', '', 'pT4', 'pN2', 'pMx', '', '', '', '', '', NULL, '', '', 2, 2, '2010-09-24 19:32:26', 1, '2010-09-24 19:32:26', 1, 0, NULL, 6, '2010-09-24 19:32:27'),
 (5, NULL, NULL, 'autopsy', 'malignant', 'primary', '1992-09-09', '', NULL, '', '', '8001/0', '', 'well differentiated', NULL, '', '', '', 'pT1', '', '', '', '', '', '', '', NULL, '', '', 2, 3, '2010-09-24 19:40:52', 1, '2010-09-24 19:40:52', 1, 0, NULL, 7, '2010-09-24 19:40:52');
 
@@ -763,14 +763,14 @@ INSERT INTO `event_masters_revs` (`id`, `event_control_id`, `disease_site`, `eve
 --
 
 INSERT INTO `family_histories` (`id`, `relation`, `family_domain`, `primary_icd10_code`, `previous_primary_code`, `previous_primary_code_system`, `age_at_dx`, `age_at_dx_accuracy`, `participant_id`, `created`, `created_by`, `modified`, `modified_by`, `deleted`, `deleted_date`) VALUES
-(1, 'mother', 'maternal', 'C50.4', '', '', NULL, '', 1, '2010-05-28 14:28:12', 1, '2010-05-28 14:28:12', 1, 0, NULL);
+(1, 'mother', 'maternal', 'C504', '', '', NULL, '', 1, '2010-05-28 14:28:12', 1, '2010-05-28 14:28:12', 1, 0, NULL);
 
 --
 -- Dumping data for table `family_histories_revs`
 --
 
 INSERT INTO `family_histories_revs` (`id`, `relation`, `family_domain`, `primary_icd10_code`, `previous_primary_code`, `previous_primary_code_system`, `age_at_dx`, `age_at_dx_accuracy`, `participant_id`, `created`, `created_by`, `modified`, `modified_by`, `deleted`, `deleted_date`, `version_id`, `version_created`) VALUES
-(1, 'mother', 'maternal', 'C50.4', '', '', NULL, '', 1, '2010-05-28 14:28:12', 1, '2010-05-28 14:28:12', 1, 0, NULL, 1, '2010-05-28 14:28:13');
+(1, 'mother', 'maternal', 'C504', '', '', NULL, '', 1, '2010-05-28 14:28:12', 1, '2010-05-28 14:28:12', 1, 0, NULL, 1, '2010-05-28 14:28:13');
 
 --
 -- Dumping data for table `materials`
@@ -863,7 +863,7 @@ INSERT INTO `order_lines_revs` (`id`, `quantity_ordered`, `min_quantity_ordered`
 --
 
 INSERT INTO `participants` (`id`, `title`, `first_name`, `middle_name`, `last_name`, `date_of_birth`, `dob_date_accuracy`, `marital_status`, `language_preferred`, `sex`, `race`, `vital_status`, `notes`, `date_of_death`, `dod_date_accuracy`, `cod_icd10_code`, `secondary_cod_icd10_code`, `cod_confirmation_source`, `participant_identifier`, `last_chart_checked_date`, `created`, `created_by`, `modified`, `modified_by`, `deleted`, `deleted_date`) VALUES
-(1, 'Ms.', 'Wonder', '', 'Woman', '1947-11-03', 'm', '', '', 'f', '', 'dead', '', '2008-03-06', 'd', 'C00.2', NULL, '', 'wwi80', '2010-05-05', '2010-05-28 13:59:42', 1, '2010-05-28 14:31:50', 1, 0, NULL),
+(1, 'Ms.', 'Wonder', '', 'Woman', '1947-11-03', 'm', '', '', 'f', '', 'dead', '', '2008-03-06', 'd', 'C002', NULL, '', 'wwi80', '2010-05-05', '2010-05-28 13:59:42', 1, '2010-05-28 14:31:50', 1, 0, NULL),
 (2, 'Ms.', 'Ryana', 'Blue', '', '1926-06-10', '', '', '', 'f', '', 'alive', '', NULL, '', NULL, NULL, '', 'RRd784', NULL, '2010-09-24 19:30:37', 1, '2010-09-24 19:30:37', 1, 0, NULL),
 (3, 'Ms.', 'Lady', '', 'Green', '1920-12-15', '', '', '', 'f', '', 'deceased', '', NULL, '', NULL, NULL, '', 'qq4213', NULL, '2010-09-24 19:40:11', 1, '2010-09-24 19:40:11', 1, 0, NULL);
 
@@ -872,8 +872,8 @@ INSERT INTO `participants` (`id`, `title`, `first_name`, `middle_name`, `last_na
 --
 
 INSERT INTO `participants_revs` (`id`, `title`, `first_name`, `middle_name`, `last_name`, `date_of_birth`, `dob_date_accuracy`, `marital_status`, `language_preferred`, `sex`, `race`, `vital_status`, `notes`, `date_of_death`, `dod_date_accuracy`, `cod_icd10_code`, `secondary_cod_icd10_code`, `cod_confirmation_source`, `participant_identifier`, `last_chart_checked_date`, `created`, `created_by`, `modified`, `modified_by`, `deleted`, `deleted_date`, `version_id`, `version_created`) VALUES
-(1, 'Ms.', 'Wonder', 'Woman', '', '1947-11-03', 'm', '', '', 'f', '', 'dead', '', '2008-03-06', 'd', 'C00.2', NULL, '', 'wwi80', '2010-05-05', '2010-05-28 13:59:42', 1, '2010-05-28 13:59:42', 1, 0, NULL, 1, '2010-05-28 13:59:42'),
-(1, 'Ms.', 'Wonder', '', 'Woman', '1947-11-03', 'm', '', '', 'f', '', 'dead', '', '2008-03-06', 'd', 'C00.2', NULL, '', 'wwi80', '2010-05-05', '2010-05-28 13:59:42', 1, '2010-05-28 14:31:50', 1, 0, NULL, 2, '2010-05-28 14:31:51'),
+(1, 'Ms.', 'Wonder', 'Woman', '', '1947-11-03', 'm', '', '', 'f', '', 'dead', '', '2008-03-06', 'd', 'C002', NULL, '', 'wwi80', '2010-05-05', '2010-05-28 13:59:42', 1, '2010-05-28 13:59:42', 1, 0, NULL, 1, '2010-05-28 13:59:42'),
+(1, 'Ms.', 'Wonder', '', 'Woman', '1947-11-03', 'm', '', '', 'f', '', 'dead', '', '2008-03-06', 'd', 'C002', NULL, '', 'wwi80', '2010-05-05', '2010-05-28 13:59:42', 1, '2010-05-28 14:31:50', 1, 0, NULL, 2, '2010-05-28 14:31:51'),
 (2, 'Ms.', 'Ryana', 'Blue', '', '1926-06-10', '', '', '', 'f', '', 'alive', '', NULL, '', NULL, NULL, '', 'RRd784', NULL, '2010-09-24 19:30:37', 1, '2010-09-24 19:30:37', 1, 0, NULL, 3, '2010-09-24 19:30:37'),
 (3, 'Ms.', 'Lady', '', 'Green', '1920-12-15', '', '', '', 'f', '', 'deceased', '', NULL, '', NULL, NULL, '', 'qq4213', NULL, '2010-09-24 19:40:11', 1, '2010-09-24 19:40:11', 1, 0, NULL, 4, '2010-09-24 19:40:11');
 
