e81b2a920865488f4669fb5a32b09420c7223cf3
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-09-29 19:45:56 +0000

    Adding validations to icdo3 + fixing dx detail (eventum 1085)

--- trunk/app/plugins/clinicalannotation/clinicalannotation_app_model.php
+++ trunk/app/plugins/clinicalannotation/clinicalannotation_app_model.php
@@ -1,6 +1,22 @@
 <?php
 
 class ClinicalannotationAppModel extends AppModel {
+	
+	function validateIcd10WhoCode($id){
+		return CodingIcd10Who::validateId($id);
+	}
+	
+	function validateIcd10CaCode($id){
+		return CodingIcd10Ca::validateId($id);
+	}
+	
+	function validateIcdo3TopoCode($id){
+		return CodingIcdo3Topo::validateId($id);
+	}
+	
+	function validateIcdo3MorphoCode($id){
+		return CodingIcdo3Morpho::validateId($id);
+	}
 }
 
 ?>
--- trunk/app/plugins/clinicalannotation/models/family_history.php
+++ trunk/app/plugins/clinicalannotation/models/family_history.php
@@ -28,22 +28,6 @@ class FamilyHistory extends ClinicalAnnotationAppModel{
 		return $return;
 	}
 	
-	function validateIcd10WhoCode($id){
-		return CodingIcd10Who::validateId($id);
-	}
-	
-	function validateIcd10CaCode($id){
-		return CodingIcd10Ca::validateId($id);
-	}
-	
-	function validateIcdo3TopoCode($id){
-		return CodingIcdo3Topo::validateId($id);
-	}
-	
-	function validateIcdo3MorphoCode($id){
-		return CodingIcdo3Morpho::validateId($id);
-	}
-	
 	/**
 	 * Replaces icd10 empty string to null values to respect foreign keys constraints
 	 * @param $participantArray
