92ec8b781c36ee37d237611f56c64c2a89dfa7fe
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-10-05 16:45:32 +0000

    Updated permissions page to show a warning about the updates effect and the description now provides a link to the wiki help on permissions config

--- app/plugins/administrate/controllers/permissions_controller.php
+++ app/plugins/administrate/controllers/permissions_controller.php
@@ -2,7 +2,7 @@
 
 class PermissionsController extends AdministrateAppController {
 	
-	var $uses = array('Aco','Aro'); 
+	var $uses = array('Aco','Aro', 'ExternalLink'); 
 	
 	function index(){
 		
@@ -137,6 +137,13 @@ class PermissionsController extends AdministrateAppController {
 		$threaded_data = $this->addPermissionStateToThreadedData($threaded_data);
 		
 		$this->data = $threaded_data;
+		AppController::addWarningMsg(__("note: permission changes will not take effect until the user logs out of the system.", true));
+		if(!isset($this->data[0]['Aco']['state'])){
+			//the root not is blank, display "denied" to make it easier to understand
+			$this->data[0]['Aco']['state'] = -1;
+		}
+		$help_url = $this->ExternalLink->find('first', array('conditions' => array('name' => 'permissions_help')));
+		$this->set("help_url", $help_url['ExternalLink']['link']);
 	}
 	
 	function addPermissionStateToThreadedData( $threaded_data=array() ) {
