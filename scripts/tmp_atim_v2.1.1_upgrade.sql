b7eacf8354313f8dfbcf8d8b3453014ca4bf66f6
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-11-03 17:59:16 +0000

    Eventum 1174: Adding a maximal string length verification + adding some validations

--- scripts/tmp_atim_v2.1.1_upgrade.sql
+++ scripts/tmp_atim_v2.1.1_upgrade.sql
@@ -16,4 +16,15 @@ ALTER TABLE structure_value_domains_permissible_values ADD FOREIGN KEY (`structu
 
 UPDATE structure_value_domains SET source = 'StructurePermissibleValuesCustom::getCustomDropdown(''quality control tools'')' WHERE `domain_name` LIKE 'custom_laboratory_qc_tool';
 
-INSERT IGNORE INTO i18n (id,en,fr) VALUES ('announcements', 'Announcements', 'Annonces');
+INSERT IGNORE INTO i18n (id,en,fr) VALUES 
+('announcements', 'Announcements', 'Annonces'),
+("a specified %s already exists for that dropdown", "A specified %s already exists for that dropdown", "Un champ %s existe déjà pour ce menu déroulant"),
+("you cannot declare the same %s more than once", "You cannot declare the same %s more than once", "Vous ne pouvez pas déclarer la même valeur plus d'une fois pour le champ %s"),
+("%s cannot exceed %d characters", "%s cannot exceed %d characters", "Le champ %s ne dois pas excéder %s caractères");
+
+ALTER TABLE `structure_permissible_values_custom_controls` ADD `values_max_length` TINYINT UNSIGNED NOT NULL;
+UPDATE structure_permissible_values_custom_controls SET values_max_length=20;
+
+INSERT INTO structure_validations (structure_field_id, rule, flag_empty, flag_required, language_message) VALUES
+((SELECT id FROM structure_fields WHERE model='StructurePermissibleValuesCustom' AND field='en'), 'notEmpty', 0, 1, 'value is required'),
+((SELECT id FROM structure_fields WHERE model='StructurePermissibleValuesCustom' AND field='fr'), 'notEmpty', 0, 1, 'value is required');
