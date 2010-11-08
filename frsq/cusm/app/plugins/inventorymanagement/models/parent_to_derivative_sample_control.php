16de8a14297ac79b2e6449a3e1a58179e6397ead
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-11-08 15:48:13 +0000

    Merge trunk->branch v2113-2133

--- frsq/cusm/app/plugins/inventorymanagement/models/parent_to_derivative_sample_control.php
+++ frsq/cusm/app/plugins/inventorymanagement/models/parent_to_derivative_sample_control.php
@@ -10,6 +10,35 @@ class ParentToDerivativeSampleControl extends InventoryManagementAppModel {
 			'className'   => 'Inventorymanagement.SampleControl',
 			 'foreignKey'  => 'derivative_sample_control_id'));  	
 	
+	function getActiveSamples(){
+		$data = $this->find('all', array('conditions' => array('flag_active' => 1), 'recursive' => -1));
+		$relations = array();
+		//arrange the data
+		foreach($data as $unit){
+			$key = $unit["ParentToDerivativeSampleControl"]["parent_sample_control_id"];
+			$value = $unit["ParentToDerivativeSampleControl"]["derivative_sample_control_id"];
+			if(!isset($relations[$key])){
+				$relations[$key] = array();
+			}
+			$relations[$key][] = $value;
+		}
+		
+		//start from the top and find the active samples
+		return self::getActiveIdsFromRelations($relations, "");
+	}
+	
+	private static function getActiveIdsFromRelations($relations, $current_check){
+		$active_ids = array();
+		foreach($relations[$current_check] as $sample_id){
+			if($current_check != $sample_id){
+				$active_ids[] = $sample_id;
+				if(isset($relations[$sample_id])){
+					$active_ids = array_merge($active_ids, self::getActiveIdsFromRelations($relations, $sample_id));
+				}
+			}
+		}
+		return array_unique($active_ids);
+	}
 }
 
 ?>
