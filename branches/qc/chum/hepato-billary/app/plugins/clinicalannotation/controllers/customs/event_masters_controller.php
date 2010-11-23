2a45d94e1b6dca27af2b6073360e0a33553f5c5d
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-11-23 18:45:27 +0000

    Validated version ready for tests: - DB validation done - Compare core files to tag v2.0.1A done - All sql scripts of the core copied into the vcrchum_hb directory have been validated

--- branches/qc/chum/hepato-billary/app/plugins/clinicalannotation/controllers/customs/event_masters_controller.php
+++ branches/qc/chum/hepato-billary/app/plugins/clinicalannotation/controllers/customs/event_masters_controller.php
@@ -14,6 +14,10 @@ class EventMastersControllerCustom extends EventMastersController {
  
 	function imageryReport( $participant_id ){
 		$this->data = $this->EventMaster->find('all', array('conditions' => array('EventMaster.participant_id' => $participant_id, 'EventControl.form_alias LIKE "qc_hb_imaging_%"')));
+		foreach($this->data as $key => $record) {
+			$this->data[$key]['EventMaster']['formated_event_date'] = $this->getFormatedDatetimeString($this->data[$key]['EventMaster']['event_date']);
+		}
+		
 		$this->Structures->set('empty');
 		$atim_menu_variables['Participant.id'] = $participant_id;
 		$this->set('atim_menu_variables', $atim_menu_variables);
--- branches/qc/chum/hepato-billary/app/plugins/clinicalannotation/models/tmp_acos.php
+++
@@ -1,6 +0,0 @@
-<?php
-class TmpAcosCtrl extends ClinicalannotationAppModel {
- 	var $useTable = 'acos';
- 	var $actsAs = array('Tree');
-}
-?>
