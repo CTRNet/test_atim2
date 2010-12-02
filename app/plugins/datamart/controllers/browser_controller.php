29107b53fe441a9547e67201ff1c9e43f6255afc
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-12-02 22:00:42 +0000

    Fixing databrowser bug where a new subset is created if a search form contains fields of the same model as the parent

--- app/plugins/datamart/controllers/browser_controller.php
+++ app/plugins/datamart/controllers/browser_controller.php
@@ -131,7 +131,8 @@ class BrowserController extends DatamartAppController {
 			$direct_id_arr = explode("_", $control_id);
 			//save selected subset
 			$parent = $this->BrowsingResult->find('first', array('conditions' => array("BrowsingResult.id" => $parent_node)));
-			if(isset($this->data[$parent['DatamartStructure']['model']])){
+			if(isset($this->data[$parent['DatamartStructure']['model']]) && isset($this->data['Browser'])){
+				//if parent model found and not from a search form
 				$ids = array();
 				if(count($this->data[$parent['DatamartStructure']['model']][$parent['DatamartStructure']['use_key']]) == 1 
 				&& strpos($this->data[$parent['DatamartStructure']['model']][$parent['DatamartStructure']['use_key']], ",") !== false){
