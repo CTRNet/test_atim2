0de9f1d22b750c5de3acd958e1a76cc8553c25be
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-12-13 16:37:55 +0000

    Changed the way uses are calculated for AliquotMaster No more use count for ViewAliquot. If we want them, we can simply add them to the view.

--- trunk/app/plugins/inventorymanagement/models/view_aliquot.php
+++ trunk/app/plugins/inventorymanagement/models/view_aliquot.php
@@ -1,22 +1,7 @@
 <?php
 
 class ViewAliquot extends InventorymanagementAppModel {
-	
-	private static $AliquotUse = null;
-	
-	/**
-	 * Defines Generated.aliquot_use_counter when ViewAliquot.aliquot_master_id is defined
-	 * @see Model::afterFind()
-	 */
-	function afterFind($results){
-		if(self::$AliquotUse == null){
-			self::$AliquotUse = AppModel::atimNew("inventorymanagement", "AliquotUse", true);
-		}
-		foreach($results as &$result){
-			$result['Generated']['aliquot_use_counter'] = self::$AliquotUse->find('count', array('conditions' => array('AliquotUse.aliquot_master_id' => $result['ViewAliquot']['aliquot_master_id'])));
-		}
-		return $results;
-	}
+
 }
 
 ?>
