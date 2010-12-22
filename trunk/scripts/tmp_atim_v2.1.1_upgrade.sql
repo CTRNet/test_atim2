510492de2e2f95dd6bb4efaf22f741050171e203
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2010-12-22 14:36:44 +0000

    Drop view_structures and add alias to view_structure_formats_simplified

--- trunk/scripts/tmp_atim_v2.1.1_upgrade.sql
+++ trunk/scripts/tmp_atim_v2.1.1_upgrade.sql
@@ -484,4 +484,25 @@ INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_col
 ((SELECT id FROM structures WHERE alias='in_stock_detail'), (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='in_stock_detail' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='aliquot_in_stock_detail')  ), '1', '1', '', '1', 'in stock detail', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0'),
 ((SELECT id FROM structures WHERE alias='in_stock_detail'), (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='in_stock' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='aliquot_in_stock_values')  ), '1', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '0');
 
+DROP VIEW view_structure_formats_simplified;
+CREATE VIEW view_structure_formats_simplified AS 
+SELECT str.alias AS structure_alias, sfo.id AS structure_format_id, sfi.id AS structure_field_id, sfo.structure_id AS structure_id,
+sfi.plugin AS plugin, sfi.model AS model, sfi.tablename AS tablename, sfi.field AS field, sfi.structure_value_domain AS structure_value_domain, svd.domain_name AS structure_value_domain_name,
+IF(sfo.flag_override_label = '1', sfo.language_label, sfi.language_label) AS language_label,
+IF(sfo.flag_override_tag = '1', sfo.language_tag, sfi.language_tag) AS language_tag,
+IF(sfo.flag_override_help = '1', sfo.language_help, sfi.language_help) AS language_help,
+IF(sfo.flag_override_type = '1', sfo.type, sfi.type) AS `type`,
+IF(sfo.flag_override_setting = '1', sfo.setting, sfi.setting) AS setting,
+IF(sfo.flag_override_default = '1', sfo.default, sfi.default) AS `default`,
+sfo.flag_add AS flag_add, sfo.flag_add_readonly AS flag_add_readonly, sfo.flag_edit AS flag_edit, sfo.flag_edit_readonly AS flag_edit_readonly,
+sfo.flag_search AS flag_search, sfo.flag_search_readonly AS flag_search_readonly, sfo.flag_addgrid AS flag_addgrid, sfo.flag_addgrid_readonly AS flag_addgrid_readonly,
+sfo.flag_editgrid AS flag_editgrid, sfo.flag_editgrid_readonly AS flag_editgrid_readonly, 
+sfo.flag_batchedit AS flag_batchedit, sfo.flag_batchedit_readonly AS flag_batchedit_readonly,
+sfo.flag_index AS flag_index, sfo.flag_detail AS flag_detail,
+sfo.flag_summary AS flag_summary, sfo.display_column AS display_column, sfo.display_order AS display_order, sfo.language_heading AS language_heading
+FROM structure_formats AS sfo
+INNER JOIN structure_fields AS sfi ON sfo.structure_field_id=sfi.id
+INNER JOIN structures AS str ON str.id = sfo.structure_id
+LEFT JOIN structure_value_domains AS svd ON svd.id = sfi.structure_value_domain;
 
+DROP VIEW view_structures;
