62ce7e4c04404eb6bf1ed3e9ae74fe7aaf5db622
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-01-10 17:49:35 +0000

    Update

--- branches/bc/ttr/scripts/create_db.sh
+++ branches/bc/ttr/scripts/create_db.sh
@@ -10,8 +10,8 @@ fi
 echo running tmp_atim_v2.1.0A_script_FULL.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < tmp_atim_v2.1.0A_script_FULL.sql
 
-echo running tmp_atim_v2.1.1_demo_data.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < tmp_atim_v2.1.1_demo_data.sql
+#echo running tmp_atim_v2.1.1_demo_data.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < tmp_atim_v2.1.1_demo_data.sql
 
 echo running tmp_atim_v2.1.1_upgrade.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < tmp_atim_v2.1.1_upgrade.sql
