88fd25bac1da149e7928cccb7883f5b91d8e8699
simondee <simondee@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-01-11 00:08:03 +0000

    added

--- ttr/scripts/v2.1.0/bc_ttr_alter_tables.sql
+++ ttr/scripts/v2.1.0/bc_ttr_alter_tables.sql
@@ -33,7 +33,10 @@ ALTER TABLE `consent_masters`
   ADD COLUMN `bc_ttr_pathology_specification` varchar(20) DEFAULT NULL, 
   ADD COLUMN `bc_ttr_date_consent_denied` date DEFAULT NULL, 
   ADD COLUMN `bc_ttr_date_consent_withdrawn` date DEFAULT NULL, 
-  ADD COLUMN `bc_ttr_date_referral_withdrawn` date DEFAULT NULL;
+  ADD COLUMN `bc_ttr_date_referral_withdrawn` date DEFAULT NULL,
+  ADD COLUMN `bc_ttr_decline_use_of_blood_samples` varchar(10) DEFAULT NULL,
+  ADD COLUMN `bc_ttr_brt_flag` varchar(10) DEFAULT NULL;
+  
 ALTER TABLE `consent_masters_revs`
   ADD COLUMN `bc_ttr_consent_closed` varchar(100) DEFAULT NULL,
   ADD COLUMN `bc_ttr_protocol` varchar(100) DEFAULT NULL,
@@ -85,7 +88,8 @@ CREATE TABLE IF NOT EXISTS `bc_ttr_correspondences` (
   PRIMARY KEY (`id`),
   KEY `participant_id` (`participant_id`)
 ) ENGINE=InnoDb  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;  
-DROP TABLE IF EXISTS `bc_ttr_correspondences`;
+
+DROP TABLE IF EXISTS `bc_ttr_correspondences_revs`;
 CREATE TABLE IF NOT EXISTS `bc_ttr_correspondences_revs` (
   `id` int(11) NOT NULL,
   `participant_id` int(11) NOT NULL DEFAULT '0',
@@ -113,6 +117,8 @@ CREATE TABLE IF NOT EXISTS `bc_ttr_correspondences_revs` (
   ADD COLUMN `bc_ttr_tb_received_by` varchar(20) DEFAULT NULL,
   ADD COLUMN `bc_ttr_tb_received_datetime` datetime DEFAULT NULL,
   ADD COLUMN `bc_ttr_tissue_collection_status` varchar(20) DEFAULT NULL;
+  
+  
 ALTER TABLE `collections_revs`
   ADD COLUMN `bc_ttr_collected_by` varchar(20) DEFAULT NULL,
   ADD COLUMN `bc_ttr_collected_datetime` datetime DEFAULT NULL, 
@@ -121,3 +127,8 @@ ALTER TABLE `collections_revs`
   ADD COLUMN `bc_ttr_tb_received_datetime` datetime DEFAULT NULL,
   ADD COLUMN `bc_ttr_tissue_collection_status` varchar(20) DEFAULT NULL;
 */  
+
+
+
+
+
