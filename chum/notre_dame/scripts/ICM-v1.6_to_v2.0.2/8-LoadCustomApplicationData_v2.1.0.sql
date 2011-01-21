b9acc6296e20eb1a47d1d1ac89d0a32a019cce92
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-01-21 19:16:26 +0000

    add comment to develop

--- chum/notre_dame/scripts/ICM-v1.6_to_v2.0.2/8-LoadCustomApplicationData_v2.1.0.sql
+++ chum/notre_dame/scripts/ICM-v1.6_to_v2.0.2/8-LoadCustomApplicationData_v2.1.0.sql
@@ -1015,4 +1015,4 @@ DELETE from aros_acos WHERE aco_id NOT IN (SELECT id FROM acos WHERE alias = 'co
 control qualiteé: Le choix appareil ne marche pas
 Aliquot ADN se met a non disponible si on fait un control de qualite
 Pour les aliquots de culture cellulaire: On aimerait un flag pour dire mycoplasme free.
-
+ bug de chantale https://10.52.47.134/ATiM/?from=/clinicalannotation/sample_masters/listall/7313
