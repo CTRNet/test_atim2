d817a9c6d9b9e2b6ab3945b188bd6c12eae1c4a9
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-02-08 21:54:34 +0000

    modifiy label generation

--- ohri/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_format.php
+++ ohri/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_format.php
@@ -4,7 +4,8 @@
 	// Set Default Aliquot Data
 	// -------------------------------------------------------------------------------- 	
 	
-	if(empty($this->data)) {
+	if(empty($this->data) && in_array($sample_data['SampleMaster']['sample_type'], array('ascite supernatant', 'ascite cell', 'cell culture', 'tissue'))) {
+		
 		// Participant participant_id and Bank Number
 		if(!isset($this->ViewSample)) {
 			App::import('Model', 'Inventorymanagement.ViewSample');
@@ -14,32 +15,33 @@
 		$bank_number = empty($tmp_view_sample_data['ViewSample']['ohri_bank_participant_id'])? 'n/a' : $tmp_view_sample_data['ViewSample']['ohri_bank_participant_id'];
 		$participant_id = empty($tmp_view_sample_data['ViewSample']['participant_id'])? null : $tmp_view_sample_data['ViewSample']['participant_id'];
 
+		// Get aliquot already created
+		$criteria = array('ViewAliquot.sample_control_id' => $tmp_view_sample_data['ViewSample']['sample_control_id'], 
+			'ViewAliquot.aliquot_control_id' => $aliquot_control_data['AliquotControl']['id']);
+		if(empty($participant_id)) {
+			$criteria['ViewAliquot.collection_id'] = $collection_id;
+		} else {
+			$criteria['ViewAliquot.participant_id'] = $participant_id;
+		}
+		$existing_aliquots_list = $this->ViewAliquot->find('all', array('conditions' => $criteria));
+		
 		switch($sample_data['SampleMaster']['sample_type']) {
 			
 			case 'ascite supernatant':
-				$aliquot_count = $this->ViewAliquot->find('count', array('conditions' => array(
-					'ViewAliquot.sample_control_id' => $tmp_view_sample_data['ViewSample']['sample_control_id'], 
-					'ViewAliquot.aliquot_control_id' => $aliquot_control_data['AliquotControl']['id'],
-					'ViewAliquot.participant_id' => $participant_id)));
-				
-				$inital_data[0]['AliquotMaster']['barcode'] = $bank_number . ' S' . ($aliquot_count+1);
+				$inital_data[0]['AliquotMaster']['barcode'] = $bank_number . ' S' . (sizeof($existing_aliquots_list)+1);
 				$inital_data[0]['AliquotMaster']['initial_volume'] = '15';
 				
 				break;
 				
 			case 'ascite cell':
-				$aliquots_list = $this->ViewAliquot->find('all', array('conditions' => array(
-					'ViewAliquot.sample_control_id' => $tmp_view_sample_data['ViewSample']['sample_control_id'], 
-					'ViewAliquot.aliquot_control_id' => $aliquot_control_data['AliquotControl']['id'],
-					'ViewAliquot.participant_id' => $participant_id)));				
 				$ids = array();
-				foreach($aliquots_list as $new_aliq) {
+				foreach($existing_aliquots_list as $new_aliq) {
 					$ids[] = $new_aliq['ViewAliquot']['aliquot_master_id'];
 				}	
 				
 				$flash_frozen_count = 0;
 				$dmso_count = 0;
-				if(!empty($aliquots_list)) {
+				if(!empty($existing_aliquots_list)) {
 					$aliquots_details = $this->AliquotMaster->find('all',  array('conditions' => array('AliquotMaster.id' => $ids)));	
 					foreach($aliquots_details as $tmp_al)	{
 						if($tmp_al['AliquotDetail']['ohri_storage_method'] == 'flash frozen') {
@@ -65,16 +67,11 @@
 				break;
 				
 			case 'tissue':
-				$aliquot_count = $this->ViewAliquot->find('count', array('conditions' => array(
-					'ViewAliquot.sample_control_id' => $tmp_view_sample_data['ViewSample']['sample_control_id'], 
-					'ViewAliquot.aliquot_control_id' => $aliquot_control_data['AliquotControl']['id'],
-					'ViewAliquot.participant_id' => $participant_id)));				
-				
 				if($aliquot_control_data['AliquotControl']['aliquot_type'] == 'block') {
-					$inital_data[0]['AliquotMaster']['barcode'] = $bank_number . ' B' . ($aliquot_count+1);
+					$inital_data[0]['AliquotMaster']['barcode'] = $bank_number . ' B' . (sizeof($existing_aliquots_list)+1);
 					$inital_data[0]['AliquotDetail']['block_type'] = 'paraffin';
 				} else if($aliquot_control_data['AliquotControl']['aliquot_type'] == 'tube') {
-					$inital_data[0]['AliquotMaster']['barcode'] = $bank_number . ' T' . ($aliquot_count+1);
+					$inital_data[0]['AliquotMaster']['barcode'] = $bank_number . ' T' . (sizeof($existing_aliquots_list)+1);
 					$inital_data[0]['AliquotDetail']['ohri_storage_method'] = 'flash frozen';
 				}
 				break;
