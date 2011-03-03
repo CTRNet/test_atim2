804a658c93bcb8befbcf725add4cccc183a8b5ff
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-03-03 20:50:29 +0000

    Eventum 1300: Restored function deleted by previous commit

--- trunk/app/plugins/labbook/labbook_app_model.php
+++ trunk/app/plugins/labbook/labbook_app_model.php
@@ -1,7 +1,20 @@
 <?php
 
 class LabBookAppModel extends AppModel {
-
+	/**
+	 * @param int $process_ctrl_id
+	 * @return array The fields managed by the process or false if the process_ctrl_id is invalid
+	 */
+	function getFields($process_ctrl_id){
+		$control = AppModel::atimNew("processdata", "ProcessDataControl", true);
+		$data = $control->findById($process_ctrl_id);
+		if(!empty($data)){
+			$detail_model = new AppModel(array('table' => $data['ProcessDataControl']['detail_tablename'], 'name' => "ProcessDataDetail", 'alias' => "ProcessDataDetail"));
+			$fields = array_keys($detail_model->_schema);
+			return array_diff($fields, array("id", "process_data_master_id", "created", "created_by", "modified", "modified_by", "deleted", "deleted_date"));
+		}
+		return false;
+	}
 }
 
 ?>
