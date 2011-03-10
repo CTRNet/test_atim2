7f578d477326c248cbeee6d61b2f5f5f9c55a275
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-03-10 19:23:35 +0000

    issue 1317: Check and correct all summaries

--- app/plugins/clinicalannotation/models/misc_identifier.php
+++ app/plugins/clinicalannotation/models/misc_identifier.php
@@ -12,17 +12,17 @@ class MiscIdentifier extends ClinicalAnnotationAppModel {
     function summary( $variables=array() ) {
 		$return = false;
 		
-		if ( isset($variables['MiscIdentifier.id']) ) {
-			
-			$result = $this->find('first', array('conditions'=>array('MiscIdentifier.id'=>$variables['MiscIdentifier.id'])));
-			
-			$return = array(
-				'name'	=>	array( NULL, $result['MiscIdentifier']['name']),
-				'participant_id' => array( NULL, $result['MiscIdentifier']['participant_id']),
-				'data'			=> $result,
-				'structure alias'=>'miscidentifiers'
-			);
-		}
+//		if ( isset($variables['MiscIdentifier.id']) ) {
+//			
+//			$result = $this->find('first', array('conditions'=>array('MiscIdentifier.id'=>$variables['MiscIdentifier.id'])));
+//			
+//			$return = array(
+//				'name'	=>	array( NULL, $result['MiscIdentifier']['name']),
+//				'participant_id' => array( NULL, $result['MiscIdentifier']['participant_id']),
+//				'data'			=> $result,
+//				'structure alias'=>'miscidentifiers'
+//			);
+//		}
 		
 		return $return;
 	}	
