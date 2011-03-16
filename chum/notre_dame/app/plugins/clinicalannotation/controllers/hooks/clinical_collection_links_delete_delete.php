42c7222f158c52dce436855bbb3104d595b7a400
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-03-16 17:48:20 +0000

    Fixes

--- chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/clinical_collection_links_delete_delete.php
+++ chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/clinical_collection_links_delete_delete.php
@@ -4,8 +4,8 @@
 	// Update participant collection sample labels
 	// -------------------------------------------------------------------------------- 	
  	if($arr_allow_deletion['allow_deletion']) {
- 		$collection = AppModel::import('Inventorymanagement', 'Collection', true);
- 		$collection->->updateCollectionSampleLabels($clinical_collection_data['Collection']['id'], '');
+ 		$collection = AppModel::atimNew('Inventorymanagement', 'Collection', true);
+ 		$collection->updateCollectionSampleLabels($clinical_collection_data['Collection']['id'], '');
 	}
 
 ?>
