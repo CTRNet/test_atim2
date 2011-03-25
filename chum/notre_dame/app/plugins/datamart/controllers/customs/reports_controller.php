25f387a3c5f6c026c59b1ad440458c2ce09f804a
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-03-25 20:00:54 +0000

    PROCURE report in working condition

--- chum/notre_dame/app/plugins/datamart/controllers/customs/reports_controller.php
+++ chum/notre_dame/app/plugins/datamart/controllers/customs/reports_controller.php
@@ -1,17 +1,17 @@
 <?php
 class ReportsControllerCustom extends ReportsController {
-	function procureConsentStat(){
+	function procureConsentStat($data){
 		$load_form = false;
 		$this->ConsentMaster = AppModel::atimNew('Clinicalannotation', 'ConsentMaster', true);
-		if(!empty($this->data) && $this->ConsentMaster->validates($this->data)){
-			$data = $this->data[0];
-			$this->set("csv", $data['action'] == "true");
-			$date_from = $data['date_from_start']['year']."-".$data['date_from_start']['month']."-".$data['date_from_start']['day']." 00:00:00";
-			$date_to = $data['date_from_end']['year']."-".$data['date_from_end']['month']."-".$data['date_from_end']['day']." 23:59:59";
+		$error = null;
+		if(!empty($data)){
+			$this->set("csv", isset($data['action']) && $data['action'] == "true");
+			$date_from = $data['report_date_range_start']['year']."-".$data['report_date_range_start']['month']."-".$data['report_date_range_start']['day']." 00:00:00";
+			$date_to = $data['report_date_range_end']['year']."-".$data['report_date_range_end']['month']."-".$data['report_date_range_end']['day']." 23:59:59";
 			$this->set("date_from", $date_from);
 			$this->set("date_to", $date_to);
 			if(!preg_match(VALID_DATETIME_YMD, $date_from) || !preg_match(VALID_DATETIME_YMD, $date_to)){
-				$this->ConsentMaster->validationErrors[] = "error in the date definitions";
+				$error = "error in the date definitions";
 				$load_form = true;
 			}else{
 				//step 1 - fetch procure consents for specified interval
@@ -24,80 +24,96 @@ class ReportsControllerCustom extends ReportsController {
 					'ConsentMaster.*');
 				
 				//build time ranges
-				$year_start = (int)$data['date_from_start']['year'];
-				$month_start = (int)$data['date_from_start']['month'];
-				$year_end = $tmp = (int)$data['date_from_end']['year'];
-				$month_end = $tmp = (int)$data['date_from_end']['month'];
+				$year_start = (int)$data['report_date_range_start']['year'];
+				$month_start = (int)$data['report_date_range_start']['month'];
+				$year_end = $tmp = (int)$data['report_date_range_end']['year'];
+				$month_end = $tmp = (int)$data['report_date_range_end']['month'];
 				$date_range = array();
+				$data = array();
+				
+				$initial_data = array(0 => array(
+						"denied" => 0,
+						"participant" => 0,
+						"blood" => 0,
+						"urine" => 0,
+						"questionnaire" => 0,
+						"annual_followup" => 0,//TODO 
+						"contact_if_info_req" => 0,
+						"contact_if_discovery" => 0,
+						"study_other_diseases" => 0,
+						"contact_if_disco_other_diseases" => 0,
+						"other_contacts_if_die" => 0//TODO
+					)
+				);
+				AppController::addWarningMsg("Les valeurs pour le suivi annuel et les autres contacts en cas de décès ne sont pas calculées");
 				while($year_start < $year_end || $month_start <= $month_end){
-					$date_range[$year_start][$month_start] = 0;
+					$key = $year_start."-".$month_start;
+					$data[$key] = $initial_data;
+					$data[$key][0]['period'] = $key;
 					$month_start ++;
 					if($month_start > 12){
 						$year_start ++;
 						$month_start = 1;
 					}
 				}
+				$tmp_data = $this->ConsentMaster->find('all', array('conditions' => $conditions, 'recursive' => 2, 'fields' => $fields, 'order' => array("ConsentMaster.consent_signed_date")));
 				
-				$tmp_data = $this->ConsentMaster->find('all', array('conditions' => $conditions, 'recursive' => 2, 'fields' => $fields));
-				$this->data = array(0 => array(
-						"denied" => $date_range,
-						"participant" => $date_range,
-						"blood" => $date_range,
-						"urine" => $date_range,
-						"questionnaire" => $date_range,
-						"annual_followup" => $date_range,//TODO 
-						"contact_if_info_req" => $date_range,
-						"contact_if_discovery" => $date_range,
-						"study_other_diseases" => $date_range,
-						"contact_if_disco_other_diseases" => $date_range,
-						"other_contacts_if_die" => $date_range//TODO
-					)
-				);
 				$participants_ids = $date_range;
-				
+				$data_result = array();
 				foreach($tmp_data as $data_unit){
 					$year = $data_unit[0]['signed_year'];
 					$month = $data_unit[0]['signed_month'];
+					$key = $year."-".$month;
 					
-					$participants_ids[$year][$month] .= ",".$data_unit['ConsentMaster']['participant_id'];
+					
 					if($data_unit['ConsentMaster']['consent_status'] == 'denied' || $data_unit['ConsentMaster']['consent_status'] == 'withdrawn'){
-						$this->data[0]["denied"][$year][$month] ++;
+						$data[$key][0]["denied"] ++;
 					}
-					$this->data[0]["participant"][$year][$month] ++;
+					$data[$key][0]["participant"] ++;
 					if($data_unit['ConsentDetail']['use_of_blood'] == "yes"){
-						$this->data[0]["blood"][$year][$month] ++;
+						$data[$key][0]["blood"] ++;
 					}
 					if($data_unit['ConsentDetail']['use_of_urine'] == "yes"){
-						$this->data[0]["urine"][$year][$month] ++;
+						$data[$key][0]["urine"] ++;
 					}
 					if($data_unit['ConsentDetail']['allow_questionnaire'] == "yes"){
-						$this->data[0]["questionnaire"][$year][$month] ++;
+						$data[$key][0]["questionnaire"] ++;
 					}
 					
 					if($data_unit['ConsentDetail']['contact_for_additional_data'] == 'yes'){
-						$this->data[0]["contact_if_info_req"][$year][$month] ++;
+						$data[$key][0]["contact_if_info_req"] ++;
 					}
 					if($data_unit['ConsentDetail']['inform_significant_discovery'] == 'yes'){
-						$this->data[0]["contact_if_discovery"][$year][$month] ++;
+						$data[$key][0]["contact_if_discovery"] ++;
 					}
 					if($data_unit['ConsentDetail']['research_other_disease'] == 'yes'){
-						$this->data[0]["study_other_diseases"][$year][$month] ++;
+						$data[$key][0]["study_other_diseases"] ++;
 					}
 					if($data_unit['ConsentDetail']['inform_discovery_on_other_disease'] == 'yes'){
-						$this->data[0]["contact_if_disco_other_diseases"][$year][$month] ++;
+						$data[$key][0]["contact_if_disco_other_diseases"] ++;
 					}
 				}
+				
+				$total = $initial_data[0];
+				foreach($data as $key => $data_unit){
+					$sub_data_unit = $data_unit[0];
+					foreach($total as $sub_key => &$value){
+						$value += $sub_data_unit[$sub_key];
+					}
+				}
+				$total['period'] = __('total', true);
+				$data['total'][0] = $total;
 			}
 		}else{
 			$load_form = true;
 		}
-		
-		$this->Structures->set("qc_nd_procure_consent_stats_report");
-		$test = $this->Structures->get('form', 'qc_nd_procure_consent_stats_report');
-		pr($test);
-		die("FMLH");
-		if($load_form){
-			$this->set("submit", true);
-		}
+		$this->set("submit", $load_form);
+
+		return array(
+			"header"		=> "PROCURE - consent report",
+			"data"			=> $data,
+			"columns_names"	=> array(),
+			"error_msg"		=> $error
+		);
 	}
 }
