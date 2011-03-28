36ecc730507529ce0c72f858ee8c667023e08759
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-03-28 20:54:14 +0000

    Add fix on misc_identifier hooks.

--- branches/qc/chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_add_postsave_process.php
+++ branches/qc/chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_add_postsave_process.php
@@ -7,7 +7,7 @@
 	// Get Bank linked to this created identifier
 	$bank_model = AppModel::atimNew('Administrate', 'Bank', true);	
 	$bank = $bank_model->find('first', array('conditions' => array('Bank.misc_identifier_control_id' => $misc_identifier_control_id)));
-	
+pr('la');	
 	if(!empty($bank)) {
 		$bank_id = $bank['Bank']['id'];
 		
@@ -16,7 +16,7 @@
 		$participant_bank_collection_list = $clinical_collection_link->find('all', array('conditions' => array('ClinicalCollectionLink.participant_id' => $participant_id, 'Collection.bank_id' => $bank_id)));
 
 		if(!empty($participant_bank_collection_list)) {
-			$collection = AppModel::import('Inventorymanagement', 'Collection', true);
+			$collection = AppModel::atimNew('Inventorymanagement', 'Collection', true);
 			foreach($participant_bank_collection_list as $new_linked_collection){
 				$collection->updateCollectionSampleLabels($new_linked_collection['Collection']['id'], $this->data['MiscIdentifier']['identifier_value']);
 			}
--- branches/qc/chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_delete_postsave_process.php
+++ branches/qc/chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_delete_postsave_process.php
@@ -16,7 +16,7 @@
 		$participant_bank_collection_list = $clinical_collection_link->find('all', array('conditions' => array('ClinicalCollectionLink.participant_id' => $participant_id, 'Collection.bank_id' => $bank_id)));
 		
 		if(!empty($participant_bank_collection_list)) {
-			$collection = AppModel::import('Inventorymanagement', 'Collection', true);
+			$collection = AppModel::atimNew('Inventorymanagement', 'Collection', true);
 			foreach($participant_bank_collection_list as $new_linked_collection) {
 				$collection->updateCollectionSampleLabels($new_linked_collection['Collection']['id'], '');
 			}
