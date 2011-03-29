803b9f2de786b3d0e98764f62812eb1721cc4f68
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-03-29 03:00:49 +0000

    Eventum 1353: Fixes random find return when an empty array is passed as a condition

--- app/plugins/inventorymanagement/controllers/sample_masters_controller.php
+++ app/plugins/inventorymanagement/controllers/sample_masters_controller.php
@@ -111,7 +111,10 @@ class SampleMastersController extends InventorymanagementAppController {
 			$aliquot_ids = $this->AliquotMaster->find('list', array('fields' => array('AliquotMaster.id'), 'conditions' => array("AliquotMaster.collection_id" => $collection_id, "AliquotMaster.sample_master_id" => $sample_master_id), 'recursive' => -1));
 			$aliquot_ids = array_diff($aliquot_ids, $this->Realiquoting->find('list', array('fields' => array('Realiquoting.child_aliquot_master_id'), 'conditions' => array("Realiquoting.child_aliquot_master_id" => $aliquot_ids), 'group' => array("Realiquoting.child_aliquot_master_id"))));
 			$aliquot_ids_has_child = array_flip($this->AliquotMaster->hasChild($aliquot_ids));
+			
+			$aliquot_ids[] = 0;//counters Eventum 1353
 			$aliquots = $this->AliquotMaster->find('all', array('conditions' => array("AliquotMaster.id" => $aliquot_ids), 'recursive' => 0));
+			
 			foreach($aliquots as &$aliquot){
 				$aliquot['children'] = array_key_exists($aliquot['AliquotMaster']['id'], $aliquot_ids_has_child);
 			}
