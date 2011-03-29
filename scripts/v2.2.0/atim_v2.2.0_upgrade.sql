52327aa994c4503d9ab6497ec0d30adb0cde29dc
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-03-29 16:01:28 +0000

    Folder and upgrade/demo scripts for v2.2.0 release. Removed developer temp scripts. Full version to follow.

--- scripts/v2.2.0/atim_v2.2.0_upgrade.sql
+++ scripts/v2.2.0/atim_v2.2.0_upgrade.sql
@@ -1,9 +1,9 @@
--- Version: v2.1.1
--- Description: This SQL script is an upgrade for ATiM v2.1.0A to 2.1.1 and must be run against
+-- Version: v2.2.0
+-- Description: This SQL script is an upgrade for ATiM v2.1.0A to 2.2.0 and must be run against
 -- an existing ATiM installation. Be sure to backup your database before running this script!
 
 -- Update version information
-INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.1.1-Alpha', NOW(), '> 2198');
+INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.2.0', NOW(), '');
 
 TRUNCATE `acos`; 
 
