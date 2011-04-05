3d3de9ceb98a8993a66642205bab4455f6432c56
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-05 19:43:21 +0000

    Eventum 1354: User active preference help bubble msg fixed

--- branches/dev/v2.2.1/scripts/tmp_atim_v2.2.1_upgrade.sql
+++ branches/dev/v2.2.1/scripts/tmp_atim_v2.2.1_upgrade.sql
@@ -5,3 +5,8 @@ ALTER TABLE users
  DROP COLUMN last_visit;
  
 UPDATE structure_formats SET `language_heading`='diagnosis' WHERE structure_id=(SELECT id FROM structures WHERE alias='clinicalcollectionlinks') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='DiagnosisMaster' AND `tablename`='diagnosis_masters' AND `field`='dx_date' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+
+REPLACE INTO i18n (id, en, fr) VALUES
+("help_flag_active", 
+ "Determines whether the account can be used to log into ATiM or not. Locked means that the account cannot be used.",
+ "Détermine si le compte peut être utiliser pour se connecter à ATiM ou non. Bloqué signifie que le compte ne peut pas être utilisé.");
