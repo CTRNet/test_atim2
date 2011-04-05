1782b82439965ff6879a5945c580e0274370f85d
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-05 15:51:33 +0000

    Eventum 1362: Missing dx header in clinical collection links detail

--- v2.2.1/scripts/tmp_atim_v2.2.1_upgrade.sql
+++ v2.2.1/scripts/tmp_atim_v2.2.1_upgrade.sql
@@ -3,3 +3,5 @@ INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.
 
 ALTER TABLE users 
  DROP COLUMN last_visit;
+ 
+UPDATE structure_formats SET `language_heading`='diagnosis' WHERE structure_id=(SELECT id FROM structures WHERE alias='clinicalcollectionlinks') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='DiagnosisMaster' AND `tablename`='diagnosis_masters' AND `field`='dx_date' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
