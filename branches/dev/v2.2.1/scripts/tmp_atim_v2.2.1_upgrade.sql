3885923d70c7cb7c64ecfd96f460cb6780dd7a23
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-05 15:46:04 +0000

    Eventum 1364: User.last_visit (removed since trackable with user_logs)

--- branches/dev/v2.2.1/scripts/tmp_atim_v2.2.1_upgrade.sql
+++ branches/dev/v2.2.1/scripts/tmp_atim_v2.2.1_upgrade.sql
@@ -1,2 +1,5 @@
 -- Update version information
 INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.2.1 alpha', NOW(), '> 2838');
+
+ALTER TABLE users 
+ DROP COLUMN last_visit;
