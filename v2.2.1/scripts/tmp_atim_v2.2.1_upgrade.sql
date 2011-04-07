f5cf858bfe23e40c251291aebff75b02adc96676
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-07 14:39:05 +0000

    E1369: Custom queries display now translates title, model and description fields.

--- v2.2.1/scripts/tmp_atim_v2.2.1_upgrade.sql
+++ v2.2.1/scripts/tmp_atim_v2.2.1_upgrade.sql
@@ -6,12 +6,17 @@ ALTER TABLE users
  
 UPDATE structure_formats SET `language_heading`='diagnosis' WHERE structure_id=(SELECT id FROM structures WHERE alias='clinicalcollectionlinks') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='DiagnosisMaster' AND `tablename`='diagnosis_masters' AND `field`='dx_date' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
 
-INSERT INTO i18n (id,en,fr) VALUES ('add participant','Add Participant','Créer participant');
-
 REPLACE INTO i18n (id, en, fr) VALUES
 ("help_flag_active", 
  "Determines whether the account can be used to log into ATiM or not. Locked means that the account cannot be used.",
- "Détermine si le compte peut être utiliser pour se connecter à ATiM ou non. Bloqué signifie que le compte ne peut pas être utilisé.");
+ "Détermine si le compte peut être utiliser pour se connecter à ATiM ou non. Bloqué signifie que le compte ne peut pas être utilisé."),
+('aliquots spent times summary', 'Aliquot Spent Times Summary', 'Résumé des temps écoulés liés aux aliquots'),
+('generate aliquots spent times summary','Generate Aliquot Spent Times Summary','Générer le résumé des temps écoulés liés aux aliquots'),
+('calculation of different aliquots spent times',
+'Summary gathering aliquot spent times like ''Collection to Storage Spent Time'', ''Creation to Storage Spent Time'', etc.',
+'Résumé contenant des temps écoulés tel que ''Le temps entre la collection et l''entreposage'', etc.'),
+('add participant','Add Participant','Créer participant');
+
 
 DELETE FROM structure_formats WHERE structure_id=(SELECT id FROM structures WHERE alias='preferences') AND structure_field_id=(SELECT id FROM structure_fields WHERE `public_identifier`='' AND `plugin`='Administrate' AND `model`='User' AND `tablename`='users' AND `field`='flag_active' AND `language_label`='account status' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain`=(SELECT id FROM structure_value_domains WHERE domain_name='user_active') AND `language_help`='help_flag_active' AND `validation_control`='open' AND `value_domain_control`='open' AND `field_control`='open' AND `flag_confidential`='0');
 INSERT INTO structures(`alias`) VALUES ('preferences_lock');
@@ -72,16 +77,6 @@ INSERT INTO `datamart_batch_processes` (`id`, `name`, `plugin`, `model`, `url`, 
 (null, 'generate aliquots spent times summary', 'Inventorymanagement', 'AliquotMaster', CONCAT('datamart/reports/manageReport/',(SELECT id FROM datamart_reports WHERE name = 'spent times summary applied to aliquots')), 1),
 (null, 'generate aliquots spent times summary', 'Inventorymanagement', 'ViewAliquot', CONCAT('datamart/reports/manageReport/',(SELECT id FROM datamart_reports WHERE name = 'spent times summary applied to aliquots')), 1);
 
-INSERT INTO i18n (id,en,fr) VALUES 
-('aliquots spent times summary', 'Aliquot Spent Times Summary', 'Résumé des temps écoulés liés aux aliquots'),
-('generate aliquots spent times summary','Generate Aliquot Spent Times Summary','Générer le résumé des temps écoulés liés aux aliquots'),
-('calculation of different aliquots spent times',
-'Summary gathering aliquot spent times like ''Collection to Storage Spent Time'', ''Creation to Storage Spent Time'', etc.',
-'Résumé contenant des temps écoulés tel que ''Le temps entre la collection et l''entreposage'', etc.');
 
-
-
-
-
-
+UPDATE structure_fields SET `structure_value_domain`=(SELECT id FROM structure_value_domains WHERE domain_name='models'), type='select' WHERE `model`='Adhoc' AND `tablename`='datamart_adhoc' AND `field`='model' AND `type`='input' AND `structure_value_domain` IS NULL;
 
