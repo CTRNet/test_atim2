1286e9154c914e58a151bdcd68b24f38ece5be87
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-08 19:35:11 +0000

    Prepping scripts fot 2.2.1 tagging

--- branches/dev/v2.2.x/scripts/v2.2.0/atim_v2.2.1_upgrade.sql
+++ branches/dev/v2.2.x/scripts/v2.2.0/atim_v2.2.1_upgrade.sql
@@ -1,5 +1,5 @@
 -- Update version information
-INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.2.1 alpha', NOW(), '> 2838');
+INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.2.1', NOW(), '2921');
 
 ALTER TABLE users 
  DROP COLUMN last_visit;
