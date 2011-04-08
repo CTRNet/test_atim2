29fb4d62349a3716aaf52c9aeecd20c98e089114
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-08 21:03:41 +0000

    tmp

---
+++ chum/notre_dame/scripts/ICM-v1.6_to_v2.2.1/iso_fix.php
@@ -0,0 +1,23 @@
+<?php
+
+$f = fopen('ATiM_utf8.sql', 'r');
+if(empty($f)) die('Unable to open file1');
+$w = fopen('ATiM_iso8859-1.sql', 'w');
+if(empty($w)) die('Unable to open file2');
+
+//echo($r."\n");
+$unfinished_line = "";
+while($r = fread($f, 300024)){
+	$lines = explode("\n", $r);
+	$lines[0] = $unfinished_line.$lines[0];
+	$unfinished_line = array_pop($lines);
+	foreach($lines as $line){
+		$line = str_replace("Ã‰", "Ã", $line);
+		fwrite($w, utf8_decode($line)."\n");
+	}
+}
+$line = $unfinished_line;
+$line = str_replace("Ã‰", "Ã", $line);
+fwrite($w, utf8_decode($line));
+
+?>
