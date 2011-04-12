c671dd90e3615fb015803a133efe4de1f86a5b25
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-12 14:47:32 +0000

    Move pbmc and blood cells solution from sample to aliquot

--- branches/qc/chum/notre_dame/scripts/ICM-v1.6_to_v2.2.1/12-LoadCustomApplicationData_v2.2.1.sql
+++ branches/qc/chum/notre_dame/scripts/ICM-v1.6_to_v2.2.1/12-LoadCustomApplicationData_v2.2.1.sql
@@ -7,3 +7,85 @@ INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_col
 INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`) VALUES 
 ((SELECT id FROM structures WHERE alias='report_aliquot_spent_times_defintion'), 
 (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND  `field`='aliquot_label'  ), '0', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+
+INSERT INTO `aliquot_controls` (`id`, `aliquot_type`, `aliquot_type_precision`, `form_alias`, `detail_tablename`, `volume_unit`, `comment`, `display_order`, `databrowser_label`) VALUES
+(1005, 'tube', 'blood cells', 'aliquot_masters,ad_der_blood_cell_tubes', 'ad_tubes', 'ml', 'Derivative tube requiring volume in ml specific for blood cells', 0, 'tube');
+INSERT INTO sample_to_aliquot_controls 
+(sample_control_id, aliquot_control_id, flag_active)
+VALUES ((SELECT id FROM sample_controls WHERE sample_type IN ('blood cell')), '1005', '1' );
+SET @id = LAST_INSERT_ID();
+INSERT INTO realiquoting_controls (parent_sample_to_aliquot_control_id, child_sample_to_aliquot_control_id, flag_active)
+VALUES (@id,@id,1);
+INSERT INTO sample_to_aliquot_controls 
+(sample_control_id, aliquot_control_id, flag_active)
+VALUES ((SELECT id FROM sample_controls WHERE sample_type IN ('pbmc')), '1005', '1' );
+SET @id = LAST_INSERT_ID();
+INSERT INTO realiquoting_controls (parent_sample_to_aliquot_control_id, child_sample_to_aliquot_control_id, flag_active)
+VALUES (@id,@id,1);
+UPDATE sample_to_aliquot_controls SET flag_active = '0'
+WHERE sample_control_id IN (SELECT id FROM sample_controls WHERE sample_type IN ('pbmc', 'blood cell'))
+AND aliquot_control_id IN (SELECT id FROM aliquot_controls WHERE aliquot_type IN ('tube') AND form_alias LIKE '%ad_der_cell_tubes_incl_ml_vol%');
+
+INSERT INTO structures(`alias`) VALUES ('ad_der_blood_cell_tubes');
+
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `language_label`, `language_tag`, `type`, `setting`, `default`, `structure_value_domain`, `language_help`) VALUES
+('Inventorymanagement', 'AliquotDetail', 'ad_tubes', 'tmp_storage_solution', 'tmp blood cell solution', '', 'select', '', '', (SELECT id FROM structure_value_domains WHERE domain_name='qc_blood_cell_storage_solution') , '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`) VALUES 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotDetail' AND `tablename`='' AND `field`='lot_number' AND `language_label`='lot number' AND `language_tag`='' AND `type`='input' AND `setting`='size=30' AND `default`='' AND `structure_value_domain`  IS NULL  AND `language_help`=''), '1', '70', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '1', '0'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='current_volume' AND `language_label`='current volume' AND `language_tag`='' AND `type`='float' AND `setting`='size=5' AND `default`='' AND `structure_value_domain`  IS NULL  AND `language_help`=''), '1', '71', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='aliquot_volume_unit' AND `language_label`='' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='aliquot_volume_unit')  AND `language_help`=''), '1', '72', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='initial_volume' AND `language_label`='initial volume' AND `language_tag`='' AND `type`='float_positive' AND `setting`='size=5' AND `default`='' AND `structure_value_domain`  IS NULL  AND `language_help`=''), '1', '73', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '0', '0', '1', '0', '1', '0', '0', '0', '0', '1', '0'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='aliquot_volume_unit' AND `language_label`='' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='aliquot_volume_unit')  AND `language_help`=''), '1', '74', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '1', '1', '1', '0', '0', '1', '1', '1', '1', '0', '0', '0', '1', '0'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='Generated' AND `tablename`='' AND `field`='creat_to_stor_spent_time_msg' AND `language_label`='creation to storage spent time' AND `language_tag`='' AND `type`='input' AND `setting`='size=30' AND `default`='' AND `structure_value_domain`  IS NULL  AND `language_help`=''), '1', '60', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotDetail' AND `tablename`='' AND `field`='cell_count' AND `language_label`='cell count' AND `language_tag`='' AND `type`='float_positive' AND `setting`='size=5' AND `default`='' AND `structure_value_domain`  IS NULL  AND `language_help`=''), '1', '75', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotDetail' AND `tablename`='' AND `field`='cell_count_unit' AND `language_label`='' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='cell_count_unit')  AND `language_help`=''), '1', '76', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotDetail' AND `tablename`='' AND `field`='concentration' AND `language_label`='aliquot concentration' AND `language_tag`='' AND `type`='float_positive' AND `setting`='size=5' AND `default`='' AND `structure_value_domain`  IS NULL  AND `language_help`=''), '1', '77', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotDetail' AND `tablename`='ad_cell_tubes' AND `field`='concentration_unit' AND `language_label`='' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='cell_concentration_unit')  AND `language_help`=''), '1', '78', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='study_summary_id' AND `language_label`='study' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='study_list')  AND `language_help`=''), '1', '1201', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '0'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='SampleMaster' AND `tablename`='sample_masters' AND `field`='initial_specimen_sample_type' AND `language_label`='initial specimen type' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='specimen_sample_type')  AND `language_help`=''), '0', '3', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='SampleMaster' AND `tablename`='' AND `field`='parent_sample_type' AND `language_label`='parent sample type' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='sample_type')  AND `language_help`='generated_parent_sample_sample_type_help'), '0', '4', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='created' AND `language_label`='created (into the system)' AND `language_tag`='' AND `type`='datetime' AND `setting`='' AND `default`='' AND `structure_value_domain`  IS NULL  AND `language_help`='help_created'), '1', '2000', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='Generated' AND `tablename`='' AND `field`='coll_to_stor_spent_time_msg' AND `language_label`='collection to storage spent time' AND `language_tag`='' AND `type`='input' AND `setting`='size=30' AND `default`='' AND `structure_value_domain`  IS NULL  AND `language_help`=''), '1', '59', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0'), 
+((SELECT id FROM structures WHERE alias='ad_der_blood_cell_tubes'), (SELECT id FROM structure_fields WHERE `model`='AliquotDetail' AND `tablename`='ad_tubes' AND `field`='tmp_storage_solution' AND `language_label`='tmp blood cell solution' AND `language_tag`='' AND `type`='select' AND `setting`='' AND `default`='' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='qc_blood_cell_storage_solution')  AND `language_help`=''), '1', '80', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '1', '0', '1', '0', '0', '0', '1', '1', '0');
+
+UPDATE aliquot_masters al, sample_masters samp
+SET al.aliquot_control_id = 1005,  aliquot_type = 'tube'
+WHERE samp.sample_type IN ('pbmc', 'blood cell')
+AND al.aliquot_type = 'tube'
+AND samp.id = al.sample_master_id;
+
+UPDATE aliquot_masters al, sample_masters samp, sd_der_pbmcs detail, ad_tubes as tube
+SET tube.tmp_storage_solution = detail.tmp_solution
+WHERE samp.sample_type IN ('pbmc')
+AND al.aliquot_type = 'tube'
+AND al.aliquot_control_id = 1005
+AND detail.sample_master_id = samp.id
+AND samp.id = al.sample_master_id 
+AND tube.aliquot_master_id = al.id;
+
+UPDATE aliquot_masters al, sample_masters samp, sd_der_blood_cells detail, ad_tubes as tube
+SET tube.tmp_storage_solution = detail.tmp_solution
+WHERE samp.sample_type IN ('blood cell')
+AND al.aliquot_type = 'tube'
+AND al.aliquot_control_id = 1005
+AND detail.sample_master_id = samp.id
+AND samp.id = al.sample_master_id 
+AND tube.aliquot_master_id = al.id;
+
+ALTER TABLE sd_der_blood_cells
+  DROP COLUMN `tmp_solution`;
+ALTER TABLE sd_der_blood_cells_revs
+  DROP COLUMN `tmp_solution`;
+ALTER TABLE sd_der_pbmcs
+  DROP COLUMN `tmp_solution`;
+ALTER TABLE sd_der_pbmcs_revs
+  DROP COLUMN `tmp_solution`;
+  
+DELETE FROM structure_formats
+WHERE structure_field_id IN (SELECT id FROM structure_fields WHERE field = 'tmp_solution' AND tablename IN ('sd_der_pbmcs','sd_der_blood_cells'));
+DELETE FROM structure_fields WHERE field = 'tmp_solution' AND tablename IN ('sd_der_pbmcs','sd_der_blood_cells');
+
+
+
+
+
