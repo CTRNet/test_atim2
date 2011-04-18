faf66fd3da5e379e63834e42378cbdcff5882a23
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-18 15:46:35 +0000

    Removing duplicate ids from i18n

--- v2.2.x/scripts/tmp_atim_v2.2.2_upgrade.sql
+++ v2.2.x/scripts/tmp_atim_v2.2.2_upgrade.sql
@@ -1,2 +1,8 @@
 -- Update version information
 INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.2.2 beta', NOW(), '> 2923');
+
+DELETE FROM i18n WHERE id=BINARY('Details') AND page_id='global';
+DELETE FROM i18n WHERE id=BINARY('inv_collection_type_defintion') AND page_id='';
+DELETE FROM i18n WHERE id=BINARY('May') AND page_id='global';
+DELETE FROM i18n WHERE id=BINARY('Next') AND page_id='global';
+DELETE FROM i18n WHERE id=BINARY('Prev') AND page_id='global';
