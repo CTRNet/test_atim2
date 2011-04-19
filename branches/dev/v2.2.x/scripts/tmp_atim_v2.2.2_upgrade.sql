d14cf786943c847a1440427fe020c3ecb47d32ff
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-19 17:20:52 +0000

    E1437: Participant contact, second phone number max length 30 char

--- branches/dev/v2.2.x/scripts/tmp_atim_v2.2.2_upgrade.sql
+++ branches/dev/v2.2.x/scripts/tmp_atim_v2.2.2_upgrade.sql
@@ -1,8 +1,14 @@
 -- Update version information
 INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.2.2 beta', NOW(), '> 2923');
 
-DELETE FROM i18n WHERE id=BINARY('Details') AND page_id='global';
-DELETE FROM i18n WHERE id=BINARY('inv_collection_type_defintion') AND page_id='';
-DELETE FROM i18n WHERE id=BINARY('May') AND page_id='global';
-DELETE FROM i18n WHERE id=BINARY('Next') AND page_id='global';
-DELETE FROM i18n WHERE id=BINARY('Prev') AND page_id='global';
+DELETE FROM i18n WHERE id='Details' AND page_id='global';
+DELETE FROM i18n WHERE id='inv_collection_type_defintion' AND page_id='';
+DELETE FROM i18n WHERE id='may' AND page_id='';
+DELETE FROM i18n WHERE id='Next' AND page_id='global';
+DELETE FROM i18n WHERE id='Prev' AND page_id='global';
+DELETE FROM i18n WHERE id='received tissue weight' AND page_id='global';
+
+ALTER TABLE participant_contacts
+ MODIFY phone_secondary VARCHAR(30) NOT NULL DEFAULT '';
+ALTER TABLE participant_contacts_revs
+ MODIFY phone_secondary VARCHAR(30) NOT NULL DEFAULT '';
