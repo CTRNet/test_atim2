b75e10ac91fc272038e9dbe42707d30ab7651208
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-20 14:00:34 +0000

    Accuracy cleanup

--- branches/dev/v2.2.x/scripts/tmp_atim_v2.2.2_upgrade.sql
+++ branches/dev/v2.2.x/scripts/tmp_atim_v2.2.2_upgrade.sql
@@ -12,3 +12,64 @@ ALTER TABLE participant_contacts
  MODIFY phone_secondary VARCHAR(30) NOT NULL DEFAULT '';
 ALTER TABLE participant_contacts_revs
  MODIFY phone_secondary VARCHAR(30) NOT NULL DEFAULT '';
+
+-- adding hour as datetime accuracy 
+INSERT IGNORE INTO structure_permissible_values (`value`, `language_alias`) VALUES
+("h", "datetime_accuracy_indicator_h"),
+("i", "datetime_accuracy_indicator_i");
+INSERT INTO structure_value_domains_permissible_values (`structure_value_domain_id`, `structure_permissible_value_id`, `display_order`, `flag_active`) VALUES
+((SELECT id FROM structure_value_domains WHERE domain_name="datetime_accuracy_indicator"),  (SELECT id FROM structure_permissible_values WHERE value="h" AND language_alias="datetime_accuracy_indicator_h"), "0", "1"),
+((SELECT id FROM structure_value_domains WHERE domain_name="datetime_accuracy_indicator"),  (SELECT id FROM structure_permissible_values WHERE value="i" AND language_alias="datetime_accuracy_indicator_i"), "0", "1");
+INSERT INTO i18n (id, en, fr) VALUES
+("datetime_accuracy_indicator_h", "h", "h"),
+("datetime_accuracy_indicator_i", "i", "i");
+
+UPDATE structure_fields SET structure_value_domain=(SELECT id FROM structure_value_domains WHERE domain_name='date_accuracy')
+WHERE field IN('dob_date_accuracy', 'dod_date_accuracy', 'dx_date_accuracy', 'start_date_accuracy', 'finish_date_accuracy');
+
+DELETE FROM structure_value_domains WHERE domain_name IN('date_status', 'lkmp_date_certainty');
+DELETE FROM structure_value_domains_permissible_values WHERE structure_value_domain_id NOT IN (SELECT id FROM structure_value_domains);
+DELETE FROM structure_value_domains_permissible_values WHERE structure_permissible_value_id NOT IN (SELECT id FROM structure_permissible_values);
+ 
+ALTER TABLE structure_value_domains_permissible_values
+ MODIFY structure_value_domain_id INT,
+ ADD FOREIGN KEY (structure_value_domain_id) REFERENCES structure_value_domains (`id`);
+
+UPDATE structure_value_domains_permissible_values
+ SET structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value=BINARY('d') AND language_alias='datetime_accuracy_indicator_d')
+ WHERE structure_permissible_value_id IN (SELECT id FROM structure_permissible_values WHERE value='D' AND language_alias IN('day uncertain', 'datetime_accuracy_indicator_d', 'day of date is uncertain'));
+UPDATE structure_value_domains_permissible_values
+ SET structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value=BINARY('m') AND language_alias='datetime_accuracy_indicator_m')
+ WHERE structure_permissible_value_id IN (SELECT id FROM structure_permissible_values WHERE value='m' AND language_alias IN('month uncertain', 'datetime_accuracy_indicator_m', 'month and day of date uncertain')); 
+UPDATE structure_value_domains_permissible_values
+ SET structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value=BINARY('y') AND language_alias='datetime_accuracy_indicator_y')
+ WHERE structure_permissible_value_id IN (SELECT id FROM structure_permissible_values WHERE value='Y' AND language_alias IN('year uncertain', 'datetime_accuracy_indicator_y')); 
+UPDATE structure_value_domains_permissible_values
+ SET structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value=BINARY('c') AND language_alias='datetime_accuracy_indicator_c')
+ WHERE structure_permissible_value_id IN (SELECT id FROM structure_permissible_values WHERE value='c' AND language_alias IN('Complete date known and verified', 'datetime_accuracy_indicator_c')); 
+
+ALTER TABLE structure_value_domains_permissible_values
+ DROP COLUMN language_alias;
+ 
+DELETE FROM structure_permissible_values WHERE id NOT IN(SELECT structure_permissible_value_id FROM structure_value_domains_permissible_values);
+
+UPDATE structure_value_domains_permissible_values
+SET display_order=1 
+WHERE structure_value_domain_id IN (SELECT id FROM structure_value_domains WHERE domain_name IN('datetime_accuracy_indicator'))
+ AND structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value='i' AND language_alias='datetime_accuracy_indicator_i');
+UPDATE structure_value_domains_permissible_values
+SET display_order=2 
+WHERE structure_value_domain_id IN (SELECT id FROM structure_value_domains WHERE domain_name IN('datetime_accuracy_indicator'))
+ AND structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value='h' AND language_alias='datetime_accuracy_indicator_h');
+UPDATE structure_value_domains_permissible_values
+SET display_order=3 
+WHERE structure_value_domain_id IN (SELECT id FROM structure_value_domains WHERE domain_name IN('datetime_accuracy_indicator', 'date_accuracy'))
+ AND structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value='d' AND language_alias='datetime_accuracy_indicator_d');
+UPDATE structure_value_domains_permissible_values
+SET display_order=4 
+WHERE structure_value_domain_id IN (SELECT id FROM structure_value_domains WHERE domain_name IN('datetime_accuracy_indicator', 'date_accuracy'))
+ AND structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value='m' AND language_alias='datetime_accuracy_indicator_m');
+UPDATE structure_value_domains_permissible_values
+SET display_order=5 
+WHERE structure_value_domain_id IN (SELECT id FROM structure_value_domains WHERE domain_name IN('datetime_accuracy_indicator', 'date_accuracy'))
+ AND structure_permissible_value_id=(SELECT id FROM structure_permissible_values WHERE value='y' AND language_alias='datetime_accuracy_indicator_y');
