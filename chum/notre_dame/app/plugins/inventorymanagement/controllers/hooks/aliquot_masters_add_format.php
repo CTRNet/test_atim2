3fa735453557a92ae051c865ece228b65ddc0e98
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-26 17:50:11 +0000

    copy/past in add aliquot and realiquot: don't copy aliquot label. See eventum 1446. Commit to fix bug on default value.

--- chum/notre_dame/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_format.php
+++ chum/notre_dame/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_format.php
@@ -1,15 +1,13 @@
 <?php
 	
 	// --------------------------------------------------------------------------------
-	// Set custom initial data including default aliquot label(s)
+	// Set default aliquot label(s)
 	// -------------------------------------------------------------------------------- 	
-	if($is_intial_display){
-		$custom_initial_data = array();
-		foreach($samples as $view_sample){
-			$default_aliquot_label = $this->AliquotMaster->generateDefaultAliquotLabel($view_sample, $aliquot_control);
-			$custom_initial_data[] = array('parent' => $view_sample, 'children' => array(array('AliquotMaster'=>array('aliquot_label' => $default_aliquot_label))));
-		}
-		$this->set('custom_initial_data', $custom_initial_data);	
+	$default_aliquot_labels = array();
+	foreach($samples as $view_sample){
+		$default_aliquot_label = $this->AliquotMaster->generateDefaultAliquotLabel($view_sample, $aliquot_control);
+		$default_aliquot_labels[$view_sample['ViewSample']['sample_master_id']] = $default_aliquot_label;
 	}
+	$this->set('default_aliquot_labels', $default_aliquot_labels);
 	
 ?>
--- chum/notre_dame/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_realiquot_format.php
+++ chum/notre_dame/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_realiquot_format.php
@@ -1,19 +1,14 @@
 <?php
 	
 	// --------------------------------------------------------------------------------
-	// Set custom initial data including default aliquot label(s)
+	// Set default aliquot label(s)
 	// -------------------------------------------------------------------------------- 	
-	// Get Samples Data
-	$sample_master_ids = array();
-	foreach($this->data as $new_data_set){ $sample_master_ids[] = $new_data_set['parent']['AliquotMaster']['sample_master_id']; }
-	$samples = $this->ViewSample->find('all', array('conditions' => array('sample_master_id' => $sample_master_ids), 'recursive' => -1));
-	$samples_from_id = array();
-	foreach($samples as $new_sample) $samples_from_id[$new_sample['ViewSample']['sample_master_id']] = $new_sample;
-	
-	// Add aliquot label
-	foreach($this->data as &$new_data_set){ 
-		$default_aliquot_label = $this->AliquotMaster->generateDefaultAliquotLabel($samples_from_id[$new_data_set['parent']['AliquotMaster']['sample_master_id']], $child_aliquot_ctrl);
-		$new_data_set['children'] = array(array('AliquotMaster'=>array('aliquot_label' => $default_aliquot_label)));
+	foreach($this->data as $new_data_set){
+		$sample_master_id = $new_data_set['parent']['AliquotMaster']['sample_master_id'];
+		$sample_data = $this->ViewSample->find('first', array('conditions' => array('sample_master_id' => $sample_master_id), 'recursive' => -1));
+		$default_aliquot_label = $this->AliquotMaster->generateDefaultAliquotLabel($sample_data, $child_aliquot_ctrl);
+		$default_aliquot_labels[$sample_master_id] = $default_aliquot_label;
 	}
+	$this->set('default_aliquot_labels', $default_aliquot_labels);
 	
 ?>
