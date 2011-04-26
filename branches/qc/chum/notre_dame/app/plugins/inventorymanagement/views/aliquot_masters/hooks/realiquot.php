4f91b91cbfbd9ba6cd8777e0b4aca7bbfc4e9e4f
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-04-26 14:05:20 +0000

    copy/past in add aliquot and realiquot: don't copy aliquot label. See eventum 1446

--- branches/qc/chum/notre_dame/app/plugins/inventorymanagement/views/aliquot_masters/hooks/realiquot.php
+++ branches/qc/chum/notre_dame/app/plugins/inventorymanagement/views/aliquot_masters/hooks/realiquot.php
@@ -1,13 +1,6 @@
 <?php 
 
 	// --------------------------------------------------------------------------------
-	// Set custom initial data including default aliquot label(s)
-	// -------------------------------------------------------------------------------- 
-	if(isset($custom_initial_data)) {
-		$this->data = $custom_initial_data;		
-	}
-	
-	// --------------------------------------------------------------------------------
 	// Prevent the paste operation on aliquot label 
 	// -------------------------------------------------------------------------------- 
 	$options_children['settings']['paste_disabled_fields'] = array('AliquotMaster.aliquot_label');	
