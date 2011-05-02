412714f43d9dd1bafcb9b0134821547fcb224dfd
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-05-02 19:32:48 +0000

    Terry Fox Report

---
+++ branches/on/ohri/scripts/custom.sql
@@ -0,0 +1,2 @@
+INSERT INTO datamart_reports (name, description, form_alias_for_search, form_alias_for_results, function, flag_active) VALUES
+('terry fox export', 'terry fox export', 'report_date_range_definition', 'foo', 'terryFox', '1');
