e688fdd112e6da5233b05a57009fefc8b10290d6
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-05-03 15:15:46 +0000

    Fixing dropdown

--- branches/on/ohri/app/plugins/inventorymanagement/models/customs/sample_detail.php
+++ branches/on/ohri/app/plugins/inventorymanagement/models/customs/sample_detail.php
@@ -5,11 +5,11 @@ class SampleDetailCustom extends SampleDetail {
 	var $useTable = false;
 	
 	function getTissueSourcePermissibleValues() {
-		$result = array();
-		
-		$result[] = array('value' => 'ovary', 'default' => __('ovary', true)); 
-		$result[] = array('value' => 'omenteum', 'default' => __('omenteum', true)); 
-		$result[] = array('value' => 'peritoneal nodule', 'default' => __('peritoneal nodule', true)); 
+		$result = array(
+			'ovary' 			=> __('ovary', true), 
+			'omenteum'			=> __('omenteum', true),
+			'peritoneal nodule'	=> __('peritoneal nodule', true)
+		); 
 		
 		return $result;
 	}
