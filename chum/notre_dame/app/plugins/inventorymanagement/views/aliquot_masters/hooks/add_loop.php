f9b6335b34712873676dd562b551ff788bc24afc
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-05-04 18:42:02 +0000

    missing commit

--- chum/notre_dame/app/plugins/inventorymanagement/views/aliquot_masters/hooks/add_loop.php
+++ chum/notre_dame/app/plugins/inventorymanagement/views/aliquot_masters/hooks/add_loop.php
@@ -1,7 +1,7 @@
 <?php 
 
-	if(isset($default_aliquot_labels[$data['parent']['ViewSample']['sample_master_id']])) {
-		$final_options_children['override']['AliquotMaster.aliquot_label'] = $default_aliquot_labels[$data['parent']['ViewSample']['sample_master_id']];
+	if(isset($default_aliquot_barcodes[$data['parent']['ViewSample']['sample_master_id']])) {
+		$final_options_children['override']['AliquotMaster.barcode'] = $default_aliquot_barcodes[$data['parent']['ViewSample']['sample_master_id']];
 	}
 	
 ?>
