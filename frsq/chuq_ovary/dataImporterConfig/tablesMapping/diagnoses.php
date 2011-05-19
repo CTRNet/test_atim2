1bdabcfc0f8a37dec6c7027f61a3e46d6c390686
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-05-19 20:08:30 +0000

    tmp

---
+++ frsq/chuq_ovary/dataImporterConfig/tablesMapping/diagnoses.php
@@ -0,0 +1,58 @@
+<?php
+$pkey = "NS";
+$child = array();
+$master_fields = array(
+	"diagnosis_control_id" => "@14",
+	"participant_id" => $pkey,
+	
+	"tumour_grade" => array("GR" => array(
+		"X"=>"",
+		"2/3"=>"2",
+		"3/3"=>"3",
+		"1/3"=>"1",
+		"1-2/3"=>"1-2",
+		"H"=>"",
+		"2"=>"2",
+		"1"=>"1",
+		"2-3/3"=>"2-3",
+		"3"=>"3")));
+$detail_fields = array(
+	"pathology_diagnosis" => "Diagnostic",
+	"figo" => array("FIGO" => array(
+		"IA"=>"Ia",
+		"IC"=>"Ic",
+		"IIIC"=>"IIIc",
+		"T3c"=>"IIIc",
+		"T4"=>"IV",
+		"T1a"=>"Ia",
+		"IV"=>"IV",
+		"X"=>"",
+		"III"=>"III",
+		"IIA"=>"IIa",
+		"IIIA"=>"IIIa",
+		"IIC"=>"IIc",
+		"IIIB"=>"IIIb",
+		"IC vs IIC"=>"Ic",
+		"IB?"=>"Ib",
+		"IIB"=>"IIb",
+		"IB"=>"Ib",
+		"IIIB?"=>"IIIb",
+		"?"=>"",
+		"1A"=>"1a")),
+	);
+
+//see the Model class definition for more info
+$model = new MasterDetailModel(0, $pkey, $child, true, "participant_id", 'diagnosis_masters', $master_fields, 'chuq_dx_all_sites', 'diagnosis_master_id', $detail_fields);
+		
+//we can then attach post read/write functions
+$model->custom_data = array();
+
+//adding this model to the config
+Config::$models['DiagnosisMaster'] = $model;
+
+		
+
+
+
+
+
---
+++ frsq/chuq_ovary/dataImporterConfig/tablesMapping/dos_identifiers.php
@@ -0,0 +1,26 @@
+<?php
+$pkey = "NS";
+$child = array();
+$fields = array(
+	"misc_identifier_control_id" => "@2",
+	"participant_id" => $pkey,
+
+	"identifier_name" => "@NO DOS",
+	"identifier_abrv" => "@NO DOS",
+	"identifier_value" => "NO DOS");
+
+//see the Model class definition for more info
+$model = new Model(0, $pkey, $child, true, "participant_id", 'misc_identifiers', $fields);
+
+//we can then attach post read/write functions
+$model->post_read_function = 'postDosNbrRead';
+$model->custom_data = array();
+
+//adding this model to the config
+Config::$models['DosMiscIdentfier'] = $model;
+
+function postDosNbrRead(Model $m){
+	if(empty($m->values['NO DOS'])) {
+		echo "<br><FONT COLOR=\"red\" >Line ".$m->line.": NO DOS is empty!</FONT><br>";
+	}
+}
