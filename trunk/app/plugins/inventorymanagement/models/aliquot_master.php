5b3d6a020d5aec58e93322cf56d353d10dcf586e
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-05-30 14:32:14 +0000

    E1497: Fixing storage tracking when aliquot modified date = storage modified date

--- trunk/app/plugins/inventorymanagement/models/aliquot_master.php
+++ trunk/app/plugins/inventorymanagement/models/aliquot_master.php
@@ -55,9 +55,9 @@ class AliquotMaster extends InventoryManagementAppModel {
 				LEFT JOIN  aliquot_masters_revs AS amn ON amn.version_id=(SELECT version_id FROM aliquot_masters_revs WHERE id=am.id AND version_id > am.version_id ORDER BY version_id ASC LIMIT 1)
 				LEFT JOIN storage_masters_revs AS sm ON am.storage_master_id=sm.id
 				LEFT JOIN storage_masters_revs AS smn ON smn.version_id=(SELECT version_id FROM storage_masters_revs WHERE id=sm.id AND version_id > sm.version_id ORDER BY version_id ASC LIMIT 1)
-				WHERE am.id='".$aliquot_master_id."' AND ((am.modified > sm.modified AND (am.modified < smn.modified OR smn.modified IS NULL)) OR (sm.modified > am.modified AND (sm.modified < amn.modified OR amn.modified IS NULL)) OR am.storage_master_id IS NULL)";
+				WHERE am.id='".$aliquot_master_id."' AND ((am.modified >= sm.modified AND (am.modified < smn.modified OR smn.modified IS NULL)) OR (sm.modified > am.modified AND (sm.modified <= amn.modified OR amn.modified IS NULL)) OR am.storage_master_id IS NULL)";
 		$storage_data_tmp = $this->query($qry);
-		
+		pr($storage_data_tmp);
 		$previous = array_shift($storage_data_tmp);
 		while($current = array_shift($storage_data_tmp)){
 			if($previous['sm']['id'] != $current['sm']['id']){
