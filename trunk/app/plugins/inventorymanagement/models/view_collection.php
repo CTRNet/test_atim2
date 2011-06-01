fcb8d0dcbde7d0aa56ab446791b03a71293b43d7
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-06-01 13:50:06 +0000

    E1470: Warning when a collection consent is not obtained

--- trunk/app/plugins/inventorymanagement/models/view_collection.php
+++ trunk/app/plugins/inventorymanagement/models/view_collection.php
@@ -13,7 +13,19 @@ class ViewCollection extends InventorymanagementAppModel {
 				'title' => array(null, __('collection', true) . ' : ' . $collection_data['ViewCollection']['acquisition_label']),
 				'structure alias' 	=> 'view_collection',
 				'data'				=> $collection_data
-			);			
+			);
+			
+			if(!empty($collection_data['ViewCollection']['consent_master_id'])){
+				$consent_model = AppModel::getInstance("Clinicalannotation", "ConsentMaster", true);
+				$consent_data = $consent_model->find('first', array(
+					'fields' => array('ConsentMaster.consent_status'), 
+					'conditions' => array('ConsentMaster.id' => $collection_data['ViewCollection']['consent_master_id']),
+					'recursive' => -1)
+				);
+				if($consent_data['ConsentMaster']['consent_status'] != 'obtained'){
+					AppController::addWarningMsg(sprintf(__('the linked consent status is [%s]', true), __($consent_data['ConsentMaster']['consent_status'], true)));
+				}
+			}
 		}
 		
 		return $return;
