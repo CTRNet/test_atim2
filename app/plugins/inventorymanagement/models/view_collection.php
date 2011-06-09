256e94fcc678609fbb8b12bbbf79925bad71a8d8
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-06-09 14:44:51 +0000

    E1459: Warning if in an order containing aliquots without proper consent. Warning if adding aliquots without a proper consent to an order.

--- app/plugins/inventorymanagement/models/view_collection.php
+++ app/plugins/inventorymanagement/models/view_collection.php
@@ -1,7 +1,7 @@
 <?php
 
 class ViewCollection extends InventorymanagementAppModel {
-	
+
 	function summary($variables=array()) {
 		$return = false;
 		
@@ -15,6 +15,14 @@ class ViewCollection extends InventorymanagementAppModel {
 				'data'				=> $collection_data
 			);
 			
+			$consent_status = $this->getUnconsentedParticipantCollections(array('data' => $collection_data));
+			if(!empty($consent_status)){
+				if($consent_status[$variables['Collection.id']] == null){
+					AppController::addWarningMsg(__('no consent is linked to the current participant collection', true));
+				}else{
+					AppController::addWarningMsg(sprintf(__('the linked consent status is [%s]', true), __($consent_status[$variables['Collection.id']], true)));
+				}
+			}
 			if(!empty($collection_data['ViewCollection']['consent_master_id'])){
 				$consent_model = AppModel::getInstance("Clinicalannotation", "ConsentMaster", true);
 				$consent_data = $consent_model->find('first', array(
@@ -23,14 +31,72 @@ class ViewCollection extends InventorymanagementAppModel {
 					'recursive' => -1)
 				);
 				if($consent_data['ConsentMaster']['consent_status'] != 'obtained'){
-					AppController::addWarningMsg(sprintf(__('the linked consent status is [%s]', true), __($consent_data['ConsentMaster']['consent_status'], true)));
+					
 				}
 			}else if($collection_data['ViewCollection']['collection_property'] == 'participant collection'){
-				AppController::addWarningMsg(__('no consent is linked to the current participant collection', true));
+				
 			}
 		}
 		
 		return $return;
+	}
+	
+	/**
+	 * @param array $collection with either a key 'id' referring to an array
+	 * of ids, or a key 'data' referring to ViewCollections.
+	 * @return array The ids of participants collections with a consent status 
+	 * other than 'obtained' as key. Their value will null if there is no linked
+	 * consent or the consent status otherwise. 
+	 */
+	function getUnconsentedParticipantCollections(array $collection){
+		$data = null;
+		if(array_key_exists('id', $collection)){
+			$data = $this->find('all', array('conditions' => array('ViewCollection.collection_id' => $collection['id'])));
+		}else{
+			$data = array_key_exists('ViewCollection', $collection['data']) ? array($collection['data']) : $collection['data']; 
+		}
+		
+		$results = array();
+		$consents_to_fetch = array();
+		foreach($data as $index => &$data_unit){
+			if($data_unit['ViewCollection']['collection_property'] != 'participant collection'){
+				//filter non participant collections
+				unset($data[$index]);
+			}else if(empty($data_unit['ViewCollection']['consent_master_id'])){
+				//removing missing consents
+				$results[$data_unit['ViewCollection']['collection_id']] = null;
+				unset($data[$index]);
+			}else{
+				$consents_to_fetch[] = $data_unit['ViewCollection']['consent_master_id']; 
+			}
+		}
+		
+		if(!empty($consents_to_fetch)){
+			//find all required consents
+			$consent_model = AppModel::getInstance("Clinicalannotation", "ConsentMaster", true);
+			$consent_data = $consent_model->find('all', array(
+				'fields' => array('ConsentMaster.id', 'ConsentMaster.consent_status'), 
+				'conditions' => array('ConsentMaster.id' => $consents_to_fetch, 'NOT' => array('ConsentMaster.consent_status' => 'obtained')),
+				'recursive' => -1)
+			);
+			
+			//put consents in array keys
+			$not_obtained_consents = array();
+			if(!empty($consent_data)){
+				foreach($consent_data as &$consent_data_unit){
+					$not_obtained_consents[$consent_data_unit['ConsentMaster']['id']] = $consent_data_unit['ConsentMaster']['consent_status'];
+				}
+				
+				//see for each collection if the consent is found in the not obtained consent array
+				foreach($data as &$data_unit){
+					if(array_key_exists($data_unit['ViewCollection']['consent_master_id'], $not_obtained_consents)){
+						$results[$data_unit['ViewCollection']['collection_id']] = $not_obtained_consents[$data_unit['ViewCollection']['consent_master_id']];
+					}
+				}
+			}
+		}
+		
+		return $results;
 	}
 	
 }
--- app/plugins/order/models/order.php
+++ app/plugins/order/models/order.php
@@ -14,6 +14,7 @@ class Order extends OrderAppModel {
 		$return = false;
 		
 		if ( isset($variables['Order.id']) ) {
+			$this->warnUnconsentedAliquots($variables['Order.id']);
 			
 			$result = $this->find('first', array('conditions'=>array('Order.id'=>$variables['Order.id'])));
 			
@@ -58,6 +59,48 @@ class Order extends OrderAppModel {
 		return array('allow_deletion' => true, 'msg' => '');
 	}
   
+	
+	function warnUnconsentedAliquots($order_id){
+		$order_line_model = AppModel::getInstance("order", "OrderLine", true);
+		$order_line_data = $order_line_model->find('all', array(
+			'conditions' => array('OrderLine.order_id' => $order_id),
+			'fields' => array('OrderLine.id'),
+			'recursive' => -1
+		));
+		
+		if(empty($order_line_data)){
+			return;
+		}
+		
+		$order_line_ids = array();
+		foreach($order_line_data as $order_line_data_unit){
+			$order_line_ids[] = $order_line_data_unit['OrderLine']['id'];
+		}
+		
+		$order_item_model = AppModel::getInstance("order", "OrderItem", true);
+		$order_item_data = $order_item_model->find('all', array(
+			'conditions' => array('OrderItem.order_line_id' => $order_line_ids),
+			'fields' => array('OrderItem.aliquot_master_id'),
+			'recursive' => -1
+		));
+		
+		if(empty($order_item_data)){
+			return;
+		}
+		
+		$aliquot_ids = array();
+		foreach($order_item_data as $order_item_data_unit){
+			$aliquot_ids[] = $order_item_data_unit['OrderItem']['aliquot_master_id'];
+		}
+		
+		if(!empty($aliquot_ids)){
+			$aliquot_master_model = AppModel::getInstance("inventorymanagement", "AliquotMaster", true);
+			$unconsented_aliquots = $aliquot_master_model->getUnconsentedAliquots(array('id' => $aliquot_ids));
+			if(!empty($unconsented_aliquots)){
+				AppController::addWarningMsg(__('aliquot(s) without a proper consent', true).": ".count($unconsented_aliquots));
+			}
+		}
+	}
 }
 
 ?>
