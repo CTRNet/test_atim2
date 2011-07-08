516d37b3701c2c7253383a0d3bff23435584b014
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-07-08 18:45:29 +0000

    Ready for 2.3.0 tag

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -10,8 +10,8 @@ fi
 echo running atim_v2.2.0_full_installation.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.2.0/atim_v2.2.0_full_installation.sql
 
-echo running atim_v2.2.0_demo_data.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.2.0/atim_v2.2.0_demo_data.sql
+#echo running atim_v2.2.0_demo_data.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.2.0/atim_v2.2.0_demo_data.sql
 
 echo running atim_v2.2.1_upgrade.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.2.0/atim_v2.2.1_upgrade.sql
@@ -20,7 +20,7 @@ echo running atim_v2.2.2_upgrade.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.2.0/atim_v2.2.2_upgrade.sql
 
 echo running tmp_atim_v2.3.0_upgrade.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < tmp_atim_v2.3.0_upgrade.sql
+mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.0_upgrade.sql
 
 
 echo done
