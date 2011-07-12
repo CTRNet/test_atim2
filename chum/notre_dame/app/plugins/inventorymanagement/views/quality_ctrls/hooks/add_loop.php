e9e56ed97b4ebcbc53b9c9eea9dbbc63beb6f95d
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-07-12 19:09:10 +0000

    Fixing add quality control to v2.3.0.

--- chum/notre_dame/app/plugins/inventorymanagement/views/quality_ctrls/hooks/add_loop.php
+++ chum/notre_dame/app/plugins/inventorymanagement/views/quality_ctrls/hooks/add_loop.php
@@ -3,8 +3,5 @@
 	// --------------------------------------------------------------------------------
 	// Generate default run id
 	// -------------------------------------------------------------------------------- 
-	if(isset($default_run_id)) {
-		$final_options['override']['QualityCtrl.run_id'] = $default_run_id;
-	}
-	
+	$final_options_children['override']['QualityCtrl.run_id'] = 'N/A';
 ?>
