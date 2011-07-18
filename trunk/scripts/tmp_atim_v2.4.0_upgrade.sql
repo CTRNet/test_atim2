740aaa8a3bd0b9d1b8289b9a5e52b3e6196e1066
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-07-18 16:27:05 +0000

    E1555: Removed deprecated functions

--- trunk/scripts/tmp_atim_v2.4.0_upgrade.sql
+++ trunk/scripts/tmp_atim_v2.4.0_upgrade.sql
@@ -1,2 +1,6 @@
 -- Update version information
 INSERT INTO `versions` (version_number, date_installed, build_number) VALUES('2.4.0', NOW(), '> 3250');
+
+INSERT INTO i18n(id, en, fr) VALUES
+('previous versions', 'Previous versions', 'Versions précédentes'),
+('add to order', 'Add To Order', 'Ajouter aux commandes');
