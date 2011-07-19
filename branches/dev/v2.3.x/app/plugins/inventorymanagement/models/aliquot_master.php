cd677355a34e810c90022f4bfe84815db1c17792
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-07-19 14:51:20 +0000

    E1560: Fixing add source aliquot (aliquot storage validation fixed)

--- branches/dev/v2.3.x/app/plugins/inventorymanagement/models/aliquot_master.php
+++ branches/dev/v2.3.x/app/plugins/inventorymanagement/models/aliquot_master.php
@@ -330,7 +330,9 @@ class AliquotMaster extends InventoryManagementAppModel {
 				}
 			}
 
-		} else if ((array_key_exists('storage_coord_x', $aliquot_data['AliquotMaster'])) || (array_key_exists('storage_coord_y', $aliquot_data['AliquotMaster']))) {
+		} else if ((array_key_exists('storage_coord_x', $aliquot_data['AliquotMaster']) && !empty($aliquot_data['AliquotMaster']['storage_coord_x'])) 
+			|| (array_key_exists('storage_coord_y', $aliquot_data['AliquotMaster']) && !empty($aliquot_data['AliquotMaster']['storage_coord_y'])) 
+		){
 			AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
 		}
 	}
