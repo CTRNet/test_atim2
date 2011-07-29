b8d908fb3ccfbf9cf6496c96158f67a9384dec11
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-07-29 17:01:10 +0000

    NL revision: trt and annotation

--- terry_fox_coeur/app/plugins/inventorymanagement/views/aliquot_masters/hooks/add.php
+++ terry_fox_coeur/app/plugins/inventorymanagement/views/aliquot_masters/hooks/add.php
@@ -1,4 +1,6 @@
 <?php
-if($final_atim_structure['Structure']['alias'] == "ad_spec_tiss_blocks"){
-	$final_options['override']['AliquotMaster.aliquot_volume_unit'] = "mm³";//counter to eventum 1185
+foreach($atim_structure['Structure'] as $new_struct) {
+	if($new_struct['alias'] == "ad_spec_tiss_blocks"){
+		$options_children['override']['AliquotMaster.aliquot_volume_unit'] = "mm³";//counter to eventum 1185
+	}
 }
