c164e64a789b165335eb072cb888be5a34161383
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-08-16 14:57:21 +0000

    Work on new adhoc features and new demo data to match 2.4.0 changes.

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -10,16 +10,19 @@ fi
 echo running atim_v2.3.0_full_installation.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.0_full_installation.sql
 
-echo running atim_v2.3.0_demo_data.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.0_demo_data.sql
-
 echo running atim_v2.3.1_upgrade.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.1_upgrade.sql
+
+echo running atim_v2.3.2_upgrade.sql
+mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.2_upgrade.sql
+
+echo running atim_v2.3.3_upgrade.sql
+mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.3_upgrade.sql
 
 echo running tmp_atim_v2.4.0_upgrade.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < tmp_atim_v2.4.0_upgrade.sql
 
-echo running fmlh.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < fmlh.sql
+echo running atim_v2.4.0_demo_data.sql
+mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.4.0/atim_v2.4.0_demo_data.sql
 
 echo done
