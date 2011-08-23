ca66ab22decda5652122f0b06b7d2c24ad22ee28
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-08-23 21:06:25 +0000

    tmp v233 review

--- branches/on/ohri/app/plugins/clinicalannotation/models/customs/participant.php
+++ branches/on/ohri/app/plugins/clinicalannotation/models/customs/participant.php
@@ -9,25 +9,23 @@ class ParticipantCustom extends Participant {
 		$return = false;
 		
 		if ( isset($variables['Participant.id']) ) {
-			$has_many_details = array(
-				'hasMany' => array('MiscIdentifier' => array(
-					'className' => 'Clinicalannotation.MiscIdentifier',
-					'foreignKey' => 'participant_id',
-					'conditions' => array("MiscIdentifier.identifier_name LIKE 'ohri_bank_participant_id'"))));			
-			
-			$this->bindModel($has_many_details, false);			
 			$result = $this->find('first', array('conditions'=>array('Participant.id'=>$variables['Participant.id'])));
-			$this->unbindModel(array('hasMany' => array('MiscIdentifier')), false);
-			
+						
+			$identifier_model = AppModel::getInstance('Clinicalannotation', 'MiscIdentifier', true);
+			$identifier_results = $identifier_model->find('first', array('conditions' => array(
+				'MiscIdentifier.participant_id' => $variables['Participant.id'],
+				'MiscIdentifierControl.misc_identifier_name LIKE' => 'ohri_bank_participant_id'))
+			);
+						
 			//Add No Labs to description
 			$bank_identifier = '';
-			if((!empty($result['MiscIdentifier'])) && ($result['MiscIdentifier'][0]['identifier_name'] == 'ohri_bank_participant_id')) {
-				$bank_identifier = $result['MiscIdentifier'][0]['identifier_value'];
+			if(isset($identifier_results['MiscIdentifier']['identifier_value'])) {
+				$bank_identifier = $identifier_results['MiscIdentifier']['identifier_value'];
 			}
 			
 			$return = array(
-					'menu'				=>	array( NULL, __('ohri_bank_participant_id', true).": ".$bank_identifier ),
-					'title'				=>	array( NULL, __('ohri_bank_participant_id', true).": ".$bank_identifier ),
+					'menu'				=>	array( NULL, empty($bank_identifier)? __('unknown', true) : $bank_identifier ),
+					'title'				=>	array( NULL, empty($bank_identifier)? __('unknown', true) : __('ohri_bank_participant_id', true).": ".$bank_identifier ),
 					'structure alias' 	=> 'participants',
 					'data'				=> $result
 			);
