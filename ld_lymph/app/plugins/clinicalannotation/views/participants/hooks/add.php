763713769ad0131949aa99cc97430fbe34208a25
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-09-06 20:53:09 +0000

    In dev...

---
+++ ld_lymph/app/plugins/clinicalannotation/views/participants/hooks/add.php
@@ -0,0 +1,5 @@
+<?php
+	// --------------------------------------------------------------------------------
+	// Generate default bank number
+	// -------------------------------------------------------------------------------- 
+	$final_options['override']['Participant.participant_identifier'] = $default_bank_number;
