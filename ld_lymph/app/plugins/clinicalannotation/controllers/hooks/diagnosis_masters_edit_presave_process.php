61cd7d90fccc6c9b04d9d6c0f34fbd1b3cd643ca
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-09-09 19:14:47 +0000

    End of first dev process step - need client revision

--- ld_lymph/app/plugins/clinicalannotation/controllers/hooks/diagnosis_masters_edit_presave_process.php
+++ ld_lymph/app/plugins/clinicalannotation/controllers/hooks/diagnosis_masters_edit_presave_process.php
@@ -1,22 +1,26 @@
 <?php
 
-	if($dx_control_data['DiagnosisControl']['controls_type'] == 'ld lymph. diagnostic') {
-		if($dx_master_data['DiagnosisMaster']['primary_number'] != $this->data['DiagnosisMaster']['primary_number']) {
-			$this->data['DiagnosisMaster']['primary_number'] = $dx_master_data['DiagnosisMaster']['primary_number'];
-			$this->DiagnosisMaster->validationErrors['dx_origin'][] = "the diagnoses group of a diagnosis can not be changed";	
-			$submitted_data_validates = false;	
-		}
-		
-	} else if($dx_control_data['DiagnosisControl']['controls_type'] == 'ld lymph. progression') {	
-		$nbr_of_grp_diagnoses = $this->DiagnosisMaster->find('count', array('conditions'=>array('DiagnosisMaster.primary_number'=>$this->data['DiagnosisMaster']['primary_number'], 'DiagnosisMaster.participant_id'=>$participant_id)));
-		if(empty($this->data['DiagnosisMaster']['primary_number']) || !$nbr_of_grp_diagnoses) {
-			$this->data['DiagnosisMaster']['primary_number'] = NULL;
-			$this->DiagnosisMaster->validationErrors['dx_origin'][] = "a progression should be linked to an existing diagnoses group";	
-			$submitted_data_validates = false;	
-		}
-		
-	} else {
-		$this->redirect( '/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true );
+	switch($dx_control_data['DiagnosisControl']['controls_type']) {
+		case 'ld lymph. diagnostic':
+			if($dx_master_data['DiagnosisMaster']['primary_number'] != $this->data['DiagnosisMaster']['primary_number']) {
+				$this->data['DiagnosisMaster']['primary_number'] = $dx_master_data['DiagnosisMaster']['primary_number'];
+				$this->addWarningMsg(__('the diagnoses group of a diagnosis can not be changed', true));
+			}
+			break;
+			
+			
+		case 'ld lymph. progression':
+		case 'ld lymph. cns relapse':
+		case 'ld lymph. histological transformation':
+			if(empty($this->data['DiagnosisMaster']['primary_number']) || !($this->DiagnosisMaster->find('count', array('conditions'=>array('DiagnosisMaster.primary_number'=>$this->data['DiagnosisMaster']['primary_number'], 'DiagnosisMaster.participant_id'=>$participant_id))))) {
+				$this->data['DiagnosisMaster']['primary_number'] = NULL;
+				$this->DiagnosisMaster->validationErrors['dx_origin'][] = "a progression should be linked to an existing diagnoses group";	
+				$submitted_data_validates = false;	
+			}			
+			break;
+
+		default:
+			$this->redirect( '/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true );
 	}
 	
 ?>
