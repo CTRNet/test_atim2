a859feb23eee096ce5d66d0902d09bc9f5e8f02f
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-09-15 18:33:41 +0000

    E1043: Generating warnings when pTNM or tumour grade value(s) do not match between linked dx and cap report.

--- scripts/tmp_atim_v2.4.0_upgrade.sql
+++ scripts/tmp_atim_v2.4.0_upgrade.sql
@@ -90,7 +90,10 @@ REPLACE INTO i18n(id, en, fr) VALUES
 ("the current diagnosis date is before the parent diagnosis date",
  "The current diagnosis date is before the parent diagnosis date.",
  "L'actuelle date de diagnostic est avant la date du diagnostic parent."),
-("detailed results", "Detailed results", "Résultats détaillés");
+("detailed results", "Detailed results", "Résultats détaillés"),
+("the diagnosis value for %s does not match the cap report value",
+ "The diagnosis value for %s does not match the cap report value",
+ "La valeur du diagnostic pour %s ne correspond pas à la valeur du rapport cap");
 
 UPDATE i18n SET id='the aliquot with barcode [%s] has reached a volume bellow 0', en='The aliquot with barcode [%s] has reached a volume below 0.' WHERE id='the aliquot with barcode [%s] has reached a volume bellow 0';
 UPDATE i18n SET id='cap report - perihilar bile duct' WHERE id='cap peport - perihilar bile duct';
