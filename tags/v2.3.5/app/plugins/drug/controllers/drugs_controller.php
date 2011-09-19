6e5ee1b4030bc984b05fcc9c71c8a63774297fd3
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-09-19 19:16:36 +0000

    TAGGING v2.3.5

---
+++ tags/v2.3.5/app/plugins/drug/controllers/drugs_controller.php
@@ -0,0 +1,134 @@
+<?php
+
+class DrugsController extends DrugAppController {
+
+	var $uses = array(
+		'Drug.Drug',
+		'Protocol.ProtocolExtend',
+		'Clinicalannotation.TreatmentExtend');
+		
+	var $paginate = array('Drug'=>array('limit' => pagination_amount,'order'=>'Drug.generic_name ASC')); 
+
+	function index() {
+		$_SESSION['ctrapp_core']['search'] = NULL; // clear SEARCH criteria
+		
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+	}
+	
+	function search() {
+		if ( $this->data ) $_SESSION['ctrapp_core']['search']['criteria'] = $this->Structures->parseSearchConditions();
+		
+		$this->data = $this->paginate($this->Drug, $_SESSION['ctrapp_core']['search']['criteria']);
+
+		// MANAGE FORM, MENU AND ACTION BUTTONS
+		$this->set( 'atim_menu', $this->Menus->get('/drug/drugs/index/') );	
+				
+		// if SEARCH form data, save number of RESULTS and URL
+		$_SESSION['ctrapp_core']['search']['results'] = $this->params['paging']['Drug']['count'];
+		$_SESSION['ctrapp_core']['search']['url'] = '/drug/drugs/search';
+		
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+	}
+
+	function add() {	
+		$this->set( 'atim_menu', $this->Menus->get('/drug/drugs/index/') );
+		
+		// CUSTOM CODE: FORMAT DISPLAY DATA		
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+		
+		if ( !empty($this->data) ) {
+			$submitted_data_validates = true;
+			
+			$hook_link = $this->hook('presave_process');
+			if( $hook_link ) { 
+				require($hook_link); 
+			}	
+						
+			if ( $submitted_data_validates && $this->Drug->save($this->data) ) {
+				$hook_link = $this->hook('postsave_process');
+				if( $hook_link ) {
+					require($hook_link);
+				}
+				$this->atimFlash( 'your data has been updated','/drug/drugs/detail/'.$this->Drug->id );
+			}
+		}
+  	}
+  
+	function edit( $drug_id ) {
+		if ( !$drug_id ) { $this->redirect( '/pages/err_plugin_funct_param_missing?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+		
+		$drug_data = $this->Drug->find('first',array('conditions'=>array('Drug.id'=>$drug_id)));
+		if(empty($drug_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+		
+		$this->set( 'atim_menu_variables', array('Drug.id'=>$drug_id) );
+		
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }	
+		
+		if ( empty($this->data) ) {
+			$this->data = $drug_data;
+		} else { 			
+			$submitted_data_validates = true;
+			
+			$hook_link = $this->hook('presave_process');
+			if( $hook_link ) { 
+				require($hook_link); 
+			}			
+			
+			if($submitted_data_validates) {
+				$this->Drug->id = $drug_id;
+				if ( $this->Drug->save($this->data) ) {
+					$hook_link = $this->hook('postsave_process');
+					if( $hook_link ) {
+						require($hook_link);
+					}
+					$this->atimFlash( 'your data has been updated','/drug/drugs/detail/'.$drug_id );
+				}
+			}
+		}
+  	}
+	
+	function detail( $drug_id ) {
+		if ( !$drug_id ) { $this->redirect( '/pages/err_plugin_funct_param_missing?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+
+		$drug_data = $this->Drug->find('first',array('conditions'=>array('Drug.id'=>$drug_id)));
+		if(empty($drug_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+		$this->data = $drug_data;
+			
+		$this->set( 'atim_menu_variables', array('Drug.id'=>$drug_id) );
+		
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+	}
+  
+	function delete( $drug_id ) {
+		if ( !$drug_id ) { $this->redirect( '/pages/err_plugin_funct_param_missing?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+
+		$drug_data = $this->Drug->find('first',array('conditions'=>array('Drug.id'=>$drug_id)));
+		if(empty($drug_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+				
+		$arr_allow_deletion = $this->Drug->allowDeletion($drug_id);
+			
+		// CUSTOM CODE
+		
+		$hook_link = $this->hook('delete');
+		if( $hook_link ) { require($hook_link); }		
+				
+		if($arr_allow_deletion['allow_deletion']) {	
+			if( $this->Drug->atim_delete( $drug_id ) ) {
+				$this->atimFlash( 'your data has been deleted', '/drug/drugs/index/');
+			} else {
+				$this->flash( 'error deleting data - contact administrator', '/drug/drugs/index/');
+			}	
+		} else {
+			$this->flash($arr_allow_deletion['msg'], '/drug/drugs/detail/'.$drug_id);
+		}	
+  	}
+}
+
+?>
---
+++ tags/v2.3.5/app/plugins/study/controllers/study_summaries_controller.php
@@ -0,0 +1,130 @@
+<?php
+
+class StudySummariesController extends StudyAppController {
+
+	var $uses = array('Study.StudySummary');
+	var $paginate = array('StudySummary'=>array('limit' => pagination_amount,'order'=>'StudySummary.title'));
+  
+	function listall( ) {
+		// MANAGE DATA
+		$this->data = $this->paginate($this->StudySummary, array());
+		
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+	}
+
+	function detail( $study_summary_id ) {
+		if (!$study_summary_id ) { $this->redirect( '/pages/err_plugin_funct_param_missing?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+		 
+		// MANAGE DATA
+		$study_summary_data = $this->StudySummary->find('first',array('conditions'=>array('StudySummary.id'=>$study_summary_id)));
+		if(empty($study_summary_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, null, true ); }		
+		$this->data = $study_summary_data;
+		
+		// MANAGE FORM, MENU AND ACTION BUTTONS
+		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id) );
+		
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+	}
+	
+	function add() {
+	
+		// MANAGE FORM, MENU AND ACTION BUTTONS
+		$this->set('atim_menu', $this->Menus->get('/study/study_summaries/listall'));
+	
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
+	
+		if ( !empty($this->data) ) {
+			$submitted_data_validates = true;
+			// ... special validations
+
+			// 3- CUSTOM CODE: PROCESS SUBMITTED DATA BEFORE SAVE
+			$hook_link = $this->hook('presave_process');
+			if( $hook_link ) { 
+				require($hook_link); 
+			}				
+		
+			if($submitted_data_validates) {
+				if ( $this->StudySummary->save($this->data) ) {
+					$hook_link = $this->hook('postsave_process');
+					if( $hook_link ) {
+						require($hook_link);
+					}
+					$this->atimFlash( 'your data has been saved','/study/study_summaries/detail/'.$this->StudySummary->id );
+				}
+			}
+		}
+  	}
+  
+	function edit( $study_summary_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_plugin_funct_param_missing?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+		
+		// MANAGE DATA
+		$study_summary_data = $this->StudySummary->find('first',array('conditions'=>array('StudySummary.id'=>$study_summary_id)));
+		if(empty($study_summary_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, null, true ); }
+
+		// MANAGE FORM, MENU AND ACTION BUTTONS
+		$this->set( 'atim_menu_variables', array('StudySummary.id'=>$study_summary_id) );
+		
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }	
+		
+		
+		if(empty($this->data)) {
+			$this->data = $study_summary_data;
+		} else {
+			$submitted_data_validates = true;
+			// ... special validations
+			
+			// 3- CUSTOM CODE: PROCESS SUBMITTED DATA BEFORE SAVE
+			$hook_link = $this->hook('presave_process');
+			if( $hook_link ) { 
+				require($hook_link); 
+			}
+
+			if($submitted_data_validates) {	
+				$this->StudySummary->id = $study_summary_id;
+				if ( $this->StudySummary->save($this->data) ) {
+					$hook_link = $this->hook('postsave_process');
+					if( $hook_link ) {
+						require($hook_link);
+					}
+					$this->atimFlash( 'your data has been updated','/study/study_summaries/detail/'.$study_summary_id );
+				}		
+			}
+		}
+  	}
+	
+	function delete( $study_summary_id ) {
+		if ( !$study_summary_id ) { $this->redirect( '/pages/err_plugin_funct_param_missing?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+
+		// MANAGE DATA
+		$study_summary_data = $this->StudySummary->find('first',array('conditions'=>array('StudySummary.id'=>$study_summary_id)));
+		if(empty($study_summary_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, null, true ); }
+		
+		$arr_allow_deletion = $this->StudySummary->allowDeletion($study_summary_id);
+		
+		// CUSTOM CODE
+		$hook_link = $this->hook('delete');
+		if( $hook_link ) { require($hook_link); }	
+		
+		if($arr_allow_deletion['allow_deletion']) {
+			// DELETE DATA
+			if( $this->StudySummary->atim_delete( $study_summary_id ) ) {
+				$this->atimFlash( 'your data has been deleted', '/study/study_summaries/listall/');
+			} else {
+				$this->flash( 'error deleting data - contact administrator', '/study/study_summaries/listall/');
+			}	
+		} else {
+			$this->flash($arr_allow_deletion['msg'], '/clinicalannotation/study_summaries/detail/'.$study_summary_id);
+		}	
+  	}
+}
+
+?>
