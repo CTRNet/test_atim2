b0590e8c6d1a542490e20eddcd8b3a4a417d2338
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-09-19 16:23:16 +0000

    Fixing script

--- branches/dev/v2.3.x/scripts/v2.3.0/atim_v2.3.5_upgrade.sql
+++ branches/dev/v2.3.x/scripts/v2.3.0/atim_v2.3.5_upgrade.sql
@@ -8,7 +8,7 @@ SELECT '****************' as msg
 UNION
 SELECT IF((SELECT COUNT(*) FROM users WHERE group_id NOT IN(SELECT id FROM groups)) > 0, 'You have users referencing non existing groups. You need to fix them before running the next query.', 'You db is ok. You can run the next query.') AS msg
 UNION 
-ALTER TABLE users 
+SELECT "ALTER TABLE users 
  ADD FOREIGN KEY (group_id) REFERENCES groups(id);" AS msg
 UNION ALL
 SELECT '****************' as msg;
