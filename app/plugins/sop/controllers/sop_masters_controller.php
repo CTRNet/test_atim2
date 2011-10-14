86baeafecdf218539d48603e5bad7bacd1d995ba
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-10-14 14:02:28 +0000

    FIX SOP issues and final modification to have working plugin

--- app/plugins/sop/controllers/sop_masters_controller.php
+++ app/plugins/sop/controllers/sop_masters_controller.php
@@ -6,19 +6,21 @@ class SopMastersController extends SopAppController {
 	var $paginate = array('SopMaster'=>array('limit' => pagination_amount,'order'=>'SopMaster.title DESC'));
 	
 	function listall() {
-		$this->hook();
-		
 		$this->data = $this->paginate($this->SopMaster, array());
 		
 		// find all EVENTCONTROLS, for ADD form
 		$this->set( 'sop_controls', $this->SopControl->find('all', array()) );
+		
+		$hook_link = $this->hook('format');
+		if( $hook_link ) { require($hook_link); }
 	}
 
-	
-	function add($sop_control_id=null) {
+	function add($sop_control_id) {
+		if (!$sop_control_id ) { $this->redirect( '/pages/err_plugin_funct_param_missing?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
 		
 		$this->set( 'atim_menu_variables', array('SopControl.id'=>$sop_control_id)); 
 		$this_data = $this->SopControl->find('first',array('conditions'=>array('SopControl.id'=>$sop_control_id)));
+		if(empty($this_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, null, true ); }		
 		
 		// set FORM ALIAS based off VALUE from CONTROL table
 		$this->Structures->set($this_data['SopControl']['form_alias']);
@@ -34,8 +36,6 @@ class SopMastersController extends SopAppController {
 		if ( !empty($this->data) ) {
 			
 			$this->data['SopMaster']['sop_control_id'] = $sop_control_id;
-			$this->data['SopMaster']['type'] = $this_data['SopControl']['type'];
-			$this->data['SopMaster']['sop_group'] = $this_data['SopControl']['sop_group'];
 			
 			$submitted_data_validates = true;
 			
@@ -46,6 +46,12 @@ class SopMastersController extends SopAppController {
 						
 			if($submitted_data_validates) {
 				if ( $this->SopMaster->save($this->data) ) {
+					
+					$hook_link = $this->hook('postsave_process');
+					if( $hook_link ) { 
+						require($hook_link); 
+					}		
+									
 					$this->atimFlash( 'your data has been updated','/sop/sop_masters/detail/'.$this->SopMaster->getLastInsertId());
 				}
 			}
@@ -58,6 +64,7 @@ class SopMastersController extends SopAppController {
 		$this->set( 'atim_menu_variables', array('SopMaster.id'=>$sop_master_id));
 		
 		$this->data = $this->SopMaster->find('first',array('conditions'=>array('SopMaster.id'=>$sop_master_id)));
+		if(empty($this->data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, null, true ); }		
 		
 		// set FORM ALIAS based off VALUE from MASTER table
 		$this->Structures->set($this->data['SopControl']['form_alias']);
@@ -73,6 +80,7 @@ class SopMastersController extends SopAppController {
 		
 		$this->set( 'atim_menu_variables', array('SopMaster.id'=>$sop_master_id) );
 		$this_data = $this->SopMaster->find('first',array('conditions'=>array('SopMaster.id'=>$sop_master_id)));
+		if(empty($this_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, null, true ); }		
 		
 		// set FORM ALIAS based off VALUE from MASTER table
 		$this->Structures->set($this_data['SopControl']['form_alias']);
@@ -82,10 +90,10 @@ class SopMastersController extends SopAppController {
 			require($hook_link); 
 		}
 			
-		if ( !empty($this->data) ) {
+		if ( empty($this->data) ) {
+			$this->data = $this_data;
 			
-			$this->SopMaster->id = $sop_master_id;
-			
+		} else {
 			$submitted_data_validates = true;
 			
 			$hook_link = $this->hook('presave_process');
@@ -93,14 +101,25 @@ class SopMastersController extends SopAppController {
 				require($hook_link); 
 			}		
 						
+			$this->SopMaster->id = $sop_master_id;
 			if($submitted_data_validates) {
-				if ( $this->SopMaster->save($this->data) ) $this->atimFlash( 'your data has been updated','/sop/sop_masters/detail/'.$sop_master_id.'/');
+				if ( $this->SopMaster->save($this->data) ) {
+					
+					$hook_link = $this->hook('postsave_process');
+					if( $hook_link ) { 
+						require($hook_link); 
+					}		
+					
+					$this->atimFlash( 'your data has been updated','/sop/sop_masters/detail/'.$sop_master_id.'/');
+				}
 			}
 		}
 	}
 	
 	function delete( $sop_master_id ) {
 		if ( !$sop_master_id ) { $this->redirect( '/pages/err_plugin_funct_param_missing?method='.__METHOD__.',line='.__LINE__, NULL, TRUE ); }
+		$this_data = $this->SopMaster->find('first',array('conditions'=>array('SopMaster.id'=>$sop_master_id)));
+		if(empty($this_data)) { $this->redirect( '/pages/err_plugin_no_data?method='.__METHOD__.',line='.__LINE__, null, true ); }		
 		
 		// Check deletion is allowed
 		$arr_allow_deletion = $this->SopMaster->allowDeletion($sop_master_id);
