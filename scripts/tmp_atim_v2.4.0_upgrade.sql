cc4feda449f6089f5dc91fbb3e80634304ce025e
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-10-18 21:16:33 +0000

    Translations

--- scripts/tmp_atim_v2.4.0_upgrade.sql
+++ scripts/tmp_atim_v2.4.0_upgrade.sql
@@ -130,7 +130,13 @@ REPLACE INTO i18n(id, en, fr) VALUES
 ("participant and consent", "Participant and consent", "Participant et consentement"),
 ("participant, consent and diagnosis", "Participant, consent and diagnosis", "Participant, consentement et diagnostic"),
 ("copy linking (if it exists) to", "Copy linking (if it exists) to", "Copier les liens (s'ils existent) à"),
-("copy options", "Copy Options", "Optoins copie");
+("delivery city", "Delivery city", "Ville de livraison"),
+("delivery country", "Delivery country", "Pays de livraison"),
+("delivery postal code", "Delivery postal code", "Code postal de livraison"),
+("delivery province", "Delivery province", "Province de livraison"),
+("delivery street address", "Delivery street address", "Adresse de livraison"),
+("copy options", "Copy options", "Options de copie");
+
 
 UPDATE i18n SET id='the aliquot with barcode [%s] has reached a volume bellow 0', en='The aliquot with barcode [%s] has reached a volume below 0.' WHERE id='the aliquot with barcode [%s] has reached a volume bellow 0';
 UPDATE i18n SET id='cap report - perihilar bile duct' WHERE id='cap peport - perihilar bile duct';
@@ -2626,6 +2632,11 @@ INSERT INTO `i18n` (`id`, `en`, `fr`) VALUES
 ('topography_short_label', 'Topo', 'Topo'),
 ('date signed', 'Signed', 'Signé');
 
+ALTER TABLE datamart_browsing_results
+ MODIFY serialized_search_params text DEFAULT NULL;
+ALTER TABLE datamart_browsing_results_revs
+ MODIFY serialized_search_params text DEFAULT NULL;
+ 
 UPDATE `menus` SET `use_summary` = 'Clinicalannotation.ClinicalCollectionLink::summary' WHERE `menus`.`id` = 'clin_CAN_67' LIMIT 1 ;
 UPDATE structure_formats SET `flag_summary`='1' WHERE structure_id=(SELECT id FROM structures WHERE alias='clinicalcollectionlinks') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='Collection' AND `tablename`='collections' AND `field`='acquisition_label' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
 UPDATE structure_formats SET `flag_summary`='1' WHERE structure_id=(SELECT id FROM structures WHERE alias='clinicalcollectionlinks') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='Collection' AND `tablename`='collections' AND `field`='bank_id' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='banks') AND `flag_confidential`='0');
