b220254f9de6219136c177b5f083cb4218b9a832
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-10-25 18:01:08 +0000

    Changed validations, menus and summary

--- branches/qc/chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/consent_masters_add_presave_process.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	if(($this->data['ConsentMaster']['consent_status'] == 'obtained') && (empty($this->data['ConsentMaster']['consent_signed_date']['year']))) {
-		$submitted_data_validates = false;
-		$this->ConsentMaster->validationErrors['consent_status'][] = 'all obtained consents should have a signed date';
-	}
-		
-?>
