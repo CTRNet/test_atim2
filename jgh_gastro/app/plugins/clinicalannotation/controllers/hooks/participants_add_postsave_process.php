9d16fb080b453a3359054430e093651e27e797b8
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-10-28 14:10:35 +0000

    Fixing date bug

--- jgh_gastro/app/plugins/clinicalannotation/controllers/hooks/participants_add_postsave_process.php
+++ jgh_gastro/app/plugins/clinicalannotation/controllers/hooks/participants_add_postsave_process.php
@@ -2,7 +2,9 @@
 
 	// --------------------------------------------------------------------------------
 	// Save Participant Identifier
-	// -------------------------------------------------------------------------------- 
-	if(!$this->Participant->save(array('Participant' => array('participant_identifier' => 'p-'.$this->Participant->id)))) $this->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+	// --------------------------------------------------------------------------------
+	if(!$this->Participant->save(array('Participant' => array('participant_identifier' => 'p-'.$this->Participant->id)), false)){
+		$this->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+	}
 	
 ?>
