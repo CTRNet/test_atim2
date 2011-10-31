5ad85b23c13bc01b6b0f4e43a04f30c86cc2307c
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-10-31 17:27:18 +0000

    Change storage_code to id value

--- scripts/v2.4.0/atim_v2.4.0_demo_data.sql
+++ scripts/v2.4.0/atim_v2.4.0_demo_data.sql
@@ -2458,6 +2458,7 @@ INSERT INTO `storage_masters` (`id`, `code`, `storage_control_id`, `parent_id`, 
 (20, 'TMA609 - 20', 20, 14, 27, 28, '938912', 'TMA-007', 'fr1-3-r2-TMA-TMA-007', '', '', '', '-82.00', 'celsius', '', '2011-10-19 13:20:47', 1, '2011-10-19 13:20:48', 1, 0),
 (21, 'B2D100 - 21', 18, 7, 6, 7, '41221123123', 'TISS1', 'fr1-2-r43-TISS1', '', '', '', '-82.00', 'celsius', '', '2011-10-19 14:28:50', 1, '2011-10-19 14:28:51', 1, 0);
 
+UPDATE storage_masters SET code = id;
 --
 -- Dumping data for table `storage_masters_revs`
 --
@@ -2524,6 +2525,8 @@ INSERT INTO `storage_masters_revs` (`id`, `code`, `storage_control_id`, `parent_
 (20, 'TMA609 - 20', 20, 14, 25, 26, '938912', 'TMA-007', 'fr1-3-r2-TMA-TMA-007', '', '', '', '-82.00', 'celsius', '', 1, 59, '2011-10-19 13:20:48'),
 (21, '', 18, 7, 0, 0, '41221123123', 'TISS1', 'fr1-2-r43-TISS1', '', '', '', '-82.00', 'celsius', '', 1, 60, '2011-10-19 14:28:51'),
 (21, 'B2D100 - 21', 18, 7, 6, 7, '41221123123', 'TISS1', 'fr1-2-r43-TISS1', '', '', '', '-82.00', 'celsius', '', 1, 61, '2011-10-19 14:28:52');
+
+UPDATE storage_masters_revs SET code = id;	
 
 --
 -- Dumping data for table `structure_permissible_values_customs`
