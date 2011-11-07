13a44f0e09087441a748314f3533140a16053685
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-11-07 20:40:05 +0000

    Add sequence number to sample

--- branches/qc/ld_lymph/app/plugins/inventorymanagement/controllers/hooks/sample_masters_add_initial_display.php
+++ branches/qc/ld_lymph/app/plugins/inventorymanagement/controllers/hooks/sample_masters_add_initial_display.php
@@ -2,12 +2,12 @@
  	
 	if($is_specimen){
 		$tmp_year = date('y', time());
-		$max_specimen_number = $this->SampleMaster->find('first', array('conditions' => array("SampleMaster.specimen_number LIKE '$tmp_year%'"), 'fields' => array('MAX(SampleMaster.specimen_number) as max_specimen_number'), 'recursive' => -1));
-		$default_number = $max_specimen_number[0]['max_specimen_number'];
+		$max_ld_lymph_specimen_number = $this->SampleMaster->find('first', array('conditions' => array("SampleMaster.ld_lymph_specimen_number LIKE '$tmp_year%'"), 'fields' => array('MAX(SampleMaster.ld_lymph_specimen_number) as max_ld_lymph_specimen_number'), 'recursive' => -1));
+		$default_number = $max_ld_lymph_specimen_number[0]['max_ld_lymph_specimen_number'];
 		if(empty($default_number)) {
 			$default_number = $tmp_year."001";
 		} else {
 			$default_number += 1;
 		}
-		$this->data['SampleMaster']['specimen_number'] = $default_number;
+		$this->data['SampleMaster']['ld_lymph_specimen_number'] = $default_number;
 	}
