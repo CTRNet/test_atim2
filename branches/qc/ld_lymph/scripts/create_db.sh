90d55270bce98e43c3e78978795d0babb0bfdf7e
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-11-08 14:50:16 +0000

    MERGED with trunk revs 3826

--- branches/qc/ld_lymph/scripts/create_db.sh
+++ branches/qc/ld_lymph/scripts/create_db.sh
@@ -7,31 +7,37 @@ if [ "$1" == "" -o "$2" == "" -o "$3" == "" ]; then
  exit 0
 fi
 
-echo running atim_v2.3.0_full_installation.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.0_full_installation.sql
+#echo running atim_v2.3.0_full_installation.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.0_full_installation.sql
 
-echo running atim_v2.3.1_upgrade.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.1_upgrade.sql
+#echo running atim_v2.3.1_upgrade.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.1_upgrade.sql
 
-echo running atim_v2.3.2_upgrade.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.2_upgrade.sql
+#echo running atim_v2.3.2_upgrade.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.2_upgrade.sql
 
-echo running atim_v2.3.3_upgrade.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.3_upgrade.sql
+#echo running atim_v2.3.3_upgrade.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.3_upgrade.sql
 
-echo running atim_v2.3.4_upgrade.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.4_upgrade.sql
+#echo running atim_v2.3.4_upgrade.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.4_upgrade.sql
 
-echo running atim_v2.3.5_upgrade.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.5_upgrade.sql
+#echo running atim_v2.3.5_upgrade.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.5_upgrade.sql
 
-echo running atim_v2.3.6_upgrade.sql
-mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.6_upgrade.sql
+#echo running atim_v2.3.6_upgrade.sql
+#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.3.0/atim_v2.3.6_upgrade.sql
+
+echo importing atim 2.3.6
+mysql -u $2 -p$3 $1 < atim_236.sql
 
 echo running tmp_atim_v2.4.0_upgrade.sql
 mysql -u $2 -p$3 $1 --default-character-set=utf8 < tmp_atim_v2.4.0_upgrade.sql
 
-#echo running atim_v2.4.0_demo_data.sql
-#mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.4.0/atim_v2.4.0_demo_data.sql
+echo running atim_v2.4.0_demo_data.sql
+mysql -u $2 -p$3 $1 --default-character-set=utf8 < v2.4.0/atim_v2.4.0_demo_data.sql
+
+echo running fmlh.sql
+mysql -u $2 -p$3 $1 < fmlh.sql
 
 echo done
