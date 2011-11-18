d17e9ec1ff0a2cc4a55d9a811a228e9adffa8c0d
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-11-18 16:41:41 +0000

    Rewritten adhoc queries

--- branches/qc/chum/hepato-billary/scripts/v2.4.0/custom.sql
+++ branches/qc/chum/hepato-billary/scripts/v2.4.0/custom.sql
@@ -243,5 +243,337 @@ ALTER TABLE qc_hb_txd_surgery_pancreas
  DROP FOREIGN KEY qc_hb_txd_surgery_pancreas_ibfk_1,
  CHANGE tx_master_id treatment_master_id INT NOT NULL,
  ADD FOREIGN KEY (treatment_master_id) REFERENCES treatment_masters(id); 
-ALTER TABLE qc_hb_txd_surgery_livers_revs
+ALTER TABLE qc_hb_txd_surgery_pancreas_revs
  CHANGE tx_master_id treatment_master_id INT NOT NULL;
+
+REPLACE INTO i18n (id, en, fr) VALUES
+('recurrent', 'Recurrent', 'Récurrent'),
+('add new diagnosis', "Add New Diagnosis", "Ajouter un nouveau diagnostic"),
+('additional dimension b', "Additional dimension b", "Dimension supplémentaire b"),
+('adjacent liver parenchyma', "Adjacent liver parenchyma", "Parenchyme adjacent au foie"),
+('adjacent liver parenchyma specify', "", ""),
+('biological glue', "Biological glue", "Colle biologique"),
+('chemo-embolization', "", ""),
+('chronic hepatitis', "Chronic hepatitis", "Hépatite chronique"),
+('contact data', "Contact data", "Données du contact"),
+('gastric tube', "Gastric tube", "Tube gastrique"),
+('heterogeneous', "Heterogeneous", "Hétérogène"),
+('histologic type', "Histologic type", "Type histologique"),
+('homogeneous', "Homogeneous", "Homogène"),
+('hospitalization', "hospitalization", "Hospitalisation"),
+('identifier name', "Identifier name", "Nom d'identifiant"),
+('lesions', "Lesions", "Lésions"),
+('margins', "Margins", "Marges"),
+('necrosis percentage', "Necrosis percentage", "Pourcentage de nécrose"),
+('operative bleeding', "Operative bleeding", "Saignement opératoire"),
+('operative pathological report', "Operative pathological report", "Rapport pathologique d'opération"),
+('operative time (mn)', "operative time (min)", "Temps d'opération (min)"),
+('parenchyma', "Parenchyma", "Parenchyme"),
+('pathological report', "Pathological report", "Rapport de pathologie"),
+('principal surgery', "Principal surgery", "Chirurgie principale"),
+('report date', "Report date", "Date du rapport"),
+('specify margin', "Specify margin", "Spécifiez la marge"),
+('steatosis', "Steatosis", "Stéatose hépatique"),
+('storage method', "Storage method", "Méthode d'entreposage"),
+('treatment date', 'Treatment date', "Date du traitement"),
+('type of drain 1', 'Type of drain 1', "Type de drain 1"),
+('type of drain 1', 'Type of drain 2', "Type de drain 2"),
+('type of drain 1', 'Type of drain 3', "Type de drain 3"),
+('type of glue', "Type of glue", "Type de colle"),
+('type of local treatment', "Type of local treatment", "Type de traitement local"),
+('type of vascular occlusion', "Type of vascular occlusion", "Type d'occlusion vasculaire"),
+('vascular occlusion', "Vascular occlusion", "Occlusion vasculaire");
+
+TRUNCATE missing_translations;
+
+UPDATE datamart_adhoc SET sql_query_for_results='
+SELECT 
+EventMaster.id,
+Participant.id,
+Participant.participant_identifier,
+Participant.first_name,
+Participant.last_name,
+
+MiscIdentifier.identifier_value,
+
+EventDetail.hepatic_artery,
+EventDetail.coeliac_trunk ,
+EventDetail.splenic_artery,
+EventDetail.superior_mesenteric_artery,
+EventDetail.portal_vein,
+EventDetail.superior_mesenteric_vein,
+EventDetail.splenic_vein,
+EventDetail.metastatic_lymph_nodes
+
+FROM event_controls AS ctrl
+INNER JOIN event_masters AS EventMaster ON ctrl.id = EventMaster.event_control_id AND ctrl.form_alias LIKE "qc_hb_imaging_%pancreas%" AND EventMaster.deleted != 1
+INNER JOIN qc_hb_ed_hepatobilary_medical_imagings AS EventDetail ON EventDetail.event_master_id = EventMaster.id
+INNER JOIN participants AS Participant ON EventMaster.participant_id = Participant.id AND Participant.deleted != 1
+LEFT JOIN misc_identifiers AS MiscIdentifier ON MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.misc_identifier_control_id = "3" AND MiscIdentifier.deleted != 1
+
+WHERE TRUE AND
+Participant.participant_identifier = "@@Participant.participant_identifier@@"
+AND MiscIdentifier.identifier_value = "@@MiscIdentifier.identifier_value@@"
+
+AND EventDetail.hepatic_artery = "@@EventDetail.hepatic_artery@@"
+AND EventDetail.coeliac_trunk = "@@EventDetail.coeliac_trunk@@"
+AND EventDetail.splenic_artery = "@@EventDetail.splenic_artery@@"
+AND EventDetail.superior_mesenteric_artery = "@@EventDetail.superior_mesenteric_artery@@"
+AND EventDetail.portal_vein = "@@EventDetail.portal_vein@@"
+AND EventDetail.superior_mesenteric_vein = "@@EventDetail.superior_mesenteric_vein@@"
+AND EventDetail.splenic_vein = "@@EventDetail.splenic_vein@@"
+AND EventDetail.metastatic_lymph_nodes = "@@EventDetail.metastatic_lymph_nodes@@";
+' WHERE id=2;
+
+
+
+UPDATE datamart_adhoc SET sql_query_for_results='
+SELECT 
+EventMaster.id,
+Participant.id,
+Participant.participant_identifier,
+Participant.first_name,
+Participant.last_name,
+
+MiscIdentifier.identifier_value,
+
+EventDetail.is_volumetry_post_pve,
+EventDetail.total_liver_volume,
+EventDetail.resected_liver_volume,
+EventDetail.remnant_liver_volume,
+EventDetail.tumoral_volume,
+EventDetail.remnant_liver_percentage
+
+FROM event_controls AS ctrl
+INNER JOIN event_masters AS EventMaster ON ctrl.id = EventMaster.event_control_id AND ctrl.form_alias LIKE "qc_hb_imaging_%volumetry%" AND EventMaster.deleted != 1
+INNER JOIN qc_hb_ed_hepatobilary_medical_imagings AS EventDetail ON EventDetail.event_master_id = EventMaster.id
+INNER JOIN participants AS Participant ON EventMaster.participant_id = Participant.id AND Participant.deleted != 1
+LEFT JOIN misc_identifiers AS MiscIdentifier ON MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.misc_identifier_control_id = "3" AND MiscIdentifier.deleted != 1
+
+WHERE TRUE AND
+Participant.participant_identifier = "@@Participant.participant_identifier@@"
+AND MiscIdentifier.identifier_value = "@@MiscIdentifier.identifier_value@@"
+
+AND EventDetail.is_volumetry_post_pve = "@@EventDetail.is_volumetry_post_pve@@"
+
+AND EventDetail.total_liver_volume >= "@@EventDetail.total_liver_volume_start@@" 
+AND EventDetail.total_liver_volume <= "@@EventDetail.total_liver_volume_end@@" 
+
+AND EventDetail.resected_liver_volume >= "@@EventDetail.resected_liver_volume_start@@" 
+AND EventDetail.resected_liver_volume <= "@@EventDetail.resected_liver_volume_end@@" 
+
+AND EventDetail.remnant_liver_volume >= "@@EventDetail.remnant_liver_volume_start@@" 
+AND EventDetail.remnant_liver_volume <= "@@EventDetail.remnant_liver_volume_end@@" 
+
+AND EventDetail.tumoral_volume >= "@@EventDetail.tumoral_volume_start@@" 
+AND EventDetail.tumoral_volume <= "@@EventDetail.tumoral_volume_end@@" 
+
+AND EventDetail.remnant_liver_percentage >= "@@EventDetail.remnant_liver_percentage_start@@" 
+AND EventDetail.remnant_liver_percentage <= "@@EventDetail.remnant_liver_percentage_end@@" ;
+' WHERE id=3;
+
+UPDATE structure_fields SET  `type`='float_positive' WHERE model='EventDetail' AND tablename='qc_hb_ed_hepatobilary_medical_imagings' AND field='remnant_liver_percentage' AND `type`='number' AND structure_value_domain  IS NULL ;
+UPDATE structure_fields SET  `type`='float_positive' WHERE model='EventDetail' AND tablename='qc_hb_ed_hepatobilary_medical_imagings' AND field='remnant_liver_volume' AND `type`='number' AND structure_value_domain  IS NULL ;
+
+
+
+
+UPDATE datamart_adhoc SET sql_query_for_results='
+SELECT 
+EventMaster.id,
+Participant.id,
+Participant.participant_identifier,
+Participant.first_name,
+Participant.last_name,
+
+MiscIdentifier.identifier_value,
+
+EventDetail.lungs_number,
+EventDetail.lungs_size,
+EventDetail.lungs_laterality,
+EventDetail.lymph_node_number,
+EventDetail.lymph_node_size,
+EventDetail.colon_number,
+EventDetail.colon_size,
+EventDetail.rectum_number,
+EventDetail.rectum_size,
+EventDetail.bones_number,
+EventDetail.bones_size,
+EventDetail.other_localisation_1,
+EventDetail.other_localisation_1_number,
+EventDetail.other_localisation_1_size,
+EventDetail.other_localisation_2,
+EventDetail.other_localisation_2_number,
+EventDetail.other_localisation_2_size,
+EventDetail.other_localisation_3,
+EventDetail.other_localisation_3_number,
+EventDetail.other_localisation_3_size
+
+FROM event_controls AS ctrl
+INNER JOIN event_masters AS EventMaster ON ctrl.id = EventMaster.event_control_id AND ctrl.form_alias LIKE "qc_hb_imaging_%other%" AND EventMaster.deleted != 1
+INNER JOIN qc_hb_ed_hepatobilary_medical_imagings AS EventDetail ON EventDetail.event_master_id = EventMaster.id
+INNER JOIN participants AS Participant ON EventMaster.participant_id = Participant.id AND Participant.deleted != 1
+LEFT JOIN misc_identifiers AS MiscIdentifier ON MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.misc_identifier_control_id = 3 AND MiscIdentifier.deleted != 1
+
+WHERE TRUE AND
+Participant.participant_identifier = "@@Participant.participant_identifier@@"
+AND MiscIdentifier.identifier_value = "@@MiscIdentifier.identifier_value@@"
+
+AND EventDetail.lungs_number >= "@@EventDetail.lungs_number_start@@" 
+AND EventDetail.lungs_number <= "@@EventDetail.lungs_number_end@@"
+
+AND EventDetail.lungs_size >= "@@EventDetail.lungs_size_start@@" 
+AND EventDetail.lungs_size <= "@@EventDetail.lungs_size_end@@"
+
+AND EventDetail.lungs_laterality = "@@EventDetail.lungs_laterality@@"
+
+AND EventDetail.lymph_node_number >= "@@EventDetail.lymph_node_number_start@@" 
+AND EventDetail.lymph_node_number <= "@@EventDetail.lymph_node_number_end@@"
+
+AND EventDetail.lymph_node_size >= "@@EventDetail.lymph_node_size_start@@" 
+AND EventDetail.lymph_node_size <= "@@EventDetail.lymph_node_size_end@@"
+
+AND EventDetail.colon_number >= "@@EventDetail.colon_number_start@@" 
+AND EventDetail.colon_number <= "@@EventDetail.colon_number_end@@"
+
+AND EventDetail.colon_size >= "@@EventDetail.colon_size_start@@" 
+AND EventDetail.colon_size <= "@@EventDetail.colon_size_end@@"
+
+AND EventDetail.rectum_number >= "@@EventDetail.rectum_number_start@@" 
+AND EventDetail.rectum_number <= "@@EventDetail.rectum_number_end@@"
+
+AND EventDetail.rectum_size >= "@@EventDetail.rectum_size_start@@" 
+AND EventDetail.rectum_size <= "@@EventDetail.rectum_size_end@@"
+
+AND EventDetail.bones_number >= "@@EventDetail.bones_number_start@@" 
+AND EventDetail.bones_number <= "@@EventDetail.bones_number_end@@"
+
+AND EventDetail.bones_size >= "@@EventDetail.bones_size_start@@" 
+AND EventDetail.bones_size <= "@@EventDetail.bones_size_end@@"
+
+AND EventDetail.other_localisation_1 = "@@EventDetail.other_localisation_1@@"
+
+AND EventDetail.other_localisation_1_number >= "@@EventDetail.other_localisation_1_number_start@@" 
+AND EventDetail.other_localisation_1_number <= "@@EventDetail.other_localisation_1_number_end@@"
+
+AND EventDetail.other_localisation_1_size >= "@@EventDetail.other_localisation_1_size_start@@" 
+AND EventDetail.other_localisation_1_size <= "@@EventDetail.other_localisation_1_size_end@@"
+
+AND EventDetail.other_localisation_2 = "@@EventDetail.other_localisation_2@@"
+
+AND EventDetail.other_localisation_2_number >= "@@EventDetail.other_localisation_2_number_start@@" 
+AND EventDetail.other_localisation_2_number <= "@@EventDetail.other_localisation_2_number_end@@"
+
+AND EventDetail.other_localisation_2_size >= "@@EventDetail.other_localisation_2_size_start@@" 
+AND EventDetail.other_localisation_2_size <= "@@EventDetail.other_localisation_2_size_end@@"
+
+AND EventDetail.other_localisation_3 = "@@EventDetail.other_localisation_3@@"
+
+AND EventDetail.other_localisation_3_number >= "@@EventDetail.other_localisation_3_number_start@@" 
+AND EventDetail.other_localisation_3_number <= "@@EventDetail.other_localisation_3_number_end@@"
+
+AND EventDetail.other_localisation_3_size >= "@@EventDetail.other_localisation_3_size_start@@" 
+AND EventDetail.other_localisation_3_size <= "@@EventDetail.other_localisation_3_size_end@@"
+' WHERE id=4;
+
+
+UPDATE datamart_adhoc SET sql_query_for_results='
+SELECT 
+EventMaster.id,
+Participant.id,
+Participant.participant_identifier,
+Participant.first_name,
+Participant.last_name,
+
+MiscIdentifier.identifier_value,
+
+EventDetail.segment_1_number,
+EventDetail.segment_1_size,
+EventDetail.segment_2_number,
+EventDetail.segment_2_size,
+EventDetail.segment_3_number,
+EventDetail.segment_3_size,
+EventDetail.segment_4a_number,
+EventDetail.segment_4a_size,
+EventDetail.segment_4b_number,
+EventDetail.segment_4b_size,
+EventDetail.segment_5_number,
+EventDetail.segment_5_size,
+EventDetail.segment_6_number,
+EventDetail.segment_6_size,
+EventDetail.segment_7_number,
+EventDetail.segment_7_size,
+EventDetail.segment_8_number,
+EventDetail.segment_8_size,
+EventDetail.density,
+EventDetail.type
+
+FROM event_controls AS ctrl
+INNER JOIN event_masters AS EventMaster ON ctrl.id = EventMaster.event_control_id AND ctrl.form_alias LIKE "qc_hb_imaging_%segment%" AND EventMaster.deleted != 1
+INNER JOIN qc_hb_ed_hepatobilary_medical_imagings AS EventDetail ON EventDetail.event_master_id = EventMaster.id
+INNER JOIN participants AS Participant ON EventMaster.participant_id = Participant.id AND Participant.deleted != 1
+LEFT JOIN misc_identifiers AS MiscIdentifier ON MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.misc_identifier_control_id = 3 AND MiscIdentifier.deleted != 1
+
+WHERE TRUE AND
+Participant.participant_identifier = "@@Participant.participant_identifier@@"
+AND MiscIdentifier.identifier_value = "@@MiscIdentifier.identifier_value@@"
+
+AND EventDetail.segment_1_number >= "@@EventDetail.segment_1_number_start@@" 
+AND EventDetail.segment_1_number <= "@@EventDetail.segment_1_number_end@@" 
+
+AND EventDetail.segment_1_size >= "@@EventDetail.segment_1_size_start@@" 
+AND EventDetail.segment_1_size <= "@@EventDetail.segment_1_size_end@@" 
+
+AND EventDetail.segment_2_number >= "@@EventDetail.segment_2_number_start@@" 
+AND EventDetail.segment_2_number <= "@@EventDetail.segment_2_number_end@@" 
+
+AND EventDetail.segment_2_size >= "@@EventDetail.segment_2_size_start@@" 
+AND EventDetail.segment_2_size <= "@@EventDetail.segment_2_size_end@@" 
+
+AND EventDetail.segment_3_number >= "@@EventDetail.segment_3_number_start@@" 
+AND EventDetail.segment_3_number <= "@@EventDetail.segment_3_number_end@@" 
+
+AND EventDetail.segment_3_size >= "@@EventDetail.segment_3_size_start@@" 
+AND EventDetail.segment_3_size <= "@@EventDetail.segment_3_size_end@@" 
+
+AND EventDetail.segment_4a_number >= "@@EventDetail.segment_4a_number_start@@" 
+AND EventDetail.segment_4a_number <= "@@EventDetail.segment_4a_number_end@@" 
+
+AND EventDetail.segment_4a_size >= "@@EventDetail.segment_4a_size_start@@" 
+AND EventDetail.segment_4a_size <= "@@EventDetail.segment_4a_size_end@@" 
+
+AND EventDetail.segment_4b_number >= "@@EventDetail.segment_4b_number_start@@" 
+AND EventDetail.segment_4b_number <= "@@EventDetail.segment_4b_number_end@@" 
+
+AND EventDetail.segment_4b_size >= "@@EventDetail.segment_4b_size_start@@" 
+AND EventDetail.segment_4b_size <= "@@EventDetail.segment_4b_size_end@@" 
+
+AND EventDetail.segment_5_number >= "@@EventDetail.segment_5_number_start@@" 
+AND EventDetail.segment_5_number <= "@@EventDetail.segment_5_number_end@@" 
+
+AND EventDetail.segment_5_size >= "@@EventDetail.segment_5_size_start@@" 
+AND EventDetail.segment_5_size <= "@@EventDetail.segment_5_size_end@@" 
+
+AND EventDetail.segment_6_number >= "@@EventDetail.segment_6_number_start@@" 
+AND EventDetail.segment_6_number <= "@@EventDetail.segment_6_number_end@@" 
+
+AND EventDetail.segment_6_size >= "@@EventDetail.segment_6_size_start@@" 
+AND EventDetail.segment_6_size <= "@@EventDetail.segment_6_size_end@@" 
+
+AND EventDetail.segment_7_number >= "@@EventDetail.segment_7_number_start@@" 
+AND EventDetail.segment_7_number <= "@@EventDetail.segment_7_number_end@@" 
+
+AND EventDetail.segment_7_size >= "@@EventDetail.segment_7_size_start@@" 
+AND EventDetail.segment_7_size <= "@@EventDetail.segment_7_size_end@@" 
+
+AND EventDetail.segment_8_number >= "@@EventDetail.segment_8_number_start@@" 
+AND EventDetail.segment_8_number <= "@@EventDetail.segment_8_number_end@@" 
+
+AND EventDetail.segment_8_size >= "@@EventDetail.segment_8_size_start@@" 
+AND EventDetail.segment_8_size <= "@@EventDetail.segment_8_size_end@@" 
+
+AND EventDetail.density >= "@@EventDetail.density_start@@" 
+AND EventDetail.density <= "@@EventDetail.density_end@@" 
+
+AND EventDetail.type = "@@EventDetail.type@@"
+' WHERE id=1;
