a585bd922a0852222f7451496672abb5e1f56b99
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-11-23 17:52:16 +0000

    tmp

--- OvCaRe/app/plugins/clinicalannotation/models/customs/participant.php
+++ OvCaRe/app/plugins/clinicalannotation/models/customs/participant.php
@@ -28,45 +28,52 @@ class ParticipantCustom extends Participant {
 	}
 	
 
-//	function beforeSave($options) {
-//		// Check date of birth and date of death completed
-//		if (!empty($this->data['Participant']['date_of_birth']) && !empty($this->data['Participant']['date_of_death'])) {
-//		
-//			$this->updateAgeAtDeath($this->data['Participant']['date_of_death'], $this->data['Participant']['date_of_birth']);
-//			
-//			// TODO Fix FK reference
-//			$dx_model = AppModel::getInstance("Clinicalannotation", "DiagnosisMaster", true);
-//			$dx_results = $dx_model->find('all', array('conditions' => array ('DiagnosisMaster.participant_id' => 1)));
-//		
-//			$this->updateAllDxAges($this->data['Participant']['date_of_birth']);
-//		}
-//		return true;
-//	}
-//
-//	function updateAgeAtDeath($dateofdeath, $dateofbirth) {
-//		$status = false;
-//		if (isset($dateofdeath) && isset($dateofbirth)) {
-//	
-//			$birthDateObj = new DateTime($dateofbirth);
-//			$deathDateObj = new DateTime($dateofdeath);
-//			$interval = $birthDateObj->diff($deathDateObj);			
-//
-//			if ($dateofdeath >= $dateofbirth ) {	
-//				$ageAtDeath = $interval->format('%r%y');
-//				$this->data['Participant']['ccbr_age_at_death'] = $ageAtDeath;
-//			} else {
-//				$this->data['Participant']['ccbr_age_at_death'] = '';
-//       		}		
-//			$status = true;
-//		}
-//		return true;		
-//	}
-//	
-//	function updateAllDxAges($dateofbirth) {
-//		// Finds all associated diagnosis records and updates
-//		$status = false;
-//		
-//		return $status;
-//	}
+	function beforeSave($options) {
+		
+		if(!empty($this->id)) {
+			// Participant data has just been updated
+			$previous_participant_data = $this->find('first', array('conditions' => array('Participant.id' => $this->id), 'recursive' => '-1'));
+			if(empty($previous_participant_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+			$date_of_birth = $this->data['Participant']['date_of_birth'];
+			$previous_date_of_birth = $previous_participant_data['Participant']['date_of_birth'];
+			
+			if($previous_date_of_birth != $date_of_birth) {
+				
+				// UPDATE AGE AT SURGERY
+				
+				$treatment_master_model = AppModel::getInstance("Clinicalannotation", "TreatmentMaster", true);
+				$participant_treatments = $treatment_master_model->find('all', array('conditions' => array ('TreatmentMaster.participant_id' => $this->id), 'recursive' => '0'));
+
+				foreach($participant_treatments as $new_trt) {
+					if(($new_trt['TreatmentControl']['disease_site'] == 'ovcare') && ($new_trt['TreatmentControl']['tx_method'] == 'surgery')) {
+						$new_surgery_id = $new_trt['TreatmentMaster']['id'];
+						$surgery_date = $new_trt['TreatmentMaster']['start_date'];
+						
+						$ageInYears = null;
+						if(!empty($surgery_date) && !empty($date_of_birth)) {
+							$birthDateObj = new DateTime($date_of_birth);
+							$surgDateObj = new DateTime($surgery_date);
+							$interval = $birthDateObj->diff($surgDateObj);
+							$ageInYears = $interval->format('%r%y');
+							if($ageInYears < 0) {
+								$ageInYears = null;
+								AppController::addWarningMsg(str_replace('%%field%%', __('age at surgery',true), __('error in the dates definitions: the field [%%field%%] can not be generated', true)));
+							}
+						}
+						
+						$new_tr_data = array();
+						$new_tr_data['TreatmentMaster']['id'] = $new_surgery_id;
+						$new_tr_data['TreatmentDetail']['ovcare_age_at_surgery'] = $ageInYears;
+						$treatment_master_model->id = null;
+						$treatment_master_model->data = array();
+						if(!$treatment_master_model->save($new_tr_data, false)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+					}
+				}
+			}
+		}
+
+		return true;
+	}
+
 }
 ?>
--- OvCaRe/app/plugins/clinicalannotation/models/customs/treatment_master.php
+++ OvCaRe/app/plugins/clinicalannotation/models/customs/treatment_master.php
@@ -5,37 +5,55 @@ class TreatmentMasterCustom extends TreatmentMaster {
 	var $useTable = 'treatment_masters';
 	
 	function beforeSave($options) {
-		$tx_control_model = AppModel::getInstance("Clinicalannotation", "TreatmentControl", true);
-		$tx_control_data = $tx_control_model->find('first', array('conditions' => array ('TreatmentControl.id' => 7)));
-
-		if(($tx_control_data['TreatmentControl']['disease_site'] == 'ovcare') && ($tx_control_data['TreatmentControl']['tx_method'] == 'surgery')) {
-			$surgery_date = $this->data['TreatmentMaster']['start_date'];
+		
+		// Check $this->data has been populated from screen and not from Participant.beforeSave() function	
+		if(!array_key_exists('ovcare_age_at_surgery', $this->data['TreatmentDetail'])) {
 			
-			$participant_id = null;
-			if(array_key_exists('participant_id', $this->data['TreatmentMaster'])) {	
-				$participant_id = $this->data['TreatmentMaster']['participant_id'];
-			} else {
-				$db_trt_data = $this->find('first', array('conditions' => array ('TreatmentMaster.id' => $this->id), 'recursive' => '-1'));	
-				$participant_id = $db_trt_data['TreatmentMaster']['participant_id'];
+			$previous_treatment_data = null;
+			$participant_id = array_key_exists('participant_id', $this->data['TreatmentMaster'])? $this->data['TreatmentMaster']['participant_id'] : null;
+			$treatment_control_id = array_key_exists('treatment_control_id', $this->data['TreatmentMaster'])? $this->data['TreatmentMaster']['treatment_control_id'] : null;
+			if(is_null($participant_id) || is_null($treatment_control_id)) {
+				// Trt update
+				$previous_treatment_data = $this->find('first', array('conditions' => array ('TreatmentMaster.id' => $this->id), 'recursive' => '-1'));
+				if(empty($previous_treatment_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+				$participant_id = $previous_treatment_data['TreatmentMaster']['participant_id'];	
+				$treatment_control_id = $previous_treatment_data['TreatmentMaster']['treatment_control_id'];	
 			}
+	
+			$tx_control_model = AppModel::getInstance("Clinicalannotation", "TreatmentControl", true);
+			$tx_control_data = $tx_control_model->find('first', array('conditions' => array ('TreatmentControl.id' => $treatment_control_id)));
+			if(empty($tx_control_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
 			
-			$participant_model = AppModel::getInstance("Clinicalannotation", "Participant", true);
-			$participant_data = $participant_model->find('first', array('conditions' => array ('Participant.id' => $participant_id), 'recursive' => '-1'));
-			$date_of_brith = $participant_data['Participant']['date_of_birth'];
-
-			$ageInYears = null;
-			if(!empty($surgery_date) && !empty($date_of_brith)) {
-				$birthDateObj = new DateTime($date_of_brith);
-				$surgDateObj = new DateTime($surgery_date);
-				$interval = $birthDateObj->diff($surgDateObj);
-				$ageInYears = $interval->format('%r%y');
-				if($ageInYears < 0) {
+			// AGE AT SURGERY
+			
+			if(($tx_control_data['TreatmentControl']['disease_site'] == 'ovcare') && ($tx_control_data['TreatmentControl']['tx_method'] == 'surgery')) {
+				$previous_surgery_date = is_null($previous_treatment_data)? null: $previous_treatment_data['TreatmentMaster']['start_date'];
+				$surgery_date = $this->data['TreatmentMaster']['start_date'];
+				
+				if(is_null($previous_surgery_date) || ($surgery_date != $previous_surgery_date)) {
+					
+					// Set ovcare_age_at_surgery
+	
+					$participant_model = AppModel::getInstance("Clinicalannotation", "Participant", true);
+					$participant_data = $participant_model->find('first', array('conditions' => array ('Participant.id' => $participant_id), 'recursive' => '-1'));
+					if(empty($participant_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+					$date_of_birth = $participant_data['Participant']['date_of_birth'];
+		
 					$ageInYears = null;
-					AppController::addWarningMsg(__(str_replace('%%field%%', __('age at surgery',true), __('error in the dates definitions: the field [%%field%%] can not be generated', true))));
+					if(!empty($surgery_date) && !empty($date_of_birth)) {
+						$birthDateObj = new DateTime($date_of_birth);
+						$surgDateObj = new DateTime($surgery_date);
+						$interval = $birthDateObj->diff($surgDateObj);
+						$ageInYears = $interval->format('%r%y');
+						if($ageInYears < 0) {
+							$ageInYears = null;
+							AppController::addWarningMsg(str_replace('%%field%%', __('age at surgery',true), __('error in the dates definitions: the field [%%field%%] can not be generated', true)));
+						}
+					}
+					
+					$this->data['TreatmentDetail']['ovcare_age_at_surgery'] = $ageInYears;
 				}
 			}
-			
-			$this->data['TreatmentDetail']['ovcare_age_at_surgery'] = $ageInYears;
 		}
 
 		return true;
