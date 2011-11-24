59e852659ee10e5caea2e10b38b6d31b80e84878
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-11-24 19:32:03 +0000

    Updated to fit 2.4.1

--- frsq/lady_davis/atim2/app/plugins/clinicalannotation/models/customs/participant.php
+++ frsq/lady_davis/atim2/app/plugins/clinicalannotation/models/customs/participant.php
@@ -6,7 +6,7 @@ class ParticipantCustom extends Participant{
 	function summary($variables=array()){
 		$result = parent::summary($variables);
 		
-		$mi_model = AppModel::atimNew("clinicalannotation", "MiscIdentifier", true);
+		$mi_model = AppModel::getInstance("clinicalannotation", "MiscIdentifier", true);
 		$mi_data = $mi_model->find('first', array('conditions' => array('misc_identifier_control_id' => 9, 'participant_id' => $variables['Participant.id']), 'recursive' => -1));
 		
 		$label = array(NULL, empty($mi_data) ? "No id" : $mi_data['MiscIdentifier']['identifier_value']);
