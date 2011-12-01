0c7f402487edebf34ed65f95ba65cea6d32c3191
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-12-01 18:15:20 +0000

    E1909: Restoring clinical annotation default search

--- chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/misc_identifiers_search_format.php
+++
@@ -1,2 +0,0 @@
-<?php
-$this->set( 'atim_menu', $this->Menus->get('/clinicalannotation/misc_identifiers/search/') );
--- chum/notre_dame/app/plugins/clinicalannotation/controllers/hooks/participants_add_format.php
+++
@@ -1,5 +0,0 @@
-<?php
-	
-	$this->set( 'atim_menu', $this->Menus->get('/clinicalannotation/misc_identifiers/search/') );
-		
-?>
