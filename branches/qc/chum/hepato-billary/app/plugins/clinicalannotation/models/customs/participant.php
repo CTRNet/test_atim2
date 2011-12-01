b0d068b39b8940aeb366491949c9e411c31b39cc
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-12-01 22:03:48 +0000

    ClinicalAnnotation.Participant Revision and clean up

--- branches/qc/chum/hepato-billary/app/plugins/clinicalannotation/models/customs/participant.php
+++ branches/qc/chum/hepato-billary/app/plugins/clinicalannotation/models/customs/participant.php
@@ -44,6 +44,30 @@ class ParticipantCustom extends Participant {
 		
 		return $return;
 	}
+	
+	function beforeSave($options) {
+		// Set Data For Trt Update
+		$this->data['HbParticipantFunctionManagement']['is_last_news_date_updated'] = false;
+		
+		if(!empty($this->id)) {
+			// Participant data has just been updated
+			$previous_participant_data = $this->find('first', array('conditions' => array('Participant.id' => $this->id), 'recursive' => '-1'));
+			if(empty($previous_participant_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+			
+			if($this->data['Participant']['last_news_date'] != $previous_participant_data['Participant']['last_news_date']) {
+				$this->data['HbParticipantFunctionManagement']['is_last_news_date_updated'] = true;
+			}
+		}
+		
+		return true;
+	}
+	
+	function afterSave($created) {
+		if($this->data['HbParticipantFunctionManagement']['is_last_news_date_updated']) {
+			$treatment_master_model = AppModel::getInstance("Clinicalannotation", "TreatmentMaster", true);
+			$treatment_master_model->updateAllSurvivalTimes($this->id);
+		}
+	}
 }
 
 ?>
--- branches/qc/chum/hepato-billary/app/plugins/clinicalannotation/models/customs/treatment_master.php
+++ branches/qc/chum/hepato-billary/app/plugins/clinicalannotation/models/customs/treatment_master.php
@@ -25,6 +25,113 @@ class TreatmentMasterCustom extends TreatmentMaster {
 		return $return;
 	}
 	
+	function beforeSave($options) {
+		
+		if(array_key_exists('start_date', $this->data['TreatmentMaster'])) { 
+			// User just clicked on submit button of Treatment form : Treatment is being created or updated
+			
+			$participant_id	= null;		
+			$previous_treatment_data =  array();
+			$tx_control_data = array();	
+			if(array_key_exists('participant_id', $this->data['TreatmentMaster'])) {
+				// Treatment is being created
+				$participant_id	= $this->data['TreatmentMaster']['participant_id'];	
+				$tx_control_model = AppModel::getInstance("Clinicalannotation", "TreatmentControl", true);
+				$tx_control_data = $tx_control_model->find('first', array('conditions' => array ('TreatmentControl.id' => $this->data['TreatmentMaster']['treatment_control_id'])));				
+			} else {
+				// Treatment is being updated
+				$previous_treatment_data = $this->find('first', array('conditions' => array('TreatmentMaster.id' => $this->id), 'recursive' => '0'));
+				if(empty($previous_treatment_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+				$participant_id = $previous_treatment_data['TreatmentMaster']['participant_id'];	
+				$tx_control_data['TreatmentControl'] = $previous_treatment_data['TreatmentControl'];				
+			}
+			if(empty($tx_control_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);	
+
+			if($tx_control_data['TreatmentControl']['tx_method'] == 'surgery') {	
+				
+				// *** SET AGE AT SURGERY ***
+					
+				$previous_surgery_date = empty($previous_treatment_data)? null: $previous_treatment_data['TreatmentMaster']['start_date'];
+				$previous_surgery_date_accuracy = empty($previous_treatment_data)? null: $previous_treatment_data['TreatmentMaster']['start_date_accuracy'];
+				
+				$surgery_date = $this->data['TreatmentMaster']['start_date'];
+				$surgery_date_accuracy = $this->data['TreatmentMaster']['start_date_accuracy'];
+				
+				if(empty($previous_surgery_date) || (($surgery_date.$surgery_date_accuracy) != ($previous_surgery_date.$previous_surgery_date_accuracy))) {
+					$participant_model = AppModel::getInstance("Clinicalannotation", "Participant", true);
+					$participant_data = $participant_model->find('first', array('conditions' => array ('Participant.id' => $participant_id), 'recursive' => '-1'));
+					if(empty($participant_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+					
+					$last_news_date = $participant_data['Participant']['last_news_date'];
+					$last_news_date_accuracy = $participant_data['Participant']['last_news_date_accuracy'];
+	
+					$this->data['TreatmentDetail']['survival_time_in_months'] = $this->getSurvivalTimeInMonth($surgery_date, $surgery_date_accuracy, $last_news_date, $last_news_date_accuracy);
+				}
+			}
+		}
+					
+		return true;
+	}
+	
+	function updateAllSurvivalTimes($participant_id) {
+		$participant_model = AppModel::getInstance("Clinicalannotation", "Participant", true);
+		$participant_data = $participant_model->find('first', array('conditions' => array ('Participant.id' => $participant_id), 'recursive' => '-1'));		
+		if(empty($participant_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+		$last_news_date = $participant_data['Participant']['last_news_date'];
+		$last_news_date_accuracy = $participant_data['Participant']['last_news_date_accuracy'];
+		
+		$conditions =  array (
+			'TreatmentMaster.participant_id' => $participant_id,
+			'TreatmentControl.tx_method' => 'surgery',
+			"TreatmentMaster.start_date != ''",
+			"TreatmentMaster.start_date IS NOT NULL"		
+		);
+		$participant_surgeries = $this->find('all', array('conditions' => $conditions, 'recursive' => '0'));
+
+		foreach($participant_surgeries as $new_trt) {
+			$surgery_id = $new_trt['TreatmentMaster']['id'];
+			$surgery_date = $new_trt['TreatmentMaster']['start_date'];
+			$surgery_date_accuracy = $new_trt['TreatmentMaster']['start_date_accuracy'];
+
+			$new_tr_data = array();
+			$new_tr_data['TreatmentMaster']['id'] = $surgery_id;
+			$new_tr_data['TreatmentDetail']['survival_time_in_months'] = $this->getSurvivalTimeInMonth($surgery_date, $surgery_date_accuracy, $last_news_date, $last_news_date_accuracy);
+			$this->id = null;
+			$this->data = array();
+			if(!$this->save($new_tr_data, false)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+		}
+	}
+	
+	
+	function getSurvivalTimeInMonth($surgery_date, $surgery_date_accuracy, $last_news_date, $last_news_date_accuracy) {
+		if(empty($surgery_date) || empty($last_news_date)) return '';
+		
+		if(in_array($surgery_date_accuracy, array('y','m')) || in_array($last_news_date_accuracy, array('y','m'))) {
+			AppController::addWarningMsg(str_replace('%%field%%', __('survival time in months',true), __('the dates accuracy is not sufficient: the field [%%field%%] can not be generated', true)));
+			return '';
+		}
+		
+		$arr_surgery_date = array_combine(array('year','month','day'), explode('-', $surgery_date));
+		$arr_last_news_date = array_combine(array('year','month','day'), explode('-', $last_news_date));
+				
+		$survival_time_months = ($arr_last_news_date['year'] - $arr_surgery_date['year']) * 12;
+		if($survival_time_months >= 0) {
+			$survival_time_months = $survival_time_months + ($arr_last_news_date['month'] - $arr_surgery_date['month']);
+			if(in_array($surgery_date_accuracy, array('c')) && in_array($last_news_date_accuracy, array('c'))) {
+				if($survival_time_months >= 0) {
+					if($arr_surgery_date['day'] > $arr_last_news_date['day']) { $survival_time_months = $survival_time_months - 1; }
+				}
+			}
+		}
+		
+		if($survival_time_months < 0) {
+			AppController::addWarningMsg(str_replace('%%field%%', __('survival time in months',true), __('error in the dates definitions: the field [%%field%%] can not be generated', true)));
+			$survival_time_months = '';			
+		}
+		
+		return $survival_time_months;	
+	}
+
 }
 
 ?>
