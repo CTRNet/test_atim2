94488dda18eed3a0adce7803c946f9983d7160e1
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-12-07 19:20:39 +0000

    Adding custom SQL files for CCBR.

---
+++ ccbr/scripts/ccbr_scripts/ccbr_v0.6_custom.sql
@@ -0,0 +1,9 @@
+-- CCBR Customization Script
+-- Version: v0.6
+-- ATiM Version: v2.4.1
+
+
+-- Fix for v2.4.1 upgrade
+-- You need to alter your existing treatment details table. 
+-- The field "tx_master_id" should now be renamed to "treatment_master_id".
+
