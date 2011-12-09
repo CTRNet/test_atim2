abe922cf79061268b598b5d76126b5d40551aac3
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-12-09 18:37:01 +0000

    queries for the next visit

---
+++ branches/qc/frsq/lady_davis/atim2/scripts/v2.4.0/custom3.sql
@@ -0,0 +1,8 @@
+-- run whenever possible and regenerate translation files
+REPLACE INTO i18n (id, page_id, en, fr) VALUES
+('rack 1-12', 'global', 'Rack 1-12', 'Étagère 1-12'),
+('rack 4x4', 'global', 'Rack 4x4', 'Étagère 4x4'),
+('freezer 4x5', 'global', 'Freezer 4x5', 'Congélateur 4x5'),
+('freezer 6x5', 'global', 'Freezer 6x5', 'Congélateur 6x5'),
+('freezer vertical 4x3', 'global', 'Vertical freezer 4x3', 'Congélateur vertical 4x3'),
+('box100 1-100', '', 'Box100 1-100', 'Boîte100 1-100');
