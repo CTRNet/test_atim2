0650c84421079e40dff2fe52cff41002c5f78a24
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-12-12 18:30:28 +0000

    Fixing query + adding translation

--- scripts/tmp_atim_v2.5.0_upgrade.sql
+++ scripts/tmp_atim_v2.5.0_upgrade.sql
@@ -1,5 +1,6 @@
-INSERT INTO `versions` (version_number, date_installed, build_number, created, created_by, modified, modified_by) 
-VALUES('2.5.0', NOW(), '> 3857', NOW(), 1, NOW(), 1);
+INSERT INTO `versions` (version_number, date_installed, build_number) 
+VALUES('2.5.0', NOW(), '> 3857');
 
 REPLACE INTO i18n (id, en, fr) VALUES
-('reserved for study', 'Reserved For Study/Project', 'Réservé pour une Étude/Projet');
+('reserved for study', 'Reserved For Study/Project', 'Réservé pour une Étude/Projet'),
+('identifier name', 'Identifier Name', "Nom d'identifiant");
