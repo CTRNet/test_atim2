3a14121949fb3084c0247849013b105c79e5076b
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-12-12 16:12:05 +0000

    MERGED with tag v2.4.2 (3874:3974)

--- trunk/app/controllers/users_controller.php
+++ trunk/app/controllers/users_controller.php
@@ -22,14 +22,17 @@ class UsersController extends AppController {
 		$this->Version->id = $version_data[0]['id'];
 		$this->Version->read();
 		if($this->Version->data['Version']['permissions_regenerated'] == 0){
-			$version_number = $this->Version->data['Version']['version_number'];
 			$this->PermissionManager->buildAcl();
 			AppController::addWarningMsg(__('permissions have been regenerated', true));
 			$this->Version->data = array('Version' => array('permissions_regenerated' => 1));
 			$this->Version->save();
-
-			//also update the i18n string
+		}
+		if($this->Version->data['Version']['version_number'] != __('core_app_version', true)){
+			//update the i18n string
+			$version_number = $this->Version->data['Version']['version_number'];
 			$this->User->query("UPDATE i18n SET en='".$version_number."', fr ='".$version_number."' WHERE id='core_app_version'");
+			
+			AppController::addWarningMsg('The language files need to be regenerated. (Invalid translation for core_app_version.)');	
 		}
 		
 		if($this->Auth->user()){
--- trunk/scripts/tmp_atim_v2.5.0_upgrade.sql
+++ trunk/scripts/tmp_atim_v2.5.0_upgrade.sql
@@ -1,2 +1,5 @@
 INSERT INTO `versions` (version_number, date_installed, build_number, created, created_by, modified, modified_by) 
 VALUES('2.5.0', NOW(), '> 3857', NOW(), 1, NOW(), 1);
+
+REPLACE INTO i18n (id, en, fr) VALUES
+('reserved for study', 'Reserved For Study/Project', 'Réservé pour une Étude/Projet');
