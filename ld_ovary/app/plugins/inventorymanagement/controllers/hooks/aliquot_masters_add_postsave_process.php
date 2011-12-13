60324adb9ddee603f98f1fea275a482a49638974
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-12-13 21:45:38 +0000

    tmp

--- ld_ovary/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_postsave_process.php
+++ ld_ovary/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_add_postsave_process.php
@@ -2,11 +2,11 @@
 
 	//Set aliquot master barcode
 	$aliquots_to_update = $this->AliquotMaster->find('all', array('conditions' => array('AliquotMaster.barcode' => ''), 'recursive' => '-1'));
-	foreach($aliquots_to_update as $new_al) {
-		$this->AliquotMaster->data = array();
-		$this->AliquotMaster->id = $new_al['AliquotMaster']['id'];
-		if(!$this->AliquotMaster->save(array('AliquotMaster' => array('barcode' => $new_al['AliquotMaster']['id'])), false)) { 
-			$this->redirect('/pages/err_plugin_record_err?method='.__METHOD__.',line='.__LINE__, null, true); 
+	foreach($aliquots_to_update as $new_al) {	
+		$query_to_update = "UPDATE aliquot_masters SET aliquot_masters.barcode = aliquot_masters.id WHERE aliquot_masters.id = ".$new_al['AliquotMaster']['id'].";";
+		if(!$this->AliquotMaster->query($query_to_update) 
+		|| !$this->AliquotMaster->query(str_replace("aliquot_masters", "aliquot_masters_revs", $query_to_update))) {
+			$this->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true); 
 		}
 	}
 	
