a7698a1f06af1302bf56f7885dcc019f17571549
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2011-12-19 14:49:09 +0000

    MERGED WITH Tag v2.4.2 : rev 3931->3973

--- OvCaRe/app/plugins/clinicalannotation/models/treatment_master.php
+++ OvCaRe/app/plugins/clinicalannotation/models/treatment_master.php
@@ -49,7 +49,7 @@ class TreatmentMaster extends ClinicalannotationAppModel {
 		}
 		if(!empty($data['TreatmentControl']['extend_tablename'])) {
 			$treatment_extend_model = new TreatmentExtend( false, $data['TreatmentControl']['extend_tablename']);
-			$nbr_extends = $treatment_extend_model->find('count', array('conditions'=>array('TreatmentExtend.tx_master_id'=>$tx_master_id), 'recursive' => '-1'));
+			$nbr_extends = $treatment_extend_model->find('count', array('conditions'=>array('TreatmentExtend.treatment_master_id'=>$tx_master_id), 'recursive' => '-1'));
 			if ($nbr_extends > 0) { 
 				return array('allow_deletion' => false, 'msg' => 'at least one drug is defined as treatment component'); 
 			}
