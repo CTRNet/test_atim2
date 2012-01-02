50720f385ff5eea219cda7a479bcc99c5441c5da
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-01-02 16:53:44 +0000

    Last modif before code freeze

--- branches/qc/ld_ovary/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_realiquot_initial_display.php
+++ branches/qc/ld_ovary/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_realiquot_initial_display.php
@@ -3,8 +3,13 @@
  	// --------------------------------------------------------------------------------
 	// Set Default Aliquot Data
 	// -------------------------------------------------------------------------------- 	
-	foreach($this->data as $key => $new_parent) {
-		$this->data[$key]['children'][0]['AliquotMaster']['aliquot_label'] = $new_parent['parent']['AliquotMaster']['aliquot_label'];
+	$custom_override_data = array();	
+	foreach($this->data as &$new_data_set) {
+		$tmp = array();
+		$tmp['AliquotMaster.aliquot_label'] = $new_data_set['parent']['AliquotMaster']['aliquot_label'];
+			
+		$custom_override_data[$new_data_set['parent']['AliquotMaster']['id']] = $tmp;
 	}
+	$this->set('custom_override_data', $custom_override_data);
 	
 ?>
