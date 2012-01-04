80a3a47b04bdea3e96b8e4f69969daec7d12a42d
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-01-04 19:11:41 +0000

    VERSION INSTALLED ON JGH SERVER ON 2012-01-04

--- ld_lymph/app/plugins/clinicalannotation/models/customs/event_master.php
+++ ld_lymph/app/plugins/clinicalannotation/models/customs/event_master.php
@@ -5,6 +5,76 @@ class EventMasterCustom extends EventMaster {
 	var $useTable = 'event_masters';	
 	var $name = 'EventMaster';	
 	
+	
+	
+	function beforeSave($options) {
+		$scan_data = array_key_exists('pe_imag_lymph_node_waldeyers_ring', $this->data['EventDetail'])? $this->data['EventDetail'] : null;
+		
+		if(!empty($scan_data)) {
+			
+			$nodal_sites_defintion = array(
+				'pe_imag_lymph_node_waldeyers_ring' => '1',
+		        
+				'pe_imag_lymph_node_pre_auricular_left' => '2',
+				'pe_imag_lymph_node_upper_cervical_left' => '2',
+				'pe_imag_lymph_node_median_lower_cervical_left' => '2',
+				'pe_imag_lymph_node_posterior_cervical_left' => '2',
+				'pe_imag_lymph_node_supraclavicular_left' => '2',
+				'pe_imag_lymph_node_infraclavicular_left' => '2',
+	            
+				'pe_imag_lymph_node_pre_auricular_right' => '3',
+				'pe_imag_lymph_node_upper_cervical_right' => '3',
+				'pe_imag_lymph_node_median_lower_cervical_right' => '3',
+				'pe_imag_lymph_node_posterior_cervical_right' => '3',
+				'pe_imag_lymph_node_supraclavicular_right' => '3',
+				'pe_imag_lymph_node_infraclavicular_right' => '3',
+	            
+				'pe_imag_lymph_node_paratracheal_left' => '4',
+				'pe_imag_lymph_node_paratracheal_right' => '4',
+				'pe_imag_lymph_node_mediastinal' => '4',
+				'pe_imag_lymph_node_hilar_left' => '4',
+				'pe_imag_lymph_node_hilar_right' => '4',
+				'pe_imag_lymph_node_retrocrural' => '4',
+	            
+				'pe_imag_lymph_node_axillary_left' => '5',
+	            
+				'pe_imag_lymph_node_axillary_right' => '6',
+	            
+				//'pe_imag_lymph_node_spleen' => '7',
+	            
+				'pe_imag_lymph_node_ceuac' => '8',
+				'pe_imag_lymph_node_splenic_hepatic_hilar' => '8',
+				'pe_imag_lymph_node_portal' => '8',
+				'pe_imag_lymph_node_mesenteric' => '8',
+	            
+				'pe_imag_lymph_node_para_aortic_left' => '9',
+				'pe_imag_lymph_node_para_aortic_right' => '9',
+				'pe_imag_lymph_node_common_illiac_left' => '9',
+				'pe_imag_lymph_node_common_illiac_right' => '9',
+				'pe_imag_lymph_node_external_illiac_left' => '9',
+				'pe_imag_lymph_node_external_illiac_right' => '9',
+	            
+				'pe_imag_lymph_node_inguinal_left' => '10',
+				'pe_imag_lymph_node_femoral_left' => '10',
+	            
+				'pe_imag_lymph_node_inguinal_right' => '11',
+				'pe_imag_lymph_node_femoral_right' => '11',
+	            
+				'pe_imag_lymph_node_epitrochlear' => '12',
+				'pe_imag_lymph_node_popliteral' => '12',
+				'pe_imag_lymph_node_other' => '12'
+			);			
+			
+			$tumoral_nodal_sites = array();
+			foreach($scan_data as $node => $value) {
+				if(isset($nodal_sites_defintion[$node]) && ($value == 'y')) $tumoral_nodal_sites[$nodal_sites_defintion[$node]] = $nodal_sites_defintion[$node];
+			}
+			$this->data['EventDetail']['nodal_sites_nbr'] = sizeof($tumoral_nodal_sites);
+		}
+		
+		return true;
+	}
+		
 	function summary( $variables=array() ) {
 		$return = false;
 	
