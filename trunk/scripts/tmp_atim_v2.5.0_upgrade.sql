c902cb66ac617f5d34a2db78855b09c9b4d2be99
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-01-12 18:58:48 +0000

    E1719: CakePHP 2 final commit

--- trunk/scripts/tmp_atim_v2.5.0_upgrade.sql
+++ trunk/scripts/tmp_atim_v2.5.0_upgrade.sql
@@ -1,5 +1,5 @@
 INSERT INTO `versions` (version_number, date_installed, build_number) 
-VALUES('2.5.0', NOW(),'> 3857');
+VALUES('2.5.0', NOW(),'> 4043');
 
 REPLACE INTO i18n (id, en, fr) VALUES
 ('reserved for study','Reserved For Study/Project','Réservé pour une Étude/Projet'),
