32100d6fa4d4e98b9b189cf8943701ae797dffc3
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-01-17 02:31:58 +0000

    Reordered logic

--- trunk/lib/Cake/Routing/Dispatcher.php
+++ trunk/lib/Cake/Routing/Dispatcher.php
@@ -163,16 +163,15 @@ class Dispatcher {
 		}
 		
 		//ATIM start-------
-		if(!class_exists($ctrlClass.'Custom')){
-			$file = APP . 'Plugin' . DS . $request->params['plugin'] . DS . 'Controller' . DS . 'Custom' . DS . $request->params['controller'].'Controller.php';
-			if(file_exists($file)){
-				require_once($file);
-			}
-		}
 		if(class_exists($ctrlClass.'Custom')){
 			$reflection_custom = new ReflectionClass($ctrlClass.'Custom');
 			if (!$reflection_custom->isAbstract() && !$reflection_custom->isInterface()){
 				return $reflection_custom->newInstance($request, $response);
+			}
+		} else {
+			$file = APP . 'Plugin' . DS . $request->params['plugin'] . DS . 'Controller' . DS . 'Custom' . DS . $request->params['controller'].'Controller.php';
+			if(file_exists($file)){
+				require_once($file);
 			}
 		}
 		//ATIM end--------
