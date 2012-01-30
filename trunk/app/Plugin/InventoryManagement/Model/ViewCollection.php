aea5c6e3d07a4e99861407b0433ea346e67c2eb1
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-01-30 15:04:54 +0000

    E1910: Added link to reach the CCL when there is not consent

--- trunk/app/Plugin/InventoryManagement/Model/ViewCollection.php
+++ trunk/app/Plugin/InventoryManagement/Model/ViewCollection.php
@@ -20,8 +20,14 @@ class ViewCollection extends InventoryManagementAppModel {
 			
 			$consent_status = $this->getUnconsentedParticipantCollections(array('data' => $collection_data));
 			if(!empty($consent_status)){
-				if($consent_status[$variables['Collection.id']] == null){
-					AppController::addWarningMsg(__('no consent is linked to the current participant collection'));
+				if(!$collection_data['ViewCollection']['participant_id']){
+					AppController::addWarningMsg(__('no participant is linked to the current participant collection'));
+				}else if($consent_status[$variables['Collection.id']] == null){
+					$link = '';
+					if(AppController::checkLinkPermission('/ClinicalAnnotation/ClinicalCollectionLinks/detail/')){
+						$link = sprintf(' <a href="%sClinicalAnnotation/ClinicalCollectionLinks/detail/%d/%d">%s</a>', AppController::getInstance()->request->webroot, $collection_data['ViewCollection']['participant_id'], $collection_data['ViewCollection']['collection_id'], __('click here to access it'));
+					}
+					AppController::addWarningMsg(__('no consent is linked to the current participant collection').'.'.$link);
 				}else{
 					AppController::addWarningMsg(__('the linked consent status is [%s]', __($consent_status[$variables['Collection.id']])));
 				}
