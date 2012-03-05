471ccbe7d72fc188e61c7d56ea930b183602cefa
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-03-05 16:46:37 +0000

    Removed default today dates

--- branches/qc/chum/notre_dame/app/plugins/inventorymanagement/views/aliquot_masters/hooks/realiquot.php
+++ branches/qc/chum/notre_dame/app/plugins/inventorymanagement/views/aliquot_masters/hooks/realiquot.php
@@ -4,5 +4,6 @@
 	// Prevent the paste operation on aliquot label 
 	// -------------------------------------------------------------------------------- 
 	$options_children['settings']['paste_disabled_fields'] = array('AliquotMaster.aliquot_label');	
-	
-?>
+
+unset($created_aliquot_override_data['Realiquoting.realiquoting_datetime']);
+unset($created_aliquot_override_data['AliquotMaster.storage_datetime']);
