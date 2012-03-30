7b8dd6374be7b346e510c99f2395c3f6f13839d4
mnatividad <mnatividad@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-03-30 21:28:17 +0000

    changed table name

--- branches/mb/mtb_v2_3_6/app/plugins/codingicd/models/coding_icd9.php
+++ branches/mb/mtb_v2_3_6/app/plugins/codingicd/models/coding_icd9.php
@@ -5,7 +5,7 @@ class CodingIcd9 extends CodingicdAppModel{
 	protected static $singleton = null;
 	
     var $name = 'CodingIcd9';
-	var $useTable = 'coding_icd9';
+	var $useTable = 'coding_icd9_v2';
 
 	var $validate = array();
 
