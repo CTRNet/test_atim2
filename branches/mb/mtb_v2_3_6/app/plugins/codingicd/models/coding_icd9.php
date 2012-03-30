db0236547b9bd70d744c25de00e0800e1df96059
mnatividad <mnatividad@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-03-30 18:47:17 +0000

    testing coding icd9

---
+++ branches/mb/mtb_v2_3_6/app/plugins/codingicd/models/coding_icd9.php
@@ -0,0 +1,30 @@
+<?php
+
+class CodingIcd9 extends CodingicdAppModel{
+
+	protected static $singleton = null;
+	
+    var $name = 'CodingIcd9';
+	var $useTable = 'coding_icd9';
+
+	var $validate = array();
+
+	function __construct(){
+		parent::__construct();
+		self::$singleton = $this;
+	}
+	
+	static function validateId($id){
+		return self::$singleton->globalValidateId($id);
+	}
+	
+	static function getInstance(){
+		return self::$singleton;
+	}
+	
+	function globalSearch($terms, $exact_search){
+		return parent::globalSearch($terms, $exact_search, array("_title", "_sub_title", "_description"), true, null);
+	}
+}
+
+?>
