f9f3ef41c536a001c5bce286c77cf79cf96a6882
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-05-01 20:50:51 +0000

    E2129: Adding hooks to main menu to allow custom messages

--- trunk/app/View/Helper/StructuresHelper.php
+++ trunk/app/View/Helper/StructuresHelper.php
@@ -193,7 +193,7 @@ class StructuresHelper extends Helper {
 			$hook_extension = '_'.$hook_extension;
 		}
 		
-		$hook_file = APP . 'Plugin' . DS . $this->params['plugin'] . DS . 'View' . DS . $this->params['controller'] . DS . 'hooks' . DS . $this->params['action'].$hook_extension.'.php';
+		$hook_file = APP . ($this->params['plugin'] ? 'Plugin' . DS . $this->params['plugin'] . DS : '') . 'View' . DS . $this->params['controller'] . DS . 'hooks' . DS . $this->params['action'].$hook_extension.'.php';
 		if(!file_exists($hook_file)){
 			$hook_file=false;
 		}
