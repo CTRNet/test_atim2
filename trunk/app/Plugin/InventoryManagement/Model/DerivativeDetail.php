32e7efbde0e69ce15cc1928c7260b4439b78e3ee
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-05-14 18:51:49 +0000

    E1988: Fixing time calculation for samples

--- trunk/app/Plugin/InventoryManagement/Model/DerivativeDetail.php
+++ trunk/app/Plugin/InventoryManagement/Model/DerivativeDetail.php
@@ -2,4 +2,9 @@
 
 class DerivativeDetail extends InventoryManagementAppModel {
 	var $primaryKey = 'sample_master_id';
+	
+	var $registered_view = array(
+			'InventoryManagement.ViewSample' => array('sample_master_id', 'parent_sample_id', 'initial_specimen_sample_id'),
+			'InventoryManagement.ViewAliquot' => array('sample_master_id')
+	);
 }
