32b7db7f9b61461a44f16a559be05172ddec8084
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-05-17 15:15:58 +0000

    Comments regarding class goal

--- app/Model/Behavior/MinMaxBehavior.php
+++ app/Model/Behavior/MinMaxBehavior.php
@@ -1,10 +1,19 @@
 <?php
+/**
+ * This behavior is used to define a minimum on certain view fields when a max is defined.
+ * It's usefull for spent time calculation because spentTime >= 0 means the value is
+ * valid, but spentTime < 0 is an error code. When searching with only a max criterion
+ * <= x, results < 0 (errors) must not be returned. This behavior is here to add the 
+ * minimum >= 0 clause if the minimum is not specified.
+ *
+ */
 class MinMaxBehavior extends ModelBehavior {
 	
 	function beforeFind(Model $model, $query){
 		if(isset($query['conditions'])){
-			$to_fix = array();
+			$to_fix = array();//contains the model -> fields to fix.
 			if(isset($model->registered_view)){
+				//if views are attached, parse each of them
 				foreach($model->registered_view as $plugin_model => $foo){
 					list($plugin, $model_name) = explode('.', $plugin_model);
 					$registered_model = AppModel::getInstance($plugin, $model_name);
@@ -13,6 +22,7 @@ class MinMaxBehavior extends ModelBehavior {
 					}
 				}
 			}else if(isset($model::$min_value_fields)){
+				//is a view itself
 				$to_fix[$model->name] = $model::$min_value_fields;
 			}
 			
