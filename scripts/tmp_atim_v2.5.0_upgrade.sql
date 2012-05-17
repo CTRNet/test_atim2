130cbe066e4876f93e457023b0c29d504c2b95dd
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-05-17 19:13:38 +0000

    translations

--- scripts/tmp_atim_v2.5.0_upgrade.sql
+++ scripts/tmp_atim_v2.5.0_upgrade.sql
@@ -9,6 +9,7 @@ REPLACE INTO i18n (id, en, fr) VALUES
 ("supported value", "Supported value", "Valeur supportée"),
 ("some identifiers were not merge because they were conflicting",
  "Some identifiers were not merge because they were conflicting.",
+ "Certains identifiants n'ont pas été fusionnés car ils étaient conflictuels."),
 ('the collection date is after the storage date','The storage date is earlier than the collection date.',"La date d'entreposage est anterieure à la date de collection."),
 ('the creation date is after the storage date','The storage date is earlier than the creation date.',"La date d'entreposage est anterieure à la date de création."),
 ('the reception date is after the storage date','The storage date is earlier than the reception date.',"La date d'entreposage est anterieure à la date de réception."),
@@ -2108,6 +2109,18 @@ UPDATE structure_formats SET `flag_editgrid_readonly`='0' WHERE structure_id=(SE
 UPDATE structure_formats SET `flag_override_setting`='0', `setting`='', `flag_search`='0' WHERE structure_id=(SELECT id FROM structures WHERE alias='derivatives') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ViewSample' AND `tablename`='view_samples' AND `field`='coll_to_creation_spent_time_msg' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
 INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
 ((SELECT id FROM structures WHERE alias='derivatives'), (SELECT id FROM structure_fields WHERE `model`='ViewSample' AND `tablename`='view_samples' AND `field`='coll_to_creation_spent_time_msg' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0'), '1', '400', '', '1', 'collection to creation spent time (min)', '0', '', '1', '', '1', 'integer_positive', '0', '', '0', '', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
+
+UPDATE aliquot_controls SET form_alias=SUBSTR(form_alias, 17) WHERE form_alias LIKE 'aliquot_masters,%';
+UPDATE consent_controls SET form_alias=SUBSTR(form_alias, 17) WHERE form_alias LIKE 'consent_masters,%';
+UPDATE diagnosis_controls SET form_alias=SUBSTR(form_alias, 18) WHERE form_alias LIKE 'diagnosismasters,%';
+UPDATE event_controls SET form_alias=SUBSTR(form_alias, 14) WHERE form_alias LIKE 'eventmasters,%';
+UPDATE lab_book_controls SET form_alias=SUBSTR(form_alias, 17) WHERE form_alias LIKE 'labbookmasters,%';
+UPDATE sample_controls SET form_alias=SUBSTR(form_alias, 16) WHERE form_alias LIKE 'sample_masters,%';
+UPDATE sop_controls SET form_alias=SUBSTR(form_alias, 12) WHERE form_alias LIKE 'sopmasters,%';
+UPDATE storage_controls SET form_alias=SUBSTR(form_alias, 16) WHERE form_alias LIKE 'storagemasters,%';
+UPDATE storage_controls SET form_alias=SUBSTR(form_alias, 15) WHERE form_alias LIKE 'storagemasters%';
+UPDATE treatment_controls SET form_alias=SUBSTR(form_alias, 18) WHERE form_alias LIKE 'treatmentmasters,%';
+
 
 
 
