88d2b98d31cb7564a666413fcfea86c7302f27fd
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-05-30 19:06:01 +0000

    Custom model validation for misc identifiers.

---
+++ branches/ab/npttb/app/plugins/clinicalannotation/models/customs/misc_identifier.php
@@ -0,0 +1,92 @@
+<?php
+// Validations for Calgary brain bank misc identifiers
+
+class MiscIdentifierCustom extends MiscIdentifier {
+	var $name = 'MiscIdentifier';
+	var $useTable = 'misc_identifiers';
+
+	var $belongsTo = array(
+		'Participant' => array(
+			'className' => 'Clinicalannotation.Participant',
+			'foreignKey' => 'participant_id'),
+		'MiscIdentifierControl' => array(
+			'className' => 'Clinicalannotation.MiscIdentifierControl',
+			'foreignKey' => 'misc_identifier_control_id'
+		)
+	);
+
+	function beforeSave($options) {
+		
+	/* 
+		Check identifier type and apply validation rules.	
+	*/
+
+		// Get control ID for current identifier
+		$previousMiscIdData = null;
+		$miscIdentifierControlId = array_key_exists('misc_identifier_control_id', $this->data['MiscIdentifier'])? $this->data['MiscIdentifier']['misc_identifier_control_id'] : null;
+		
+		$previousMiscIdData = null;
+			
+		if(is_null($miscIdentifierControlId)) {
+
+			$previousMiscIdData = $this->find('first', array('conditions' => array ('MiscIdentifier.id' => $this->id), 'recursive' => '-1'));
+			if(empty($previousMiscIdData)) {
+				AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+			}
+			$miscIdentifierControlId = $previousMiscIdData['MiscIdentifier']['misc_identifier_control_id'];	
+		}	
+		
+		// Get control data for current identifier
+		$miscIdentControlData = array();
+		$miscIdentControlModel = AppModel::getInstance("Clinicalannotation", "MiscIdentifierControl", true);
+		$miscIdentControlData = $miscIdentControlModel->find('first', array('conditions' => array ('MiscIdentifierControl.id' => $miscIdentifierControlId)));
+			
+		if (empty($miscIdentControlData)) {
+				AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+		}	
+
+		// Execute appropriate validation depending on identifier name
+		$value_validated = false;
+		$identifierValue = null;
+
+		$deleted = array_key_exists('deleted', $this->data['MiscIdentifier'])? $this->data['MiscIdentifier']['deleted'] : null;
+			
+		if ($deleted) {
+			$identifierValue = $previousMiscIdData['MiscIdentifier']['identifier_value'];
+		} else {
+			$identifierValue = $this->data['MiscIdentifier']['identifier_value'];
+		}		
+		
+		switch ($miscIdentControlData['MiscIdentifierControl']['misc_identifier_name']) {
+			case "Pathology Case Number":
+				// Validate: SF 00-00000
+				// 	Two letters usual possibilities are SF, SA, AA, BM
+				// 	First two zeros indicate year of procedure
+				// 	Five zeros after the hyphen indicate number assigned by hospital usually a chronological logging of procedures for the year
+				if (preg_match("^\A\w{2}\s{1}\d{2}(-)\d{5}$^", $identifierValue)) {
+					$value_validated = true;
+				} else {
+					$this->validationErrors['MiscIdentifier']['identifier_value'] = "npbttb Pathology Case Number validation error";
+					$value_validated = false;
+				}
+				break;
+				
+			case "TTB Number":
+				// Validate: 3 digit number
+				if (preg_match("^\A\d{3}$^", $identifierValue)) {
+					$value_validated = true;
+				} else {
+					$this->validationErrors['MiscIdentifier']['identifier_value'] = "npbttb TTB validation error";
+					$value_validated = false;
+				}				
+				break;
+				
+			default:
+				echo "DEFAULT";
+				AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+		}
+		
+		return $value_validated;
+	}
+}
+?>
