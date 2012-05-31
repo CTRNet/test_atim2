b2fc6fd04a83a4974b9d782035fb83182f46a04b
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-05-31 18:10:17 +0000

    Update database file with npttb bank info

--- branches/ab/npttb/app/config/database.php
+++ branches/ab/npttb/app/config/database.php
@@ -82,9 +82,9 @@ class DATABASE_CONFIG {
 		'driver' => 'mysql',
 		'persistent' => true,
 		'host' => 'localhost',
-		'login' => 'root',
-		'password' => 'root',
-		'database' => 'atim_new',
+		'login' => 'ctrnadmi_bttb',
+		'password' => '1Qaz1Qaz',
+		'database' => 'ctrnadmi_bttb',
 		'prefix' => '',
 		'encoding' => 'utf8'
 	);
