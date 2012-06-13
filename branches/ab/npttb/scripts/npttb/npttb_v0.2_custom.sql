855c2d4a30a555593fc42fdb6860f5a3eb26ac97
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-06-13 17:58:17 +0000

    Start of v0.2 dev changes.

--- branches/ab/npttb/scripts/npttb/npttb_v0.2_custom.sql
+++ branches/ab/npttb/scripts/npttb/npttb_v0.2_custom.sql
@@ -2,5 +2,26 @@
 -- Version: v0.2
 -- ATiM Version: v2.4.3A
 
--- This script must be run against a v2.4.3A ATiM database with the previous dev script applied.
+-- This script must be run against a v2.4.3A ATiM database with the previous dev script applied (v0.1)
+
+-- Update bank name for version tracking during customization
+REPLACE INTO `i18n` (`id`, `en`, `fr`)
+	VALUES ('core_installname', 'Clark H. Smith NPTTB - v0.2 DEV', '');
+
+/*
+	------------------------------------------------------------
+	Eventum ID: 2282 - Misc Identifier - Medical Record Number
+	------------------------------------------------------------
+*/
+
+-- Create new control value for MRN number
+
+REPLACE INTO `i18n` (`id`, `en`, `fr`) VALUES
+	('Medical Record Number', 'Medical Record Number', ''),
+	('TTB Number', 'TTB Number', ''),
+	('npbttb MRN validation error', 'Medical Record Number invalid. Must be 11 or 12 digits.', '');
+
+INSERT INTO `misc_identifier_controls` (`misc_identifier_name`, `flag_active`, `display_order`, `flag_once_per_participant`, `flag_confidential`) VALUES ('Medical Record Number', 1, 3, 1, 0);
+
+
 
