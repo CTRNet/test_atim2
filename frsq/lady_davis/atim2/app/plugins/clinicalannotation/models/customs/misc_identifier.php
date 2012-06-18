d0c86cc14cb521a29a2fcdb6fab538fff30d3263
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-06-18 14:39:08 +0000

    New identifiers with validations

--- frsq/lady_davis/atim2/app/plugins/clinicalannotation/models/customs/misc_identifier.php
+++ frsq/lady_davis/atim2/app/plugins/clinicalannotation/models/customs/misc_identifier.php
@@ -18,4 +18,28 @@ class MiscIdentifierCustom extends MiscIdentifier{
 		}
 		return $result;
 	}
+	
+	function validates($options = array()){
+		$errors = parent::validates($options);
+		if(isset($this->validationErrors['identifier_value']) && !is_array($this->validationErrors['identifier_value'])){
+			$this->validationErrors['identifier_value'] = array($this->validationErrors['identifier_value']);
+		}
+		$control_id = null;
+		if($this->id){
+			$current = $this->findById($this->id);
+			$control_id = $current['MiscIdentifier']['misc_identifier_control_id']; 
+		}else{
+			$control_id = $this->data['MiscIdentifier']['misc_identifier_control_id'];
+		}
+		if($control_id == 10 && !preg_match("#^NEO-[\d]+$#", $this->data['MiscIdentifier']['identifier_value'])){
+			//NEO-xx
+			$this->validationErrors['identifier_value'][] = sprintf(__('the identifier expected format is %s', true), 'NEO-#');
+			return false;
+		}else if($control_id == 11 && !preg_match("#^MET-[\d]+$#", $this->data['MiscIdentifier']['identifier_value'])){
+			//MET-xx
+			$this->validationErrors['identifier_value'][] = sprintf(__('the identifier expected format is %s', true), 'MET-#');
+			return false;
+		}
+		return $errors;
+	}
 }
