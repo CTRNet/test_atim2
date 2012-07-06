5d4b37ea552161c45a8f84167e56c352d3777da3
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-07-06 19:34:40 +0000

    procure

---
+++ branches/qc/procure/app/Plugin/Administrate/Model/Bank.php
@@ -0,0 +1,48 @@
+<?php
+
+class Bank extends AdministrateAppModel {
+	
+	var $registered_view = array(
+			'InventoryManagement.ViewCollection' => array('bank_id'),
+			'InventoryManagement.ViewSample' => array('bank_id'),
+			'InventoryManagement.ViewAliquot' => array('bank_id')
+	);
+	
+	function summary( $variables=array() ) {
+		$return = false;
+		if ( isset($variables['Bank.id']) ) {
+			$result = $this->find('first', array('conditions'=>array('Bank.id'=>$variables['Bank.id'])));
+			
+			$return = array(
+				'menu'			=>	array( NULL, $result['Bank']['name'] ),
+				'title'			=>	array( NULL, $result['Bank']['name'] ),
+				'data'			=> $result,
+				'structure alias'=>'banks'
+			);
+		}
+		return $return;
+	}
+	
+ 	/**
+	 * Get permissible values array gathering all existing banks.
+	 *
+	 * @author N. Luc
+	 * @since 2010-05-26
+	 * @updated N. Luc
+	 */  		
+	function getBankPermissibleValues(){
+		$result = array();
+		foreach($this->find('all', array('order' => 'Bank.name ASC')) as $bank){
+			$result[$bank["Bank"]["id"]] = $bank["Bank"]["name"];
+		}
+		return $result;
+	}
+	
+	function isBeingUsed($bank_id){
+		$this->Group = AppModel::getInstance("Administrate", "Group", true);
+		$data = $this->Group->find('first', array('conditions' => array('Group.bank_id' => $bank_id)));
+		return !empty($data);
+	}
+}
+
+?>
---
+++ branches/qc/procure/app/Plugin/InventoryManagement/Model/SpecimenDetail.php
@@ -0,0 +1,10 @@
+<?php
+
+class SpecimenDetail extends InventoryManagementAppModel {
+	var $primaryKey = 'sample_master_id';
+
+	var $registered_view = array(
+			'InventoryManagement.ViewSample' => array('sample_master_id', 'parent_sample_id', 'initial_specimen_sample_id'),
+			'InventoryManagement.ViewAliquot' => array('sample_master_id')
+	);
+}
