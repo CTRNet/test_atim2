4ad17bb53b4e0f21209c7325078672a87773d4c6
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-07-06 14:10:17 +0000

    delete scripts v2.4.0

--- branches/qc/terry_fox_cpcbn/scripts/v2.4.0/custom_post241.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE qc_tf_txd_hormonotherapies
- DROP FOREIGN KEY qc_tf_txd_hormonotherapies_ibfk_1,
- CHANGE tx_master_id treatment_master_id INT NOT NULL,
- ADD FOREIGN KEY (treatment_master_id) REFERENCES treatment_masters(id);
-ALTER TABLE qc_tf_txd_hormonotherapies_revs
- CHANGE tx_master_id treatment_master_id INT NOT NULL;
- 
- 
