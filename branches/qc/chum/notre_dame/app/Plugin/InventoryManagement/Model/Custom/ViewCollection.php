8498d7635f5969dd244f48a9f34a4b79858ffde9
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-07-09 20:28:04 +0000

    Inventory custom fix

---
+++ branches/qc/chum/notre_dame/app/Plugin/InventoryManagement/Model/Custom/ViewCollection.php
@@ -0,0 +1,33 @@
+<?php
+class ViewCollectionCustom extends ViewCollection{
+	
+	var $name = 'ViewCollection';
+	static $table_query = '
+	SELECT
+	Collection.id AS collection_id,Collection.bank_id AS bank_id,
+	Collection.sop_master_id AS sop_master_id,
+	Collection.participant_id AS participant_id,
+	Collection.diagnosis_master_id AS diagnosis_master_id,
+	Collection.consent_master_id AS consent_master_id,
+	Collection.treatment_master_id AS treatment_master_id,
+	Collection.event_master_id AS event_master_id,
+	Participant.participant_identifier AS participant_identifier,
+	Collection.acquisition_label AS acquisition_label,
+	Collection.collection_site AS collection_site,
+	Collection.collection_datetime AS collection_datetime,
+	Collection.collection_datetime_accuracy AS collection_datetime_accuracy,
+	Collection.collection_property AS collection_property,
+	Collection.collection_notes AS collection_notes,Bank.name AS bank_name,
+	Collection.created AS created,
+	
+	MiscIdentifier.identifier_value AS identifier_value,
+	MiscIdentifierControl.misc_identifier_name AS identifier_name,
+	Collection.visit_label AS visit_label
+	
+	FROM collections AS Collection
+	LEFT JOIN participants AS Participant ON Collection.participant_id = Participant.id AND Participant.deleted <> 1
+	LEFT JOIN banks As Bank ON Collection.bank_id = Bank.id AND Bank.deleted <> 1
+	LEFT JOIN misc_identifiers AS MiscIdentifier on MiscIdentifier.misc_identifier_control_id = Bank.misc_identifier_control_id AND MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.deleted <> 1
+    LEFT JOIN misc_identifier_controls AS MiscIdentifierControl ON MiscIdentifier.misc_identifier_control_id=MiscIdentifierControl.id
+	WHERE Collection.deleted <> 1 %%WHERE%%';
+}
---
+++ branches/qc/chum/notre_dame/app/Plugin/InventoryManagement/Model/Custom/ViewSample.php
@@ -0,0 +1,61 @@
+<?php
+class ViewSampleCustom extends ViewSample{
+	var $name = 'ViewSample';
+	
+	static $table_query = '
+		SELECT SampleMaster.id AS sample_master_id,
+		SampleMaster.parent_id AS parent_sample_id,
+		SampleMaster.initial_specimen_sample_id,
+		SampleMaster.collection_id AS collection_id,
+		
+		Collection.bank_id,
+		Collection.sop_master_id,
+		Collection.participant_id,
+		
+		Participant.participant_identifier,
+		
+		Collection.acquisition_label,
+		
+		SpecimenSampleControl.sample_type AS initial_specimen_sample_type,
+		SpecimenSampleMaster.sample_control_id AS initial_specimen_sample_control_id,
+		ParentSampleControl.sample_type AS parent_sample_type,
+		ParentSampleMaster.sample_control_id AS parent_sample_control_id,
+		SampleControl.sample_type,
+		SampleMaster.sample_control_id,
+		SampleMaster.sample_code,
+		SampleControl.sample_category,
+		
+		IF(SpecimenDetail.reception_datetime IS NULL, NULL,
+		IF(Collection.collection_datetime IS NULL, -1,
+		IF(Collection.collection_datetime_accuracy != "c" OR SpecimenDetail.reception_datetime_accuracy != "c", -2,
+		IF(Collection.collection_datetime > SpecimenDetail.reception_datetime, -3,
+		TIMESTAMPDIFF(MINUTE, Collection.collection_datetime, SpecimenDetail.reception_datetime))))) AS coll_to_rec_spent_time_msg,
+			
+		IF(DerivativeDetail.creation_datetime IS NULL, NULL,
+		IF(Collection.collection_datetime IS NULL, -1,
+		IF(Collection.collection_datetime_accuracy != "c" OR DerivativeDetail.creation_datetime_accuracy != "c", -2,
+		IF(Collection.collection_datetime > DerivativeDetail.creation_datetime, -3,
+		TIMESTAMPDIFF(MINUTE, Collection.collection_datetime, DerivativeDetail.creation_datetime))))) AS coll_to_creation_spent_time_msg,
+		
+		MiscIdentifier.identifier_value AS identifier_value,
+		Collection.visit_label AS visit_label,
+		Collection.diagnosis_master_id AS diagnosis_master_id,
+		Collection.consent_master_id AS consent_master_id
+		
+		FROM sample_masters AS SampleMaster
+		INNER JOIN sample_controls as SampleControl ON SampleMaster.sample_control_id=SampleControl.id
+		INNER JOIN collections AS Collection ON Collection.id = SampleMaster.collection_id AND Collection.deleted != 1
+		LEFT JOIN specimen_details AS SpecimenDetail ON SpecimenDetail.sample_master_id=SampleMaster.id
+		LEFT JOIN derivative_details AS DerivativeDetail ON DerivativeDetail.sample_master_id=SampleMaster.id
+		LEFT JOIN sample_masters AS SpecimenSampleMaster ON SampleMaster.initial_specimen_sample_id = SpecimenSampleMaster.id AND SpecimenSampleMaster.deleted != 1
+		LEFT JOIN sample_controls AS SpecimenSampleControl ON SpecimenSampleMaster.sample_control_id = SpecimenSampleControl.id
+		LEFT JOIN sample_masters AS ParentSampleMaster ON SampleMaster.parent_id = ParentSampleMaster.id AND ParentSampleMaster.deleted != 1
+		LEFT JOIN sample_controls AS ParentSampleControl ON ParentSampleMaster.sample_control_id = ParentSampleControl.id
+		LEFT JOIN participants AS Participant ON Collection.participant_id = Participant.id AND Participant.deleted != 1
+		LEFT JOIN banks As Bank ON Collection.bank_id = Bank.id AND Bank.deleted <> 1
+		LEFT JOIN misc_identifiers AS MiscIdentifier on MiscIdentifier.misc_identifier_control_id = Bank.misc_identifier_control_id AND MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.deleted <> 1
+	    LEFT JOIN misc_identifier_controls AS MiscIdentifierControl ON MiscIdentifier.misc_identifier_control_id=MiscIdentifierControl.id
+		
+		WHERE SampleMaster.deleted != 1 %%WHERE%%';
+}
+
