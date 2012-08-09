88cebe1b8073639aa3d07a244ce73a8a2bf2adae
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-08-09 20:21:32 +0000

    Fix issu 2369: Unable to delete a collection - participant link Create atim_v251 upgrade script. Add check on fields of detail table and having plugin value empty set flag_search = 1 for participant contact form

--- scripts/v2.5.0/atim_v2.5.0_upgrade.sql
+++ scripts/v2.5.0/atim_v2.5.0_upgrade.sql
@@ -2770,4 +2770,3 @@ INSERT IGNORE INTO i18n (id,en,fr) VALUES
 
 UPDATE versions SET permissions_regenerated = 0;
 
-SELECT IF(COUNT(*) > 0, "At least one row of structure_fields table contains a field linked to detail model with no plugin defintion.", "No error") AS 'structure_fields.plugin check msg' FROM structure_fields WHERE model LIKE '%Detail' AND (plugin LIKE '' OR plugin IS NULL);
