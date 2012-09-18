259740c9368b95225a2422400625ede7b4ad134b
fmlheureux <fmlheureux@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-09-18 02:33:27 +0000

    Fixed E2382: Editing profile causes an error in aco.parent_id.

--- trunk/app/Plugin/Customize/Controller/ProfilesController.php
+++ trunk/app/Plugin/Customize/Controller/ProfilesController.php
@@ -10,25 +10,22 @@ class ProfilesController extends CustomizeAppController {
 		
 		$this->hook();
 		
-		$this->request->data = $this->User->find('first',array('conditions'=>array('User.id'=>$_SESSION['Auth']['User']['id'])));
+		$this->request->data = $this->User->find('first',array('conditions'=>array('User.id'=>$this->Session->read('Auth.User.id'))));
 	}
 	
 	function edit() {
-		//TODO
-		$this->atimFlash( 'this function is temporarily unusable', '/Customize/Profiles/index');
-		
-		$this->Structures->set('users' );
-		$user_data = $this->User->getOrRedirect($_SESSION['Auth']['User']['id']);
+		$this->Structures->set('users');
+		$user_data = $this->User->getOrRedirect($this->Session->read('Auth.User.id'));
 	
 		
 		$this->hook();
 		
 		if ( !empty($this->request->data) ) {
-			$this->request->data['User']['id'] = $_SESSION['Auth']['User']['id'];
-			$this->request->data['User']['group_id'] = $_SESSION['Auth']['User']['group_id'];
-			$this->request->data['Group']['id'] = $_SESSION['Auth']['User']['group_id'];
+			$this->request->data['User']['id'] = $this->Session->read('Auth.User.id');
+			$this->request->data['User']['group_id'] = $this->Session->read('Auth.User.group_id');
+			$this->request->data['Group']['id'] = $this->Session->read('Auth.User.group_id');
 			
-			$this->User->id = $_SESSION['Auth']['User']['id'];
+			$this->User->id = $this->Session->read('Auth.User.id');
 			
 			$submitted_data_validates	= true;
 			
@@ -49,7 +46,8 @@ class ProfilesController extends CustomizeAppController {
 			if( $hook_link ) { 
 				require($hook_link); 
 			}
-							
+			
+			$this->User->addWritableField('group_id');
 			if ($submitted_data_validates && $this->User->save($this->request->data) ) {
 				$hook_link = $this->hook('postsave_process');
 				if( $hook_link ) {
