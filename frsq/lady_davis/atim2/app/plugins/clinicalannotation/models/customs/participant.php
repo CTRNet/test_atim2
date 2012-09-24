6cfea93b11c7967de63a4d56e62544492599f0f7
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-09-24 14:47:51 +0000

    Update to match jgh server content

--- frsq/lady_davis/atim2/app/plugins/clinicalannotation/models/customs/participant.php
+++ frsq/lady_davis/atim2/app/plugins/clinicalannotation/models/customs/participant.php
@@ -14,4 +14,19 @@ class ParticipantCustom extends Participant{
 		$result['title'] = $label;
 		return $result;
 	}
+	
+	function beforeSave($options = array()) {
+		$val = parent::BeforeSave($options);
+		
+		if($this->id != null && isset($this->data['Participant']['qc_lady_has_family_history']) && $this->data['Participant']['qc_lady_has_family_history'] != 'y'){
+			$fam_hist_model = AppModel::getInstance('Clinicalannotation', 'FamilyHistory', true);
+			$fam_hist = $fam_hist_model->find('first', array('conditions' => array('FamilyHistory.participant_id' => $this->id)));
+			if($fam_hist){
+				unset($this->data['Participant']['qc_lady_has_family_history']);
+				AppController::addWarningMsg('Family history exists.');
+			}
+		}
+		
+		return $val;
+	}
 }
