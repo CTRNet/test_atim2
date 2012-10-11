38651720ae7eb06a80ab5997252e4ca91276e1de
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-10-11 20:54:58 +0000

    First corrections from Ur. and Jos.

---
+++ branches/qc/frsq/lady_davis/atim2/app/Plugin/InventoryManagement/Controller/Hook/Collections_edit_presave_process.php
@@ -0,0 +1,10 @@
+<?php 
+	
+	if(empty($collection_data['Collection']['qc_lady_specimen_type_precision']) || empty($collection_data['Collection']['qc_lady_specimen_type_precision'])) {
+		if(!empty($this->request->data['Collection']['qc_lady_specimen_type_precision'])) {
+			$this->Collection->addWritableField(array('qc_lady_specimen_type'));
+			$qc_lady_specimen_type_precision = $this->request->data['Collection']['qc_lady_specimen_type_precision'];
+			$this->request->data['Collection']['qc_lady_specimen_type'] = substr($qc_lady_specimen_type_precision, 0, strpos($qc_lady_specimen_type_precision,'||'));
+		}
+	}
+	
