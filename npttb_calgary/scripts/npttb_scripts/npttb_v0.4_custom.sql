7ecabec328ae2c1b42d8c1493710e18facebd14e
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-10-30 15:58:27 +0000

    SQL for age at treatment field. Eventum ID 2330

--- npttb_calgary/scripts/npttb_scripts/npttb_v0.4_custom.sql
+++ npttb_calgary/scripts/npttb_scripts/npttb_v0.4_custom.sql
@@ -38,3 +38,29 @@ ALTER TABLE `cd_npttb_consent_sno_calgary_revs` CHANGE COLUMN `version_id` `vers
 */
 
 UPDATE `structure_validations` SET `rule`='/^\\A\\w{2}\\s{1}\\d{2}(-)\\d{4}$/' WHERE `id`= (SELECT `id` FROM `structure_fields` WHERE model = 'TreatmentDetail' AND tablename = 'txd_surgeries' AND field = 'path_num');
+
+/*
+	------------------------------------------------------------
+	Eventum ID: 2330 - Treatment -> Surgery -> Age at Surgery
+	------------------------------------------------------------
+*/
+
+-- Add column to master table
+ALTER TABLE `treatment_masters` ADD COLUMN `npttb_age_at_tx` INT(11) NULL DEFAULT NULL  AFTER `notes` ;
+ALTER TABLE `treatment_masters_revs` ADD COLUMN `npttb_age_at_tx` INT(11) NULL DEFAULT NULL  AFTER `notes` ;
+
+DELETE FROM structure_formats WHERE structure_id=(SELECT id FROM structures WHERE alias='txd_surgeries') AND structure_field_id=(SELECT id FROM structure_fields WHERE `public_identifier`='' AND `plugin`='ClinicalAnnotation' AND `model`='TreatmentDetail' AND `tablename`='txd_surgeries' AND `field`='npttb_age_at_surgery' AND `language_label`='npttb age at surgery' AND `language_tag`='' AND `type`='integer' AND `setting`='' AND `default`='' AND `structure_value_domain` IS NULL  AND `language_help`='' AND `validation_control`='open' AND `value_domain_control`='open' AND `field_control`='open' AND `flag_confidential`='0');
+-- Delete obsolete structure fields and validations
+DELETE FROM structure_validations WHERE structure_field_id IN (SELECT id FROM structure_fields WHERE (`public_identifier`='' AND `plugin`='ClinicalAnnotation' AND `model`='TreatmentDetail' AND `tablename`='txd_surgeries' AND `field`='npttb_age_at_surgery' AND `language_label`='npttb age at surgery' AND `language_tag`='' AND `type`='integer' AND `setting`='' AND `default`='' AND `structure_value_domain` IS NULL  AND `language_help`='' AND `validation_control`='open' AND `value_domain_control`='open' AND `field_control`='open' AND `flag_confidential`='0'));
+DELETE FROM structure_fields WHERE (`public_identifier`='' AND `plugin`='ClinicalAnnotation' AND `model`='TreatmentDetail' AND `tablename`='txd_surgeries' AND `field`='npttb_age_at_surgery' AND `language_label`='npttb age at surgery' AND `language_tag`='' AND `type`='integer' AND `setting`='' AND `default`='' AND `structure_value_domain` IS NULL  AND `language_help`='' AND `validation_control`='open' AND `value_domain_control`='open' AND `field_control`='open' AND `flag_confidential`='0');
+
+-- Add field to surgery form
+REPLACE INTO `i18n` (`id`, `en`, `fr`)
+	VALUES ('npttb age treatment', 'Age at Treatment', '');
+	
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('ClinicalAnnotation', 'TreatmentMaster', 'treatment_masters', 'npttb_age_at_tx', 'integer',  NULL , '0', '', '', '', 'npttb age treatment', '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='txd_surgeries'), (SELECT id FROM structure_fields WHERE `model`='TreatmentMaster' AND `tablename`='treatment_masters' AND `field`='npttb_age_at_tx' AND `type`='integer' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='npttb age treatment' AND `language_tag`=''), '1', '10', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '1', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0');
+	
+	
