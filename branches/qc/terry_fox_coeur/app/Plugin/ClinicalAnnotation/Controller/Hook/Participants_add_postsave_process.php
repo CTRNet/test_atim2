4313aae73a86fb981cb671a466966c740b2dd1d9
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-11-16 19:51:25 +0000

    Migration to v253 (rev 4977): add custom code

--- branches/qc/terry_fox_coeur/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_add_postsave_process.php
+++ branches/qc/terry_fox_coeur/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_add_postsave_process.php
@@ -3,6 +3,7 @@
 	// --------------------------------------------------------------------------------
 	// Save Participant Identifier
 	// -------------------------------------------------------------------------------- 
+	$this->Participant->addWritableField(array('participant_identifier'));
 	if(!$this->Participant->save(array('Participant' => array('participant_identifier' => $this->Participant->id)))) $this->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
 	
 ?>
