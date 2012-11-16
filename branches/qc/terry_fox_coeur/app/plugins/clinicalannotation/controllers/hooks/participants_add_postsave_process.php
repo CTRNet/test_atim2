4530bff1e05aae569ed3fa37eaadb00a23a1e8a2
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-11-16 16:11:04 +0000

    delete all to migrate to 2.5

--- branches/qc/terry_fox_coeur/app/plugins/clinicalannotation/controllers/hooks/participants_add_postsave_process.php
+++
@@ -1,8 +0,0 @@
-<?php
-	
-	// --------------------------------------------------------------------------------
-	// Save Participant Identifier
-	// -------------------------------------------------------------------------------- 
-	if(!$this->Participant->save(array('Participant' => array('participant_identifier' => $this->Participant->id)))) $this->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
-	
-?>
