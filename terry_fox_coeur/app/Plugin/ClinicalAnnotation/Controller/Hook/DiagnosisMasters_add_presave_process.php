6d6daa0f8c0b8d359291c80651ae66a4cb689c8f
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-11-16 19:51:25 +0000

    Migration to v253 (rev 4977): add custom code

--- terry_fox_coeur/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_add_presave_process.php
+++ terry_fox_coeur/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_add_presave_process.php
@@ -5,17 +5,16 @@
 	// -------------------------------------------------------------------------------- 
 	
 	if(($dx_control_data['DiagnosisControl']['controls_type'] == 'EOC') || ($dx_control_data['DiagnosisControl']['controls_type'] == 'other primary cancer')) {
-		$existing_primary_number_data = $this->DiagnosisMaster->find('first', array('conditions' => array('DiagnosisMaster.participant_id' => $participant_id, 'DiagnosisMaster.primary_number' => $this->data['DiagnosisMaster']['primary_number']), 'fields'=> array('DiagnosisMaster.primary_number'), 'recursive' => '-1'));
-		
-		$this->data['DiagnosisMaster']['qc_tf_dx_origin'] = 'primary';	
+		$this->DiagnosisMaster->addWritableField(array('qc_tf_dx_origin','qc_tf_tumor_site','qc_tf_progression_detection_method'));
+		$this->request->data['DiagnosisMaster']['qc_tf_dx_origin'] = 'primary';	
 		if($dx_control_data['DiagnosisControl']['controls_type'] == 'EOC'){
-			$this->data['DiagnosisMaster']['qc_tf_tumor_site'] = 'Female Genital-Ovary';	
+			$this->request->data['DiagnosisMaster']['qc_tf_tumor_site'] = 'Female Genital-Ovary';	
 		}
-		$this->data['DiagnosisMaster']['qc_tf_progression_detection_method'] = 'not applicable';
+		$this->request->data['DiagnosisMaster']['qc_tf_progression_detection_method'] = 'not applicable';
 		
 	} else if($dx_control_data['DiagnosisControl']['controls_type'] == 'progression and recurrence') {
-		
-		$this->data['DiagnosisMaster']['qc_tf_dx_origin'] = 'progression';
+		$this->DiagnosisMaster->addWritableField(array('qc_tf_dx_origin'));
+		$this->request->data['DiagnosisMaster']['qc_tf_dx_origin'] = 'progression';
 		
 	} else {
 		$this->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
