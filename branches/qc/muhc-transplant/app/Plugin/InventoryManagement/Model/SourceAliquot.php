64e20673bcac79f512b81e44dc9379e0c1362a49
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2012-11-20 14:26:19 +0000

    MUHC / Montreal McGill HPB and Transplant Program

---
+++ branches/qc/muhc-transplant/app/Plugin/InventoryManagement/Model/SourceAliquot.php
@@ -0,0 +1,15 @@
+<?php
+
+class SourceAliquot extends InventoryManagementAppModel {
+
+	var $belongsTo = array(        
+		'SampleMaster' => array(           
+			'className'    => 'InventoryManagement.SampleMaster',            
+			'foreignKey'    => 'sample_master_id'),       
+		'AliquotMaster' => array(           
+			'className'    => 'InventoryManagement.AliquotMaster',            
+			'foreignKey'    => 'aliquot_master_id'));
+			
+}
+
+?>
