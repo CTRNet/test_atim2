d165091a572d195aa0323d313b11ceab88670f7c
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-01-16 21:47:27 +0000

    Migration: Revisions of profile, identifier, consent, diagnosis, annotation, treatment

--- branches/on/ohri/app/Plugin/ClinicalAnnotation/Model/Custom/Participant.php
+++ branches/on/ohri/app/Plugin/ClinicalAnnotation/Model/Custom/Participant.php
@@ -11,23 +11,20 @@ class ParticipantCustom extends Participant {
 		if ( isset($variables['Participant.id']) ) {
 			$result = $this->find('first', array('conditions'=>array('Participant.id'=>$variables['Participant.id'])));
 							
-			$identifier_model = AppModel::getInstance('Clinicalannotation', 'MiscIdentifier', true);
+			$identifier_model = AppModel::getInstance('ClinicalAnnotation', 'MiscIdentifier', true);
 			$identifier_results = $identifier_model->find('first', array('conditions' => array(
 				'MiscIdentifier.participant_id' => $variables['Participant.id'],
 				'MiscIdentifierControl.misc_identifier_name LIKE' => 'ohri_bank_participant_id'))
 			);
 						
 			//Add No Labs to description
-			$bank_identifier = '';
-			if(isset($identifier_results['MiscIdentifier']['identifier_value'])) {
-				$bank_identifier = $identifier_results['MiscIdentifier']['identifier_value'];
-			}
+			$title = (isset($identifier_results['MiscIdentifier']['identifier_value'])? __('ohri_bank_participant_id').' '.$identifier_results['MiscIdentifier']['identifier_value'] : __('no ohri_bank_participant_id')).' ['.$result['Participant']['participant_identifier'].']';
 			
 			$return = array(
-					'menu'				=>	array( NULL, empty($bank_identifier)? __('unknown', true) : $bank_identifier ),
-					'title'				=>	array( NULL, empty($bank_identifier)? __('unknown', true) : __('ohri_bank_participant_id', true).": ".$bank_identifier ),
-					'structure alias' 	=> 'participants',
-					'data'				=> $result
+				'menu'				=>	array( NULL, $title),
+				'title'				=>	array( NULL, $title),
+				'structure alias' 	=> 'participants',
+				'data'				=> $result
 			);
 		}
 		
--- branches/on/ohri/app/Plugin/ClinicalAnnotation/View/DiagnosisMasters/Hook/add.php
+++ branches/on/ohri/app/Plugin/ClinicalAnnotation/View/DiagnosisMasters/Hook/add.php
@@ -3,8 +3,6 @@
 	// --------------------------------------------------------------------------------
 	// Set default value
 	// -------------------------------------------------------------------------------- 
-	if(isset($default_ohri_tumor_site)) {
-		$final_options['override']['DiagnosisMaster.ohri_tumor_site'] = $default_ohri_tumor_site;
-	}
+	if(isset($default_ohri_tumor_site)) $final_options['override']['DiagnosisMaster.ohri_tumor_site'] = $default_ohri_tumor_site;
 	
 ?>
