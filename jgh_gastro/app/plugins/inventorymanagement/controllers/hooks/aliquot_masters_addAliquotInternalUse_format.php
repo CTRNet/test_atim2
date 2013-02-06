44de76f73e761de8741bd0e68bbf2d6c1fb02976
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-02-06 19:26:52 +0000

    Delete all v236 source code to merge with 253

--- jgh_gastro/app/plugins/inventorymanagement/controllers/hooks/aliquot_masters_addAliquotInternalUse_format.php
+++
@@ -1,9 +0,0 @@
-<?php
-	
-	// --------------------------------------------------------------------------------
-	//Add thickness and number of slices to tissue
-	//--------------------------------------------------------------------------
-	$this->Structures->set('aliquotinternaluses,qc_gastro_tissueinternaluses', 'tissueinternaluses_structure');
-	//$this->Structures->set('aliquotinternaluses_volume,aliquotinternaluses,qc_gastro_tissueinternaluses', 'tissueinternaluses_volume_structure');
-		
-?>
