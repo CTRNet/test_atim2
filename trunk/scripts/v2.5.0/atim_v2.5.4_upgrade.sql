e6d97ae5e6e3967219bd73a0553e0dd295580854
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-03-11 18:05:02 +0000

    Change 'delete' to 'delete collection link'

---
+++ trunk/scripts/v2.5.0/atim_v2.5.4_upgrade.sql
@@ -0,0 +1,4 @@
+INSERT INTO `versions` (version_number, date_installed, build_number) VALUES
+('2.5.4', NOW(), 'xxx');
+
+INSERT IGNORE INTO i18n (id,en,fr) VALUES ('delete collection link','Delete Collection Link','Supprimer Lien à la Collection');
