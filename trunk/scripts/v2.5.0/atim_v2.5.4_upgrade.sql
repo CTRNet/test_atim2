bef9edb203f7eeab91e4defc40cfbab6b8a00184
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-03-18 15:44:07 +0000

    Add link to TreatmentMasterModel in TreatmentExtendModel

--- trunk/scripts/v2.5.0/atim_v2.5.4_upgrade.sql
+++ trunk/scripts/v2.5.0/atim_v2.5.4_upgrade.sql
@@ -1,4 +1,10 @@
-INSERT INTO `versions` (version_number, date_installed, build_number) VALUES
+INSERT INTO `versions` (version_number, date_installed, trunk_build_number) VALUES
 ('2.5.4', NOW(), 'xxx');
 
 INSERT IGNORE INTO i18n (id,en,fr) VALUES ('delete collection link','Delete Collection Link','Supprimer Lien à la Collection');
+
+INSERT IGNORE INTO i18n (id,en,fr) 
+VALUES 
+('treatment precisions','Treatment Precisions','Précisions du traitement'),
+('chemotherapy drugs','Chemotherapy Drugs','Molécules de Chimiothérapie'),
+('surgeries','Surgeries','Chirurgies');
