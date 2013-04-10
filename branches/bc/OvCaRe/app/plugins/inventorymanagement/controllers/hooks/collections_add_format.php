f1a8a0f481ca6840ea4d70712555ef6f7a2cbe1a
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-04-10 12:55:27 +0000

    Migration to v2.5.0 step 1

--- branches/bc/OvCaRe/app/plugins/inventorymanagement/controllers/hooks/collections_add_format.php
+++
@@ -1,7 +0,0 @@
-<?php
-	
-	if(!$need_to_save){
-		$this->data['Collection']['bank_id'] = 1;
-	}
-
-?>
--- branches/bc/OvCaRe/app/plugins/inventorymanagement/controllers/hooks/sample_masters_add_initial_display.php
+++
@@ -1,8 +0,0 @@
-<?php
- 	
-	if($this->data['SampleControl']['sample_type'] == 'tissue') {
-		$this->data['SampleDetail']['tissue_size_unit'] = 'cm';
-		$this->data['SampleDetail']['tissue_weight_unit'] = 'gr';
-	}
-	
-?>
