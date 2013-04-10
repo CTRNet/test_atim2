de2ee67eada3c15ec147568b7c6fc8d20697b18e
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-04-10 18:41:34 +0000

    SQL file for next version of NPTTB

---
+++ branches/ab/npttb_calgary/scripts/npttb_scripts/npttb_v0.5_custom.sql
@@ -0,0 +1,13 @@
+-- NPTTB Custom Script
+-- Version: v0.5
+-- ATiM Version: v2.5.2
+
+-- Update bank name for version tracking during customization
+REPLACE INTO `i18n` (`id`, `en`, `fr`)
+	VALUES ('core_installname', 'Clark H. Smith NPTTB - v0.5 DEV', '');
+	
+/*
+	------------------------------------------------------------
+	Eventum ID: 
+	------------------------------------------------------------
+*/
