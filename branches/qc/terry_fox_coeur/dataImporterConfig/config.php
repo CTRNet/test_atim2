9244488bccd0524bf38e53fd5d5a44c9d24978e3
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-04-12 14:20:38 +0000

    Merged with trunk v254 (rev 5117-5152)

--- branches/qc/terry_fox_coeur/dataImporterConfig/config.php
+++ branches/qc/terry_fox_coeur/dataImporterConfig/config.php
@@ -27,7 +27,7 @@ class Config{
 	//if reading excel file
 	
  	//static $xls_file_path = "C:/_My_Directory/Local_Server/ATiM/tfri_coeur/data/OVCARE#3-138pts (June 11_2012) TFRI-COEUR -v3.0 2011-09-01(kim)_rev20121221.xls";
- 	static $xls_file_path = "C:/_Perso/Server/tfri_coeur/data/ohri_coeur.xls";
+ 	static $xls_file_path = "C:/_Perso/Server/tfri_coeur/data/TFRI-COEUR OHRI#1-2013-03.xls";
  	//static $xls_file_path = "C:/_My_Directory/Local_Server/ATiM/tfri_coeur/data/Test.xls";
  	
  	
@@ -340,7 +340,7 @@ function addonFunctionEnd(){
 			if(!$eoc_dx_date) {
 				Config::$summary_msg['EOC Dx fields update']['@@ERROR@@']['Survival from ovarectomy update'][] = 'EOC Dx Date is missing. See patient '.$qc_tf_bank_identifier;
 			} else if($eoc_dx_date_accuracy != 'c') {
-				onfig::$summary_msg['EOC Dx fields update']['@@ERROR@@']['Survival from ovarectomy update'][] = "EOC Dx Date accuracy != c (=$eoc_dx_date_accuracy). See patient .$qc_tf_bank_identifier";
+				Config::$summary_msg['EOC Dx fields update']['@@ERROR@@']['Survival from ovarectomy update'][] = "EOC Dx Date accuracy != c (=$eoc_dx_date_accuracy). See patient .$qc_tf_bank_identifier";
 				$eoc_dx_date = null;
 			}
 			if($qc_tf_last_contact && $eoc_dx_date) {
---
+++ branches/qc/terry_fox_coeur/scripts/v2.5.0/custom_post254.sql
@@ -0,0 +1,11 @@
+
+UPDATE `versions` SET branch_build_number = '5188' WHERE version_number = '2.5.4';
+
+ALTER TABLE participants ADD COLUMN qc_tf_post_chemo char(1) DEFAULT '';
+ALTER TABLE participants_revs ADD COLUMN qc_tf_post_chemo char(1) DEFAULT '';
+
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('ClinicalAnnotation', 'Participant', 'participants', 'qc_tf_post_chemo', 'yes_no',  NULL , '0', '', '', '', 'post chemo', '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='participants'), (SELECT id FROM structure_fields WHERE `model`='Participant' AND `tablename`='participants' AND `field`='qc_tf_post_chemo' AND `type`='yes_no' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='post chemo' AND `language_tag`=''), '3', '8', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '0');
+INSERT IGNORE INTO i18n (id,en,fr) VALUES ('post chemo','Post Chemo', 'Post Chimio');
