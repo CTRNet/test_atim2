13a176bec7abfd3b8d96a8d3021972ae7e7c6af2
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-04-15 20:57:48 +0000

    in dev

--- branches/bc/OvCaRe/app/Plugin/ClinicalAnnotation/Model/Custom/EventControl.php
+++ branches/bc/OvCaRe/app/Plugin/ClinicalAnnotation/Model/Custom/EventControl.php
@@ -6,15 +6,15 @@ class EventControlCustom extends EventControl {
 	
 	function buildAddLinks($event_ctrl_data, $participant_id, $event_group){
 		$links = array();
-		$experimental_test_found = false;
 		foreach($event_ctrl_data as $new_ctrl) { 
 			if($event_group == 'lab' && $new_ctrl['EventControl']['event_type'] == 'experimental tests') {
-				$links[__($new_ctrl['EventControl']['event_type'])] = "/ClinicalAnnotation/EventMasters/addExperimentalTestsInBatch/$participant_id/".$new_ctrl['EventControl']['id'].'/0';
-				$links[__($new_ctrl['EventControl']['event_type']).' - '.__('in batch')] = "/ClinicalAnnotation/EventMasters/addExperimentalTestsInBatch/$participant_id/".$new_ctrl['EventControl']['id'].'/1';
+				$links[__($new_ctrl['EventControl']['event_type'])] = "/ClinicalAnnotation/EventMasters/add/$participant_id/".$new_ctrl['EventControl']['id'];
+				$links[__($new_ctrl['EventControl']['event_type']).' - '.__('in batch')] = "/ClinicalAnnotation/EventMasters/add/$participant_id/".$new_ctrl['EventControl']['id'].'/0/1';
 			} else {
 				$links[__($new_ctrl['EventControl']['event_type'])] = "/ClinicalAnnotation/EventMasters/add/$participant_id/".$new_ctrl['EventControl']['id'];
 			}
 		}
+		ksort($links);
 		return $links;
 	}
 	
