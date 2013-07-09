24e54d660071da57d674bd187108c2ff856d540b
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-07-09 23:45:26 +0000

    Issue #2511 : Saved Browsing Steps : See Steps details

--- scripts/v2.6.0/atim_v2.6.0_upgrade.sql
+++ scripts/v2.6.0/atim_v2.6.0_upgrade.sql
@@ -1210,3 +1210,24 @@ DROP TABLE datamart_browsing_indexes_revs;
 
 INSERT INTO `datamart_browsing_controls` (`id1`, `id2`, `flag_active_1_to_2`, `flag_active_2_to_1`, `use_field`) VALUES
 (1, 2, 1, 1, 'collection_id');
+
+-- -----------------------------------------------------------------------------------------------------------------------------------
+-- Saved Browsing Steps : See Steps details #2511
+-- -----------------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('Datamart', 'Generated', '', 'description', 'textarea',  NULL , '0', '', '', '', 'saved browsing description', '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='datamart_saved_browsing'), (SELECT id FROM structure_fields WHERE `model`='Generated' AND `tablename`='' AND `field`='description' AND `type`='textarea' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='saved browsing description' AND `language_tag`=''), '1', '4', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0');
+INSERT IGNORE INTO i18n (id,en,fr) VALUES ('saved browsing description', 'Steps', 'Étapes'), ('no search criteria', 'No search criteria', 'Aucun critères de recherche');
+REPLACE INTO i18n (id,en,fr) VALUES ('saved browsing description', 'Steps', 'Étapes'), ('no search criteria', 'No search criteria', 'Aucun critères de recherche');
+
+
+
+
+
+
+
+
+
+
