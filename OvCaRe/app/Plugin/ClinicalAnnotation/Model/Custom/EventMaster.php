29de88e8634b22f605caa624798d742e11274bb1
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-08-09 22:08:47 +0000

    Revision (2)

--- OvCaRe/app/Plugin/ClinicalAnnotation/Model/Custom/EventMaster.php
+++ OvCaRe/app/Plugin/ClinicalAnnotation/Model/Custom/EventMaster.php
@@ -24,5 +24,17 @@ class EventMasterCustom extends EventMaster {
 		return $return;
 	}
 	
+	function validates($options = array()) {
+		parent::validates($options);
+	
+		if(isset($this->data['EventDetail']) && array_key_exists('vital_status', $this->data['EventDetail'])) {
+			if(empty($this->data['EventMaster']['event_date'])){ 
+				$this->validationErrors['result'][] = 'follow-up date can not be empty';
+			}
+		}
+	
+		return empty($this->validationErrors);
+	}
+	
 }
 ?>
---
+++ OvCaRe/app/Plugin/ClinicalAnnotation/View/ClinicalCollectionLinks/Hook/edit_trt_detail.php
@@ -0,0 +1,4 @@
+<?php 
+	$final_options['settings']['actions'] = true;
+	$final_options['settings']['form_bottom'] = true;
+	$final_options['links']['bottom'] = array('cancel' => '/ClinicalAnnotation/ClinicalCollectionLinks/detail/'.$atim_menu_variables['Participant.id'].'/'.$atim_menu_variables['Collection.id'].'/');
