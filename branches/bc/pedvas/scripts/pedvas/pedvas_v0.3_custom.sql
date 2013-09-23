f9cadc5bdf055633a5f58424c824faf96d1bf6d4
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-09-23 16:36:52 +0000

    Script for v0.3 update

---
+++ branches/bc/pedvas/scripts/pedvas/pedvas_v0.3_custom.sql
@@ -0,0 +1,10 @@
+-- PedVas Custom Script
+-- Version: v0.3
+-- ATiM Version: v2.5.2
+
+-- This script must be run against a v2.5.2 ATiM database with all previous custom scripts applied.
+-- NOTE: PedVas wished to keep data entered on the development site. Be sure to run this update on that version!
+
+
+
+
