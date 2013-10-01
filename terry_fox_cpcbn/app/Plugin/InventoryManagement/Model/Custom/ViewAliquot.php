c2891e7d72d86e9399e1f1315178a17a4866b8ed
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-10-01 14:04:18 +0000

    Add TMA label field to viea aliquot and aliqut  + add patient exclusion field

--- terry_fox_cpcbn/app/Plugin/InventoryManagement/Model/Custom/ViewAliquot.php
+++ terry_fox_cpcbn/app/Plugin/InventoryManagement/Model/Custom/ViewAliquot.php
@@ -35,6 +35,7 @@ Collection.qc_tf_collection_type AS qc_tf_collection_type,
 		
 		StorageMaster.code,
 		StorageMaster.selection_label,
+StorageMaster.qc_tf_tma_name,
 		AliquotMaster.storage_coord_x,
 		AliquotMaster.storage_coord_y,
 		
