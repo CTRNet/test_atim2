af62415fb320a8a018cb324bd5706d96fc626b72
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-10-04 17:21:25 +0000

    Minor fixes - TTB not unique and update last mod date

--- npttb_calgary/scripts/npttb_scripts/npttb_v0.7_custom.sql
+++ npttb_calgary/scripts/npttb_scripts/npttb_v0.7_custom.sql
@@ -15,3 +15,24 @@ REPLACE INTO `i18n` (`id`, `en`, `fr`)
 
 INSERT INTO structure_permissible_values (value, language_alias) VALUES("Gliosarcoma", "npttb Gliosarcoma");
 INSERT INTO structure_value_domains_permissible_values (structure_value_domain_id, structure_permissible_value_id, display_order, flag_active) VALUES ((SELECT id FROM structure_value_domains WHERE domain_name="npttb_final_diagnosis"), (SELECT id FROM structure_permissible_values WHERE value="Gliosarcoma" AND language_alias="npttb Gliosarcoma"), "28", "1");
+
+REPLACE INTO `i18n` (`id`, `en`, `fr`)
+	VALUES ('npttb Gliosarcoma', 'Gliosarcoma', '');
+	
+/*
+	------------------------------------------------------------
+	Eventum ID: 2726 - Invalid last modification datetime
+	------------------------------------------------------------
+*/
+	
+UPDATE participants
+SET `last_modification` = NOW();
+
+
+/*
+	------------------------------------------------------------
+	Eventum ID: 2727 - TTB number is not unique
+	------------------------------------------------------------
+*/
+
+UPDATE `misc_identifier_controls` SET `flag_unique`='0' WHERE `misc_identifier_name`='TTB Number (Pre-2010)';
