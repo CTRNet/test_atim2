259dfbbf1ed4e4a9328ae11be8a38805ec9a15e9
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-11-27 20:01:54 +0000

    Consent form layout updates

--- childrens/scripts/ccbr_scripts/ccbr_v0.94_custom.sql
+++ childrens/scripts/ccbr_scripts/ccbr_v0.94_custom.sql
@@ -40,13 +40,15 @@ ALTER TABLE `cd_ccbr_consents`
 ADD COLUMN `ccbr_consent_left_over_csf` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_withdraw_all_samples`,
 ADD COLUMN `ccbr_consent_left_over_leukapheresis` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_consent_left_over_csf`,
 ADD COLUMN `ccbr_permission_to_contact` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_consent_left_over_leukapheresis`,
-ADD COLUMN `ccbr_old_biospecimens` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_permission_to_contact`;
+ADD COLUMN `ccbr_old_biospecimens` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_permission_to_contact`,
+ADD COLUMN `ccbr_consent_donation_bone_marrow` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_old_biospecimens`;
 
 ALTER TABLE `cd_ccbr_consents_revs` 
 ADD COLUMN `ccbr_consent_left_over_csf` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_withdraw_all_samples`,
 ADD COLUMN `ccbr_consent_left_over_leukapheresis` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_consent_left_over_csf`,
 ADD COLUMN `ccbr_permission_to_contact` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_consent_left_over_leukapheresis`,
-ADD COLUMN `ccbr_old_biospecimens` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_permission_to_contact`;
+ADD COLUMN `ccbr_old_biospecimens` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_permission_to_contact`,
+ADD COLUMN `ccbr_consent_donation_bone_marrow` VARCHAR(45) NULL DEFAULT NULL AFTER `ccbr_old_biospecimens`;
 
 -- Add fields to consent detail form
 INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
@@ -60,6 +62,11 @@ INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_col
 ((SELECT id FROM structures WHERE alias='cd_ccbr_consents'), (SELECT id FROM structure_fields WHERE `model`='ConsentDetail' AND `tablename`='cd_ccbr_consents' AND `field`='ccbr_permission_to_contact' AND `type`='select' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='ccbr_consent_yesnona')  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='ccbr permission to contact' AND `language_tag`=''), '1', '40', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0'), 
 ((SELECT id FROM structures WHERE alias='cd_ccbr_consents'), (SELECT id FROM structure_fields WHERE `model`='ConsentDetail' AND `tablename`='cd_ccbr_consents' AND `field`='ccbr_old_biospecimens' AND `type`='select' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='ccbr_consent_yesnona')  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='ccbr old biospecimens' AND `language_tag`=''), '1', '45', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0');
 
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('ClinicalAnnotation', 'ConsentDetail', 'cd_ccbr_consents', 'ccbr_consent_donation_bone_marrow', 'select', (SELECT id FROM structure_value_domains WHERE domain_name='ccbr_consent_yesnona') , '0', '', '', '', 'ccbr consent donation bone marrow', '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='cd_ccbr_consents'), (SELECT id FROM structure_fields WHERE `model`='ConsentDetail' AND `tablename`='cd_ccbr_consents' AND `field`='ccbr_consent_donation_bone_marrow' AND `type`='select' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='ccbr_consent_yesnona')  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='ccbr consent donation bone marrow' AND `language_tag`=''), '1', '48', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0');
+
 -- Move master fields
 UPDATE structure_fields SET  `setting`='cols=35,rows=6' WHERE model='ConsentMaster' AND tablename='consent_masters' AND field='notes' AND `type`='textarea' AND structure_value_domain  IS NULL ;
 UPDATE structure_formats SET `display_order`='50' WHERE structure_id=(SELECT id FROM structures WHERE alias='consent_masters') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ConsentMaster' AND `tablename`='consent_masters' AND `field`='consent_status' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='consent_status') AND `flag_confidential`='0');
@@ -70,7 +77,36 @@ UPDATE structure_formats SET `display_order`='99' WHERE structure_id=(SELECT id 
 
 
 REPLACE INTO `i18n` (`id`, `en`, `fr`) VALUES
- ('ccbr consent left over csf', '', ''),
- ('ccbr consent left over leukapheresi', '', ''),
- ('ccbr_permission_to_contact', '', ''), 
- ('ccbr old biospecimens', '', '');   
+ ('ccbr consent left over csf', 'Use of Left Over CSF', ''),
+ ('ccbr consent left over leukapheresis', 'Use of Left Over Leukapheresis', ''),
+ ('ccbr_permission_to_contact', 'Permission to Contact', ''), 
+ ('ccbr consent donation bone marrow', 'Consent to Bone Marrow Donation', ''),  
+ ('ccbr old biospecimens', 'Use of Old Biospecimens', '');
+ 
+--  --------------------------------------------------------------------------
+--	EVENTUM ISSUE: #2806 - Consent - Hide status date and consent signed
+--	-------------------------------------------------------------------------- 
+
+UPDATE structure_formats SET `flag_add`='0', `flag_edit`='0', `flag_search`='0', `flag_index`='0', `flag_detail`='0', `flag_summary`='0' WHERE structure_id=(SELECT id FROM structures WHERE alias='consent_masters') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ConsentMaster' AND `tablename`='consent_masters' AND `field`='status_date' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+UPDATE structure_formats SET `flag_add`='0', `flag_edit`='0', `flag_search`='0', `flag_index`='0', `flag_detail`='0', `flag_summary`='0' WHERE structure_id=(SELECT id FROM structures WHERE alias='consent_masters') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ConsentMaster' AND `tablename`='consent_masters' AND `field`='consent_signed_date' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+
+--  --------------------------------------------------------------------------
+--	EVENTUM ISSUE: #2807 - Consent form - Improve layout
+--	--------------------------------------------------------------------------
+
+-- Move withdrawal reason to second column
+UPDATE structure_formats SET `display_column`='2', `display_order`='5' WHERE structure_id=(SELECT id FROM structures WHERE alias='consent_masters') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ConsentMaster' AND `tablename`='consent_masters' AND `field`='reason_denied' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+
+-- Move status
+UPDATE structure_formats SET `display_order`='6' WHERE structure_id=(SELECT id FROM structures WHERE alias='consent_masters') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ConsentMaster' AND `tablename`='consent_masters' AND `field`='consent_status' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='consent_status') AND `flag_confidential`='0');   
+
+-- Move translator
+UPDATE structure_formats SET `display_order`='7' WHERE structure_id=(SELECT id FROM structures WHERE alias='cd_ccbr_consents') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ConsentMaster' AND `tablename`='consent_masters' AND `field`='translator_indicator' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+UPDATE structure_formats SET `display_order`='7' WHERE structure_id=(SELECT id FROM structures WHERE alias='cd_ccbr_consents') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ConsentMaster' AND `tablename`='consent_masters' AND `field`='translator_signature' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+
+-- Add heading for quesions section
+UPDATE structure_formats SET `language_heading`='ccbr_consent details' WHERE structure_id=(SELECT id FROM structures WHERE alias='cd_ccbr_consents') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='ConsentDetail' AND `tablename`='cd_ccbr_consents' AND `field`='ccbr_consent_blood_donation' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='ccbr_consent_yesnona') AND `flag_confidential`='0');
+
+REPLACE INTO `i18n` (`id`, `en`, `fr`) VALUES
+ ('ccbr_consent details', 'Consent Details', '');
+
