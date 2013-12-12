455e1757051c155c2edf719fea3932a471cb32a6
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2013-12-12 16:15:05 +0000

    Add custom default value for chus

--- branches/qc/procure/app/Plugin/InventoryManagement/Controller/Hook/Collections_add_presave_process.php
+++ branches/qc/procure/app/Plugin/InventoryManagement/Controller/Hook/Collections_add_presave_process.php
@@ -2,11 +2,13 @@
 	$this->request->data['Collection']['collection_property'] = 'participant collection';
 	if($collection_data) {	
 		$this->Collection->writable_fields_mode = 'add';
+/*		
 		if(empty($collection_data['Collection']['collection_property'])) {
 			// To Fix A Bug
 			$this->Collection->tryCatchQuery("UPDATE collections SET collection_property =  'participant collection' WHERE id = ".$collection_data['Collection']['id']);
 			$this->Collection->tryCatchQuery("UPDATE collections_revs SET collection_property =  'participant collection' WHERE id = ".$collection_data['Collection']['id']);
 		}
+*/
 		$this->Collection->addWritableField('collection_property', 'procure_chus_collection_specimen_sample_control_id');
 	} else {
 		$this->Collection->addWritableField('collection_property');
