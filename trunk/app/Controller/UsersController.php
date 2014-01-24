94852301ba8b99b3bd9dd8032297f18a58511451
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-01-24 22:21:00 +0000

    Issue #2943: Login Error Management : New rules

--- trunk/app/Controller/UsersController.php
+++ trunk/app/Controller/UsersController.php
@@ -28,7 +28,29 @@ class UsersController extends AppController {
 		
 		$this->set('skip_expiration_cookie', true);
 		
-		if($this->Auth->login() && (!isset($this->passedArgs['login']) || !empty($this->request->data))){
+		// Test last login results from IP adress
+		$is_locked_IP = true;
+		$max_login_attempts_from_IP = Configure::read('max_login_attempts_from_IP');
+		$mn_IP_disabled = Configure::read('time_mn_IP_disabled');	
+		$last_login_attempts_from_ip = $this->UserLoginAttempt->find('all', array('conditions' => array('UserLoginAttempt.ip_addr' => $_SERVER['REMOTE_ADDR']), 'order' => array('UserLoginAttempt.id DESC'), 'limit' => $max_login_attempts_from_IP));	
+		if(sizeof($last_login_attempts_from_ip) < $max_login_attempts_from_IP) {
+			$is_locked_IP = false;
+		} else {
+			foreach($last_login_attempts_from_ip as $login_attempt) if($login_attempt['UserLoginAttempt']['succeed']) $is_locked_IP = false;
+			if($is_locked_IP) {
+				$last_attempt_time = $last_login_attempts_from_ip[($max_login_attempts_from_IP-1)]['UserLoginAttempt']['attempt_time'];
+				$current_timestamp = $this->UserLoginAttempt->tryCatchQuery('SELECT CURRENT_TIMESTAMP');			
+				$start_date = new DateTime($last_attempt_time);
+				$end_date = new DateTime($current_timestamp[0][0]['CURRENT_TIMESTAMP']);
+				$interval = $start_date->diff($end_date);			
+				if(!$interval->invert) {		
+					if($interval->y || $interval->m || $interval->d || (($interval->h*60 + $interval->i) >= $mn_IP_disabled)) $is_locked_IP = false;
+				}
+			}
+		}	
+		if($is_locked_IP) {
+			$this->Auth->flash(__('your connection has been temporarily disabled'));
+		} else if($this->Auth->login() && (!isset($this->passedArgs['login']) || !empty($this->request->data))){
 			if(!empty($this->request->data)){
 				//successfulll login
 				$login_data = array(
@@ -64,7 +86,21 @@ class UsersController extends AppController {
 			if(!empty($data) && !$data['User']['flag_active'] && $data['User']['username'] == $this->request->data['User']['username']){
 				$this->Auth->flash(__("that username is disabled"));
 			}else{
-				$this->Auth->flash(__('Login failed. Invalid username or password.'));
+				$login_failed_message = 'Login failed. Invalid username or password.';
+				if(!empty($data) && $data['User']['username'] == $this->request->data['User']['username']){
+					$last_login_attempts_for_username = $this->UserLoginAttempt->find('all', array('conditions' => array('UserLoginAttempt.username' => $this->request->data['User']['username']), 'order' => array('UserLoginAttempt.id DESC'), 'limit' => Configure::read('max_user_login_attempts')));
+					$disable_user = true;
+					foreach($last_login_attempts_for_username as $login_attempt) if($login_attempt['UserLoginAttempt']['succeed']) $disable_user = false;
+					if($disable_user) {
+						$this->User->check_writable_fields = false;
+						$this->User->id = $data['User']['id'];
+						if(!$this->User->save(array('User' => array('id' => $data['User']['id'], 'flag_active' => 0)), false)) {
+							$this->redirect('/Pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+						}
+						$login_failed_message = 'login failed. that username has been disabled';
+					}
+				}				
+				$this->Auth->flash(__($login_failed_message));
 			}
 		}
 		
