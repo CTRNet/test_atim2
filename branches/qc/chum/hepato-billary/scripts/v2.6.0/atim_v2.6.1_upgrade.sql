69f589fe84972da45b69f07de538a7f8dd5f712b
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-03-11 17:11:39 +0000

    Merge with Trunk Rev:5620-5642.

---
+++ branches/qc/chum/hepato-billary/scripts/v2.6.0/atim_v2.6.1_upgrade.sql
@@ -0,0 +1,93 @@
+-- ------------------------------------------------------
+-- ATiM v2.6.1 Upgrade Script
+-- version: 2.6.1
+--
+-- For more information: 
+--    http://www.ctrnet.ca/mediawiki/index.php/Main_Page
+-- ------------------------------------------------------
+
+-- -----------------------------------------------------------------------------------------------------------------------------------
+-- Issue#3026: Participant.last_modification update and date accuracy 
+-- Create additional check
+-- -----------------------------------------------------------------------------------------------------------------------------------
+
+SELECT '----------------------------------------------------------------------------------------------------------------' AS 'TODO: Test on profiled date accuracies'
+UNION ALL
+SELECT 'Please review participant profile dates accuracies for participants listed below (nothing to do if section below empty)' AS  'TODO: Test on profiled date accuracies'
+UNION ALL
+SELECT 'Test is based to the issue #3026' AS  'TODO: Test on profiled date accuracies'
+UNION ALL
+SELECT 'Please add reviewed fields if custom dates have been created in participant table' AS  'TODO: Test on profiled date accuracies'
+UNION ALL
+SELECT '----------------------------------------------------------------------------------------------------------------' AS 'TODO: Test on profiled date accuracies';
+SELECT DISTINCT p.id AS participant_id, 
+p.participant_identifier, 
+'date_of_birth' AS studied_field,
+p.date_of_birth AS date, 
+p.date_of_birth_accuracy AS date_accuracy, 
+p_revs.date_of_birth_accuracy AS date_accuracy_in_revs  
+FROM participants p
+INNER JOIN participants_revs p_revs ON p.id = p_revs.id
+WHERE p.date_of_birth IS NOT NULL AND p.date_of_birth_accuracy = 'c'
+AND p.date_of_birth = p_revs.date_of_birth
+AND p_revs.date_of_birth_accuracy NOT IN ('', 'c')
+UNION ALL
+SELECT DISTINCT p.id AS participant_id, 
+p.participant_identifier, 
+'date_of_death' AS studied_field,
+p.date_of_death AS date, 
+p.date_of_death_accuracy AS date_accuracy, 
+p_revs.date_of_death_accuracy AS date_accuracy_in_revs  
+FROM participants p
+INNER JOIN participants_revs p_revs ON p.id = p_revs.id
+WHERE p.date_of_death IS NOT NULL AND p.date_of_death_accuracy = 'c'
+AND p.date_of_death = p_revs.date_of_death
+AND p_revs.date_of_death_accuracy NOT IN ('', 'c')
+UNION ALL
+SELECT DISTINCT p.id AS participant_id, 
+p.participant_identifier, 
+'last_chart_checked_date' AS studied_field,
+p.last_chart_checked_date AS date, 
+p.last_chart_checked_date_accuracy AS date_accuracy, 
+p_revs.last_chart_checked_date_accuracy AS date_accuracy_in_revs  
+FROM participants p
+INNER JOIN participants_revs p_revs ON p.id = p_revs.id
+WHERE p.last_chart_checked_date IS NOT NULL AND p.last_chart_checked_date_accuracy = 'c'
+AND p.last_chart_checked_date = p_revs.last_chart_checked_date
+AND p_revs.last_chart_checked_date_accuracy NOT IN ('', 'c');
+SELECT '----------------------------------------------------------------------------------------------------------------' AS 'END: Test on profiled date accuracies';
+
+-- -----------------------------------------------------------------------------------------------------------------------------------
+-- 
+-- -----------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- -----------------------------------------------------------------------------------------------------------------------------------
+-- Versions table
+-- -----------------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO `versions` (version_number, date_installed, trunk_build_number, branch_build_number) 
+VALUES('2.6.1', NOW(),'5???','n/a');
