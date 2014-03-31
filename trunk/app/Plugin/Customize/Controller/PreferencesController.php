d9f83cc7dc0f1221a3aaab5a0d02cc761c2f43bb
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-03-31 19:45:00 +0000

    Issue#3042: User Config Creation Error 2

--- trunk/app/Plugin/Customize/Controller/PreferencesController.php
+++ trunk/app/Plugin/Customize/Controller/PreferencesController.php
@@ -27,8 +27,8 @@ class PreferencesController extends CustomizeAppController {
 		if(!empty($this->request->data)){
 			$this->Config->preSave($config_results,
 								   $this->request->data,
-								   $_SESSION['Auth']['User']['id'],
-								   $_SESSION['Auth']['User']['group_id']);
+								   $_SESSION['Auth']['User']['group_id'],
+								   $_SESSION['Auth']['User']['id']);
 			
 			$this->Config->set($this->request->data);
 			
