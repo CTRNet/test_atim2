d80ce3777096aa2172db7839e709ef29413633bb
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-03-31 17:57:04 +0000

    Add frsq-onco consent to v2.5.4

--- chum/notre_dame/app/Plugin/ClinicalAnnotation/Model/Custom/ConsentMaster.php
+++ chum/notre_dame/app/Plugin/ClinicalAnnotation/Model/Custom/ConsentMaster.php
@@ -10,7 +10,14 @@ class ConsentMasterCustom extends ConsentMaster {
 			$result = false;
 			$this->validationErrors['consent_status'][] = 'all obtained consents should have a signed date';
 		}
-		
+		foreach(array('biological_material_use', 'use_of_blood', 'additional_tumor_collection') as $new_field) {
+			if(array_key_exists($new_field, $this->data['ConsentDetail']) && array_key_exists($new_field.'_not_applicable', $this->data['ConsentDetail'])) {
+				if(strlen($this->data['ConsentDetail'][$new_field] && $this->data['ConsentDetail'][$new_field.'_not_applicable'])) {
+					$result = false;
+					$this->validationErrors[$new_field.'_not_applicable'][] = 'no value has to be completed for agreement if not applicable';
+				}
+			}
+		}	
 		return $result;
 	}
 }
