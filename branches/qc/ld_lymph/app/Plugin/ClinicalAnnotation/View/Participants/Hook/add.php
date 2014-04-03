07dba37bd5fffebb3e021ad971773f4f43d8fcb5
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-04-03 17:30:54 +0000

    Migration to v2.6.1 (step 1): delete old code

--- branches/qc/ld_lymph/app/Plugin/ClinicalAnnotation/View/Participants/Hook/add.php
+++
@@ -1,5 +0,0 @@
-<?php
-	// --------------------------------------------------------------------------------
-	// Generate default bank number
-	// -------------------------------------------------------------------------------- 
-	$final_options['override']['Participant.participant_identifier'] = $default_bank_number;
--- branches/qc/ld_lymph/app/View/Menus/update.ctp
+++
@@ -1,4 +0,0 @@
-<?php
-	$menu_array = $this->Shell->menu( $ajax_menu, array('variables'=>$ajax_menu_variables) );
-	echo $menu_array[0];
-?>
