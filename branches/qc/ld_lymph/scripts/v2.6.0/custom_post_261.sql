41a587ca8f339c09e514b9ee01312e5ffd0a2415
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-04-04 14:42:00 +0000

    Migration to v2.6.1 (step 4) : New Customization

--- branches/qc/ld_lymph/scripts/v2.6.0/custom_post_261.sql
+++ branches/qc/ld_lymph/scripts/v2.6.0/custom_post_261.sql
@@ -34,18 +34,6 @@ UPDATE event_controls SET flag_use_for_ccl = 1 WHERE flag_active = 1;
 
 UPDATE parent_to_derivative_sample_controls SET flag_active=true WHERE id IN(24, 118);
 
-
-
-
-
-
-
-
-
-
-
-
-
 INSERT INTO sample_controls (sample_type, sample_category, detail_form_alias, detail_tablename, display_order, databrowser_label) VALUES
 ('other fluid', 'specimen', 'ld_lymph_sd_spe_other_fluids,specimens', 'ld_lymph_sd_spe_other_fluids', 0, 'other fluid'),
 ('other fluid cell suspension', 'derivative', 'sd_undetailed_derivatives,derivatives', 'ld_lymph_sd_der_other_fl_cell_susps', 0, 'other fluid cell suspension'),
@@ -146,7 +134,37 @@ VALUES
 SET @control_id = LAST_INSERT_ID(); 
 INSERT INTO realiquoting_controls (parent_aliquot_control_id, child_aliquot_control_id, flag_active) VALUES (@control_id,@control_id,1);
 
+INSERT INTO structure_validations(structure_field_id, rule) VALUES ((SELECT id FROM structure_fields WHERE `model`='SampleMaster' AND `field`='ld_lymph_specimen_number'), 'notEmpty');
 
+UPDATE structure_formats SET `flag_addgrid`='1' WHERE structure_id=(SELECT id FROM structures WHERE alias='specimens') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='SpecimenDetail' AND `tablename`='specimen_details' AND `field`='ld_lymph_mdl_mb_nbr' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
 
+UPDATE aliquot_controls SET flag_active = 1 WHERE sample_control_id IN (SELECT id FROM sample_controls WHERE sample_type LIKE 'other fluid%');
 
+ALTER TABLE ld_lymph_dxd_histo_transformations ADD COLUMN `lymphoma_type` varchar(250) NOT NULL DEFAULT '';
+ALTER TABLE ld_lymph_dxd_histo_transformations_revs ADD COLUMN `lymphoma_type` varchar(250) NOT NULL DEFAULT '';
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('ClinicalAnnotation', 'DiagnosisDetail', 'ld_lymph_dxd_histo_transformations', 'lymphoma_type', 'select', (SELECT id FROM structure_value_domains WHERE domain_name='custom_lymphoma_type_list') , '0', '', '', '', 'lymphoma type', '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `margin`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='ld_lymph_dx_histological_transformation'), (SELECT id FROM structure_fields WHERE `model`='DiagnosisDetail' AND `tablename`='ld_lymph_dxd_histo_transformations' AND `field`='lymphoma_type' AND `type`='select' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='custom_lymphoma_type_list')  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='lymphoma type' AND `language_tag`=''), '1', '11', '', '0', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0');
+UPDATE structure_formats SET `display_order`='9' WHERE structure_id=(SELECT id FROM structures WHERE alias='ld_lymph_dx_histological_transformation') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='DiagnosisDetail' AND `tablename`='ld_lymph_dxd_histo_transformations' AND `field`='lymphoma_type' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='custom_lymphoma_type_list') AND `flag_confidential`='0');
+
+ALTER TABLE ld_lymph_dxd_histo_transformations MODIFY `unusual_site_value` varchar(50) DEFAULT '';
+ALTER TABLE ld_lymph_dxd_histo_transformations_revs MODIFY `unusual_site_value` varchar(50) DEFAULT '';
+DELETE svdpv FROM structure_value_domains_permissible_values AS svdpv INNER JOIN structure_permissible_values AS spv ON svdpv.structure_permissible_value_id=spv.id INNER JOIN structure_value_domains AS svd ON svd.id = svdpv .structure_value_domain_id WHERE svd.domain_name="ld_lymph_dx_histo_transf_unusual_site" AND spv.value="bone" AND spv.language_alias="bone";
+DELETE svdpv FROM structure_value_domains_permissible_values AS svdpv INNER JOIN structure_permissible_values AS spv ON svdpv.structure_permissible_value_id=spv.id INNER JOIN structure_value_domains AS svd ON svd.id = svdpv .structure_value_domain_id WHERE svd.domain_name="ld_lymph_dx_histo_transf_unusual_site" AND spv.value="liver" AND spv.language_alias="liver";
+DELETE svdpv FROM structure_value_domains_permissible_values AS svdpv INNER JOIN structure_permissible_values AS spv ON svdpv.structure_permissible_value_id=spv.id INNER JOIN structure_value_domains AS svd ON svd.id = svdpv .structure_value_domain_id WHERE svd.domain_name="ld_lymph_dx_histo_transf_unusual_site" AND spv.value="ms" AND spv.language_alias="unusual site ms";
+DELETE svdpv FROM structure_value_domains_permissible_values AS svdpv INNER JOIN structure_permissible_values AS spv ON svdpv.structure_permissible_value_id=spv.id INNER JOIN structure_value_domains AS svd ON svd.id = svdpv .structure_value_domain_id WHERE svd.domain_name="ld_lymph_dx_histo_transf_unusual_site" AND spv.value="cns" AND spv.language_alias="unusual site cns";
+DELETE FROM structure_permissible_values WHERE value="liver" AND language_alias="liver" AND id NOT IN (SELECT DISTINCT structure_permissible_value_id FROM structure_value_domains_permissible_values);
+DELETE FROM structure_permissible_values WHERE value="ms" AND language_alias="unusual site ms" AND id NOT IN (SELECT DISTINCT structure_permissible_value_id FROM structure_value_domains_permissible_values);
+DELETE FROM structure_permissible_values WHERE value="cns" AND language_alias="unusual site cns" AND id NOT IN (SELECT DISTINCT structure_permissible_value_id FROM structure_value_domains_permissible_values);
+
+UPDATE structure_value_domains SET source = "StructurePermissibleValuesCustom::getCustomDropdown(\'Histo Transformation : Unusual Sites\')" WHERE domain_name = 'ld_lymph_dx_histo_transf_unusual_site';
+INSERT INTO structure_permissible_values_custom_controls (name, flag_active, values_max_length, category) VALUES ('Histo Transformation : Unusual Sites', 1, 50, 'clinical - diagnosis');
+SET @control_id = LAST_INSERT_ID();
+INSERT INTO `structure_permissible_values_customs` (`value`, `en`, `fr`, `use_as_input`, `control_id`, `modified`, `created`, `created_by`, `modified_by`) 
+VALUES
+('bone', 'Bone', 'Os', '1', @control_id, NOW(), NOW(), 1, 1),
+('liver', 'Liver', 'Foie', '1', @control_id, NOW(), NOW(), 1, 1),
+('unusual site cns', 'CNS', 'CNS', '1', @control_id, NOW(), NOW(), 1, 1),
+('unusual site ms', 'MS', 'MS', '1', @control_id, NOW(), NOW(), 1, 1);
 
