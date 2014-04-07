58c02240efa2c2282a4e3b56571e5534fe42b1ff
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-04-07 19:06:36 +0000

    Change custom report

--- ld_lymph/app/Plugin/Datamart/Controller/Custom/ReportsController.php
+++ ld_lymph/app/Plugin/Datamart/Controller/Custom/ReportsController.php
@@ -2,13 +2,12 @@
 
 class ReportsControllerCustom extends ReportsController {
 
-	
 	function specimenNumbersReport($parameters) {
 		
 		// Build criteria
 		
 		$criterias = array("ConsentMaster.consent_status" => array('obtained'));
-		foreach(array('Participant' => 'id', 'SampleControl' => 'sample_type', 'DiagnosisMaster' => 'ld_lymph_lymphoma_type') as $model => $field) {
+		foreach(array('Participant' => 'id', 'Participant' => 'participant_identifier', 'SampleControl' => 'sample_type', 'DiagnosisMaster' => 'ld_lymph_lymphoma_type') as $model => $field) {
 			if(isset($parameters[$model][$field])) {
 				foreach($parameters[$model][$field] as $new_val) {
 					if($new_val) {
@@ -18,16 +17,21 @@ class ReportsControllerCustom extends ReportsController {
 				}
 			}
 		}
-		
-		// Get data
-		$AVAILABLE_ALIQUOT_JOIN = ($parameters['FunctionManagement']['ld_lymph_limit_to_available_sample'][0])? "INNER JOIN aliquot_masters AliquotMaster ON AliquotMaster.sample_master_id = SampleMaster.id AND AliquotMaster.deleted <> 1 AND AliquotMaster.in_stock != 'no'" : '';
-		$DX_JOIN = isset($criterias['DiagnosisMaster.ld_lymph_lymphoma_type'])? 'INNER' : 'LEFT';	
 		$WHERE_CRITERIA = 'true';
 		if($criterias) {
 			$tmp_criterias = array();
 			foreach($criterias as $mode_field => $values) $tmp_criterias[] = "$mode_field IN ('".implode("','", str_replace("'","''",$values))."')";
 			$WHERE_CRITERIA = implode(" AND ", $tmp_criterias);
 		}
+		if(isset($parameters['Participant']['participant_identifier_start'])) {
+			if(strlen($parameters['Participant']['participant_identifier_start'])) $WHERE_CRITERIA .= ' AND Participant.participant_identifier >= '.$parameters['Participant']['participant_identifier_start'];
+			if(strlen($parameters['Participant']['participant_identifier_end'])) $WHERE_CRITERIA .= ' AND Participant.participant_identifier <= '.$parameters['Participant']['participant_identifier_end'];
+		}
+		
+		// Get data
+		
+		$AVAILABLE_ALIQUOT_JOIN = ($parameters['FunctionManagement']['ld_lymph_limit_to_available_sample'][0])? "INNER JOIN aliquot_masters AliquotMaster ON AliquotMaster.sample_master_id = SampleMaster.id AND AliquotMaster.deleted <> 1 AND AliquotMaster.in_stock != 'no'" : '';
+		$DX_JOIN = isset($criterias['DiagnosisMaster.ld_lymph_lymphoma_type'])? 'INNER' : 'LEFT';	
 		$report_query = "
 			SELECT DISTINCT
 				Participant.id,
@@ -82,7 +86,5 @@ class ReportsControllerCustom extends ReportsController {
 			'columns_names' => null,
 			'error_msg' => null);
 	}
-	
-	
 
 }
