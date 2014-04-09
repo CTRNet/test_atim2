a0459bdf3fcb8f1b3806e5ff692c9f3385d3fac0
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-04-09 20:17:21 +0000

    Add new requests

--- UHN_Shaw/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_add_presave_process.php
+++ UHN_Shaw/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_add_presave_process.php
@@ -1,13 +1,17 @@
 <?php
 	
-	if($dx_ctrl['DiagnosisControl']['controls_type'] == 'primary diagnosis unknown') {
-		$this->request->data['DiagnosisMaster']['uhn_site'] = 'unknown';
-		$this->DiagnosisMaster->addWritableField(array('uhn_site'));		
-	} else if($dx_ctrl['DiagnosisControl']['controls_type'] == 'ovary') {
+	if($dx_ctrl['DiagnosisControl']['controls_type'] == 'ovary') {
 		$this->request->data['DiagnosisMaster']['uhn_site'] = 'female genital-ovary';
 		$this->DiagnosisMaster->addWritableField(array('uhn_site'));		
-	} else if(isset($this->request->data['DiagnosisMaster']['uhn_site']) && $this->request->data['DiagnosisMaster']['uhn_site'] == 'female genital-ovary') {
-		$submitted_data_validates = false;
-		$this->DiagnosisMaster->validationErrors['uhn_site'][] = 'use ovarian diagnosis for any either ovarian tumor or ovarian metastasis ';
+	} else if($dx_ctrl['DiagnosisControl']['controls_type'] == 'endometrium') {
+		$this->request->data['DiagnosisMaster']['uhn_site'] = 'female genital-endometrium';	
+		$this->DiagnosisMaster->addWritableField(array('uhn_site'));	
+	} else if($dx_ctrl['DiagnosisControl']['controls_type'] == 'primary diagnosis unknown') {
+		$this->request->data['DiagnosisMaster']['uhn_site'] = 'unknown';
+		$this->DiagnosisMaster->addWritableField(array('uhn_site'));		
 	}
-	
+	if(isset($this->request->data['DiagnosisMaster']['uhn_site']) && in_array($this->request->data['DiagnosisMaster']['uhn_site'], array('female genital-endometrium', 'female genital-ovary'))
+	&& $dx_ctrl['DiagnosisControl']['category'] == 'primary' && !in_array($dx_ctrl['DiagnosisControl']['controls_type'], array('ovary','endometrium'))) {
+		$submitted_data_validates = false;
+		$this->DiagnosisMaster->validationErrors['uhn_site'][] = 'use either ovarian or endometrium diagnosis for this type of tumor';
+	}
