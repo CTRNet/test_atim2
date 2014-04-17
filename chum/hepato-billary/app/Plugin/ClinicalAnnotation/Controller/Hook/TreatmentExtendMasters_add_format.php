cb184669ff11f6a5314bb532efc12eaa6a8bcd58
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-04-17 14:07:23 +0000

    Last Update before New Test

--- chum/hepato-billary/app/Plugin/ClinicalAnnotation/Controller/Hook/TreatmentExtendMasters_add_format.php
+++ chum/hepato-billary/app/Plugin/ClinicalAnnotation/Controller/Hook/TreatmentExtendMasters_add_format.php
@@ -4,3 +4,12 @@
 	$atim_menu = $this->TreatmentMaster->inactivatePreOperativeDataMenu($atim_menu, $tx_master_data['TreatmentControl']['detail_tablename']);
 	$this->set('atim_menu', $atim_menu);
 	
+	$is_chemo_complications = false;
+	if(in_array('chemo_complications', $this->passedArgs)) {
+		$tx_extend_control_data = $this->TreatmentExtendControl->find('first', array('conditions' => array('TreatmentExtendControl.type' => 'chemotherapy complications')));
+		$tx_master_data['TreatmentControl']['treatment_extend_control_id'] = $tx_extend_control_data['TreatmentExtendControl']['id']; 
+		$this->Structures->set($tx_extend_control_data['TreatmentExtendControl']['form_alias']);
+		$is_chemo_complications = true;
+	}
+	$this->set('is_chemo_complications', $is_chemo_complications);	
+	
---
+++ chum/hepato-billary/app/Plugin/ClinicalAnnotation/View/TreatmentExtendMasters/Hook/add.php
@@ -0,0 +1,7 @@
+<?php 
+
+	if($is_chemo_complications) {
+		$final_options['links']['top'] = $final_options['links']['top'].'/chemo_complications';
+	}
+
+?>
