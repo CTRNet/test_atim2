fafedb399f2ae5a6ba98fa5604b132322cd3e028
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-04-23 20:03:13 +0000

    Last commit before to migrate to v2.6.0

--- chum/hepato-billary/scripts/v2.6.0/custom_post_262.sql
+++ chum/hepato-billary/scripts/v2.6.0/custom_post_262.sql
@@ -1,7 +1,14 @@
 
+REPLACE INTO i18n (id,en,fr) 
+VALUES 
+('necrosis percentage','Necrosis &#37;','Nécrose &#37;'),
+('necrosis percentage list','Necrosis &#37; (List)','Nécrose &#37; (liste)'),
+('viability percentage','Viability &#37;','&#37; de viabilit');
+
+UPDATE datamart_structure_functions fct, datamart_structures str SET fct.flag_active = 0  WHERE label = 'print barcodes';
+
 -- --------------------------------------------------------------------------------------------------------
 -- VERSION
 -- --------------------------------------------------------------------------------------------------------
 
-UPDATE versions SET branch_build_number = '5711' WHERE version_number = '2.6.2';
-
+UPDATE versions SET branch_build_number = '5713' WHERE version_number = '2.6.2';
