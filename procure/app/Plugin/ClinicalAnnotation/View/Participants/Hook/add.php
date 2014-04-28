f065dd3945f015c33b410ba62d708165521462e1
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-04-28 19:01:54 +0000

    Migration to v2.6 (step 1)

--- procure/app/Plugin/ClinicalAnnotation/View/Participants/Hook/add.php
+++
@@ -1,3 +0,0 @@
-<?php
-	$final_options['override']['Participant.participant_identifier'] = $default_participant_identifier;
-	
--- procure/app/Plugin/InventoryManagement/Controller/Hook/Collections_add_presave_process.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$this->request->data['Collection']['collection_property'] = 'participant collection';
-	if($collection_data) {	
-		$this->Collection->addWritableField(array('collection_property', 'procure_chus_collection_specimen_sample_control_id'));
-	} else {
-		$this->Collection->addWritableField('collection_property');
-	}
-
-?>
