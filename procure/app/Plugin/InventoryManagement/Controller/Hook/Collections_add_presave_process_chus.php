4597cc46733627c9f565e8e4354db42566115aef
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-04-29 14:38:42 +0000

    Migration to v2.6 (step 3)

---
+++ procure/app/Plugin/InventoryManagement/Controller/Hook/Collections_add_presave_process_chus.php
@@ -0,0 +1,9 @@
+<?php
+	
+	// CHUS
+	pr('bip');
+	if($collection_data) {	
+		$this->Collection->addWritableField(array('procure_chus_collection_specimen_sample_control_id'));
+	}
+
+?>
--- procure/scripts/v2.6.0/custom_post262.sql
+++ procure/scripts/v2.6.0/custom_post262.sql
@@ -8,44 +8,23 @@ UPDATE treatment_controls SET disease_site = '' WHERE flag_active = 1;
 
 UPDATE menus SET flag_active = 1 WHERE use_link LIKE '/ClinicalAnnotation/EventMasters/listall/Clinical/%%Participant.id%%';
 
+UPDATE datamart_browsing_controls 
+SET flag_active_1_to_2 = 0, flag_active_2_to_1 = 0 
+WHERE id1 IN (SELECT id FROM datamart_structures WHERE model = 'DiagnosisMaster') AND id2 IN (SELECT id FROM datamart_structures WHERE model = 'DiagnosisMaster');
+UPDATE datamart_browsing_controls 
+SET flag_active_1_to_2 = 0, flag_active_2_to_1 = 0 
+WHERE id1 IN (SELECT id FROM datamart_structures WHERE model = 'AliquotReviewMaster') AND id2 IN (SELECT id FROM datamart_structures WHERE model = 'ViewAliquot');
+UPDATE datamart_browsing_controls 
+SET flag_active_1_to_2 = 0, flag_active_2_to_1 = 0 
+WHERE id1 IN (SELECT id FROM datamart_structures WHERE model = 'AliquotReviewMaster') AND id2 IN (SELECT id FROM datamart_structures WHERE model = 'SpecimenReviewMaster');
 
+UPDATE datamart_structure_functions fct SET fct.flag_active = 0 WHERE fct.label IN ('print barcodes', 'list all related diagnosis');
 
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('Datamart', '0', '', 'RAMQ', 'input',  NULL , '0', 'size=20', '', '', 'RAMQ', '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `margin`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='report_participant_identifiers_result'), (SELECT id FROM structure_fields WHERE `model`='0' AND `tablename`='' AND `field`='RAMQ' AND `type`='input' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0' AND `setting`='size=20' AND `default`='' AND `language_help`='' AND `language_label`='RAMQ' AND `language_tag`=''), '0', '6', '', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0');
+UPDATE structure_formats SET `flag_index`='0' WHERE structure_id=(SELECT id FROM structures WHERE alias='report_participant_identifiers_result') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='0' AND `tablename`='' AND `field`='BR_Nbr' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+UPDATE structure_formats SET `flag_index`='0' WHERE structure_id=(SELECT id FROM structures WHERE alias='report_participant_identifiers_result') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='0' AND `tablename`='' AND `field`='PR_Nbr' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
 
-
-
-
-
-
-
-
-
-
-
-
-
-PARTICIPANT IDENTIFIER REPORT
-----------------------------------------------------------------------------------------------------------
-Queries to desactivate 'Participant Identifiers' demo report
-----------------------------------------------------------------------------------------------------------
-UPDATE datamart_reports SET flag_active = 0 WHERE name = 'participant identifiers';
-UPDATE datamart_structure_functions SET flag_active = 0 WHERE link = (SELECT CONCAT('/Datamart/Reports/manageReport/',id) FROM datamart_reports WHERE name = 'participant identifiers');
-
-
-
-
-
-
-
-
-TODO
-----------------------------------------------------------------------------------------------------------
-Added new relationsips into databrowser
-Please flag inactive relationsips if required (see queries below). Don't forget Collection to Annotation, Treatment,Consent, etc if not requried.
-SELECT str1.model AS model_1, str2.model AS model_2, use_field FROM datamart_browsing_controls ctrl, datamart_structures str1, datamart_structures str2 WHERE str1.id = ctrl.id1 AND str2.id = ctrl.id2 AND (ctrl.flag_active_1_t
-o_2 = 1 OR ctrl.flag_active_2_to_1 = 1);
-UPDATE datamart_structure_functions fct, datamart_structures str SET fct.flag_active = 0 WHERE fct.datamart_structure_id = str.id AND/OR str.model IN ('Model1', 'Model2', 'Model...');
-Please flag inactive datamart structure functions if required (see queries below).
-UPDATE datamart_browsing_controls SET flag_active_1_to_2 = 0, flag_active_2_to_1 = 0 WHERE id1 IN (SELECT id FROM datamart_structures WHERE model IN ('Model1', 'Model2', 'Model...')) OR id2 IN (SELECT id FROM datamart_structu
-res WHERE model IN ('Model1', 'Model2', 'Model...'));
-Please change datamart_structures_relationships_en(and fr).png in appwebrootimgdataBrowser
-----------------------------------------------------------------------------------------------------------
+UPDATE versions SET branch_build_number = '???' WHERE version_number = '2.6.1';
