8ebbccb7dc589a008f1127cb163fac32b8edd0fd
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-05-14 15:23:14 +0000

    Add sardo data migration:GPA and Fam. histories.php Last Commit before live

---
+++ branches/qc/frsq/lady_davis/atim2/scripts/v2.6.0/custom_post_262.sql
@@ -0,0 +1,16 @@
+
+UPDATE datamart_structure_functions SET flag_active = 0 WHERE label = 'print barcodes';
+
+ALTER TABLE participants ADD COLUMN `qc_lady_sardo_data_migration_date` date DEFAULT NULL;
+ALTER TABLE participants_revs ADD COLUMN `qc_lady_sardo_data_migration_date` date DEFAULT NULL;
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('ClinicalAnnotation', 'Participant', 'participants', 'qc_lady_sardo_data_migration_date', 'date',  NULL , '0', '', '', '', 'sardo migration date', '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `margin`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='participants'), (SELECT id FROM structure_fields WHERE `model`='Participant' AND `tablename`='participants' AND `field`='qc_lady_sardo_data_migration_date' AND `type`='date' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='sardo migration date' AND `language_tag`=''), '3', '101', '', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0');
+INSERT INTO i18n (id,en,fr) VALUES ('sardo migration date','SARDO Data Migration','Migration Données SARDO');
+
+UPDATE versions SET branch_build_number = '5736' WHERE version_number = '2.6.2';
+
+
+
+
