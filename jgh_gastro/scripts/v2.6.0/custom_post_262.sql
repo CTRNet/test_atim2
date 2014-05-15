2af01fda0759397d1b09a05a8dd95eb516acbbcb
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-05-15 16:01:11 +0000

    Add missing fix for v262.

--- jgh_gastro/scripts/v2.6.0/custom_post_262.sql
+++ jgh_gastro/scripts/v2.6.0/custom_post_262.sql
@@ -2,4 +2,4 @@
 UPDATE datamart_structure_functions SET flag_active = 0 WHERE label = 'print barcodes';
 UPDATE datamart_structure_functions SET flag_active = 0 WHERE label = 'participant identifiers report';
 
-UPDATE versions SET branch_build_number = '5738' WHERE version_number = '2.6.2';
+UPDATE versions SET branch_build_number = '5740' WHERE version_number = '2.6.2';
