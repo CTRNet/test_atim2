c47b46f47d137b52bd4c446aabf68086963f8458
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-05-15 13:48:46 +0000

    Merged with trunk 5684-5709

---
+++ branches/qc/jgh_gastro/scripts/v2.6.0/custom_post_262.sql
@@ -0,0 +1,8 @@
+﻿
+UPDATE datamart_structure_functions SET flag_active = 0 WHERE label = 'print barcodes';
+
+UPDATE versions SET branch_build_number = '5737' WHERE version_number = '2.6.2';
+
+
+
+
