bd72448382d017dbbbb43745fa273b5a12102177
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-08-27 16:08:18 +0000

    Customized PROCURE Summary

--- procure_chum/app/Plugin/Datamart/Controller/Custom/ReportsController.php
+++ procure_chum/app/Plugin/Datamart/Controller/Custom/ReportsController.php
@@ -75,7 +75,16 @@ class ReportsControllerCustom extends ReportsController {
 		} else {
 			$this->redirect('/Pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
 		}
-			
+		if(isset($parameters['MiscIdentifier']['identifier_value'])) {
+			$no_labos  = array_filter($parameters['MiscIdentifier']['identifier_value']);
+			if($no_labos) $conditions[] = "MiscIdentifier.identifier_value IN ('".implode("','",$no_labos)."')";
+		} else if(isset($parameters['MiscIdentifier']['identifier_value_start'])) {
+			$identifier_value_start = (!empty($parameters['MiscIdentifier']['identifier_value_start']))? $parameters['MiscIdentifier']['identifier_value_start']: null;
+			$identifier_value_end = (!empty($parameters['MiscIdentifier']['identifier_value_end']))? $parameters['MiscIdentifier']['identifier_value_end']: null;
+			if($identifier_value_start) $conditions[] = "MiscIdentifier.identifier_value >= '$identifier_value_start'";
+			if($identifier_value_end) $conditions[] = "MiscIdentifier.identifier_value <= '$identifier_value_end'";
+		}  
+		
 		//Get Controls Data
 		$participant_model = AppModel::getInstance("ClinicalAnnotation", "Participant", true);
 		$query = "SELECT id,event_type, detail_tablename FROM event_controls WHERE flag_active = 1;";
@@ -90,6 +99,9 @@ class ReportsControllerCustom extends ReportsController {
 		$diagnosis_event_detail_tablename = $event_controls['procure diagnostic information worksheet']['detail_tablename'];
 		$pathology_event_control_id = $event_controls['procure pathology report']['id'];
 		$pathology_event_detail_tablename = $event_controls['procure pathology report']['detail_tablename'];
+		$query = "SELECT id FROM misc_identifier_controls WHERE flag_active = 1 AND misc_identifier_name = 'prostate bank no lab';";
+		$misc_identifier_control_id = null;
+		foreach($participant_model->query($query) as $res) $misc_identifier_control_id = $res['misc_identifier_controls']['id'];
 		
 		//Get participants data
 		$query = "SELECT 
@@ -107,18 +119,21 @@ class ReportsControllerCustom extends ReportsController {
 			DiagnosisEventDetail.aps_pre_surgery_total_ng_ml,
 			DiagnosisEventDetail.aps_pre_surgery_free_ng_ml,
 			DiagnosisEventDetail.aps_pre_surgery_date,
-			DiagnosisEventDetail.aps_pre_surgery_date_accuracy
+			DiagnosisEventDetail.aps_pre_surgery_date_accuracy,
+			MiscIdentifier.identifier_value
 			FROM participants Participant
 			LEFT JOIN event_masters DiagnosisEventMaster ON DiagnosisEventMaster.participant_id = Participant.id AND DiagnosisEventMaster.event_control_id = $diagnosis_event_control_id AND DiagnosisEventMaster.deleted <> 1
 			LEFT JOIN $diagnosis_event_detail_tablename DiagnosisEventDetail ON DiagnosisEventDetail.event_master_id = DiagnosisEventMaster.id
 			LEFT JOIN event_masters PathologyEventMaster ON PathologyEventMaster.participant_id = Participant.id AND PathologyEventMaster.event_control_id = $pathology_event_control_id AND PathologyEventMaster.deleted <> 1
 			LEFT JOIN $pathology_event_detail_tablename PathologyEventDetail ON PathologyEventDetail.event_master_id = PathologyEventMaster.id
+			LEFT JOIN misc_identifiers MiscIdentifier ON MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.deleted <> 1 AND MiscIdentifier.misc_identifier_control_id = $misc_identifier_control_id
 			WHERE Participant.deleted <> 1 AND ". implode(' AND ', $conditions);
 		$data = array();
 		foreach($participant_model->query($query) as $res) {
 			$participant_id = $res['Participant']['id'];
 			if(isset($data[$participant_id])) AppController::addWarningMsg('at least one participant is linked to more than one diagnosis or pathology worksheet');
 			$data[$participant_id]['Participant'] = $res['Participant'];
+			$data[$participant_id]['MiscIdentifier'] = $res['MiscIdentifier'];
 			$data[$participant_id]['EventMaster'] = $res['PathologyEventMaster'];
 			$data[$participant_id]['EventDetail'] = array_merge($res['PathologyEventDetail'], $res['DiagnosisEventDetail']);	
 			$data[$participant_id]['0'] = array(
@@ -129,9 +144,13 @@ class ReportsControllerCustom extends ReportsController {
 				'procure_pre_op_chemo' => '',
 				'procure_pre_op_radio' => '',
 				'procure_inaccurate_date_use' => '',
-				'procure_pre_op_psa_date' => ''
+				'procure_pre_op_psa_date' => '',
+				'procure_aborted_prostatectomy' => '',
+				'procure_curietherapy' => ''
 			);
 			$data[$participant_id]['EventDetail']['total_ngml'] = '';
+			$data[$participant_id]['TreatmentMaster']['start_date'] = '';
+			$data[$participant_id]['TreatmentMaster']['start_date_accuracy'] = '';
 		}
 		if(sizeof($data) > self::$display_limit) {
 			return array(
@@ -147,21 +166,31 @@ class ReportsControllerCustom extends ReportsController {
 		//Analyze participants treatments
 		$treatment_model = AppModel::getInstance("ClinicalAnnotation", "TreatmentMaster", true);
 		$treatment_control_id = $tx_controls['procure follow-up worksheet - treatment']['id'];
-		$conditions = array(
-				'TreatmentMaster.participant_id' => $participant_ids,
-				'TreatmentMaster.treatment_control_id' => $treatment_control_id,
-				'TreatmentMaster.start_date IS NOT NULL',
-				'OR' => array("TreatmentDetail.treatment_type LIKE '%radiotherapy%'", "TreatmentDetail.treatment_type LIKE '%hormonotherapy%'", "TreatmentDetail.treatment_type LIKE '%chemotherapy%'"));
 		$tx_join = array(
 				'table' => 'procure_txd_followup_worksheet_treatments',
 				'alias' => 'TreatmentDetail',
 				'type' => 'INNER',
 				'conditions' => array('TreatmentDetail.treatment_master_id = TreatmentMaster.id'));
+		$conditions = array(
+				'TreatmentMaster.participant_id' => $participant_ids,
+				'TreatmentMaster.treatment_control_id' => $treatment_control_id,
+				'TreatmentDetail.treatment_type' => 'prostatectomy');
+		$all_participants_prostatectomy = $treatment_model->find('all', array('conditions' => $conditions, 'joins' => array($tx_join)));
+		foreach($all_participants_prostatectomy as $new_prostatectomy) {
+			$participant_id = $new_prostatectomy['TreatmentMaster']['participant_id'];
+			$data[$participant_id]['TreatmentMaster']['start_date'] = $new_prostatectomy['TreatmentMaster']['start_date'];
+			$data[$participant_id]['TreatmentMaster']['start_date_accuracy'] = $new_prostatectomy['TreatmentMaster']['start_date_accuracy'];
+		}
+		$conditions = array(
+				'TreatmentMaster.participant_id' => $participant_ids,
+				'TreatmentMaster.treatment_control_id' => $treatment_control_id,
+				'TreatmentMaster.start_date IS NOT NULL',
+				'OR' => array("TreatmentDetail.treatment_type LIKE '%radiotherapy%'", "TreatmentDetail.treatment_type LIKE '%hormonotherapy%'", "TreatmentDetail.treatment_type LIKE '%chemotherapy%'"));
 		$all_participants_treatment = $treatment_model->find('all', array('conditions' => $conditions, 'joins' => array($tx_join)));
 		foreach($all_participants_treatment as $new_treatment) {
 			$participant_id = $new_treatment['TreatmentMaster']['participant_id'];
-			$pathology_report_date = $data[$participant_id]['EventMaster']['event_date'];
-			$pathology_report_date_accuracy = $data[$participant_id]['EventMaster']['event_date_accuracy'];
+			$pathology_report_date = $data[$participant_id]['TreatmentMaster']['start_date'];
+			$pathology_report_date_accuracy = $data[$participant_id]['TreatmentMaster']['start_date_accuracy'];
 			if($pathology_report_date) {
 				$administrated_treatment_types = array();
 				if(preg_match('/chemotherapy/', $new_treatment['TreatmentDetail']['treatment_type'])) $administrated_treatment_types[] = 'chemo';
@@ -178,6 +207,19 @@ class ReportsControllerCustom extends ReportsController {
 						foreach($administrated_treatment_types as $tx_type) $data[$participant_id][0]['procure_post_op_'.$tx_type] = 'y';
 					}
 				}
+			}
+		}
+		$conditions = array(
+				'TreatmentMaster.participant_id' => $participant_ids,
+				'TreatmentMaster.treatment_control_id' => $treatment_control_id,
+				'TreatmentDetail.treatment_type' => array('aborted prostatectomy', 'curietherapy'));
+		$all_participants_other_treatment = $treatment_model->find('all', array('conditions' => $conditions, 'joins' => array($tx_join)));
+		foreach($all_participants_other_treatment as $new_other_treatment) {
+			$participant_id = $new_other_treatment['TreatmentMaster']['participant_id'];
+			if($new_other_treatment['TreatmentDetail']['treatment_type'] == 'curietherapy') {
+				$data[$participant_id][0]['procure_curietherapy'] = 'y';
+			} else {
+				$data[$participant_id][0]['procure_aborted_prostatectomy'] = 'y';
 			}
 		}
 		
