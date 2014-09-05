bc675974d530658523e1064fc1994aedc4afafb9
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-09-05 21:17:12 +0000

    new version development

--- terry_fox_cpcbn/app/Plugin/ClinicalAnnotation/Controller/Hook/TreatmentMasters_add_format.php
+++ terry_fox_cpcbn/app/Plugin/ClinicalAnnotation/Controller/Hook/TreatmentMasters_add_format.php
@@ -1,7 +1,6 @@
 <?php 
 	if($tx_control_data['TreatmentControl']['tx_method'] == 'surgery' && $tx_control_data['TreatmentControl']['disease_site'] == 'RP') {
 		$participant_RPs = $this->TreatmentMaster->find('count', array('conditions' => array('TreatmentMaster.participant_id'=> $participant_id, 'TreatmentControl.id' => $tx_control_id)));
-		pr($participant_RPs);
 		if($participant_RPs) {
 			$this->flash(__('a RP can not be created twice for the same participant'), '/ClinicalAnnotation/TreatmentMasters/listall/'.$participant_id);
 			return;
