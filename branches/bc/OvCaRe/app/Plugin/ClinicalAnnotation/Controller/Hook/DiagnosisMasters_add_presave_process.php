d43215058ba9053418b45d3eab57220c5252ede2
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-10-10 15:29:00 +0000

    New design and migration in progress

--- branches/bc/OvCaRe/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_add_presave_process.php
+++ branches/bc/OvCaRe/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_add_presave_process.php
@@ -3,16 +3,16 @@
 	if($dx_ctrl['DiagnosisControl']['controls_type'] == 'primary diagnosis unknown') {
 		$this->DiagnosisMaster->addWritableField(array('ovcare_tumor_site'));
 		$this->request->data['DiagnosisMaster']['ovcare_tumor_site'] = 'unknown';	
-	} else if(isset($this->request->data['DiagnosisMaster']['ovcare_tumor_site'])) {
-		if($dx_ctrl['DiagnosisControl']['controls_type'] == 'ovary or endometrium') {
+	} else if(isset($this->request->data['DiagnosisMaster']['ovcare_tumor_site']) && $dx_ctrl['DiagnosisControl']['category'] != 'secondary') {
+		if($dx_ctrl['DiagnosisControl']['controls_type'] == 'ovary or endometrium tumor') {
 			if(!in_array($this->request->data['DiagnosisMaster']['ovcare_tumor_site'], array('female genital-ovary','female genital-endometrium'))) {
 				$submitted_data_validates = false;
-				$this->DiagnosisMaster->validationErrors[][] = __('wrong selected tumor site');
+				$this->DiagnosisMaster->validationErrors[][] = __('wrong selected tumor site').' : '.__('either ovary or endometrium tumor tumor site should be selected');
 			}			
 		} else {
 			if(in_array($this->request->data['DiagnosisMaster']['ovcare_tumor_site'], array('female genital-ovary','female genital-endometrium'))) {
 				$submitted_data_validates = false;
-				$this->DiagnosisMaster->validationErrors[][] = __('wrong selected tumor site');
+				$this->DiagnosisMaster->validationErrors[][] = __('wrong selected tumor site').' : '.__('selected tumor site should be different than ovary or endometrium tumor');
 			}
 		}
 	}
