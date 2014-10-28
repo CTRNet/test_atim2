829d87f1a59807502132fc4452b524f82696b2d3
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-10-28 19:52:24 +0000

    Added change to participant identifer from Naveen

--- childrens/scripts/ccbr_scripts/ccbr_v1.0_custom.sql
+++ childrens/scripts/ccbr_scripts/ccbr_v1.0_custom.sql
@@ -1,4 +1,13 @@
 -- CCBR Customization Script
 -- Version: v1.00
--- ATiM Version: v2.5.2
--- Notes: Run against an upgraded v2.5.2 CCBR installation.
+-- ATiM Version: v2.5.1
+-- Notes: Run against an upgraded v2.5.1 CCBR installation.
+
+UPDATE 
+	`structure_validations`
+SET 
+	`rule` = '/^[0-9]+$/'
+WHERE 
+	`structure_field_id` = (SELECT `id` FROM `structure_fields` WHERE `field` = 'participant_identifier' AND `tablename` = 'participants')
+AND 
+	`rule` = '/^CCBR[0-9]+$/';
