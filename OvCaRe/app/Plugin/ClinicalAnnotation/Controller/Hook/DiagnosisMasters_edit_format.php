f0a8b21511d1733b5e5edc423a798d9d794bb654
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-10-30 19:36:04 +0000

    Script to migrate TFRI data (in progress)

--- OvCaRe/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_edit_format.php
+++ OvCaRe/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_edit_format.php
@@ -3,7 +3,6 @@
 	if(!is_null($redefined_primary_control_id) && $new_primary_ctrl['DiagnosisControl']['controls_type'] == 'ovary or endometrium tumor') {
 		$this->DiagnosisMaster->tryCatchQuery("UPDATE diagnosis_masters SET ovcare_tumor_site = 'female genital-ovary' WHERE id = $diagnosis_master_id;");
 		$dx_master_data['DiagnosisMaster']['ovcare_tumor_site'] = 'female genital-ovary';
-		$this->DiagnosisMaster->updateCalculatedFields($participant_id);
 	}
 
 ?>
