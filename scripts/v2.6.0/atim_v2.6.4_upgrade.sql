881da257306779267c84d6b2968ab898d838b123
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-11-07 16:24:21 +0000

    Issue #3114:Use of Manage reusable identifiers generates bugs

---
+++ scripts/v2.6.0/atim_v2.6.4_upgrade.sql
@@ -0,0 +1,21 @@
+﻿-- ------------------------------------------------------
+-- ATiM v2.6.4 Upgrade Script
+-- version: 2.6.4
+--
+-- For more information: 
+--    http://www.ctrnet.ca/mediawiki/index.php/Main_Page
+-- ------------------------------------------------------
+
+-- -----------------------------------------------------------------------------------------------------------------------------------
+-- Issue #3114: Use of Manage reusable identifiers generates bug
+-- -----------------------------------------------------------------------------------------------------------------------------------
+
+UPDATE menus SET use_link = '/Administrate/ReusableMiscIdentifiers/index' WHERE use_link = '/Administrate/MiscIdentifiers/index';
+INSERT INTO i18n (id,en,fr) VALUES ('manage', 'Manage', 'Gérer');
+
+-- -----------------------------------------------------------------------------------------------------------------------------------
+-- Versions table
+-- -----------------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO `versions` (version_number, date_installed, trunk_build_number, branch_build_number) 
+VALUES('2.6.4', NOW(),'???','n/a');
