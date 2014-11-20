764613982e763415694380902eb8ce7959ddb48a
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-11-20 20:21:50 +0000

    redefine unknown primary. Copy notes to clinical diagnosis.

--- branches/bc/OvCaRe/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_edit_format.php
+++ branches/bc/OvCaRe/app/Plugin/ClinicalAnnotation/Controller/Hook/DiagnosisMasters_edit_format.php
@@ -1,8 +1,14 @@
 <?php
 
-	if(!is_null($redefined_primary_control_id) && $new_primary_ctrl['DiagnosisControl']['controls_type'] == 'ovary or endometrium tumor') {
-		$this->DiagnosisMaster->tryCatchQuery("UPDATE diagnosis_masters SET ovcare_tumor_site = 'female genital-ovary' WHERE id = $diagnosis_master_id;");
-		$dx_master_data['DiagnosisMaster']['ovcare_tumor_site'] = 'female genital-ovary';
+	if(!is_null($redefined_primary_control_id)) {
+		if($new_primary_ctrl['DiagnosisControl']['controls_type'] == 'ovary or endometrium tumor') {
+			$this->DiagnosisMaster->tryCatchQuery("UPDATE diagnosis_masters SET ovcare_tumor_site = 'female genital-ovary' WHERE id = $diagnosis_master_id;");
+			$dx_master_data['DiagnosisMaster']['ovcare_tumor_site'] = 'female genital-ovary';
+		}
+		$this->DiagnosisMaster->tryCatchQuery("UPDATE diagnosis_masters SET ovcare_clinical_diagnosis = notes, notes = '' WHERE id = $diagnosis_master_id;");
+		$dx_master_data['DiagnosisMaster']['ovcare_clinical_diagnosis'] = $dx_master_data['DiagnosisMaster']['notes'];
+		$dx_master_data['DiagnosisMaster']['notes'] = '';
+		if(!$this->DiagnosisMaster->save(array())) $this->redirect('/Pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true); ;
 	}
 
 ?>
