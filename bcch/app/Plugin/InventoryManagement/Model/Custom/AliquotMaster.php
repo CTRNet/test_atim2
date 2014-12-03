bb3119b7029c2e7deca80407dc1ad9c1f893a267
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2014-12-03 18:54:12 +0000

    Barcode now a random 10-digit value. Added aliquot label generation code

--- bcch/app/Plugin/InventoryManagement/Model/Custom/AliquotMaster.php
+++ bcch/app/Plugin/InventoryManagement/Model/Custom/AliquotMaster.php
@@ -24,95 +24,59 @@ class AliquotMasterCustom extends AliquotMaster {
 		return $return;
 	}
 	
-	function generateDefaultAliquotLabel($view_sample, $aliquot_control_data) {
+	function generateAliquotLabel($view_sample, $aliquot_control_data, $current_aliquot_count) {
 		// Parameters check: Verify parameters have been set
-/*		if(empty($view_sample) || empty($aliquot_control_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
-			
-		$default_aliquot_label = '';
-		if($view_sample['ViewSample']['bank_id'] == 4) {
-			//*** Prostate Bank ***		
-			$idenitfier_value = empty($view_sample['ViewSample']['identifier_value'])? '?': $view_sample['ViewSample']['identifier_value'];
-			$visit_label = empty($view_sample['ViewSample']['visit_label'])? '?': $view_sample['ViewSample']['visit_label'];
-			$label = '-?';
-			switch($view_sample['ViewSample']['sample_type'].'-'.$aliquot_control_data['AliquotControl']['aliquot_type']) {
-				case 'blood-tube':
-					$this->SampleMaster = AppModel::getInstance('InventoryManagement', 'SampleMaster', true);
-					$sample_data = $this->SampleMaster->find('first', array('conditions' => array('SampleMaster.id' => $view_sample['ViewSample']['sample_master_id']), 'recursive' => '0'));
-					if($sample_data) {
-						switch($sample_data['SampleDetail']['blood_type']) {
-							case 'gel SST':
-								$label = '-SRB';
-								break;
-							case 'paxgene':
-								$label = '-RNB';
-								break;
-							case 'EDTA':
-								$label = '-EDB';
-								break;
-						}
-					}
-					break;
-				case 'blood-whatman paper':
-					$label = '-WHT';
-					break;
-				case 'serum-tube':
-					$label = '-SER';
-					break;
-				case 'plasma-tube':
-					$label = '-PLA';
-					break;
-				case 'pbmc-tube':
-					$label = '-BFC';
-					break;
-				case 'urine-cup':
-					$label = '-URI';
-					break;
-				case 'centrifuged urine-tube':
-					$label = '-URN';
-					break;
-				case 'tissue-block':
-					$label = '-FRZ';
-					break;
-				case 'rna-tube':
-					$label = '-RNA';
-					break;
-				case 'dna-tube':
-					$label = '-DNA';
-					break;
-			}
-			$default_aliquot_label = $idenitfier_value . ' ' . $visit_label . ' ' . $label;
-		} else {
-			// *** Other Bank ***
-			
-			// Set date for aliquot label			
-			$aliquot_creation_date = '';			
-			if($view_sample['ViewSample']['sample_category'] == 'specimen'){
-				// Specimen Aliquot
-				if(!isset($this->SpecimenDetail)){
-					$this->SpecimenDetail = AppModel::getInstance('InventoryManagement', 'SpecimenDetail', true);
-				}
-				$specimen_detail = $this->SpecimenDetail->getOrRedirect($view_sample['ViewSample']['sample_master_id']);
-				
-				$aliquot_creation_date = $specimen_detail['SpecimenDetail']['reception_datetime'];
-	
-			}else{
-				// Derviative Aliquot
-				if(!isset($this->DerivativeDetail)){
-					$this->DerivativeDetail = AppModel::getInstance('InventoryManagement', 'DerivativeDetail', true);
-				}
-				
-				$derivative_detail = $this->DerivativeDetail->getOrRedirect($view_sample['ViewSample']['sample_master_id']);
-				
-				$aliquot_creation_date = $derivative_detail['DerivativeDetail']['creation_datetime'];
-			} 
-			
-			$default_aliquot_label =
-				(empty($view_sample['ViewSample']['qc_nd_sample_label'])? 'n/a' : $view_sample['ViewSample']['qc_nd_sample_label']) .
-				(empty($aliquot_creation_date)? '' : ' ' . substr($aliquot_creation_date, 0, strpos($aliquot_creation_date," ")));
+		if(empty($view_sample) || empty($aliquot_control_data)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+		
+		// Participant Identifier + Sample Type + Sample #(AI) + Aliquot #(AI) Ex: C0001BL0101	
+		$aliquot_label = '';
+		
+		// Get current participant identifier
+		$participant_identifier = 'C00000';
+		
+		// $identifier_value = empty($view_sample['ViewSample']['identifier_value'])? '?': $view_sample['ViewSample']['identifier_value'];
+
+		// Set sample label based on sample type				
+		$sample_label = '';
+		switch($view_sample['ViewSample']['sample_type']) {
+			case 'blood':
+				$sample_label = 'BL';
+				break;
+			case 'bone marrow':
+				$sample_label = 'BM';
+				break;
+			case 'cell culture':
+				$sample_label = 'CC';
+				break;
+			case 'cell lysate':
+				$sample_label = 'CL';
+				break;
+			case 'csf':
+				$sample_label = 'CSF';
+				break;
+			case 'rna':
+				$sample_label = 'RNA';
+				break;
+			case 'saliva':
+				$sample_label = 'SL';
+				break;
+			case 'tissue':
+				$sample_label = 'TI';
+				break;
+			case 'tissue suspension':
+				$sample_label = 'TS';
+				break;
 		}
-		*/
-		$default_aliquot_label = "BCCH Custom Label";
-		return $default_aliquot_label;
+		
+		// Get sample number for current participant
+		$sample_count = '01'; 
+		
+		// Get sample number for current participant
+		// $aliquot_count = '01'; 
+		
+		$aliquot_label = $participant_identifier . '' . $sample_label . $sample_count . $current_aliquot_count;
+		
+		return $aliquot_label;
 	}
 	
 	function generateAliquotBarcode($aliquotIDs) {
@@ -121,30 +85,38 @@ class AliquotMasterCustom extends AliquotMaster {
 		$aliquots_to_update = 
 			$this->find('all', array('conditions' => array('AliquotMaster.id' => $aliquotIDs)));			
 		
-		// Find next barcode to use
-		$new_barcode = $this->findNextBarcode();
-		
 		// Update barcode for selected aliquots
 		foreach($aliquots_to_update as $aliquot) {
+			// Find next barcode to use
+			$new_barcode = $this->findNextBarcode();
+			
 			$aliquot_data = array('AliquotMaster' => array('barcode' => $new_barcode), 'AliquotDetail' => array());
 			$this->id = $aliquot['AliquotMaster']['id'];
 			$this->data = null;
 			$this->addWritableField(array('barcode'));
 			$this->save($aliquot_data, false);
 		}
-	
 	}
 	
 	function findNextBarcode() {
 		
-		// Find aliquot with the highest barcode
-		$lastAliquot = 
-			$this->find('first', array('conditions' => array('AliquotMaster.deleted' => array(0,1)),
-			'order' => array('AliquotMaster.barcode' => 'desc')));
-		$next_barcode = $lastAliquot['AliquotMaster']['barcode'];
+		// Find all barcodes, including for deleted aliquots
+		$barcodes = 
+			$this->find('all', array('conditions' => array('AliquotMaster.deleted' => array(0,1)),
+			'fields' => array('AliquotMaster.barcode')));
 		
-		// Increment last used barcode by 1	
- 		$next_barcode = $next_barcode + 1;
+		// Generate random barcodes until one not used is created
+		do {
+			$valid_barcode = true;
+			$next_barcode = rand(1000000000, 9999999999);
+			$next_barcode = (string)$next_barcode;
+			// Search existing set of barcodes for a match to newly generated barcode
+			foreach ($barcodes as $key => $val) {
+       			if ($val['AliquotMaster']['barcode'] === $next_barcode) {
+           			$valid_barcode = false;
+       			}
+   			}
+		} while (!$valid_barcode);
 		return $next_barcode;
 	}
 }
