dd5654d3abff2ccc6e224378887fb0bf3683f0a0
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-01-14 18:27:16 +0000

    Commiting changes before working copy rebuild

--- bcch/scripts/bcch_scripts/bcch_v0.1_custom.sql
+++ bcch/scripts/bcch_scripts/bcch_v0.1_custom.sql
@@ -5,10 +5,10 @@
 -- Update bank name for version tracking during customization
 REPLACE INTO `i18n` (`id`, `en`, `fr`)
 	VALUES ('core_installname', "BCCH Biobank - BCCH v0.1", '');
-	
---  --------------------------------------------------------------------------
---	v2.6.0 Upgrade changes
---	--------------------------------------------------------------------------
+
+--  =========================================================================
+--	Eventum ID: #3108 - Core Upgrade - v2.6.3
+--	=========================================================================	
 
 -- Deactivate Participant Identifiers demo report
 UPDATE datamart_reports SET flag_active = 0 WHERE name = 'participant identifiers';
@@ -67,10 +67,19 @@ ADD COLUMN `study_summary_id` INT(11) NULL DEFAULT NULL AFTER `event_master_id` 
 ALTER TABLE `collections_revs` 
 ADD COLUMN `study_summary_id` INT(11) NULL DEFAULT NULL AFTER `event_master_id` ;
 
+-- Add to collections structure
 INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
 ('InventoryManagement', 'Collection', 'collections', 'study_summary_id', 'select', (SELECT id FROM structure_value_domains WHERE domain_name='study_list') , '0', '', '', '', 'study summary id', '');
 INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
 ((SELECT id FROM structures WHERE alias='collections'), (SELECT id FROM structure_fields WHERE `model`='Collection' AND `tablename`='collections' AND `field`='study_summary_id' AND `type`='select' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='study_list')  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='study summary id' AND `language_tag`=''), '0', '5', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '1', '0');
+
+-- Add to view
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='view_collection'), (SELECT id FROM structure_fields WHERE `model`='Collection' AND `tablename`='collections' AND `field`='study_summary_id' AND `type`='select' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='study_list')  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='study summary id' AND `language_tag`=''), '1', '4', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '1', '1', '0');
+
+-- Linked collections
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='linked_collections'), (SELECT id FROM structure_fields WHERE `model`='Collection' AND `tablename`='collections' AND `field`='study_summary_id' AND `type`='select' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='study_list')  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='study summary id' AND `language_tag`=''), '1', '4', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0');
 
 REPLACE INTO `i18n` (`id`, `en`, `fr`) VALUES
 ('study summary id', "Study", '');
@@ -96,8 +105,74 @@ INSERT INTO `misc_identifier_controls` (`misc_identifier_name`, `flag_active`, `
 INSERT INTO `misc_identifiers` (`participant_id`, `identifier_value`, `misc_identifier_control_id`, `created`, `created_by`, `modified`, `modified_by`, `deleted`, `flag_unique`)
 SELECT `id`, `participant_identifier`, (SELECT `id` FROM `misc_identifier_controls` WHERE `misc_identifier_name` = 'CCBR Identifier'), NOW(), (SELECT `id` FROM `users` WHERE `username` = 'administrator'), NOW(), (SELECT `id` FROM `users` WHERE `username` = 'administrator'), 0, 1 FROM `participants`;
 
--- Update all existing identifiers to prep for new values
+-- Clear all existing identifiers to prep for new identifier values
 UPDATE `participants` SET `participant_identifier` = '';
+
+--  Add new identifiers for all existing participants
+DROP TABLE IF EXISTS `temp_participant_ids`;
+CREATE TABLE `temp_participant_ids` (
+	`id` MEDIUMINT NOT NULL AUTO_INCREMENT,
+	`new_id` CHAR(5) DEFAULT NULL,
+	PRIMARY KEY (`id`)
+) ;
+
+DROP PROCEDURE IF EXISTS `create_new_ids`;
+
+DELIMITER |
+CREATE PROCEDURE `create_new_ids`()
+BEGIN
+	DECLARE num_participants INT;
+	DECLARE generated_id CHAR(5);
+	
+	SET generated_id = 'C0000';
+	SELECT COUNT(*) INTO num_participants FROM `participants`;
+	
+	WHILE num_participants > 0 DO
+		INSERT INTO `temp_participant_ids` (`new_id`) VALUES (`generated_id`);
+		SET num_participants = num_participants - 1;
+	END WHILE;
+END|
+DELIMITER ;
+
+CALL create_new_ids();
+
+DROP PROCEDURE IF EXISTS `create_new_ids`;
+
+/*
+
+INSERT INTO `temp_participant_ids` (`new_id`
+SELECT `id` AS CONVERT (CHAR(5), CHAR(ASCII('C') + (`id`),
+FROM `participants`;
+
+create table T (
+    CoreValue int not null,
+    DisplayValue as CONVERT(varchar(10),(CoreValue / 26)+1) + CHAR(ASCII('A') + (CoreValue-1) % 26)
+)
+go
+insert into T (CoreValue)
+select ROW_NUMBER() OVER (ORDER BY so1.object_id)
+from sys.objects so1,sys.objects so2
+go
+select * from T
+
+'ABC' + RIGHT('000' + LTRIM(IdField),3)
+select top 1 
+    case when SequenceChar = 'Z' then
+        cast((SequenceNum + 1) as varchar) + 'A'
+    else
+        cast(SequenceNum as varchar) + char(ascii(SequenceChar) + 1)
+    end as NextSequence
+from (
+    select Value, 
+        cast(substring(Value, 1, CharIndex - 1) as int) as SequenceNum, 
+        substring(Value, CharIndex, len(Value)) as SequenceChar
+    from (
+        select Value, patindex('%[A-Z]%', Value) as CharIndex
+        from MyTable
+    ) a
+) b
+order by SequenceNum desc, SequenceChar desc
+*/
 
 -- Fix validations for new format using C00001, required and unique
 UPDATE `structure_validations` SET `rule`='notEmpty' WHERE `language_message`='error_participant identifier required';
@@ -109,3 +184,18 @@ WHERE `structure_field_id` = 521 AND `rule` = '/^[0-9]+$/' AND `language_message
 -- New error message
 REPLACE INTO `i18n` (`id`, `en`, `fr`) VALUES
 ('ccbr error participant identifier', "BCCH Biobank Identifier requires format: C00000 (C, followed by 5 digits)", '');
+
+--  =========================================================================
+--	Eventum ID: #3111 - Barcode generation
+--	=========================================================================
+
+-- Hide barcode field for aliquots. Will be auto-generated.
+UPDATE structure_formats SET `flag_add`='0', `flag_addgrid`='0', `flag_editgrid_readonly`='1' WHERE structure_id=(SELECT id FROM structures WHERE alias='aliquot_masters') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='barcode' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+
+--  =========================================================================
+--	Eventum ID: #3110 - Label field for aliquots
+--	=========================================================================
+
+-- Set label to read-only on all add/edit forms
+UPDATE structure_formats SET `flag_add_readonly`='1', `flag_addgrid_readonly`='1', `flag_editgrid_readonly`='1' WHERE structure_id=(SELECT id FROM structures WHERE alias='aliquot_masters') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='AliquotMaster' AND `tablename`='aliquot_masters' AND `field`='aliquot_label' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0');
+
