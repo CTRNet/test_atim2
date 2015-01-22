5cfd79e3dfcce23bad4db600502bc38c41a6b994
aaronsuggitt <aaronsuggitt@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-01-22 22:57:18 +0000

    Fixed participant identifer padding with leading zeros.

--- branches/bc/bcch/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_add_postsave_process.php
+++ branches/bc/bcch/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_add_postsave_process.php
@@ -2,7 +2,7 @@
 	
 	// --------------------------------------------------------------------------------
 	// Save participant identifier. Format: C00000 (C, followed by 5 digits)
-	// Example: C00001, C00002, C00003, C00004 array(1,0) 
+	// Example: C00001, C00002, C00003, C00004 
 	// -------------------------------------------------------------------------------- 
 	
 	// Find last used ID
@@ -13,10 +13,20 @@
 	$lastID = $lastParticipant['Participant']['participant_identifier'];
 
 	// Increment last used participant ID by one
-	$newID = substr($lastID, 1);
+	$newID = ltrim($lastID, 'C');
 	$newID = $newID+1;
-	$newID = "C".$newID;
-	
+	if ($newID < 10) {
+		$newID = 'C0000'.$newID;
+	} elseif ($newID < 100) {
+		$newID = 'C000'.$newID;
+	} elseif ($newID < 1000) {
+		$newID = 'C00'.$newID;
+	} elseif ($newID < 10000) {
+		$newID = 'C0'.$newID;
+	} else {
+		$newID = 'C'.$newID;
+	}
+
 	// Update record with new participant ID
 	$query_to_update = "UPDATE participants SET participants.participant_identifier = "."'".$newID."'"." WHERE participants.id = ".$this->Participant->id.";";
 	$this->Participant->tryCatchQuery($query_to_update);
