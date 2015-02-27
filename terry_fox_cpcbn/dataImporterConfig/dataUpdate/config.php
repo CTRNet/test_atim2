139d6cbc2a2cd5b090addee68a40198e5c7d01cc
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-02-27 19:41:05 +0000

    Data update script (final version to test)

--- terry_fox_cpcbn/dataImporterConfig/dataUpdate/config.php
+++ terry_fox_cpcbn/dataImporterConfig/dataUpdate/config.php
@@ -18,7 +18,12 @@ $migration_user_id = 1;
 $files_path = "C:/_Perso/Server/tfri_cpcbn/data/update2014";
 // Serial number $windows_xls_offset = 36526 & $mac_xls_offset = 35064;
 $excel_files_names = array(
-	'UHN -RTupdate.xls' => $mac_xls_offset
+	'UHN -RTupdate_nl_revised_20150227.xls' => $mac_xls_offset,
+	'mcgill2014 update_nl_revised_20150227.xls' => $windows_xls_offset,
+	'Nov-25-2014 VPC Update TFRI-CPCBN Ali-1_nl_revised_20150227.xls' => $mac_xls_offset,
+	'TFRI-CPCBN-clinical dataV2(1)   UpDate  2015-01-14 -HDQ_nl_revised_20150227.xls' => $mac_xls_offset,
+	'TMA3 mcgill update patient and Pathology sent_nl_revised_20150227.xls' => $mac_xls_offset,
+	'update 2014fleshner_nl_revised_2015022.xls' => $windows_xls_offset
 );
 
 ?>
