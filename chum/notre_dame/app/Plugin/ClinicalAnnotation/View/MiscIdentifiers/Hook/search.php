fe696b465bd0fa78b78037aa2adde41262ae7e28
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-03-03 21:50:53 +0000

    Remove unused line

--- chum/notre_dame/app/Plugin/ClinicalAnnotation/View/MiscIdentifiers/Hook/search.php
+++ chum/notre_dame/app/Plugin/ClinicalAnnotation/View/MiscIdentifiers/Hook/search.php
@@ -1,3 +1,2 @@
 <?php
-	$structure_links['index']['event'] = '/ClinicalAnnotation/Participants/profile/%%Participant.id%%';
 	$final_options['links']['index']['lab direct access'] =  array('link' => '/ClinicalAnnotation/EventMasters/listall/lab/%%Participant.id%%', 'icon' => 'annotation');	
