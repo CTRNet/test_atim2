39670de922687f6e024c286bd8c187b48ca6874c
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-03-03 21:41:44 +0000

    Add button to access event lab from misc identifier search + add control on study deletion

---
+++ chum/notre_dame/app/Plugin/ClinicalAnnotation/View/MiscIdentifiers/Hook/search.php
@@ -0,0 +1,3 @@
+<?php
+	$structure_links['index']['event'] = '/ClinicalAnnotation/Participants/profile/%%Participant.id%%';
+	$final_options['links']['index']['lab direct access'] =  array('link' => '/ClinicalAnnotation/EventMasters/listall/lab/%%Participant.id%%', 'icon' => 'annotation');	
