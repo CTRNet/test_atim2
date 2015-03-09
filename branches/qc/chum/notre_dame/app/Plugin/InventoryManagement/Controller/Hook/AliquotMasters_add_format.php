52a02151da18fd7fb3dc3fef00de6717d8193071
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-03-09 18:24:25 +0000

    Add default in stock value. Clean up existing in stock value for edta or gel sst blood tubes, urine tubes and paxgene tube. Add 2 scripts to create bank identifiers to reuse + empty barcode clean up.

--- branches/qc/chum/notre_dame/app/Plugin/InventoryManagement/Controller/Hook/AliquotMasters_add_format.php
+++ branches/qc/chum/notre_dame/app/Plugin/InventoryManagement/Controller/Hook/AliquotMasters_add_format.php
@@ -7,12 +7,14 @@ if($aliquot_control_id == 54){
 // Set default aliquot label(s)
 // --------------------------------------------------------------------------------
 $default_aliquot_labels = array();
+$default_in_stocks = array();
 foreach($samples as $view_sample){
 	$default_aliquot_label = $this->AliquotMaster->generateDefaultAliquotLabel($view_sample, $aliquot_control);
 	$default_aliquot_labels[$view_sample['ViewSample']['sample_master_id']] = $default_aliquot_label;
+	if(preg_match('/(\-EDB)|(\-SRB)|(\-URI)/', $default_aliquot_label)) $default_in_stocks[$view_sample['ViewSample']['sample_master_id']] = 'no';
 }
 $this->set('default_aliquot_labels', $default_aliquot_labels);
-
+$this->set('default_in_stocks', $default_in_stocks);
 
 //remove default storage date
 unset($override_data['AliquotMaster.storage_datetime']);
