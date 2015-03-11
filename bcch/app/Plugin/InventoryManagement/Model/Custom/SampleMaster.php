a0ff463f97316ddfd020d00dc11758ed9ee7bb13
sfung1 <sfung1@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-03-11 20:50:55 +0000

    Corrected Sample Code Abbreviation

--- bcch/app/Plugin/InventoryManagement/Model/Custom/SampleMaster.php
+++ bcch/app/Plugin/InventoryManagement/Model/Custom/SampleMaster.php
@@ -1,17 +1,17 @@
 <?php
 class SampleMasterCustom extends SampleMaster {
 
-	var $useTable = 'sample_masters';	
-	var $name = 'SampleMaster';	
-	
+	var $useTable = 'sample_masters';
+	var $name = 'SampleMaster';
+
 	function generateSampleLabel($collection_data, $sample_type, $sample_master_id) {
 		// Parameters check: Verify parameters have been set
 		if(empty($sample_master_id) || empty($collection_data) || empty($sample_type)) AppController::getInstance()->redirect('/pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
-		
+
 		// Set participant identifer
 		$participant_identifier = $collection_data['Participant']['participant_identifier'];
-		
-		// Set sample label based on sample type				
+
+		// Set sample label based on sample type
 		$sample_label = 'XX';
 		switch($sample_type) {
 			case 'blood':
@@ -28,9 +28,9 @@ class SampleMasterCustom extends SampleMaster {
 				break;
 			case 'cord blood':
 				$sample_label = 'CB';
-				break;				
+				break;
 			case 'csf':
-				$sample_label = 'CS';
+				$sample_label = 'CF';
 				break;
 			case 'dna':
 				$sample_label = 'DN';
@@ -40,13 +40,13 @@ class SampleMasterCustom extends SampleMaster {
 				break;
 			case 'ccbr leukapheresis':
 				$sample_label = 'LK';
-				break;							
+				break;
 			case 'pbmc':
 				$sample_label = 'MC';
-				break;					
+				break;
 			case 'plasma':
 				$sample_label = 'PL';
-				break;							
+				break;
 			case 'rna':
 				$sample_label = 'RN';
 				break;
@@ -56,15 +56,12 @@ class SampleMasterCustom extends SampleMaster {
 			case 'ccbr stem cells':
 				$sample_label = 'SC';
 				break;
-			case 'saliva':
-				$sample_label = 'SW';
-				break;	
 			case 'stool':
-				$sample_label = 'SB';
-				break;					
+				$sample_label = 'ST';
+				break;
 			case 'ccbr swab':
 				$sample_label = 'SB';
-				break;											
+				break;
 			case 'tissue':
 				$sample_label = 'TI';
 				break;
@@ -73,51 +70,60 @@ class SampleMasterCustom extends SampleMaster {
 				break;
 			case 'urine':
 				$sample_label = 'UR';
-				break;				
+				break;
+			case 'csf cells':
+				$sample_label = 'CE';
+				break;
+			case 'csf supernatant':
+				$sample_label = 'CS';
+				break;
+			case 'blood cell':
+				$sample_label = 'BC';
+				break;
 		}
-		
+
 		// Set participant ID for finding collections and sample counts
 		$participant_id = $collection_data['Collection']['participant_id'];
-		
+
 		// Find all collection ID's for current participant
-		$participant_collection_ids = 
+		$participant_collection_ids =
 			$this->Collection->find('list', array('conditions' => array('Collection.participant_id' => $participant_id), 'fields' => array('Collection.id')));
 
 		// For each collecton, find number of samples in each collection
 		$total_sample_count = 0;
 		$collection_sample_count = 0;
-		
+
 		foreach ($participant_collection_ids as $collection_id) {
 			$collection_sample_count =
 				$this->find('count', array('conditions' => array('SampleMaster.collection_id' => $participant_collection_ids[$collection_id])));
-		
+
 			// Add current collection sample count to total sample count
 			$total_sample_count = $total_sample_count + $collection_sample_count;
 			$collection_sample_count = 0;
 		}
-		
+
 		// Pad sample count to three digits
 		if ($total_sample_count < 10) {
 			$total_sample_count = '00'.$total_sample_count;
 		} elseif ($total_sample_count < 100) {
 			$total_sample_count = '0'.$total_sample_count;
 		}
-				
-		// Set sample code 
+
+		// Set sample code
 		$sample_code = $participant_identifier . $sample_label . $total_sample_count;
 
 		// Save sample code
 		$query_to_update = null;
 		$query_to_update = "UPDATE sample_masters SET sample_code = '$sample_code' WHERE id = $sample_master_id;";
 		$this->tryCatchQuery($query_to_update);
-		
+
 		// Update revs table
 		$this->tryCatchQuery(str_replace("sample_masters", "sample_masters_revs", $query_to_update));
-		
+
 		// Update ViewSample
 		$query_to_update = "UPDATE view_samples SET sample_code = '$sample_code' WHERE sample_master_id = $sample_master_id;";
 		$this->tryCatchQuery($query_to_update);
-		
+
 	}
 }
 
