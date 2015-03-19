0d35b7c00ad810d1d9ecea999c11dfce15f463bc
sfung1 <sfung1@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-03-19 23:47:47 +0000

    Fixed SQL syntax

--- branches/bc/bcch/scripts/bcch_scripts/bcch_v0.1_custom_patch.sql
+++ branches/bc/bcch/scripts/bcch_scripts/bcch_v0.1_custom_patch.sql
@@ -15,8 +15,9 @@ SET `initial_specimen_sample_id`=`id`
 WHERE `id` NOT IN (SELECT `sample_master_id` FROM derivative_details);
 
 UPDATE view_samples
-SET `initial_specimen_sample_id`=`sample_master_id`, `initial_specimen_sample_control_id`=`sample_control_id`, `initial_specimen_sample_type`=`sample_type`
-WHERE `sample_master_id` NOT IN (SELECT `sample_master_id` FROM derivative_details) AND `parent_id` IS NULL;
+SET `initial_specimen_sample_id`=`sample_master_id`,
+`initial_specimen_sample_control_id`=`sample_control_id`, `initial_specimen_sample_type`=`sample_type`
+WHERE `sample_master_id` NOT IN (SELECT `sample_master_id` FROM derivative_details) AND `parent_sample_id` IS NULL;
 
 
 -- Fixing derivatives with old sample label
@@ -30,5 +31,6 @@ SET `initial_specimen_sample_id`=`parent_id`
 WHERE `id` IN (SELECT `sample_master_id` FROM derivative_details);
 
 UPDATE view_samples
-SET `initial_specimen_sample_id`=`parent_id`, `initial_specimen_sample_type`=`parent_sample_type`, `initial_specimen_sample_control_id`=`parent_sample_control_id`
-WHERE `sample_master_id` IN (SELECT `sample_master_id` FROM derivative_details) AND `parent_id` IS NOT NULL;
+SET `initial_specimen_sample_id`=`parent_sample_id`,
+`initial_specimen_sample_control_id`=`parent_sample_control_id`, `initial_specimen_sample_type`=`parent_sample_type`
+WHERE `sample_master_id` IN (SELECT `sample_master_id` FROM derivative_details) AND `parent_sample_id` IS NOT NULL;
