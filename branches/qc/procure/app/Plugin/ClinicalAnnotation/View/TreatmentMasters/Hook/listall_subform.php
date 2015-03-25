2ed6c8b9f38b191efebbcbb2932db03febc93c21
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-03-25 20:53:48 +0000

    In DEv

--- branches/qc/procure/app/Plugin/ClinicalAnnotation/View/TreatmentMasters/Hook/listall_subform.php
+++ branches/qc/procure/app/Plugin/ClinicalAnnotation/View/TreatmentMasters/Hook/listall_subform.php
@@ -3,5 +3,7 @@
 	if(isset($new_control['TreatmentControl']['tx_method']) && in_array($new_control['TreatmentControl']['tx_method'], array('procure medication worksheet - drug'))) {
 		$final_options['settings']['language_heading'] = $final_options['settings']['header'];
 		unset($final_options['settings']['header']);
+	} else {
+		unset($final_options['settings']['language_heading']);
 	}
 	
