ed5a1c6f6a5b308a4b4006de9b9605b2d1be492c
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-05-06 17:30:52 +0000

    Fix bug on add aliquot (when 3 aliquots were created, the data of the last one was replaced by the data of the second one). Calculate the current rna quantity.

--- procure/app/Plugin/InventoryManagement/Controller/Hook/AliquotMasters_add_presave_process.php
+++ procure/app/Plugin/InventoryManagement/Controller/Hook/AliquotMasters_add_presave_process.php
@@ -1,11 +1,11 @@
 <?php 
 	if(empty($errors)){
 		$quantity_calculated = false;
-		foreach($this->request->data as &$created_aliquots){
-			if($created_aliquots['parent']['ViewSample']['sample_type'] == 'rna') {
+		foreach($this->request->data as &$tmp_created_aliquots){
+			if($tmp_created_aliquots['parent']['ViewSample']['sample_type'] == 'rna') {
 				$quantity_calculated = true;
-				foreach($created_aliquots['children'] as &$new_aliquot) {
-					list($new_aliquot['AliquotDetail']['procure_total_quantity_ug'], $new_aliquot['AliquotDetail']['procure_total_quantity_ug_nanodrop'])  = $this->AliquotMaster->calculateRnaQuantity($new_aliquot);
+				foreach($tmp_created_aliquots['children'] as &$tmp_new_aliquot) {
+					list($tmp_new_aliquot['AliquotDetail']['procure_total_quantity_ug'], $tmp_new_aliquot['AliquotDetail']['procure_total_quantity_ug_nanodrop'])  = $this->AliquotMaster->calculateRnaQuantity($tmp_new_aliquot);
 				}
 			}
 		}
