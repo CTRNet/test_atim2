bc84b4e6c979ba361a311f5b68ab6d56285196c8
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-05-26 20:48:32 +0000

    Fixed bug on realiquot initial display

--- jgh_qcroc/app/Plugin/InventoryManagement/Controller/Hook/AliquotMasters_realiquot_format.php
+++ jgh_qcroc/app/Plugin/InventoryManagement/Controller/Hook/AliquotMasters_realiquot_format.php
@@ -3,15 +3,15 @@
 	// --------------------------------------------------------------------------------
 	// Set default aliquot label(s)
 	// -------------------------------------------------------------------------------- 	
-	foreach($this->request->data as &$new_data_set){
-		$sample_master_id = $new_data_set['parent']['AliquotMaster']['sample_master_id'];
+	foreach($this->AliquotMaster->find('all', array('conditions' => array('AliquotMaster.id' => explode(",", $parent_aliquots_ids)), 'recursive' => 0)) as $tmp_new_parent){
+		$sample_master_id = $tmp_new_parent['AliquotMaster']['sample_master_id'];
 		$default_aliquot_label = $this->AliquotMaster->generateDefaultAliquotLabel($child_aliquot_ctrl,
-			$new_data_set['parent']['ViewAliquot']['acquisition_label'],
-			$new_data_set['parent']['ViewAliquot']['sample_type'],
-			$new_data_set['parent']['SampleMaster']['initial_specimen_sample_id'],
-			$new_data_set['parent']['ViewAliquot']['sample_master_id'],
-			$new_data_set['parent']['ViewAliquot']);
-		$default_aliquot_labels[$sample_master_id] = $default_aliquot_label;	
+			$tmp_new_parent['ViewAliquot']['acquisition_label'],
+			$tmp_new_parent['ViewAliquot']['sample_type'],
+			$tmp_new_parent['SampleMaster']['initial_specimen_sample_id'],
+			$tmp_new_parent['ViewAliquot']['sample_master_id'],
+			$tmp_new_parent['ViewAliquot']);
+		$default_aliquot_labels[$sample_master_id] = $default_aliquot_label;
 	}
 	$this->set('default_aliquot_labels', $default_aliquot_labels);
 	
