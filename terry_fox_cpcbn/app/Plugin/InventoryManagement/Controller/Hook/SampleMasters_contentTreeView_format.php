f6d8352cfb5134a0c84f6e8852f010fead6cfe60
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-06-04 02:49:02 +0000

    Updated cpcbn summary

---
+++ terry_fox_cpcbn/app/Plugin/InventoryManagement/Controller/Hook/SampleMasters_contentTreeView_format.php
@@ -0,0 +1,15 @@
+<?php
+
+if(!$sample_master_id) {
+	$array_to_sort = array();
+	foreach($this->request->data as $tmp_new_sample) $array_to_sort[strtolower($tmp_new_sample['SampleMaster']['qc_tf_tma_sample_control_code'])][] = $tmp_new_sample;
+	$this->request->data = array();
+	ksort($array_to_sort);
+	foreach($array_to_sort as $tmp_new_sample_level_1){
+		foreach($tmp_new_sample_level_1 as $tmp_new_sample_level_2){
+			$this->request->data[] = $tmp_new_sample_level_2;
+		}	
+	}
+}
+
+?>
