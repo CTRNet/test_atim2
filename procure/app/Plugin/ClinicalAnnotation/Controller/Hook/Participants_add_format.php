676a0fe6df4f5ff6ee17773d23222e1e4294d71c
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-08-12 20:34:41 +0000

    Clinical Annotation Module Clean Up to create CENTRAL version and clean up PROCESSING VERSION

--- procure/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_add_format.php
+++ procure/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_add_format.php
@@ -1,5 +1,11 @@
 <?php 
 	
+	//Cannot be added to Participant model because participant record will be updated by system for any MiscIdentifier creation/modification
+	//And there are no interest to ad control for CENTRAL BANK because data will be erased
+	if(Configure::read('procure_atim_version') != 'BANK') $this->redirect('/Pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+	
+	// Set default values
+	
 	$bank_identification = $this->Participant->bank_identification;
 	$last_id = $this->Participant->find('first', array('conditions' => array("participant_identifier LIKE '$bank_identification%'"), 'fields' => array(" MAX(CAST(REPLACE(`participant_identifier`, '$bank_identification', '') AS UNSIGNED)) AS max_val"), 'recursive'=>'-1'));
 	$new_id = empty($last_id[0]['max_val'])? '001': $last_id[0]['max_val']+1;
@@ -7,3 +13,5 @@
 	if($zero_to_add > 0) { for($i =0; $i < $zero_to_add; $i++) $new_id = '0'.$new_id; }
 	$default_participant_identifier = $bank_identification.$new_id;
 	$this->set('default_participant_identifier', $default_participant_identifier);
+
+	$this->set('default_procure_transferred_participant', 'n');
