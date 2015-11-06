24e2e46c7a03f8936e88c00e6655f3fc83b884fc
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-11-06 19:01:58 +0000

    Merged with trunk v2.6.6 (revs 6117 to 6315)

--- branches/bc/OvCaRe/app/Config/database.php
+++ branches/bc/OvCaRe/app/Config/database.php
@@ -65,7 +65,7 @@ class DATABASE_CONFIG {
 		'host' => '127.0.0.1',
 		'login' => 'root',
 		'password' => '',
-		'database' => 'ovcare',
+		'database' => 'bcovcare',
 		'prefix' => '',
 		'encoding' => 'utf8',
 		'port'	=> 3306
--- branches/bc/OvCaRe/app/Plugin/InventoryManagement/Model/Custom/ViewCollection.php
+++ branches/bc/OvCaRe/app/Plugin/InventoryManagement/Model/Custom/ViewCollection.php
@@ -27,8 +27,8 @@ TreatmentDetail.path_num
 		FROM collections AS Collection
 		LEFT JOIN participants AS Participant ON Collection.participant_id = Participant.id AND Participant.deleted <> 1
 LEFT JOIN txd_surgeries as TreatmentDetail ON TreatmentDetail.treatment_master_id = Collection.treatment_master_id
-		WHERE Collection.deleted <> 1 %%WHERE%%';
-		
+		WHERE Collection.deleted <> 1 %%WHERE%%';	
+	
 	function summary($variables=array()) {
 		$return = false;
 		
