cd0b14d0a55b48c3cb6786299f9a31f7d8cc38a7
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2015-11-24 21:54:20 +0000

    Issue #3291: Order configuration with no order line - No information about the order in the Aliquot Uses/Events list

--- trunk/app/Plugin/InventoryManagement/Model/ViewAliquotUse.php
+++ trunk/app/Plugin/InventoryManagement/Model/ViewAliquotUse.php
@@ -169,8 +169,8 @@ class ViewAliquotUse extends InventoryManagementAppModel {
 		JOIN aliquot_masters AS AliquotMaster ON AliquotMaster.id = OrderItem.aliquot_master_id
 		JOIN shipments AS Shipment ON Shipment.id = OrderItem.shipment_id
 		JOIN sample_masters SampleMaster ON SampleMaster.id = AliquotMaster.sample_master_id
-		JOIN order_lines AS OrderLine ON  OrderLine.id = OrderItem.order_line_id
-		JOIN `orders` AS `Order` ON  Order.id = OrderLine.order_id			
+		LEFT JOIN order_lines AS OrderLine ON  OrderLine.id = OrderItem.order_line_id
+		JOIN `orders` AS `Order` ON  Order.id = OrderItem.order_id			
 		WHERE OrderItem.deleted <> 1 %%WHERE%%
 	
 		UNION ALL
