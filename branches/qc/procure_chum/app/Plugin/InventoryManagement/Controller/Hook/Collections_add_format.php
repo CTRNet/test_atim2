94ad7dcfc6dab468f9afc4e2bee5ed2ee4fea6b4
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-01-26 15:52:32 +0000

    Merged with procure 6375 to 6381

--- branches/qc/procure_chum/app/Plugin/InventoryManagement/Controller/Hook/Collections_add_format.php
+++ branches/qc/procure_chum/app/Plugin/InventoryManagement/Controller/Hook/Collections_add_format.php
@@ -1,7 +1,7 @@
 <?php 
 	
 	//Cannot be added to Collection model because user of PROCESSING bank can delete a collection
-	//Note there are no interest to add control for CENTRAL BANK because data will be erased
+	//Note there is no interest to add control for CENTRAL BANK because data will be erased
 	if(Configure::read('procure_atim_version') != 'BANK') {
 		AppController::addWarningMsg(__("you have been redirected to the 'add transferred aliquots' form"));
 		$this->redirect('/InventoryManagement/Collections/loadTransferredAliquotsData/1', null, true);
--- branches/qc/procure_chum/app/Plugin/InventoryManagement/Controller/Hook/SampleMasters_add_initial_display.php
+++ branches/qc/procure_chum/app/Plugin/InventoryManagement/Controller/Hook/SampleMasters_add_initial_display.php
@@ -1,6 +1,6 @@
 <?php 
 		
-	//Note there are no interest to add control for CENTRAL BANK because data will be erased
+	//Note there is no interest to add control for CENTRAL BANK because data will be erased
 	if(Configure::read('procure_atim_version') != 'BANK') {
 		if($sample_control_data['SampleControl']['sample_category'] == 'specimen') {
 			$this->redirect('/Pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
