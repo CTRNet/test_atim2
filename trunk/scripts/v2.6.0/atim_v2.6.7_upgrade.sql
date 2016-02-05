798df1b41dd18929706999a26ebc85fdc6c44d82
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-02-05 21:31:21 +0000

    Clean up duplicated field in index view of form tma_blocks_for_slide_creation

--- trunk/scripts/v2.6.0/atim_v2.6.7_upgrade.sql
+++ trunk/scripts/v2.6.0/atim_v2.6.7_upgrade.sql
@@ -7,6 +7,12 @@ ALTER TABLE aliquot_masters MODIFY storage_coord_y varchar(11) DEFAULT NULL;
 ALTER TABLE aliquot_masters_revs MODIFY storage_coord_x varchar(11) DEFAULT NULL;
 ALTER TABLE aliquot_masters_revs MODIFY storage_coord_y varchar(11) DEFAULT NULL;
 
+
+-- tma_blocks_for_slide_creation duplicated field in index view
+
+UPDATE structure_formats SET `flag_index`='0' WHERE structure_id=(SELECT id FROM structures WHERE alias='tma_blocks_for_slide_creation') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='Block' AND `tablename`='storage_masters' AND `field`='storage_control_id' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='storage_types_from_control_id') AND `flag_confidential`='0');
+UPDATE structure_formats SET `flag_index`='1' WHERE `language_heading` = 'tma block' AND structure_id=(SELECT id FROM structures WHERE alias='tma_blocks_for_slide_creation') AND structure_field_id=(SELECT id FROM structure_fields WHERE `model`='Block' AND `tablename`='storage_masters' AND `field`='storage_control_id' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='storage_types_from_control_id') AND `flag_confidential`='0');
+
 -- -----------------------------------------------------------------------------------------------------------------------------------
 -- Versions table
 -- -----------------------------------------------------------------------------------------------------------------------------------
