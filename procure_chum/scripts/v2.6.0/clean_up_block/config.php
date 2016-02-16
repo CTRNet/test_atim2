5c7f2b5646de649554b56dd9206be6eaf74cdb47
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-02-16 19:33:39 +0000

    Add procure block cleanup script

---
+++ procure_chum/scripts/v2.6.0/clean_up_block/config.php
@@ -0,0 +1,21 @@
+<?php 
+
+$migration_process_version = 'v0.1';
+
+//-- DB PARAMETERS ---------------------------------------------------------------------------------------------------------------------------
+
+$db_ip			= "localhost";
+$db_port 		= "";
+$db_user 		= "root";
+$db_pwd			= "";
+$procure_db_schema		= "procurechum";
+$chumoncoaxis_db_schema = 'chumoncoaxis';
+$db_charset		= "utf8";
+
+$migration_user_id = 9;
+
+//-- EXCEL FILE NAMES ---------------------------------------------------------------------------------------------------------------------------
+
+$files_path = "C:/_NicolasLuc/Server/www/procure_chum/data/";
+
+?>
