98ec821df663ce514acf14a292c17b9b5f009eeb
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-03-08 14:42:39 +0000

    Removed the wrong committed folder v2.6.7 plus has  re-set the trunk_build_number of the v2.6.7.

--- v2.6.7/app/Plugin/ClinicalAnnotation/Model/TreatmentExtendDetail.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-class TreatmentExtendDetail extends ClinicalAnnotationAppModel {
-	var $useTable = false;
-}
-
-?>
--- v2.6.7/app/Plugin/Customize/Model/CustomizeAppModel.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-class CustomizeAppModel extends AppModel {
-}
-
-?>
