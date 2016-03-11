cf450e6abdc3e8cf3810fbcb7c33167b935999f4
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-03-11 21:11:09 +0000

    QBCF is not a project of tfri - Built branch QBCF

--- terry_fox_qbcf/app/Plugin/ClinicalAnnotation/View/TreatmentMasters/Hook/edit.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-	$final_options['settings']['actions'] = true;
-	$final_options['settings']['form_bottom'] = true;
-	$final_options['links']['bottom']['cancel'] = '/ClinicalAnnotation/TreatmentMasters/listall/'.$atim_menu_variables['Participant.id'];
-	
