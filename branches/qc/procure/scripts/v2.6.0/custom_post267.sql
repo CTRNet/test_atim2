428c2a3f0c1723cb64b911e70bff48ed9222be92
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-03-29 18:45:21 +0000

    Last commit - First version tagged for the 6 installations.

--- branches/qc/procure/scripts/v2.6.0/custom_post267.sql
+++ branches/qc/procure/scripts/v2.6.0/custom_post267.sql
@@ -88,7 +88,18 @@ VALUES
 -- ----------------------------------------------------------------------------------------------------------------------------------------
 -- ----------------------------------------------------------------------------------------------------------------------------------------
 
-UPDATE versions SET branch_build_number = '6440' WHERE version_number = '2.6.7';
+UPDATE datamart_browsing_controls 
+SET flag_active_1_to_2 = '0', flag_active_2_to_1 = '0'
+WHERE id1 IN (SELECT id FROM datamart_structures WHERE model IN ('TmaBlock', 'TmaSlide'))
+OR id2 IN (SELECT id FROM datamart_structures WHERE model IN ('TmaBlock', 'TmaSlide'));
+
+UPDATE datamart_structure_functions SET flag_active = 0
+WHERE datamart_structure_id IN (SELECT id FROM datamart_structures WHERE model IN ('TmaBlock', 'TmaSlide'));
+
+-- ----------------------------------------------------------------------------------------------------------------------------------------
+-- ----------------------------------------------------------------------------------------------------------------------------------------
+
+UPDATE versions SET branch_build_number = '6461' WHERE version_number = '2.6.7';
 UPDATE versions SET site_branch_build_number = '?' WHERE version_number = '2.6.7';
 UPDATE versions SET permissions_regenerated = 0;
 
