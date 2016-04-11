0319360f64403aa12912f0bc388a6f94b44586e5
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-04-11 14:08:13 +0000

    Merged With Procure Branch 267 (revs 6456 to 6461)

--- procure_chuq/app/Plugin/InventoryManagement/Controller/Hook/ALiquotMasters_contentTreeView_format.php
+++ procure_chuq/app/Plugin/InventoryManagement/Controller/Hook/ALiquotMasters_contentTreeView_format.php
@@ -1,20 +1,37 @@
 <?php
 
-foreach($this->request->data as &$tree_node){
-	$procure_is_transferred_aliquot = false;
-	if(isset($tree_node['AliquotMaster'])) {
-		//ALiquot is a transferred parent 
-		if($this->Realiquoting->find('count', array('conditions' => array('Realiquoting.parent_aliquot_master_id' => $tree_node['AliquotMaster']['id'], 'Realiquoting.procure_central_is_transfer' => '1'), 'recursive' => '-1'))) $procure_is_transferred_aliquot = true;
-		//ALiquot is a transferred child
-		if(isset($tree_node['Realiquoting']) && $tree_node['Realiquoting']['procure_central_is_transfer'] == '1') $procure_is_transferred_aliquot = true;
-	}
-	if($procure_is_transferred_aliquot) {
-		$keys_disabled = array_keys($tree_node['css'], 'disabled');
-		if(!$keys_disabled) {
-			$tree_node['css'][] = 'transfered_aliquot';
-		} else {
-			foreach($keys_disabled as $new_key) unset($tree_node['css'][$new_key]);
-			$tree_node['css'][] = 'disabled_transfered_aliquot';
-		}
+if(Configure::read('procure_atim_version') == 'CENTRAL') {
+	foreach($this->request->data as &$tree_node){
+		if(isset($tree_node['AliquotMaster'])) {
+			$keys_disabled = array_keys($tree_node['css'], 'disabled');
+			if($this->Realiquoting->find('count', array('conditions' => array('Realiquoting.parent_aliquot_master_id' => $tree_node['AliquotMaster']['id'], 'Realiquoting.procure_central_is_transfer' => '1'), 'recursive' => '-1'))) {
+				//Aliquot created in a bank transferred to processing site
+				if(!$keys_disabled) {
+					$tree_node['css'][] = 'transfered_bank_aliquot';
+				} else {
+					foreach($keys_disabled as $new_key) unset($tree_node['css'][$new_key]);
+					$tree_node['css'][] = 'disabled_transfered_bank_aliquot';
+				}
+			} else if(isset($tree_node['Realiquoting']) && $tree_node['Realiquoting']['procure_central_is_transfer'] == '1') {
+				//Aliquot created by processing site received from a bank
+				if(!$keys_disabled) {
+					$tree_node['css'][] = 'transfered_psp_aliquot';
+				} else {
+					foreach($keys_disabled as $new_key) unset($tree_node['css'][$new_key]);
+					$tree_node['css'][] = 'disabled_transfered_psp_aliquot';
+				}
+			} else if($tree_node['AliquotMaster']['procure_created_by_bank'] == 'p') {
+				//Aliquot created by processing site
+				if(!$keys_disabled) {
+					$tree_node['css'][] = 'psp_aliquot';
+				} else {
+					foreach($keys_disabled as $new_key) unset($tree_node['css'][$new_key]);
+					$tree_node['css'][] = 'disabled_psp_aliquot';
+				}
+			}
+		} else if(isset($tree_node['ViewAliquotUse']) && $tree_node['ViewAliquotUse']['procure_created_by_bank'] == 'p') {
+			//Aliquot use created in psp site
+			$tree_node['css'][] = 'disabled_psp_record';
+		}	
 	}
 }
