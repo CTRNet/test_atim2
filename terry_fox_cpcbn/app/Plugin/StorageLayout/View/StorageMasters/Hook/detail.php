b38285bae417517b96cdf6fb1d645474cd98d935
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-04-26 15:44:45 +0000

    Add mnany types of TMA Layout csv export....

--- terry_fox_cpcbn/app/Plugin/StorageLayout/View/StorageMasters/Hook/detail.php
+++ terry_fox_cpcbn/app/Plugin/StorageLayout/View/StorageMasters/Hook/detail.php
@@ -3,8 +3,10 @@
 	if($_SESSION['Auth']['User']['group_id'] == '1' && array_key_exists('export as CSV file (comma-separated values)', $final_options['links']['bottom'])) {
 		//Administartor: Allow export for TMA review
 		$final_options['links']['bottom']['export as CSV file (comma-separated values)'] = array(
-			'generic' => sprintf("javascript:setCsvPopup('/StorageLayout/StorageMasters/storageLayout/".$atim_menu_variables['StorageMaster.id']."/0/1/');", 0),
-			'for review' => sprintf("javascript:setCsvPopup('/StorageLayout/StorageMasters/storageLayout/".$atim_menu_variables['StorageMaster.id']."/0/2/');", 0)
+			'classical' => sprintf("javascript:setCsvPopup('/StorageLayout/StorageMasters/storageLayout/".$atim_menu_variables['StorageMaster.id']."/0/1/');", 0),
+			'for review (participant_identifier+aliquot_label)' => sprintf("javascript:setCsvPopup('/StorageLayout/StorageMasters/storageLayout/".$atim_menu_variables['StorageMaster.id']."/0/2/');", 0),
+			'for review (participant_identifier+aliquot_label+aliquot_barcode)' => sprintf("javascript:setCsvPopup('/StorageLayout/StorageMasters/storageLayout/".$atim_menu_variables['StorageMaster.id']."/0/3/');", 0),
+			'for review (aliquot_label+aliquot_barcode)' => sprintf("javascript:setCsvPopup('/StorageLayout/StorageMasters/storageLayout/".$atim_menu_variables['StorageMaster.id']."/0/4/');", 0)
 		);
 	}
 	
