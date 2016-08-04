66202646db64051b5f16db732d82ffaa52f326b5
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-08-04 17:25:35 +0000

    Merged with procure branch (6513)

--- procure_chum/scripts/v2.6.0/custom_post267.sql
+++ procure_chum/scripts/v2.6.0/custom_post267.sql
@@ -124,6 +124,6 @@ INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `s
 INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `margin`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
 ((SELECT id FROM structures WHERE alias='procure_next_followup_report_result'), (SELECT id FROM structure_fields WHERE `model`='0' AND `tablename`='' AND `field`='procure_next_followup_finish_date'), '0', '5', '', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0');
 
-UPDATE versions SET branch_build_number = '6511' WHERE version_number = '2.6.7';
+UPDATE versions SET branch_build_number = '6513' WHERE version_number = '2.6.7';
 UPDATE versions SET site_branch_build_number = '?' WHERE version_number = '2.6.7';
 UPDATE versions SET permissions_regenerated = 0;
