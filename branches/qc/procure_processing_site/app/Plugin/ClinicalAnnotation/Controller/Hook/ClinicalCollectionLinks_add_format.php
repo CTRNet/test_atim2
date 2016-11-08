38de7364757333dc7e65c47c1a9ad60dd9396f2c
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2016-11-08 19:05:01 +0000

    Merged with procure branch 6587

--- branches/qc/procure_processing_site/app/Plugin/ClinicalAnnotation/Controller/Hook/ClinicalCollectionLinks_add_format.php
+++ branches/qc/procure_processing_site/app/Plugin/ClinicalAnnotation/Controller/Hook/ClinicalCollectionLinks_add_format.php
@@ -3,3 +3,10 @@
 	//Cannot be added to Collection model because user of PROCESSING bank can delete a collection
 	//Note there is no interest to add control for CENTRAL BANK because data will be erased
 	if(Configure::read('procure_atim_version') != 'BANK') $this->redirect('/Pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
+	
+	if($this->request->data) {
+		
+		//todo chercher collection...
+		$this->request->data['Collection']['id'] = null;
+
+	}
