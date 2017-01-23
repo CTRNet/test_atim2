78e2c70334bdd900f536b2b4077a6cd2fee509b9
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-01-23 20:12:58 +0000

    New change requests

--- branches/qc/procure/app/Plugin/ClinicalAnnotation/Controller/Hook/TreatmentMasters_add_postsave_process.php
+++ branches/qc/procure/app/Plugin/ClinicalAnnotation/Controller/Hook/TreatmentMasters_add_postsave_process.php
@@ -1,5 +1,5 @@
 <?php 
 	
 	//Redirect visit data entry worklfow
-	if($next_url_to_flash_for_visit_data_entry) $url_to_flash = $next_url_to_flash_for_visit_data_entry;
+	if(isset($_SESSION['procure_clinical_file_update_process'])) $url_to_flash = $_SESSION['procure_clinical_file_update_process']['next_page_url'];
 	
--- branches/qc/procure/app/Plugin/ClinicalAnnotation/View/TreatmentMasters/Hook/add_dx_list.php
+++ branches/qc/procure/app/Plugin/ClinicalAnnotation/View/TreatmentMasters/Hook/add_dx_list.php
@@ -1,11 +1,4 @@
 <?php
 	
-	//Set skip button for visit data entry workflow
-	if($next_url_to_flash_for_visit_data_entry) {
-		unset($final_options['links']['bottom']['cancel']);
-		$final_options['links']['bottom'][__('skip go to %s', __($next_url_to_flash_for_visit_data_entry['next_url_title']))] = array('link' => $next_url_to_flash_for_visit_data_entry['next_url'], 'icon' => 'procureskip');
-	}
-	
-	//To hide the related diagnosis event section
-	$final_options = array('links'	=> $final_options['links']);
+	$display_next_sub_form = false;
 	
