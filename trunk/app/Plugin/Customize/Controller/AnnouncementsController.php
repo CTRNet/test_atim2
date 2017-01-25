7951efe21aa4f963943ae2ca98f26559f404b17a
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-01-25 23:35:27 +0000

    Issue #3360: User 'Account Status' field value does not match list value in edit mode Issue #3365 : Annoucements : Unable to create annoucements for a bank or a user Issue #3364 : Unknow User Search Result Display - Empty result list replaced by search form

--- trunk/app/Plugin/Customize/Controller/AnnouncementsController.php
+++ trunk/app/Plugin/Customize/Controller/AnnouncementsController.php
@@ -5,27 +5,32 @@ class AnnouncementsController extends CustomizeAppController {
 	var $uses = array('Announcement');
 	var $paginate = array('Announcement'=>array('order'=>'Announcement.date DESC')); 
 	
-	function index() {;
-		
-		$conditions = array();
-		$conditions[] = 'date_start<=NOW()';
-		$conditions[] = 'date_end>=NOW()';
-		$conditions[] = '(group_id="0" OR group_id="'.$_SESSION['Auth']['User']['group_id'].'")';
-		$conditions = array_filter( $conditions );
-		
-		$this->hook();
+	function index() {
+		$conditions = array(
+			'OR' => array(
+				array('Announcement.bank_id'=> $_SESSION['Auth']['User']['Group']['bank_id']),
+				array('Announcement.group_id' => $_SESSION['Auth']['User']['group_id'], 'Announcement.user_id' => $_SESSION['Auth']['User']['id'])
+			)
+		);
 		
 		$this->request->data = $this->paginate($this->Announcement, $conditions);
+		
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) {
+			require($hook_link);
+		}
 	}
 	
 	function detail( $announcement_id=NULL ) {
-		$this->set( 'atim_menu_variables', array('Announcement.id'=>$announcement_id) );
+		$this->request->data = $this->Announcement->getOrRedirect($announcement_id);
 		
-		$this->hook();
-		
-		$this->request->data = $this->Announcement->find('first',array('conditions'=>array('Announcement.id'=>$announcement_id)));
+		// CUSTOM CODE: FORMAT DISPLAY DATA
+		$hook_link = $this->hook('format');
+		if( $hook_link ) {
+			require($hook_link);
+		}
 	}
-
 }
 
 ?>
