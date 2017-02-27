552d7c1d214365bf94141b60e7dd34a2b5127af9
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-02-27 15:20:41 +0000

    Add aliquot_review_masters procure_created_by_bank....

--- branches/qc/procure/app/Plugin/InventoryManagement/Controller/Hook/SpecimenReviews_add_presave_process.php
+++ branches/qc/procure/app/Plugin/InventoryManagement/Controller/Hook/SpecimenReviews_add_presave_process.php
@@ -3,3 +3,8 @@
 	$specimen_review_data['SpecimenReviewMaster']['procure_created_by_bank'] = Configure::read('procure_bank_id');
 	$this->SpecimenReviewMaster->addWritableField(array('procure_created_by_bank'));
 	
+	foreach($aliquot_review_data as &$procure_new_aliquot_review_to_save) {
+		$procure_new_aliquot_review_to_save['AliquotReviewMaster']['procure_created_by_bank'] = Configure::read('procure_bank_id');
+	}
+	$this->AliquotReviewMaster->addWritableField(array('procure_created_by_bank'));
+	
