4c6523e36e37bbce685b3ccba86b4bc639f11e8a
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-06-25 22:24:08 +0000

    Merged with PROCURE branch revs 6713 to 6739.

---
+++ procure_cusm/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_chronology_end.php
@@ -0,0 +1,15 @@
+<?php
+
+    // Manage warnings
+    
+    ksort($procure_chronology_warnings);
+    foreach($procure_chronology_warnings as $procure_tmp1) {
+        foreach($procure_tmp1 as $procure_tmp2) {
+            AppController::addWarningMsg($procure_tmp2);
+        }
+    }
+
+
+
+
+    
