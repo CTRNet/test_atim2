828109c76758e4838ca1859cb2b8e155992b36fc
Yaser Naderi <42739735+yasnaderi@users.noreply.github.com>
2017-09-08 15:25:15 +0000

    - Update the scripts of custom codes.

---
+++ v2.7.Z/scripts/V2.7.0/CustomCodeSnifer/RemovePhpTagAndSpaceAtTheEndOfFile-custum.bat
@@ -0,0 +1,6 @@
+REM You can add the parametere --commit at the end for changng automatically.
+
+@echo off
+REM php addPublic-custom.php atims/root/app
+php RemovePhpTagAndSpaceAtTheEndOfFile-custum.php C:\wamp\www\ATiMs\AtimChumFinalTest\app 
+pause
---
+++ v2.7.Z/scripts/V2.7.0/CustomCodeSnifer/snakeToCamel-custom.bat
@@ -0,0 +1,6 @@
+REM You can add the parametere --commit at the end for changng automatically.
+
+@echo off
+REM php snakeToCamel-custom.php atims/root/app
+php snakeToCamel-custom.php C:\wamp\www\ATiMs\AtimChumFinalTest\app 
+pause
