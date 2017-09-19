01bff64be04daa1df760b745647d89799c334164
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-09-19 20:20:44 +0000

    Last Commit before trunk v2.7.0.

--- branches/dev/v2.7.Z/app/Config/database.php
+++ branches/dev/v2.7.Z/app/Config/database.php
@@ -67,7 +67,7 @@ class DATABASE_CONFIG
         'host' => '127.0.0.1',
         'login' => 'root',
         'password' => '',
-        'database' => 'atim_1',
+        'database' => 'v27z',
         'prefix' => '',
         'encoding' => 'utf8',
         'port' => 3306
