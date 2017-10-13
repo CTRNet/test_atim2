4ea92d18412529024e666ef7dc8aa6d7bfd6776e
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-10-13 21:22:40 +0000

    New migration script version.

--- branches/qc/terry_fox_coeur/dataImporterConfig/ClinicalDataUpdate/config.php
+++ branches/qc/terry_fox_coeur/dataImporterConfig/ClinicalDataUpdate/config.php
@@ -11,20 +11,20 @@ $db_pwd			= "";
 $db_schema		= "tfricoeur";
 $db_charset		= "utf8";
 
-$excel_files_paths = 'C:/_NicolasLuc/Server/www/tfri_coeur/TmpData/';
+$excel_files_paths = 'C:\_NicolasLuc\Server\www\tfri_coeur\TmpData\2017-10-05\2017-08-00/';
 $migration_user_id = 2;
 
 // $file_xls_offset Serial number $windows_xls_offset = 36526 & $mac_xls_offset = 35064 (Use for excel date parsing)
 $windows_xls_offset = 36526;
 $mac_xls_offset = 35064;
 $bank_excel_files = array(
-    array(
-        'bank' => 'OHRI',
-        'file' => 'COEUR-OHRI-master2016 corrected_nl_revised.xls',
-        'worksheets' => array('data', 'treatment'),
-        'file_xls_offset' => $windows_xls_offset,
-        'parser_function' => 'General'
-    ),	
+//     array(
+//         'bank' => 'OHRI',
+//         'file' => 'COEUR-OHRI-master2016 corrected_nl_revised.xls',
+//         'worksheets' => array('data', 'treatment'),
+//         'file_xls_offset' => $windows_xls_offset,
+//         'parser_function' => 'General'
+//     ),	
     array(
 	    'bank' => 'McGill',
 		'file' => 'COEUR-Dec2016-McGill2016_nl_revised.xls',
