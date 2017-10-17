16a11f1c72edc327e8840b862bedd1924bbf2548
Yaser Naderi <42739735+yasnaderi@users.noreply.github.com>
2017-10-17 17:02:41 +0000

    - Datamart & CSV error. - CSV warning (Warning to the users to check the CSV file).

--- v2.7.Z/app/webroot/js/default.js
+++ v2.7.Z/app/webroot/js/default.js
@@ -2744,6 +2744,10 @@ if (typeof DEBUG_MODE !=='undefined' && DEBUG_MODE>0){
             if (visible) {
                 $("#csvPopup").popup();
             }
+
+            var attention='<ul class="warning"><li><span class="icon16 warning mr5px"></span>'+csvWarning+'</li></ul>'
+            $("#csvPopup h4").after(attention);
+            
         });
     }
     $("#csvPopup").popup();
