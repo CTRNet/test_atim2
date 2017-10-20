3e429817753d8475e62e9c665204453d37c75e91
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-10-20 15:09:11 +0000

    Populate clinical notes based on existing data plus change participant chronology content

--- branches/qc/chus_onco_axis/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_chronology_annotation.php
+++ branches/qc/chus_onco_axis/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_chronology_annotation.php
@@ -1,8 +1,15 @@
 <?php 
-	
+
+    $chronolgy_data_annotation['event'] = __($annotation['EventControl']['event_type']);
 	switch($annotation['EventControl']['event_type']) {
 		case 'medical history':
-			$chronolgy_data_annotation['chronology_details'] = $annotation['EventDetail']['body_system'];
+			$chronolgy_data_annotation['chronology_details'] = $this->StructurePermissibleValuesCustom->getTranslatedCustomDropdownValue('Medical History Body System', $annotation['EventDetail']['body_system']);
 			break;
+		case 'medication history':
+		    $chronolgy_data_annotation['chronology_details'] = $annotation['EventDetail']['medication'];
+			break;
+		case 'clinical note':
+		    $chronolgy_data_annotation['chronology_details'] = $this->StructurePermissibleValuesCustom->getTranslatedCustomDropdownValue('Clinical Note Types', $annotation['EventDetail']['type']);
+		    break;
 		default:
 	}
--- branches/qc/chus_onco_axis/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_chronology_treatment.php
+++ branches/qc/chus_onco_axis/app/Plugin/ClinicalAnnotation/Controller/Hook/Participants_chronology_treatment.php
@@ -1,41 +1,31 @@
 <?php 
 
+    $chronolgy_data_annotation['event'] = __($tx['TreatmentControl']['tx_method']);
 	switch($tx['TreatmentControl']['tx_method']) {
 		case 'biopsy':
 		case 'surgery':
-			$tmps_sites = array();
-			foreach($this->TreatmentExtendMaster->find('all', array('conditions' => array('TreatmentExtendMaster.treatment_master_id' => $tx['TreatmentMaster']['id']))) as $tmp_new_tx_extend) {
-				$tmp_icd_description = $this->CodingIcdo3Topo->getDescription($tmp_new_tx_extend['TreatmentExtendDetail']['surgical_site']);
-				if($tmp_icd_description) $tmps_sites[$tmp_icd_description] = $tmp_icd_description;
-			}
-			ksort($tmps_sites);
-			if($chronolgy_data_treatment_start) $chronolgy_data_treatment_start['chronology_details'] = implode(', ', $tmps_sites);
-			if($chronolgy_data_treatment_finish) $chronolgy_data_treatment_finish['chronology_details'] = implode(', ', $tmps_sites);
-			break;
 		case 'radiotherapy':
 			$tmps_sites = array();
 			foreach($this->TreatmentExtendMaster->find('all', array('conditions' => array('TreatmentExtendMaster.treatment_master_id' => $tx['TreatmentMaster']['id']))) as $tmp_new_tx_extend) {
-				$tmp_icd_description = $this->CodingIcdo3Topo->getDescription($tmp_new_tx_extend['TreatmentExtendDetail']['site']);
+				$tmp_icd_description = $this->CodingIcdo3Topo->getDescription($tmp_new_tx_extend['TreatmentExtendDetail'][(($tx['TreatmentControl']['tx_method'] == 'radiotherapy')? 'site' : 'surgical_site')]);
 				if($tmp_icd_description) $tmps_sites[$tmp_icd_description] = $tmp_icd_description;
 			}
 			ksort($tmps_sites);
-			if($chronolgy_data_treatment_start) $chronolgy_data_treatment_start['chronology_details'] = implode(', ', $tmps_sites);
-			if($chronolgy_data_treatment_finish) $chronolgy_data_treatment_finish['chronology_details'] = implode(', ', $tmps_sites);
-			break;
+			$tmps_sites = implode(' & ', $tmps_sites);
+			if($chronolgy_data_treatment_start) $chronolgy_data_treatment_start['chronology_details'] = $tmps_sites;
+			if($chronolgy_data_treatment_finish) $chronolgy_data_treatment_finish['chronology_details'] = $tmps_sites;
 			break;
 		case 'systemic therapy':
 			$drugs = array();
 			foreach($this->TreatmentExtendMaster->find('all', array('conditions' => array('TreatmentExtendMaster.treatment_master_id' => $tx['TreatmentMaster']['id']))) as $new_drug) {
-				if(isset($new_drug['TreatmentExtendDetail']['drug_id']) && isset($all_drugs[$new_drug['TreatmentExtendDetail']['drug_id']])) {
-					$drugs[$all_drugs[$new_drug['TreatmentExtendDetail']['drug_id']]] = $all_drugs[$new_drug['TreatmentExtendDetail']['drug_id']];
-				}
+			    if(strlen($new_drug['Drug']['generic_name'])) {
+			        $drugs[$new_drug['Drug']['generic_name']] = $new_drug['Drug']['generic_name'];
+			    }
 			}
-			if($chronolgy_data_treatment_start) $chronolgy_data_treatment_start['chronology_details'] = implode(', ', $drugs);
-			if($chronolgy_data_treatment_finish) $chronolgy_data_treatment_finish['chronology_details'] = implode(', ', $drugs);
-			break;
-		case 'medication history':
-			if($chronolgy_data_treatment_start) $chronolgy_data_treatment_start['chronology_details'] = $tx['TreatmentDetail']['medication'];
-			if($chronolgy_data_treatment_finish) $chronolgy_data_treatment_finish['chronology_details'] = $tx['TreatmentDetail']['medication'];
+			ksort($drugs);
+			$drugs = implode(', ', $drugs);
+			if($chronolgy_data_treatment_start) $chronolgy_data_treatment_start['chronology_details'] = $drugs;
+			if($chronolgy_data_treatment_finish) $chronolgy_data_treatment_finish['chronology_details'] = $drugs;
 			break;
 	}
 	
