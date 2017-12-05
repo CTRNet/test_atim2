3dd2f23c23b0bbbe8b927774553db9fb93aecdb8
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-12-05 14:48:44 +0000

    Complete the cost recovery report

--- qbcf/scripts/v2.6.0/custom_post268.sql
+++ qbcf/scripts/v2.6.0/custom_post268.sql
@@ -2275,6 +2275,7 @@ VALUES
 ('QBCF Summary - Cost Recovery', 'QBCF Summary - Cost Recovery', ''),
 ('QBCF Summary - Cost Recovery - Description', 'Help bank administrator to follow-up the block cost recovery activity.', '');
 
+INSERT INTO structures(`alias`) VALUES ('qbcf_cost_recovery_results');
 INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
 ('Datamart', '0', '', 'qbcf_generated_paid_block', 'yes_no',  NULL , '0', '', '', '', 'cost recovery', ''), 
 ('Datamart', '0', '', 'qbcf_generated_paid_returned', 'yes_no',  NULL , '0', '', '', '', 'returned to bank', ''), 
@@ -2325,4 +2326,9 @@ VALUES
 ('tissue\'s path review\'s warnings', 'Path Reviews Warnings', '');
 
 UPDATE versions SET branch_build_number = '6956'  WHERE version_number = '2.6.8';
+
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `margin`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='qbcf_cost_recovery_results'), (SELECT id FROM structure_fields WHERE `model`='Participant' AND `tablename`='participants' AND `field`='qbcf_study_exclusion_reason' AND `type`='select' AND `structure_value_domain` =(SELECT id FROM structure_value_domains WHERE domain_name='qbcf_study_exclusion_reasons')  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='' AND `language_tag`='reason'), '1', '15', '', '0', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0');
+
+UPDATE versions SET branch_build_number = '6963'  WHERE version_number = '2.6.8';
 
