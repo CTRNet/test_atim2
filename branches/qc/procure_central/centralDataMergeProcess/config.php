4297af282c63c31d216b0fa4593e357465359e4a
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2017-12-27 20:05:53 +0000

    Remove any site information from merge process.

--- branches/qc/procure_central/centralDataMergeProcess/config.php
+++ branches/qc/procure_central/centralDataMergeProcess/config.php
@@ -6,10 +6,10 @@ $db_pwd		= 	"";
 $db_charset		= "utf8";
 
 $db_central_schemas	= "procurecentral";
-$db_chum_schemas = "atim_procure_chum_dump_dor_central";
-$db_chuq_schemas = "atim_procure_chuq_dump_dor_central";
-$db_chus_schemas = "atim_procure_chus_dump_dor_central";
-$db_cusm_schemas = "atim_procure_cusm_dump_dor_central";
+$db_ps1_schemas = "atim_procure_ps1_dump_dor_central";
+$db_ps1_schemas = "atim_procure_ps2_dump_dor_central";
+$db_ps1_schemas = "atim_procure_ps3_dump_dor_central";
+$db_ps1_schemas = "atim_procure_ps4_dump_dor_central";
 $db_processing_schemas = "";
 
 $track_queries = false;
---
+++ branches/qc/procure_central/centralDataMergeProcess/sql_statments_to_create_ps1_db_dump_for_central_db.sql
@@ -0,0 +1,213 @@
+-- Participants
+-- ..................................................................................................................
+-- Delete first name, last name and notes
+-- Replace any date of birth by 'year_of_birth-01-01'
+
+UPDATE participants SET first_name = 'confid.', last_name = 'confid.', notes = '';
+UPDATE participants SET date_of_birth = CONCAT(SUBSTR(date_of_birth, 1, 4),'-01-01');
+UPDATE participants SET date_of_birth_accuracy = 'm' WHERE date_of_birth_accuracy IN ('c','d');
+
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'chum', 'ps1');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'CHUM', 'ps1');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'chuq', 'ps2');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'CHUQ', 'ps2');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'muhc', 'ps3');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'MUHC', 'ps3');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'cusm', 'ps3');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'CUSM', 'ps3');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'chus', 'ps4');
+UPDATE participants SET procure_patient_withdrawn_reason = REPLACE(procure_patient_withdrawn_reason, 'CHUS', 'ps4');
+
+-- Participant Indetifiers (RAMQ, Hospital Numbers, etc)
+-- ..................................................................................................................
+-- Keep only the study participant identifiers
+
+DELETE FROM misc_identifiers WHERE misc_identifier_control_id NOT IN (SELECT id FROM misc_identifier_controls WHERE misc_identifier_name IN ('participant study number'));
+UPDATE misc_identifier_controls SET misc_identifier_name = id WHERE misc_identifier_name NOT IN ('participant study number');
+
+-- Event & Treatment
+-- ..................................................................................................................
+-- Manage notes with banks names
+
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'chum', 'ps1');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'CHUM', 'ps1');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'chuq', 'ps2');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'CHUQ', 'ps2');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'muhc', 'ps3');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'MUHC', 'ps3');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'cusm', 'ps3');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'CUSM', 'ps3');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'chus', 'ps4');
+UPDATE event_masters SET event_summary = REPLACE(event_summary, 'CHUS', 'ps4');
+
+UPDATE treatment_masters SET notes = REPLACE(notes, 'chum', 'ps1');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'CHUM', 'ps1');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'chuq', 'ps2');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'CHUQ', 'ps2');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'muhc', 'ps3');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'MUHC', 'ps3');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'cusm', 'ps3');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'CUSM', 'ps3');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'chus', 'ps4');
+UPDATE treatment_masters SET notes = REPLACE(notes, 'CHUS', 'ps4');
+
+-- Pathology Report
+-- ..................................................................................................................
+-- Remove pathology number
+
+UPDATE procure_ed_lab_pathologies SET path_number = 'confid.';
+UPDATE sd_spe_tissues SET procure_report_number = 'confid.';
+
+-- Collection
+-- ..................................................................................................................
+
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'chum', 'ps1');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'CHUM', 'ps1');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'chuq', 'ps2');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'CHUQ', 'ps2');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'muhc', 'ps3');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'MUHC', 'ps3');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'cusm', 'ps3');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'CUSM', 'ps3');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'chus', 'ps4');
+UPDATE collections SET collection_notes = REPLACE(collection_notes, 'CHUS', 'ps4');
+
+-- Sample
+-- ..................................................................................................................
+
+UPDATE sample_masters SET notes = REPLACE(notes, 'chum', 'ps1');
+UPDATE sample_masters SET notes = REPLACE(notes, 'CHUM', 'ps1');
+UPDATE sample_masters SET notes = REPLACE(notes, 'chuq', 'ps2');
+UPDATE sample_masters SET notes = REPLACE(notes, 'CHUQ', 'ps2');
+UPDATE sample_masters SET notes = REPLACE(notes, 'muhc', 'ps3');
+UPDATE sample_masters SET notes = REPLACE(notes, 'MUHC', 'ps3');
+UPDATE sample_masters SET notes = REPLACE(notes, 'cusm', 'ps3');
+UPDATE sample_masters SET notes = REPLACE(notes, 'CUSM', 'ps3');
+UPDATE sample_masters SET notes = REPLACE(notes, 'chus', 'ps4');
+UPDATE sample_masters SET notes = REPLACE(notes, 'CHUS', 'ps4');
+
+-- Aliquot & Storage
+-- ..................................................................................................................
+-- Remove bank names from 'aliquot use and event type' 
+
+SET @control_id = (SELECT id FROM structure_permissible_values_custom_controls WHERE name = 'aliquot use and event types');
+UPDATE structure_permissible_values_customs SET value = 'sent to ps1', en = '', fr = '' WHERE control_id = @control_id AND value = 'sent to chum';
+UPDATE structure_permissible_values_customs SET value = 'received from ps1', en = '', fr = '' WHERE control_id = @control_id AND value = 'received from chum';
+UPDATE structure_permissible_values_customs SET value = 'sent to ps2', en = '', fr = '' WHERE control_id = @control_id AND value = 'sent to chuq';
+UPDATE structure_permissible_values_customs SET value = 'received from ps2', en = '', fr = '' WHERE control_id = @control_id AND value = 'received from chuq';
+UPDATE structure_permissible_values_customs SET value = 'sent to ps3', en = '', fr = '' WHERE control_id = @control_id AND value = 'sent to muhc';
+UPDATE structure_permissible_values_customs SET value = 'received from ps3', en = '', fr = '' WHERE control_id = @control_id AND value = 'received from muhc';
+UPDATE structure_permissible_values_customs SET value = 'sent to ps4', en = '', fr = '' WHERE control_id = @control_id AND value = 'sent to chus';
+UPDATE structure_permissible_values_customs SET value = 'received from ps4', en = '', fr = '' WHERE control_id = @control_id AND value = 'received from chus';
+
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'chum', 'ps1');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'CHUM', 'ps1');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'chuq', 'ps2');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'CHUQ', 'ps2');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'muhc', 'ps3');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'MUHC', 'ps3');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'cusm', 'ps3');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'CUSM', 'ps3');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'chus', 'ps4');
+UPDATE aliquot_masters SET notes = REPLACE(notes, 'CHUS', 'ps4');
+
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'chum', 'ps1');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'CHUM', 'ps1');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'chuq', 'ps2');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'CHUQ', 'ps2');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'muhc', 'ps3');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'MUHC', 'ps3');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'cusm', 'ps3');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'CUSM', 'ps3');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'chus', 'ps4');
+UPDATE aliquot_masters_revs SET notes = REPLACE(notes, 'CHUS', 'ps4');
+
+UPDATE storage_masters SET notes = REPLACE(notes, 'chum', 'ps1');
+UPDATE storage_masters SET notes = REPLACE(notes, 'CHUM', 'ps1');
+UPDATE storage_masters SET notes = REPLACE(notes, 'chuq', 'ps2');
+UPDATE storage_masters SET notes = REPLACE(notes, 'CHUQ', 'ps2');
+UPDATE storage_masters SET notes = REPLACE(notes, 'muhc', 'ps3');
+UPDATE storage_masters SET notes = REPLACE(notes, 'MUHC', 'ps3');
+UPDATE storage_masters SET notes = REPLACE(notes, 'cusm', 'ps3');
+UPDATE storage_masters SET notes = REPLACE(notes, 'CUSM', 'ps3');
+UPDATE storage_masters SET notes = REPLACE(notes, 'chus', 'ps4');
+UPDATE storage_masters SET notes = REPLACE(notes, 'CHUS', 'ps4');
+
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'chum', 'ps1');
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'CHUM', 'ps1'); 
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'chuq', 'ps2');
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'CHUQ', 'ps2');
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'muhc', 'ps3');
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'MUHC', 'ps3');
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'cusm', 'ps3');
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'CUSM', 'ps3');
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'chus', 'ps4');
+UPDATE storage_masters_revs SET notes = REPLACE(notes, 'CHUS', 'ps4'); 
+  
+-- Study
+-- ..................................................................................................................
+-- Rename study fields with banks names
+
+ALTER TABLE `study_summaries`
+  CHANGE COLUMN `procure_site_ethics_committee_convenience_chum` `procure_site_ethics_committee_convenience_ps1` char(1) DEFAULT '',
+  CHANGE COLUMN `procure_site_ethics_committee_convenience_chus` `procure_site_ethics_committee_convenience_ps4` char(1) DEFAULT '',
+  CHANGE COLUMN `procure_site_ethics_committee_convenience_chuq` `procure_site_ethics_committee_convenience_ps2` char(1) DEFAULT '',
+  CHANGE COLUMN `procure_site_ethics_committee_convenience_cusm` `procure_site_ethics_committee_convenience_ps3` char(1) DEFAULT '';
+  
+-- ..................................................................................................................
+-- Generate date of script creation
+  
+CREATE TABLE atim_procure_dump_information (created datetime NOT NULL);;
+INSERT INTO atim_procure_dump_information (created) (SELECT NOW() FROM aliquot_controls LIMIT 0 ,1);
+  
+-- ------------------------------------------------------------------------------------------------------------------------
+-- PS1
+-- ------------------------------------------------------------------------------------------------------------------------
+
+UPDATE aliquot_controls SET detail_form_alias = REPLACE(detail_form_alias, 'qc_nd_', 'ps1_');
+UPDATE consent_controls SET detail_form_alias = REPLACE(detail_form_alias, 'qc_nd_', 'ps1_');
+UPDATE sample_controls SET detail_form_alias = REPLACE(detail_form_alias, 'qc_nd_', 'ps1_');
+
+UPDATE structure_value_domains SET domain_name = REPLACE(domain_name, 'qc_nd_', 'ps1_');
+
+ALTER TABLE ad_blocks 
+    DROP COLUMN qc_nd_gleason_primary_grade, 
+    DROP COLUMN qc_nd_gleason_secondary_grade, 
+    DROP COLUMN qc_nd_tissue_primary_desc,
+    DROP COLUMN qc_nd_tissue_secondary_desc, 
+    DROP COLUMN qc_nd_tumor_presence,
+    DROP COLUMN qc_nd_sample_position_code; 
+ALTER TABLE ad_tubes 
+    DROP COLUMN qc_nd_storage_solution, 
+    DROP COLUMN qc_nd_purification_method; 
+ALTER TABLE aliquot_masters
+    DROP COLUMN qc_nd_stored_by; 
+ALTER TABLE aliquot_masters_revs 
+    DROP COLUMN qc_nd_stored_by; 
+ALTER TABLE participants 
+    DROP COLUMN qc_nd_last_contact; 
+ALTER TABLE procure_ed_lab_pathologies 
+    DROP COLUMN qc_nd_margins_extensive_apex; 
+ALTER TABLE sd_der_dnas 
+    DROP COLUMN qc_nd_extraction_method; 
+ALTER TABLE sd_der_rnas 
+    DROP COLUMN qc_nd_extraction_method; 
+ALTER TABLE sd_der_urine_cents 
+    DROP COLUMN qc_nd_pellet_presence;
+ALTER TABLE procure_ed_lab_pathologies 
+    DROP COLUMN qc_nd_surgeon;
+
+ALTER TABLE procure_cd_sigantures
+  CHANGE COLUMN `qc_nd_biological_material_use` `ps1_biological_material_use` varchar(50) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_use_of_urine` `ps1_use_of_urine` varchar(50) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_use_of_blood` `ps1_use_of_blood` varchar(50) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_research_other_disease` `ps1_research_other_disease` varchar(50) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_urine_blood_use_for_followup` `ps1_urine_blood_use_for_followup` varchar(10) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_stop_followup` `ps1_stop_followup` varchar(10) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_stop_followup_date` `ps1_stop_followup_date` date DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_allow_questionnaire` `ps1_allow_questionnaire` varchar(10) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_stop_questionnaire` `ps1_stop_questionnaire` varchar(10) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_stop_questionnaire_date` `ps1_stop_questionnaire_date` date DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_contact_for_additional_data` `ps1_contact_for_additional_data` varchar(10) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_inform_significant_discovery` `ps1_inform_significant_discovery` varchar(50) DEFAULT NULL,
+  CHANGE COLUMN `qc_nd_inform_discovery_on_other_disease` `ps1_inform_discovery_on_other_disease` varchar(10) DEFAULT NULL;
