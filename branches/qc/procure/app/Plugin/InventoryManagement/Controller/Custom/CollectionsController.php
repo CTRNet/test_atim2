f09fda793ecbdc1c4eb7a287958f127fc62fdf51
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-01-11 02:58:55 +0000

    Change business rules to deal with aliquot of the ATiM Processing Site merged into the PS3 site.

--- branches/qc/procure/app/Plugin/InventoryManagement/Controller/Custom/CollectionsController.php
+++
@@ -1,8 +0,0 @@
-<?php
-class CollectionsControllerCustom extends CollectionsController{
-	
-	function template($collection_id, $template_id){
-		if(Configure::read('procure_atim_version') != 'BANK') $this->redirect('/Pages/err_plugin_system_error?method='.__METHOD__.',line='.__LINE__, null, true);
-		parent::template($collection_id, $template_id);
-	}
-}
