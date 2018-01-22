32981fb026a92d60f03cc46e1d1eb5a84b7de1d2
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-01-22 20:24:26 +0000

    End of merge process before first test

--- procure_central/centralDataMergeProcess/config.php
+++ procure_central/centralDataMergeProcess/config.php
@@ -5,12 +5,11 @@ $db_user	= 	"root";
 $db_pwd		= 	"";
 $db_charset		= "utf8";
 
-$db_central_schemas	= "procurecentral";
+$db_central_schemas	= "procurecentralsansfusion";
 $db_ps1_schemas = "atim_procure_ps1_dump_dor_central";
-$db_ps1_schemas = "atim_procure_ps2_dump_dor_central";
-$db_ps1_schemas = "atim_procure_ps3_dump_dor_central";
-$db_ps1_schemas = "atim_procure_ps4_dump_dor_central";
-$db_processing_schemas = "";
+$db_ps2_schemas = "atim_procure_ps2_dump_dor_central";
+$db_ps3_schemas = "atim_procure_ps3_dump_dor_central";
+$db_ps4_schemas = "atim_procure_ps4_dump_dor_central";
 
 $track_queries = false;
 
