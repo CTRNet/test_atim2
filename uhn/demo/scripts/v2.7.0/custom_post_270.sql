58ca4412c7c3c82704befd1fde93cec3564a186d
Yaser Naderi <42739735+yasnaderi@users.noreply.github.com>
2018-03-22 12:54:02 +0000

    - Cancel force for reseting password for administrator

--- uhn/demo/scripts/v2.7.0/custom_post_270.sql
+++ uhn/demo/scripts/v2.7.0/custom_post_270.sql
@@ -6,7 +6,9 @@
 
 REPLACE INTO i18n (id,en,fr) VALUES ('core_installname', 'UHN - Demo', 'UHN - Demo');
 UPDATE groups SET flag_show_confidential = '1' WHERE id = 1;
-UPDATE users SET flag_active = '1' WHERE id = 1;
+UPDATE users SET flag_active = '1', `force_password_reset` = '0' WHERE id = 1;
+
+
 
 -- xxxx
 -- -----------------------------------------------------------------------------------------------------------------------------------
