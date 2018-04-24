2bfb1c5b23dcd5feca227b38a0753a0df19dfd37
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-04-24 19:40:11 +0000

    Add specimen review code default value. Add new consent fields, email of the study contact. PSA CA125 to float.

--- branches/qc/chum/notre_dame/scripts/v2.7.0/custom_post_270.sql
+++ branches/qc/chum/notre_dame/scripts/v2.7.0/custom_post_270.sql
@@ -572,3 +572,47 @@ VALUES
 -- -----------------------------
 
 UPDATE versions SET branch_build_number = '7025' WHERE version_number = '2.7.0';
+
+-- -----------------------------------------------------------------------------------------------------------------------------------------------------------
+-- 20180424
+-- -----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Study Contact Email
+
+ALTER TABLE study_summaries ADD COLUMN qc_nd_contact_email varchar(150) DEFAULT NULL;
+ALTER TABLE study_summaries_revs ADD COLUMN qc_nd_contact_email varchar(150) DEFAULT NULL;
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('Study', 'StudySummary', 'study_summaries', 'qc_nd_contact_email', 'input',  NULL , '0', '', '', '', '', 'email');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `margin`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='studysummaries'), (SELECT id FROM structure_fields WHERE `model`='StudySummary' AND `tablename`='study_summaries' AND `field`='qc_nd_contact_email' AND `type`='input' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='' AND `language_tag`='email'), '1', '10', '', '0', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0');
+
+-- Consent - New Field
+
+ALTER TABLE cd_icm_generics ADD COLUMN qc_nd_additional_collection char(1) DEFAULT '';
+ALTER TABLE cd_icm_generics_revs ADD COLUMN qc_nd_additional_collection char(1) DEFAULT '';
+INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
+('ClinicalAnnotation', 'ConsentDetail', 'cd_icm_generics', 'qc_nd_additional_collection', 'yes_no',  NULL , '0', '', '', '', 'additional collection', '');
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `margin`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='cd_icm_frsq_gyneco'), (SELECT id FROM structure_fields WHERE `model`='ConsentDetail' AND `tablename`='cd_icm_generics' AND `field`='qc_nd_additional_collection' AND `type`='yes_no' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0' AND `setting`='' AND `default`='' AND `language_help`='' AND `language_label`='additional collection' AND `language_tag`=''), '2', '6', 'agreements (2)', '0', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '1', '0', '1', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '1', '0', '0');
+INSERT IGNORE INTO i18n (id,en,fr)
+VALUES
+('additional collection', 'Additional Collection', 'Prélèvement supplémentaire'),
+('agreements (2)', 'Agreements (2nd version)', 'Authorisation (2ème version)');
+REPLACE INTO i18n (id,en,fr)
+VALUES
+('additional tumor collection', 'Additional tumor collection', 'Collection supplémentaire (de tumeur)'),
+('use of faeces', 'Use of Faeces', 'Utilisation matières fécales');
+
+-- PSA et CA125 `Float
+
+UPDATE structure_fields SET  `type`='float_positive' WHERE model='EventDetail' AND tablename='qc_nd_ed_ca125s' AND field='value' AND `type`='integer_positive' AND structure_value_domain  IS NULL ;
+ALTER TABLE qc_nd_ed_ca125s MODIFY value decimal(12,3) DEFAULT NULL;
+ALTER TABLE qc_nd_ed_ca125s_revs MODIFY value decimal(12,3) DEFAULT NULL;
+
+UPDATE structure_fields SET  `type`='float_positive' WHERE model='EventDetail' AND tablename='qc_nd_ed_psas' AND field='value' AND `type`='integer_positive' AND structure_value_domain  IS NULL ;
+ALTER TABLE qc_nd_ed_psas MODIFY value decimal(12,3) DEFAULT NULL;
+ALTER TABLE qc_nd_ed_psas_revs MODIFY value decimal(12,3) DEFAULT NULL;
+
+-- -----------------------------
+
+UPDATE versions SET branch_build_number = '7097' WHERE version_number = '2.7.0';
