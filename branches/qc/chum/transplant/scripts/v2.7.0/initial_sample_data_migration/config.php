211cf3e74cd81186677ca02eef32862fd539f0cd
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-06-01 20:55:31 +0000

    Add script to migrate aliquot from file created from CaTissue.

---
+++ branches/qc/chum/transplant/scripts/v2.7.0/initial_sample_data_migration/config.php
@@ -0,0 +1,23 @@
+<?php 
+
+$migration_process_version = 'v0.1';
+
+//-- DB PARAMETERS ---------------------------------------------------------------------------------------------------------------------------
+
+$db_ip			= "localhost";
+$db_port 		= "";
+$db_user 		= "root";
+$db_pwd			= "";
+$db_schema		= "chumtransplant";
+
+$db_charset		= "utf8";
+
+$migration_user_id = 9;
+
+//-- EXCEL FILE NAMES ---------------------------------------------------------------------------------------------------------------------------
+
+$excel_files_paths = "C:/_NicolasLuc/Server/www/chum_transplant/scripts/v2.7.0/data/CRCHUM_biorepository_data/";
+$excel_file_names = 'CRCHUM-specimens-2018-01-19.xls';
+
+
+?>
