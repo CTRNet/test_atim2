2eb12d734a47894526327527a02266b6026ec87b
Yaser Naderi <42739735+yasnaderi@users.noreply.github.com>
2018-06-12 14:12:35 +0000

    - Remove the comment at the end of the script.

--- v2.7.Z/scripts/v2.7.0/atim_v2.7.1_upgrade.sql
+++ v2.7.Z/scripts/v2.7.0/atim_v2.7.1_upgrade.sql
@@ -472,5 +472,3 @@ UPDATE versions SET permissions_regenerated = 0;
 INSERT INTO `versions` (version_number, date_installed, trunk_build_number, branch_build_number) 
 VALUES
 ('2.7.1', NOW(),'xxxx','n/a');
-
-Select ">            Nicolas, may you check the translation of storage layout?             <" as ">            Nicolas, may you check the translation of storage layout?             <";	
