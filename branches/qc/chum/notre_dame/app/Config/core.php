14a0b3427c24e8b3c51a127ada1d9d57a100fbdc
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-08-08 14:01:16 +0000

    Validated code and added correction to CHUM ATiM Onco code to be exaclty similar to the code of tag 2.7.1.

--- branches/qc/chum/notre_dame/app/Config/core.php
+++ branches/qc/chum/notre_dame/app/Config/core.php
@@ -40,7 +40,8 @@ Configure::write('debug', $debug);
  * 1: Core install name color will be changed to red and 'Test' word will be added to the install name.
  * 0: Install name will be displayed as usual.
  */
-$isTest = 0;
+ 
+$isTest = 1;
 Configure::write('IsTest', $isTest);
 
 /**
