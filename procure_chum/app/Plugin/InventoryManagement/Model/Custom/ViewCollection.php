1bfbf51263ad9ba89cc3acdd0fa1f061899be41b
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-08-16 18:16:08 +0000

    Fixed bug on view_colelctions

--- procure_chum/app/Plugin/InventoryManagement/Model/Custom/ViewCollection.php
+++ procure_chum/app/Plugin/InventoryManagement/Model/Custom/ViewCollection.php
@@ -28,11 +28,11 @@ Participant.procure_participant_attribution_number,
 		Collection.collection_datetime_accuracy AS collection_datetime_accuracy,
 		Collection.collection_property AS collection_property,
 		Collection.collection_notes AS collection_notes,
-		Collection.created AS created 
-		FROM collections AS Collection,
-MiscIdentifier.identifier_value AS qc_nd_no_labo 
+		Collection.created AS created ,
+MiscIdentifier.identifier_value AS qc_nd_no_labo
+		FROM collections AS Collection
 		LEFT JOIN participants AS Participant ON Collection.participant_id = Participant.id AND Participant.deleted <> 1 
-LEFT JOIN misc_identifiers AS MiscIdentifier on MiscIdentifier.misc_identifier_control_id = 5 AND MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.deleted <> 1
+LEFT JOIN misc_identifiers AS MiscIdentifier ON MiscIdentifier.misc_identifier_control_id = 5 AND MiscIdentifier.participant_id = Participant.id AND MiscIdentifier.deleted <> 1
         WHERE Collection.deleted <> 1 %%WHERE%%';
 
     public function summary($variables = array())
