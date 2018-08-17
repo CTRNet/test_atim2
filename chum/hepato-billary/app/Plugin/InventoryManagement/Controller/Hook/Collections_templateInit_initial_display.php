41a5cbeebe2af17bd7a78bedcfdd2aad25b3f9dc
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-08-17 20:24:12 +0000

    Fixed bug on template init hook call

--- chum/hepato-billary/app/Plugin/InventoryManagement/Controller/Hook/Collections_templateInit_initial_display.php
+++ chum/hepato-billary/app/Plugin/InventoryManagement/Controller/Hook/Collections_templateInit_initial_display.php
@@ -10,10 +10,19 @@ $tmpCollectionDateTimeAccuracy = in_array($viewCollectionData['ViewCollection'][
     'c',
     'h'
 )) ? 'h' : $viewCollectionData['ViewCollection']['collection_datetime_accuracy'];
+
+
+
 $this->request->data['SpecimenDetail']['reception_datetime'] = $tmpCollectionDateTime;
 $this->request->data['SpecimenDetail']['reception_datetime_accuracy'] = $tmpCollectionDateTimeAccuracy;
+$this->request->data['SpecimenDetail']['reception_by'] = 'louise rousseau';
+
 $this->request->data['DerivativeDetail']['creation_datetime'] = $tmpCollectionDateTime;
 $this->request->data['DerivativeDetail']['creation_datetime_accuracy'] = $tmpCollectionDateTimeAccuracy;
-
-//$this->request->data['DerivativeDetail']['creation_site'] = "cr. st-luc";
 $this->request->data['DerivativeDetail']['creation_by'] = 'louise rousseau';
+//$this->request->data['DerivativeDetail']['creation_site'] = "cr. st-luc";
+
+
+$this->request->data['AliquotMaster']['storage_datetime'] = $tmpCollectionDateTime;
+$this->request->data['AliquotMaster']['storage_datetime_accuracy'] = $tmpCollectionDateTimeAccuracy;
+$this->request->data['AliquotMaster']['qc_hb_stored_by'] = 'louise rousseau';
