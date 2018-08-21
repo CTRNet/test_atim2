06507ecdc6e5bf03877e4872b4e04bfd21edf1ee
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-08-21 15:25:17 +0000

    v271 prod

--- qbcf/app/Config/core.php
+++ qbcf/app/Config/core.php
@@ -31,7 +31,7 @@
  * In production mode, flash messages redirect after a time interval.
  * In development mode, you need to click the flash message to continue.
  */
-$debug = 0;
+$debug = 2;
 Configure::write('debug', $debug);
 
 /**
@@ -40,7 +40,7 @@ Configure::write('debug', $debug);
  * 1: Core install name color will be changed to red and 'Test' word will be added to the install name.
  * 0: Install name will be displayed as usual.
  */
-$isTest = 0;
+$isTest = 1;
 Configure::write('IsTest', $isTest);
 
 /**
