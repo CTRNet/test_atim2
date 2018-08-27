4e57c3c042fa97a1ce5711712f4e279877037576
Yaser Naderi <42739735+yasnaderi@users.noreply.github.com>
2018-08-27 18:20:38 +0000

    Merged revision(s) 7355-7371 from trunk

--- chum/hepato-billary/scripts/v2.7.0/custom_post_271.sql
+++ chum/hepato-billary/scripts/v2.7.0/custom_post_271.sql
@@ -26,5 +26,5 @@ INSERT IGNORE INTO i18n (id,en,fr)
 VALUES
 ('other if applicable', 'Other (If Applicable', 'Autre (Si applicable)');
         
-UPDATE versions SET branch_build_number = '7336' WHERE version_number = '2.7.1';
+UPDATE versions SET branch_build_number = '7379' WHERE version_number = '2.7.1';
         
