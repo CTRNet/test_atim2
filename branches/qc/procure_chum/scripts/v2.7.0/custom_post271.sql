f33cb515013817a7141c724df76c21cc8278ec97
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-09-06 14:15:43 +0000

    Merged with procure revs 7403 (v2.7.1)

--- branches/qc/procure_chum/scripts/v2.7.0/custom_post271.sql
+++ branches/qc/procure_chum/scripts/v2.7.0/custom_post271.sql
@@ -205,19 +205,19 @@ ALTER TABLE procure_cd_sigantures
   CHANGE COLUMN `qc_nd_inform_significant_discovery` `ps1_inform_significant_discovery` varchar(50) DEFAULT NULL,
   CHANGE COLUMN `qc_nd_inform_discovery_on_other_disease` `ps1_inform_discovery_on_other_disease` varchar(10) DEFAULT NULL;
 -- ALTER TABLE procure_cd_sigantures
---   ADD COLUMN `ps1_biological_material_use` varchar(50) DEFAULT NULL,
---   ADD COLUMN `ps1_use_of_urine` varchar(50) DEFAULT NULL,
---   ADD COLUMN `ps1_use_of_blood` varchar(50) DEFAULT NULL,
---   ADD COLUMN `ps1_research_other_disease` varchar(50) DEFAULT NULL,
---   ADD COLUMN `ps1_urine_blood_use_for_followup` varchar(10) DEFAULT NULL,
---   ADD COLUMN `ps1_stop_followup` varchar(10) DEFAULT NULL,
---   ADD COLUMN `ps1_stop_followup_date` date DEFAULT NULL,
---   ADD COLUMN `ps1_allow_questionnaire` varchar(10) DEFAULT NULL,
---   ADD COLUMN `ps1_stop_questionnaire` varchar(10) DEFAULT NULL,
---   ADD COLUMN `ps1_stop_questionnaire_date` date DEFAULT NULL,
---   ADD COLUMN `ps1_contact_for_additional_data` varchar(10) DEFAULT NULL,
---   ADD COLUMN `ps1_inform_significant_discovery` varchar(50) DEFAULT NULL,
---   ADD COLUMN `ps1_inform_discovery_on_other_disease` varchar(10) DEFAULT NULL; 
+--  ADD COLUMN `ps1_biological_material_use` varchar(50) DEFAULT NULL,
+--  ADD COLUMN `ps1_use_of_urine` varchar(50) DEFAULT NULL,
+--  ADD COLUMN `ps1_use_of_blood` varchar(50) DEFAULT NULL,
+--  ADD COLUMN `ps1_research_other_disease` varchar(50) DEFAULT NULL,
+--  ADD COLUMN `ps1_urine_blood_use_for_followup` varchar(10) DEFAULT NULL,
+--  ADD COLUMN `ps1_stop_followup` varchar(10) DEFAULT NULL,
+--  ADD COLUMN `ps1_stop_followup_date` date DEFAULT NULL,
+--  ADD COLUMN `ps1_allow_questionnaire` varchar(10) DEFAULT NULL,
+--  ADD COLUMN `ps1_stop_questionnaire` varchar(10) DEFAULT NULL,
+--  ADD COLUMN `ps1_stop_questionnaire_date` date DEFAULT NULL,
+--  ADD COLUMN `ps1_contact_for_additional_data` varchar(10) DEFAULT NULL,
+--  ADD COLUMN `ps1_inform_significant_discovery` varchar(50) DEFAULT NULL,
+--  ADD COLUMN `ps1_inform_discovery_on_other_disease` varchar(10) DEFAULT NULL; 
 UPDATE structure_fields SET field = 'ps1_biological_material_use' WHERE field = 'qc_nd_biological_material_use';
 UPDATE structure_fields SET field = 'ps1_use_of_urine' WHERE field = 'qc_nd_use_of_urine';
 UPDATE structure_fields SET field = 'ps1_use_of_blood' WHERE field = 'qc_nd_use_of_blood';
@@ -235,16 +235,16 @@ UPDATE structure_fields SET field = 'ps1_inform_discovery_on_other_disease' WHER
 -- Linked to PS2 fields
 
 -- ALTER TABLE procure_cd_sigantures
---  CHANGECOLUMN `procure_chuq_tissue` `ps2_tissue` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_blood` `ps2_blood` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_urine` `ps2_urine` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_followup` `ps2_followup` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_questionnaire` `ps2_questionnaire` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_contact_for_additional_data` `ps2_contact_for_additional_data` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_inform_significant_discovery` `ps2_inform_significant_discovery` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_contact_in_case_of_death` `ps2_contact_in_case_of_death` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_witness` `ps2_witness` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chuq_complete` `ps2_complete` char(1) DEFAULT '';    
+--   CHANGE COLUMN `procure_chuq_tissue` `ps2_tissue` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_blood` `ps2_blood` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_urine` `ps2_urine` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_followup` `ps2_followup` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_questionnaire` `ps2_questionnaire` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_contact_for_additional_data` `ps2_contact_for_additional_data` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_inform_significant_discovery` `ps2_inform_significant_discovery` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_contact_in_case_of_death` `ps2_contact_in_case_of_death` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_witness` `ps2_witness` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chuq_complete` `ps2_complete` char(1) DEFAULT '';    
 ALTER TABLE procure_cd_sigantures
   ADD COLUMN `ps2_tissue` char(1) DEFAULT '',
   ADD COLUMN `ps2_blood` char(1) DEFAULT '',
@@ -270,11 +270,11 @@ UPDATE structure_fields SET field = 'ps2_complete' WHERE field = 'procure_chuq_c
 -- Linked to PS4 fields
 
 -- ALTER TABLE procure_cd_sigantures
---  CHANGECOLUMN `procure_chus_contact_for_more_info` `ps4_contact_for_more_info` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chus_contact_if_scientific_discovery` `ps4_contact_if_scientific_discovery` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chus_study_on_other_diseases` `ps4_study_on_other_diseases` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chus_contact_if_discovery_on_other_diseases` `ps4_contact_if_discovery_on_other_diseases` char(1) DEFAULT '',
---  CHANGECOLUMN `procure_chus_other_contacts_in_case_of_death` `ps4_other_contacts_in_case_of_death` char(1) DEFAULT '';
+--   CHANGE COLUMN `procure_chus_contact_for_more_info` `ps4_contact_for_more_info` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chus_contact_if_scientific_discovery` `ps4_contact_if_scientific_discovery` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chus_study_on_other_diseases` `ps4_study_on_other_diseases` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chus_contact_if_discovery_on_other_diseases` `ps4_contact_if_discovery_on_other_diseases` char(1) DEFAULT '',
+--   CHANGE COLUMN `procure_chus_other_contacts_in_case_of_death` `ps4_other_contacts_in_case_of_death` char(1) DEFAULT '';
 ALTER TABLE procure_cd_sigantures
   ADD COLUMN `ps4_contact_for_more_info` char(1) DEFAULT '',
   ADD COLUMN `ps4_contact_if_scientific_discovery` char(1) DEFAULT '',
@@ -291,4 +291,4 @@ UPDATE versions SET branch_build_number = '7347' WHERE version_number = '2.7.1';
 
 UPDATE versions SET branch_build_number = '7389' WHERE version_number = '2.7.1';
 
-articipant does not allow follow-up
+UPDATE versions SET branch_build_number = '7403' WHERE version_number = '2.7.1';
