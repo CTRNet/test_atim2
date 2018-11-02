c6b7d094ebc3446a45cbc34f0f109ae1695d0562
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-11-02 19:42:03 +0000

    Added missing files

---
+++ branches/qc/cusm/app/Plugin/ClinicalAnnotation/Controller/Hook/ClinicalCollectionLinks_listall_before_find.php
@@ -0,0 +1,20 @@
+<?php
+/** **********************************************************************
+ * CUSM
+ * ***********************************************************************
+ *
+ * Clinical Annotation plugin custom code
+ * 
+ * @author N. Luc - CTRNet (nicol.luc@gmail.com)
+ * @since 2018-10-15
+ */
+
+// Set identifier list
+$joins[] = array(
+    'table' => 'misc_identifiers',
+    'alias' => 'MiscIdentifier',
+    'type' => 'LEFT',
+    'conditions' => array(
+        'Collection.misc_identifier_id = MiscIdentifier.id'
+    )
+);
