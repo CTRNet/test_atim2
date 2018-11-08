5dcb056540be61f939450ed3540d939a74bdb042
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-11-08 16:55:20 +0000

    New dataImport script and tissue control annotation fields

--- branches/qc/qbcf/dataImporter/TmaBlock/config.php
+++ branches/qc/qbcf/dataImporter/TmaBlock/config.php
@@ -11,15 +11,26 @@ $db_pwd			= "";
 $db_schema		= "qbcf";
 $db_charset		= "utf8";
 
+
+$db_pwd			= "";
+$db_schema		= "qbcf";
+
 $migration_user_id = 1;
 
 //-- EXCEL FILE NAMES ---------------------------------------------------------------------------------------------------------------------------
 
-$files_path = "C:/_NicolasLuc/Server/www/qbcf/data/20161221/TMA/";
-//$files_path = "/ATiM/atim-tfri/dataUpdate/cpcbn/UpdateClinicalData/data/";
+$files_path = "C:\_NicolasLuc\Server\www\qbcf\dataImporter\TmaBlock/";
+//$files_path = "/ATiM/atim-qbcf/dataImporter/data/TMA construction/";
 // Serial number $windows_xls_offset = 36526 & $mac_xls_offset = 35064;
 $excel_files_names = array(
-	array('161206_MAP_Opti_QBCF_2MB_chri_bloc_TOUS_ATiM_pourNicolasVOedit2.xls', 'Sheet1')
+	array('MAP_OPT_ATiM_2018-10-10_nl_rev.xls', 'Feuil1'),
+	array('TMADiscovery_map_ATiM_formigration_CC_181010_nl_rev.xls', 'Discovery A'),
+	array('TMADiscovery_map_ATiM_formigration_CC_181010_nl_rev.xls', 'Discovery B'),
+	array('TMADiscovery_map_ATiM_formigration_CC_181010_nl_rev.xls', 'Discovery C'),
+);
+
+$excel_files_names_control_annotation = array(
+    array('MAP_OPT_ATiM_2018-10-10_nl_rev - Control Sample Annotation.xls', 'Feuil1')
 );
 
 ?>
