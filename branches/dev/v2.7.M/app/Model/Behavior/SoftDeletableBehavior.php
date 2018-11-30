8140c36ef493b5dae2a6e156ad228c226de32a52
mgoursolas <mgoursolas@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-11-30 15:02:08 +0000

    Add license information to Model files

--- branches/dev/v2.7.M/app/Model/Behavior/SoftDeletableBehavior.php
+++ branches/dev/v2.7.M/app/Model/Behavior/SoftDeletableBehavior.php
@@ -1,4 +1,19 @@
 <?php
+ /**
+ *
+ * ATiM - Advanced Tissue Management Application
+ * Copyright (c) Canadian Tissue Repository Network (http://www.ctrnet.ca)
+ *
+ * Licensed under GNU General Public License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Canadian Tissue Repository Network <info@ctrnet.ca>
+ * @copyright     Copyright (c) Canadian Tissue Repository Network (http://www.ctrnet.ca)
+ * @link          http://www.ctrnet.ca
+ * @since         ATiM v 2
+ * @license       http://www.gnu.org/licenses  GNU General Public License
+ */
 
 /* SVN FILE: $Id: soft_deletable.php 38 2007-11-26 19:36:27Z mgiglesias $ */
 
