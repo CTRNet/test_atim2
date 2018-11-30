3bfac710f1bf6dc20943de31ed07378a519dba92
mgoursolas <mgoursolas@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2018-11-30 18:25:24 +0000

    Add license information to Plugin files

--- branches/dev/v2.7.M/app/Plugin/ClinicalAnnotation/View/DiagnosisMasters/add_popup.php
+++ branches/dev/v2.7.M/app/Plugin/ClinicalAnnotation/View/DiagnosisMasters/add_popup.php
@@ -1,4 +1,19 @@
 <?php
+ /**
+ *
+ * ATiM - Advanced Tissue Management Application
+ * Copyright (c) Canadian Tissue Repository Network (http://www.ctrnet.ca)
+ *
+ * Licensed under GNU General Public License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @author        Canadian Tissue Repository Network <info@ctrnet.ca>
+ * @copyright     Copyright (c) Canadian Tissue Repository Network (http://www.ctrnet.ca)
+ * @link          http://www.ctrnet.ca
+ * @since         ATiM v 2
+ * @license       http://www.gnu.org/licenses  GNU General Public License
+ */
 $options = array();
 $secondaryCtrlId = array();
 AppController::$highlightMissingTranslations = false;
@@ -54,7 +69,7 @@ if ($hookLink) {
 }
 ?>
 <div id="popupSelect" class="hidden">
-			<?php
+<?php
 echo $this->Form->input("data[DiagnosisControl][id]", array(
     'type' => 'select',
     'label' => false,
