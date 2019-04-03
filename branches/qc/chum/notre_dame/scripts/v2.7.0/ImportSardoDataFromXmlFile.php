fd81f8f1a8b64424f5edbc8f0fc215db0e2788ef
ynaderi <ynaderi@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2019-04-03 16:16:27 +0000

    Added GravidaParaAborta to sardo_diagnostic table.

--- branches/qc/chum/notre_dame/scripts/v2.7.0/ImportSardoDataFromXmlFile.php
+++ branches/qc/chum/notre_dame/scripts/v2.7.0/ImportSardoDataFromXmlFile.php
@@ -141,6 +141,7 @@ $sql_sardo_tables_creations = array(
 		CodeTNMG varchar(255) DEFAULT NULL,
 		GradeICDO varchar(255) DEFAULT NULL,
         StatutMenopause varchar(255) DEFAULT NULL,
+        GravidaParaAborta varchar(255) DEFAULT NULL,
 		DateDerniereVisite date DEFAULT NULL,
 		DateDerniereVisite_accuracy char(1) NOT NULL DEFAULT '',
 		Duree varchar(255) DEFAULT NULL,
