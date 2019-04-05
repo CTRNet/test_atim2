4e8978ee909946cacd49e039a6b8fd897137b5e1
nluc <nluc@6c67f226-5cfb-4d63-bf10-f1b4f2819cef>
2019-04-05 13:47:51 +0000

    Add SARDO Nbr to participant identifiers report.

--- chum/notre_dame/scripts/v2.7.0/custom_post_271.sql
+++ chum/notre_dame/scripts/v2.7.0/custom_post_271.sql
@@ -202,7 +202,6 @@ VALUES
 -- 20190228 : Add new Consent for melanome
 -- -----------------------------------------------------------------------------------------------------------------------------------
 
-
 INSERT INTO structure_fields(`plugin`, `model`, `tablename`, `field`, `type`, `structure_value_domain`, `flag_confidential`, `setting`, `default`, `language_help`, `language_label`, `language_tag`) VALUES
 ('ClinicalAnnotation', 'ConsentDetail', 'qc_nd_cd_chum_melanoma_skins', 'tissue_from_clinical_act', 'yes_no',  NULL , '0', '', '', '', 'tissue from clinical act', ''), 
 ('ClinicalAnnotation', 'ConsentDetail', 'qc_nd_cd_chum_melanoma_skins', 'tissue_for_banking_only', 'yes_no',  NULL , '0', '', '', '', 'tissue for banking only', ''), 
@@ -223,8 +222,13 @@ ADD `tissue_for_banking_only` char(1) COLLATE 'latin1_swedish_ci' NOT NULL DEFAU
 ADD `blood` char(1) COLLATE 'latin1_swedish_ci' NOT NULL DEFAULT '' AFTER `tissue_for_banking_only`,
 ADD `stool` char(1) COLLATE 'latin1_swedish_ci' NOT NULL DEFAULT '' AFTER `blood`;
 
-
-
-
-
 UPDATE versions SET branch_build_number = '7586' WHERE version_number = '2.7.1';
+
+-- -----------------------------------------------------------------------------------------------------------------------------------
+-- 201900405 : Add SARDO to participant identifier
+-- -----------------------------------------------------------------------------------------------------------------------------------
+
+INSERT INTO structure_formats(`structure_id`, `structure_field_id`, `display_column`, `display_order`, `language_heading`, `margin`, `flag_override_label`, `language_label`, `flag_override_tag`, `language_tag`, `flag_override_help`, `language_help`, `flag_override_type`, `type`, `flag_override_setting`, `setting`, `flag_override_default`, `default`, `flag_add`, `flag_add_readonly`, `flag_edit`, `flag_edit_readonly`, `flag_search`, `flag_search_readonly`, `flag_addgrid`, `flag_addgrid_readonly`, `flag_editgrid`, `flag_editgrid_readonly`, `flag_batchedit`, `flag_batchedit_readonly`, `flag_index`, `flag_detail`, `flag_summary`, `flag_float`) VALUES 
+((SELECT id FROM structures WHERE alias='report_participant_identifiers_result'), (SELECT id FROM structure_fields WHERE `model`='Participant' AND `tablename`='participants' AND `field`='qc_nd_sardo_rec_number' AND `type`='input' AND `structure_value_domain`  IS NULL  AND `flag_confidential`='0' AND `setting`='class=range' AND `default`='' AND `language_help`='' AND `language_label`='sardo participant id' AND `language_tag`=''), '0', '31', '', '0', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '', '0', '0', '0', '0', '1', '0', '0', '0', '0', '0', '0', '0', '1', '0', '0', '0');
+
+UPDATE versions SET branch_build_number = '7625' WHERE version_number = '2.7.1';
